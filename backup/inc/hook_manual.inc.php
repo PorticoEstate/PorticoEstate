da5da6c20607e3476c0c032fa55effdb8cd9969e
nobody <nobody@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2007-10-15 14:05:13 +0000

    initial import of backup

---
+++ backup/inc/hook_manual.inc.php
@@ -0,0 +1,18 @@
+<?php
+    /**************************************************************************\
+    * phpGroupWare - Manual                                                    *
+    * http://www.phpgroupware.org                                              *
+    * Written by Mark Peters <skeeter@phpgroupware.org>                        *
+    * --------------------------------------------                             *
+    *  This program is free software; you can redistribute it and/or modify it *
+    *  under the terms of the GNU General Public License as published by the   *
+    *  Free Software Foundation; either version 2 of the License, or (at your  *
+    *  option) any later version.                                              *
+    \**************************************************************************/
+	/* $Id: hook_manual.inc.php,v 1.1.1.1 2001/12/04 01:41:58 bettina Exp $ */
+
+	// Only Modify the $file variable.....
+	$file = Array();
+//Do not modify below this line
+	display_manual_section($appname,$file);
+?>
