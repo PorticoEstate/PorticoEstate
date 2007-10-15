ce5cb8adea53c118a214598dd75a70e1836a8387
nobody <nobody@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2007-10-15 14:14:21 +0000

    initial import of registration

---
+++ trunk/registration/inc/hook_admin.inc.php
@@ -0,0 +1,22 @@
+<?php
+	/**************************************************************************\
+	* phpGroupWare - Registration                                              *
+	* http://www.phpgroupware.org                                              *
+	* This application written by Joseph Engo <jengo@phpgroupware.org>         *
+	* --------------------------------------------                             *
+	* Funding for this program was provided by http://www.checkwithmom.com     *
+	* --------------------------------------------                             *
+	*  This program is free software; you can redistribute it and/or modify it *
+	*  under the terms of the GNU General Public License as published by the   *
+	*  Free Software Foundation; either version 2 of the License, or (at your  *
+	*  option) any later version.                                              *
+	\**************************************************************************/
+	/* $Id: hook_admin.inc.php,v 1.8 2006/09/09 09:04:50 skwashd Exp $ */
+
+	$file = array
+	(
+		'Site Configuration'	=> $GLOBALS['phpgw']->link('/index.php', array('menuaction' => 'admin.uiconfig.index', 'appname' => 'registration') ),
+		'Manage Fields'			=> $GLOBALS['phpgw']->link ('/index.php', array('menuaction' => 'registration.uimanagefields.admin') )
+	);
+	$GLOBALS['phpgw']->common->display_mainscreen($appname,$file);
+?>
