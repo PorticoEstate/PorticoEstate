4d144d11d900c1eb33d25be93cdb2e9b9280019d
nobody <nobody@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2007-10-15 14:14:48 +0000

    initial import of stocks

---
+++ trunk/stocks/setup/tables_baseline.inc.php
@@ -0,0 +1,14 @@
+<?php
+	/**************************************************************************\
+	* phpGroupWare - Setup                                                     *
+	* http://www.phpgroupware.org                                              *
+	* --------------------------------------------                             *
+	* This program is free software; you can redistribute it and/or modify it  *
+	* under the terms of the GNU General Public License as published by the    *
+	* Free Software Foundation; either version 2 of the License, or (at your   *
+	* option) any later version.                                               *
+	\**************************************************************************/
+	/* $Id: tables_baseline.inc.php,v 1.4 2002/01/24 21:18:03 ceb Exp $ */
+
+	$phpgw_baseline = array();
+?>
