c97b9822c93228f701894f85ebca4fac6b56a810
nobody <nobody@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2007-10-15 14:08:02 +0000

    initial import of news_admin

---
+++ trunk/news_admin/inc/hook_home.inc.php
@@ -0,0 +1,19 @@
+<?php
+	/**************************************************************************\
+	* phpGroupWare - Webpage news admin                                        *
+	* http://www.phpgroupware.org                                              *
+	* --------------------------------------------                             *
+	*  This program is free software; you can redistribute it and/or modify it *
+	*  under the terms of the GNU General Public License as published by the   *
+	*  Free Software Foundation; either version 2 of the License, or (at your  *
+	*  option) any later version.                                              *
+	* --------------------------------------------                             *
+	* This program was sponsered by Golden Glair productions                   *
+	* http://www.goldenglair.com                                               *
+	\**************************************************************************/
+
+	/* $Id: hook_home.inc.php,v 1.5 2005/08/04 03:54:31 skwashd Exp $ */
+
+// 	$news = createobject('news_admin.uinews');
+// 	$news->show_news_home();
+?>
---
+++ trunk/news_admin/index.php
@@ -0,0 +1,30 @@
+<?php
+	/**************************************************************************\
+	* phpGroupWare - Webpage news admin                                        *
+	* http://www.phpgroupware.org                                              *
+	* --------------------------------------------                             *
+	*  This program is free software; you can redistribute it and/or modify it *
+	*  under the terms of the GNU General Public License as published by the   *
+	*  Free Software Foundation; either version 2 of the License, or (at your  *
+	*  option) any later version.                                              *
+	* --------------------------------------------                             *
+	* This program was sponsered by Golden Glair productions                   *
+	* http://www.goldenglair.com                                               *
+	\**************************************************************************/
+
+	/* $Id: index.php,v 1.11 2005/08/04 03:54:31 skwashd Exp $ */
+
+	$phpgw_info = array();
+	$GLOBALS['phpgw_info']['flags'] = array(
+		'currentapp' => 'news_admin',
+		'noheader' => True,
+		'nonavbar' => True,
+	);
+
+	include_once('../header.inc.php');
+
+	$ui= createobject('news_admin.uinews');
+	$ui->read_news();
+
+	$GLOBALS['phpgw']->common->phpgw_footer();
+?>
