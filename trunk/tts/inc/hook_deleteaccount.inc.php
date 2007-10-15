740b003937dc8d46ae2d65318a9a4c5467bcdce0
nobody <nobody@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2007-10-15 14:15:03 +0000

    initial import of tts

---
+++ trunk/tts/inc/hook_deleteaccount.inc.php
@@ -0,0 +1,27 @@
+<?php
+	/**
+	* Trouble Ticket System
+	*
+	* @author Mark Peters <skeeter@phpgroupware.org>
+	* @copyright Copyright (C) 2000-2005 Free Software Foundation, Inc. http://www.fsf.org/
+	* @license http://www.gnu.org/licenses/gpl.html GNU General Public License
+	* @package tts
+	* @subpackage hooks
+	* @version $Id: hook_deleteaccount.inc.php,v 1.2 2005/05/10 16:12:38 powerstat Exp $
+	*/
+
+	$GLOBALS['phpgw']->db->query("UPDATE phpgw_tts_tickets SET ticket_owner = " 
+		. intval($GLOBALS['HTTP_POST_VARS']['new_owner'])
+		. " WHERE ticket_owner=" . intval($GLOBALS['HTTP_POST_VARS']['account_id']) 
+		);
+
+	$GLOBALS['phpgw']->db->query("UPDATE phpgw_tts_tickets SET ticket_assignedto = "
+		. intval($GLOBALS['HTTP_POST_VARS']['new_owner'])
+		. " WHERE ticket_assignedto=" . intval($GLOBALS['HTTP_POST_VARS']['account_id'])
+	);
+
+	$GLOBALS['phpgw']->db->query("UPDATE phpgw_tts_views SET view_account_id = "
+		. intval($GLOBALS['HTTP_POST_VARS']['new_owner'])
+		. " WHERE view_account_id=" . intval($GLOBALS['HTTP_POST_VARS']['account_id'])
+	);
+?>
