601cb78c5f045e05c8bbd250eb2a4208e01db6dc
nobody <nobody@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2007-10-15 14:07:54 +0000

    initial import of manual

---
+++ trunk/manual/index.php
@@ -0,0 +1,24 @@
+<?php
+	/**
+	* Manual - User manual
+	*
+	* @copyright Copyright (C) 2000-2002,2005 Free Software Foundation, Inc. http://www.fsf.org/
+	* @license http://www.gnu.org/licenses/gpl.html GNU General Public License
+	* @package manual
+	* @version $Id: index.php,v 1.14 2007/02/20 13:34:37 sigurdne Exp $
+	*/
+
+	$GLOBALS['phpgw_info']['flags'] = array
+	(
+		'noheader'		=> true,
+		'nonavbar'		=> true,
+		'currentapp'	=> 'manual'
+	);
+	
+	/**
+	 * Include phpgroupware header
+	 */
+	include('../header.inc.php');
+
+	$GLOBALS['phpgw']->redirect_link('/index.php', array('menuaction' => 'manual.uimanual.index') );
+?>
