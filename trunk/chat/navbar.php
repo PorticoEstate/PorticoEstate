5fa6f129af3adcdb863b20d20a1819435dc4b202
nobody <nobody@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2007-10-15 14:05:38 +0000

    initial import of chat

---
+++ trunk/chat/navbar.php
@@ -0,0 +1,20 @@
+<?php
+	/**************************************************************************\
+	* phpGroupWare - Chat                                                      *
+	* http://www.phpgroupware.org                                              *
+	* This application written by Joseph Engo <jengo@phpgroupware.org>         *
+	* --------------------------------------------                             *
+	*  This program is free software; you can redistribute it and/or modify it *
+	*  under the terms of the GNU General Public License as published by the   *
+	*  Free Software Foundation; either version 2 of the License, or (at your  *
+	*  option) any later version.                                              *
+	\**************************************************************************/
+
+	/* $Id: navbar.php,v 1.6 2001/11/19 05:03:35 milosch Exp $ */
+
+	$phpgw_info['flags'] = array(
+		'currentapp'    => 'chat', 
+		'navbar_target' => '_top'
+	);
+	include('../header.inc.php');
+?>
