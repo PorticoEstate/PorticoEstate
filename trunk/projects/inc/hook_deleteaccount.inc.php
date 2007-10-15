6524cef7e9ab7a416976b9fd939e42f45fb710f8
nobody <nobody@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2007-10-15 14:14:01 +0000

    initial import of projects

---
+++ trunk/projects/inc/hook_deleteaccount.inc.php
@@ -0,0 +1,24 @@
+<?php
+	/**
+	* Project Manager
+	*
+	* @author Bettina Gille [ceb@phpgroupware.org]
+	* @copyright Copyright (C) 2000-2006 Free Software Foundation, Inc. http://www.fsf.org/
+	* @license http://www.gnu.org/licenses/gpl.html GNU General Public License
+	* @package projects
+	* @version $Id: hook_deleteaccount.inc.php,v 1.9 2006/12/05 19:40:45 sigurdne Exp $
+	* $Source: /sources/phpgroupware/projects/inc/hook_deleteaccount.inc.php,v $
+	*/
+
+	// Delete all records for a user
+	$pro = CreateObject('projects.boprojects');
+
+	if(intval($_POST['new_owner']) == 0)
+	{
+		$pro->delete_project(intval($_POST['account_id']),0,'account');
+	}
+	else
+	{
+		$pro->change_owner(intval($_POST['account_id']),intval($_POST['new_owner']));
+	}
+?>
---
+++ trunk/projects/templates/base/proid_help_popup.tpl
@@ -0,0 +1,15 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+	<head>
+	<title>projects nr. help</title>
+	</head>
+	<body>
+		<table>
+			<tr>
+				<td>
+				{helpmsg}
+				</td>
+			</tr>
+		</table>
+	</body>
+</html>
