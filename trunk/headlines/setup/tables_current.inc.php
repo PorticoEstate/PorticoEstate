af7f95b2f50a548ef23457de43ca625a6f622066
nobody <nobody@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2007-10-15 14:07:34 +0000

    initial import of headlines

---
+++ trunk/headlines/setup/tables_current.inc.php
@@ -0,0 +1,48 @@
+<?php
+  /**************************************************************************\
+  * phpGroupWare - Setup                                                     *
+  * http://www.phpgroupware.org                                              *
+  * --------------------------------------------                             *
+  *  This program is free software; you can redistribute it and/or modify it *
+  *  under the terms of the GNU General Public License as published by the   *
+  *  Free Software Foundation; either version 2 of the License, or (at your  *
+  *  option) any later version.                                              *
+  \**************************************************************************/
+
+  /**************************************************************************\
+  * This file should be generated for you. It should never be edited by hand *
+  \**************************************************************************/
+
+  /* $Id: tables_current.inc.php,v 1.5 2001/09/22 02:05:38 jengo Exp $ */
+
+  // table array for headlines
+	$phpgw_baseline = array(
+		'phpgw_headlines_sites' => array(
+			'fd' => array(
+				'con' => array('type' => 'auto','nullable' => False),
+				'display' => array('type' => 'varchar', 'precision' => 255,'nullable' => True),
+				'base_url' => array('type' => 'varchar', 'precision' => 255,'nullable' => True),
+				'newsfile' => array('type' => 'varchar', 'precision' => 255,'nullable' => True),
+				'lastread' => array('type' => 'int', 'precision' => 4,'nullable' => True),
+				'newstype' => array('type' => 'varchar', 'precision' => 15,'nullable' => True),
+				'cachetime' => array('type' => 'int', 'precision' => 4,'nullable' => True),
+				'listings' => array('type' => 'int', 'precision' => 4,'nullable' => True)
+			),
+			'pk' => array('con'),
+			'fk' => array(),
+			'ix' => array(),
+			'uc' => array()
+		),
+		'phpgw_headlines_cached' => array(
+			'fd' => array(
+				'site' => array('type' => 'int', 'precision' => 4,'nullable' => False),
+				'title' => array('type' => 'varchar', 'precision' => 255,'nullable' => True),
+				'link' => array('type' => 'varchar', 'precision' => 255,'nullable' => True)
+			),
+			'pk' => array(),
+			'fk' => array(),
+			'ix' => array(),
+			'uc' => array()
+		)
+	);
+?>
