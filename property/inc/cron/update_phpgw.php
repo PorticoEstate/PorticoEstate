7607268857a561797c1590bc9086a6816efd42ad
nobody <nobody@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2007-10-22 13:12:36 +0000

    Sigurd: clear db-cache for property in case property is updated

--- property/inc/cron/update_phpgw.php
+++ property/inc/cron/update_phpgw.php
@@ -136,6 +136,7 @@
 			$setup_info = $GLOBALS['phpgw_setup']->detection->compare_versions($setup_info);
 			$setup_info = $GLOBALS['phpgw_setup']->detection->check_depends($setup_info);
 			ksort($setup_info);
+			$clear_cache = '';
 			foreach($setup_info as $app => $appinfo)
 			{
 				if(isset($appinfo['status']) && $appinfo['status']=='U' && isset($appinfo['currentver']) && $appinfo['currentver'])
@@ -145,8 +146,16 @@
 					$GLOBALS['phpgw_setup']->process->upgrade($terror,false);
 					$GLOBALS['phpgw_setup']->process->upgrade_langs($terror,false);
 					$this->receipt['message'][]=array('msg'=> 'Upgraded application: ' . $appinfo['name']);
+					if($appinfo['name']=='property')
+					{
+						$clear_cache = True;
+					}
 				}
-			}	
+			}
+			if($clear_cache)
+			{
+				$this->db->query('DELETE FROM fm_cache');
+			}
 		}
 	}
 ?>
