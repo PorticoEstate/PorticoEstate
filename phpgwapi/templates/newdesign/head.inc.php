2135fdd872701268ed897d9718ad6d743ff5739c
Jan Åge Johnsen <jan.age.johnsen@gmail.com>
2007-12-13 10:39:23 +0000

    Doing some DRY

--- phpgwapi/templates/newdesign/head.inc.php
+++ phpgwapi/templates/newdesign/head.inc.php
@@ -6,43 +6,34 @@
 		$GLOBALS['phpgw_info']['server']['site_title'] = lang('please set a site name in admin &gt; siteconfig');
 	}
 
-	$stylesheets = array
-	(
-		"{$GLOBALS['phpgw_info']['server']['webserver_url']}/phpgwapi/js/yahoo/reset-fonts-grids/reset-fonts-grids.css",
-		"{$GLOBALS['phpgw_info']['server']['webserver_url']}/phpgwapi/templates/newdesign/css/base.css",
-		"{$GLOBALS['phpgw_info']['server']['webserver_url']}/phpgwapi/templates/newdesign/css/icons.css"
-	);
-
 	$app = $GLOBALS['phpgw_info']['flags']['currentapp'];
 
-	if(file_exists(PHPGW_SERVER_ROOT . '/phpgwapi/templates/newdesign/css/' . $GLOBALS['phpgw_info']['user']['preferences']['common']['theme'] . '.css'))
-		$theme_styles[] = "{$GLOBALS['phpgw_info']['server']['webserver_url']}/phpgwapi/templates/newdesign/css/{$GLOBALS['phpgw_info']['user']['preferences']['common']['theme']}.css";
-
-	if(file_exists(PHPGW_SERVER_ROOT . "/{$app}/templates/base/css/base.css"))
-		$stylesheets[] = "{$GLOBALS['phpgw_info']['server']['webserver_url']}/{$app}/templates/base/css/base.css";
-
-	if(file_exists(PHPGW_SERVER_ROOT . "/{$app}/templates/newdesign/css/base.css"))
-		$stylesheets[] = "{$GLOBALS['phpgw_info']['server']['webserver_url']}/{$app}/templates/newdesign/css/base.css";
-
-	if(file_exists(PHPGW_SERVER_ROOT . "/{$app}/templates/newdesign/css/{$GLOBALS['phpgw_info']['user']['preferences']['common']['theme']}.css"))
-		$stylesheets[] = "{$GLOBALS['phpgw_info']['server']['webserver_url']}/{$app}/templates/idots/css/{$GLOBALS['phpgw_info']['user']['preferences']['common']['theme']}.css";
-
+	$stylesheets = array
+	(
+		"/phpgwapi/js/yahoo/reset-fonts-grids/reset-fonts-grids.css",
+		"/phpgwapi/templates/newdesign/css/base.css",
+		"/phpgwapi/templates/newdesign/css/icons.css",
+		"/phpgwapi/templates/newdesign/css/{$GLOBALS['phpgw_info']['user']['preferences']['common']['theme']}.css",
+		"/{$app}/templates/base/css/base.css",
+		"/{$app}/templates/newdesign/css/base.css",
+		"/{$app}/templates/newdesign/css/{$GLOBALS['phpgw_info']['user']['preferences']['common']['theme']}.css"
+	);
 
 	$GLOBALS['phpgw']->template->set_root(PHPGW_TEMPLATE_DIR);
 	$GLOBALS['phpgw']->template->set_unknowns('remove');
 	$GLOBALS['phpgw']->template->set_file('head', 'head.tpl');
 	$GLOBALS['phpgw']->template->set_block('head', 'stylesheet', 'stylesheets');
 
-	//FIXME: support other CSS inclusions - see idots head.inc.php for an example
+	phpgwapi_yui::load_widget( 'dragdrop' );
+
 	foreach ( $stylesheets as $stylesheet )
 	{
-		$GLOBALS['phpgw']->template->set_var('stylesheet_uri', $stylesheet);
-		$GLOBALS['phpgw']->template->parse('stylesheets', 'stylesheet', true);
+		if( file_exists( PHPGW_SERVER_ROOT . $stylesheet ) )
+		{
+			$GLOBALS['phpgw']->template->set_var( 'stylesheet_uri', $GLOBALS['phpgw_info']['server']['webserver_url'] . $stylesheet );
+			$GLOBALS['phpgw']->template->parse('stylesheets', 'stylesheet', true);
+		}
 	}
-
-	phpgwapi_yui::load_widget( 'dragdrop' );
-	//
-	//$GLOBALS['phpgw']->js->validate_file( 'newdesign', 'base', '' );
 
 	$GLOBALS['phpgw']->template->set_var(array
 	(
