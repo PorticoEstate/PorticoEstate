fbe3413b963717df72c091f9ef95ef3235d96558
dave <dave.hall@skwashd.com>
2007-12-17 07:16:34 +0000

    cleanups and optimisations

--- phpgwapi/templates/idots/head.inc.php
+++ phpgwapi/templates/idots/head.inc.php
@@ -1,7 +1,7 @@
 <?php
 	/**
 	* Template header
-	* @copyright Copyright (C) 2003-2005 Free Software Foundation, Inc. http://www.fsf.org/
+	* @copyright Copyright (C) 2003-2007 Free Software Foundation, Inc. http://www.fsf.org/
 	* @license http://www.gnu.org/licenses/gpl.html GNU General Public License
 	* @package phpgwapi
 	* @subpackage gui
@@ -20,36 +20,28 @@
 
 	$app = $GLOBALS['phpgw_info']['flags']['currentapp'];
 
-	$theme_styles[] = "{$GLOBALS['phpgw_info']['server']['webserver_url']}/phpgwapi/templates/idots/css/base.css";
-	if(file_exists(PHPGW_SERVER_ROOT . '/phpgwapi/templates/idots/css/' . $GLOBALS['phpgw_info']['user']['preferences']['common']['theme'] . '.css'))
+	$theme_styles = array("/phpgwapi/templates/idots/css/base.css");
+
+	if ( file_exists(PHPGW_SERVER_ROOT . "/phpgwapi/templates/idots/css/{$GLOBALS['phpgw_info']['user']['preferences']['common']['theme']}.css") )
 	{
-		$theme_styles[] = "{$GLOBALS['phpgw_info']['server']['webserver_url']}/phpgwapi/templates/idots/css/{$GLOBALS['phpgw_info']['user']['preferences']['common']['theme']}.css";
+		$theme_styles[] = "/phpgwapi/templates/idots/css/{$GLOBALS['phpgw_info']['user']['preferences']['common']['theme']}.css";
 	}
 	else
 	{
-		$theme_styles[] = "{$GLOBALS['phpgw_info']['server']['webserver_url']}/phpgwapi/templates/idots/css/idots.css";
+		$theme_styles[] = "/phpgwapi/templates/idots/css/idots.css";
 		$GLOBALS['phpgw_info']['user']['preferences']['common']['theme'] = 'idots';
 	}
-
-	if(file_exists(PHPGW_SERVER_ROOT . "/{$app}/templates/base/css/base.css"))
-	{
-		$theme_styles[] = "{$GLOBALS['phpgw_info']['server']['webserver_url']}/{$app}/templates/base/css/base.css";
-	}
-
-	if(file_exists(PHPGW_SERVER_ROOT . "/{$app}/templates/idots/css/base.css"))
-	{
-		$theme_styles[] = "{$GLOBALS['phpgw_info']['server']['webserver_url']}/{$app}/templates/idots/css/base.css";
-	}
-
-	if(file_exists(PHPGW_SERVER_ROOT . "/{$app}/templates/idots/css/{$GLOBALS['phpgw_info']['user']['preferences']['common']['theme']}.css"))
-	{
-		$theme_styles[] = "{$GLOBALS['phpgw_info']['server']['webserver_url']}/{$app}/templates/idots/css/{$GLOBALS['phpgw_info']['user']['preferences']['common']['theme']}.css";
-	}
+	$theme_styles[] = "/{$app}/templates/base/css/base.css";
+	$theme_styles[] = "/{$app}/templates/idots/css/base.css";
+	$theme_styles[] = "/{$app}/templates/idots/css/{$GLOBALS['phpgw_info']['user']['preferences']['common']['theme']}.css";
 
 	foreach ( $theme_styles as $style )
 	{
-		$GLOBALS['phpgw']->template->set_var('theme_style', $style);
-		$GLOBALS['phpgw']->template->parse('theme_stylesheets', 'theme_stylesheet', true);
+		if( file_exists( PHPGW_SERVER_ROOT . $style ) )
+		{
+			$GLOBALS['phpgw']->template->set_var('theme_style', "{$GLOBALS['phpgw_info']['server']['webserver_url']}$style");
+			$GLOBALS['phpgw']->template->parse('theme_stylesheets', 'theme_stylesheet', true);
+		}
 	}
 	
 	$app = $app ? ' ['.(isset($GLOBALS['phpgw_info']['apps'][$app]) ? $GLOBALS['phpgw_info']['apps'][$app]['title'] : lang($app)).']':'';
@@ -59,9 +51,8 @@
 		'css'			=> $GLOBALS['phpgw']->common->get_css(),
 		'javascript'	=> $GLOBALS['phpgw']->common->get_javascript(),
 		'img_icon'      => PHPGW_IMAGES_DIR . '/favicon.ico',
-		'img_shortcut'  => PHPGW_IMAGES_DIR . '/favicon.ico',
 		'str_base_url'	=> $GLOBALS['phpgw']->link('/', array(), true),		
-		'website_title'	=> $GLOBALS['phpgw_info']['server']['site_title'] . $app,
+		'website_title'	=> "{$GLOBALS['phpgw_info']['server']['site_title']} | {$app}",
 		'win_on_events'	=> $GLOBALS['phpgw']->common->get_on_events(),
 	));
 
--- phpgwapi/templates/idots/head.tpl
+++ phpgwapi/templates/idots/head.tpl
@@ -9,35 +9,17 @@
 		<meta name="keywords" content="phpGroupWare">
 		<meta name="robots" content="none">
 		<link rel="icon" href="{img_icon}" type="image/x-ico">
-		<link rel="shortcut icon" href="{img_shortcut}">
+		<link rel="shortcut icon" href="{img_icon}">
 		{css}
-                <!-- BEGIN theme_stylesheet -->
-                <link href="{theme_style}" type="text/css" rel="StyleSheet">
-                <!-- END theme_stylesheet -->
+		<!-- BEGIN theme_stylesheet -->
+		<link href="{theme_style}" type="text/css" rel="StyleSheet">
+		<!-- END theme_stylesheet -->
 		<title>{website_title}</title>
 		<script type="text/javascript">
 			<!--
 			var strBaseURL = '{str_base_url}';
 			{win_on_events}
 			-->
-			var page;
-
-			function openwindow(url)
-			{
-				if (page)
-				{
-					if (page.closed)
-					{
-						page.stop;
-						page.close;
-					}
-				}
-				page = window.open(url, "pageWindow","width=700,height=600,location=no,menubar=no,directories=no,toolbar=no,scrollbars=yes,resizable=yes,status=no");
-				if (page.opener == null)
-				{
-					page.opener = window;
-				}
-			}
 		</script>
 		{javascript}
 	</head>
