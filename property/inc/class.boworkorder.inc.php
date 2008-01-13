9dbba0b5d7a54c5ee09cf04482bd26f6a8be9d8f
Sigurd Nes <sigurdne@users.noreply.github.com>
2008-01-13 10:49:37 +0000

    merge from stable

--- property/inc/class.boworkorder.inc.php
+++ property/inc/class.boworkorder.inc.php
@@ -252,9 +252,12 @@
 			$dateformat						= $GLOBALS['phpgw_info']['user']['preferences']['common']['dateformat'];
 			$workorder['start_date']		= $GLOBALS['phpgw']->common->show_date($workorder['start_date'],$dateformat);
 			$workorder['end_date']			= $GLOBALS['phpgw']->common->show_date($workorder['end_date'],$dateformat);
-			$vendor							= $contacts->read_single(array('actor_id'=>(int)$workorder['vendor_id']));
-			if(is_array($vendor))
+
+			if(isset($workorder['vendor_id']) && $workorder['vendor_id'])
 			{
+				$custom 		= createObject('phpgwapi.custom_fields');
+				$vendor['attributes'] = $custom->get_attribs('property','.vendor', 0, '', 'ASC', 'attrib_sort', true, true);
+				$vendor			= $contacts->read_single($workorder['vendor_id'],$vendor);
 				foreach($vendor['attributes'] as $attribute)
 				{
 					if($attribute['name']=='org_name')
