b810fbdaad085e9d94bb0e0b964a6d593a39b866
janaage@hikt.no <janaage@hikt.no@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2008-01-14 14:30:35 +0000

    Class for storing and retriving appsessions through JSON ref #48

---
+++ trunk/phpgwapi/inc/class.template_newdesign.inc.php
@@ -0,0 +1,61 @@
+<?php
+
+	class phpgwapi_template_newdesign
+	{
+		var $public_functions = array
+		(
+			'store'			=> True,
+			'retrive'		=> True
+		);
+
+		function store()
+		{
+			$location = phpgw::get_var('location');
+			$data = phpgw::get_var('data', 'raw');
+
+			if( $location == null )
+			{
+				header("HTTP/1.0 406 Not Acceptable");
+				return "Missing location parameter";
+			}
+
+			$json = execMethod('phpgwapi.Services_JSON.decode', $data);
+
+			if( $json == null )
+			{
+				header("HTTP/1.0 406 Not Acceptable");
+				return "Invalid JSON data parameter";
+			}
+
+			$GLOBALS['phpgw']->session->appsession($location,'template_newdesign', $json);
+			return $json;
+		}
+
+		function retrive($location=null)
+		{
+			$location = phpgw::get_var('location');
+
+			if( $location == null )
+			{
+				header("HTTP/1.0 406 Not Acceptable");
+				return "Missing location parameter";
+			}
+
+			$data = $GLOBALS['phpgw']->session->appsession($location,'template_newdesign');
+
+			if($data == null)
+			{
+				header("HTTP/1.0 404 Not Found");
+				return "No data found on that location";
+			}
+
+			return $data;
+		}
+
+		function retrive_local($location)
+		{
+			$data = $GLOBALS['phpgw']->session->appsession($location,'template_newdesign');
+			return execMethod('phpgwapi.Services_JSON.encode', $data);
+		}
+	}
+?>
