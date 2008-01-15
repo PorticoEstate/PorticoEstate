9ecae70e0d6001b7325194fc838d8e5af639ecc5
dave <dave.hall@skwashd.com>
2008-01-15 09:48:25 +0000

    test checkin updating tickets (removing trailing whitespace) - see #55

--- property/inc/class.menu.inc.php
+++ property/inc/class.menu.inc.php
@@ -6,7 +6,7 @@
 	 * @author Sigurd Nes <sigurdne@online.no>
 	 * @copyright Copyright (C) 2007 Free Software Foundation, Inc. http://www.fsf.org/
 	 * @license http://www.gnu.org/licenses/gpl.html GNU General Public License
-	 * @package property 
+	 * @package property
 	 * @version $Id$
 	 */
 
@@ -30,7 +30,7 @@
 	 * Menus
 	 *
 	 * @package property
-	 */	
+	 */
 	class property_menu
 	{
 		/**
