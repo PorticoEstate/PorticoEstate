307a04945131fea273d1311bc39ce103fc6330cd
janaage@hikt.no <janaage@hikt.no@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2008-02-01 13:58:28 +0000

    Fixed notices and better solution to empty navbar_config Fixes #155

--- trunk/phpgwapi/templates/newdesign/head.inc.php
+++ trunk/phpgwapi/templates/newdesign/head.inc.php
@@ -48,24 +48,26 @@
 	// get set at expanded even though that isn't needed.
 
 	$navbar_config = execMethod('phpgwapi.template_newdesign.retrieve_local', 'navbar_config');
-	if(!isset($navbar_config))
-	{
-		$navbar_config = array();
-	}
 
-	$current_selection = $GLOBALS['phpgw_info']['flags']['menu_selection'];
-	while($current_selection)
+	if( isset($GLOBALS['phpgw_info']['flags']['menu_selection']) )
 	{
-		$navbar_config["navbar::$current_selection"] = true;
-		$current_selection = implode("::", explode("::", $current_selection, -1));
-	}
+		if(!isset($navbar_config))
+		{
+			$navbar_config = array();
+		}
 
-	if(count($navbar_config)) {
+		$current_selection = $GLOBALS['phpgw_info']['flags']['menu_selection'];
+
+		while($current_selection)
+		{
+			$navbar_config["navbar::$current_selection"] = true;
+			$current_selection = implode("::", explode("::", $current_selection, -1));
+		}
+
 		phpgwapi_template_newdesign::store_local('navbar_config', $navbar_config);
 	}
-	else {
-		$navbar_config = null;
-	}
+
+	var_dump($navbar_config);
 
 	$app = lang($app);
 	$tpl_vars = array
