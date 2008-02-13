b52a0884f6036512262665c5fbf0129aa9be5b3d
Sigurd Nes <sigurdne@users.noreply.github.com>
2008-02-13 12:24:45 +0000

    merge from stable - see #82

--- property/inc/class.uilocation.inc.php
+++ property/inc/class.uilocation.inc.php
@@ -259,7 +259,7 @@
 								$content[$j]['row'][$i]['name'] 			= $uicols['name'][$i];
 								$content[$j]['row'][$i]['lookup'] 			= $lookup;
 								$content[$j]['row'][$i]['align'] 			= (isset($uicols['align'][$i])?$uicols['align'][$i]:'center');
-								if($uicols['input_type'][$i]=='link' && $location[$uicols['name'][$i]])
+								if($uicols['datatype'][$i]=='link' && $location[$uicols['name'][$i]])
 								{
 									$content[$j]['row'][$i]['text']		= lang('link');
 									$content[$j]['row'][$i]['link']		= $location[$uicols['name'][$i]];
