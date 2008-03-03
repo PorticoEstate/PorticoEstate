b47c13a5b954bb29c597f20586f1a0c311da7cca
Sigurd Nes <sigurdne@users.noreply.github.com>
2008-03-03 18:24:26 +0000

    Corrected version - see #132

--- phpgwapi/setup/setup.inc.php
+++ phpgwapi/setup/setup.inc.php
@@ -12,7 +12,7 @@
 	// Basic information about this app
 	$setup_info['phpgwapi']['name']      = 'phpgwapi';
 	$setup_info['phpgwapi']['title']     = 'phpgwapi';
-	$setup_info['phpgwapi']['version']   = '0.9.17.512';
+	$setup_info['phpgwapi']['version']   = '0.9.17.514';
 	$setup_info['phpgwapi']['versions']['current_header'] = '1.28';
 	$setup_info['phpgwapi']['enable']    = 3;
 	$setup_info['phpgwapi']['app_order'] = 1;
