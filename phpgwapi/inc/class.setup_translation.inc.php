0d33f01beda65e2e069ed9abb23f496253ecd536
Sigurd Nes <sigurdne@users.noreply.github.com>
2008-04-13 16:34:24 +0000

    really fix reference to lang file  - see #192

--- phpgwapi/inc/class.setup_translation.inc.php
+++ phpgwapi/inc/class.setup_translation.inc.php
@@ -32,10 +32,10 @@
 			$ConfigLang = phpgw::get_var('ConfigLang');
 			$this->set_userlang($ConfigLang);
 
-			$fn = "./phpgwapi/setup/phpgw_{$this->userlang}.lang";
+			$fn = PHPGW_SERVER_ROOT . "/phpgwapi/setup/phpgw_{$this->userlang}.lang";
 			if (!file_exists($fn))
 			{
-				$fn = './phpgwapi/setup/phpgw_en.lang';
+				$fn = PHPGW_SERVER_ROOT . '/phpgwapi/setup/phpgw_en.lang';
 			}
 
 			$strings = $this->parse_lang_file($fn, $this->userlang);
