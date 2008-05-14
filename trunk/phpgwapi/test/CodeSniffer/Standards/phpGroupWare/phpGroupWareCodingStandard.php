9967998a5543abd47ceacc88aa61a750554fbfdf
dave <dave@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2008-05-14 08:36:46 +0000

    improved checks - see #199

--- trunk/phpgwapi/test/CodeSniffer/Standards/phpGroupWare/phpGroupWareCodingStandard.php
+++ trunk/phpgwapi/test/CodeSniffer/Standards/phpGroupWare/phpGroupWareCodingStandard.php
@@ -6,66 +6,79 @@
  *
  * @category  PHP
  * @package   PHP_CodeSniffer
- * @author	  Sigurd Nes <sigurdne@online.no>
+ * @author      Sigurd Nes <sigurdne@online.no>
+ * @author    Dave Hall <skwashd@phpgroupware.org>
+ * @copyright 2008 Free Software Foundation, Inc http://fsf.org
  * @license   http://matrix.squiz.net/developer/tools/php_cs/licence BSD Licence
  * @version   CVS: $Id: coding-standard-tutorial.xml,v 1.6 2007/10/15 03:28:17 squiz Exp $
- * @link	  http://pear.php.net/package/PHP_CodeSniffer
+ * @link      http://pear.php.net/package/PHP_CodeSniffer
  */
 
-require_once 'PHP/CodeSniffer/Standards/CodingStandard.php';
+if (class_exists('PHP_CodeSniffer_Standards_CodingStandard', true) === false) {
+    throw new PHP_CodeSniffer_Exception('Class PHP_CodeSniffer_Standards_CodingStandard not found');
+}
 
 /**
  * phpGroupWare Coding Standard.
  *
  * @category  PHP
  * @package   PHP_CodeSniffer
- * @author	  Sigurd Nes <sigurdne@online.no>
+ * @author      Sigurd Nes <sigurdne@online.no>
+ * @author    Dave Hall <skwashd@phpgroupware.org>
+ * @copyright 2008 Free Software Foundation, Inc http://fsf.org
  * @license   http://matrix.squiz.net/developer/tools/php_cs/licence BSD Licence
  * @version   Release: @package_version@
- * @link	  http://pear.php.net/package/PHP_CodeSniffer
+ * @link      http://pear.php.net/package/PHP_CodeSniffer
 */
 class PHP_CodeSniffer_Standards_phpGroupWare_phpGroupWareCodingStandard extends PHP_CodeSniffer_Standards_CodingStandard
 {
-	/**
-	 * Return a list of external sniffs to include with this standard.
-	 *
-	 * The Zend standard uses some PEAR sniffs.
-	 *
-	 * @return array
-	 */
-	public function getIncludedSniffs()
-	{
-		return array(
-		//		'Generic/Sniffs/Files/LineLengthSniff.php',
-				'Generic/Sniffs/PHP/DisallowShortOpenTagSniff.php',
-				'Generic/Sniffs/PHP/LowerCaseConstantSniff.php',
-				'Generic/Sniffs/Functions/OpeningFunctionBraceBsdAllmanSniff.php',
-				'Generic/Sniffs/Files/LineEndingsSniff.php',
-				'Generic/Sniffs/Formatting/MultipleStatementAlignmentSniff.php',
-				'Generic/Sniffs/Formatting/SpaceAfterCastSniff.php',
-		//		'PEAR/Sniffs/Functions/FunctionCallArgumentSpacingSniff.php',
-				'PEAR/Sniffs/Functions/FunctionCallSignatureSniff.php',
-				'PEAR/Sniffs/Functions/ValidDefaultValueSniff.php',
-				'PEAR/Sniffs/Commenting/FunctionCommentSniff.php',
-				'Squiz/Sniffs/Functions/GlobalFunctionSniff.php',
-				'Squiz/Sniffs/Arrays/ArrayBracketSpacingSniff.php',
-		//		'Squiz/Sniffs/Arrays/ArrayDeclarationSniff.php',
-				'Squiz/Sniffs/ControlStructures/ElseIfDeclarationSniff.php',
-				'PEAR/Sniffs/Commenting/InlineCommentSniff.php',
-				'Generic/Sniffs/Metrics/CyclomaticComplexitySniff.php',
-				'Generic/Sniffs/Metrics/NestingLevelSniff.php',
-				'Generic/Sniffs/NamingConventions/UpperCaseConstantNameSniff.php',
-		//		'Squiz/Sniffs/Commenting/', we need something similar to this that meets our requirements
-				'Zend/Sniffs/Files/ClosingTagSniff.php',						// Sigurd: closing tag not allowed?
-				'Squiz/Sniffs/PHP/ForbiddenFunctionsSniff.php',
-				'Squiz/Sniffs/PHP/DisallowCountInLoopsSniff.php',				// performance
-				'Squiz/Sniffs/PHP/DisallowMultipleAssignmentsSniff.php',		// readability
-				'Squiz/Sniffs/PHP/EvalSniff.php',								// security 
-				'Squiz/Sniffs/PHP/InnerFunctionsSniff.php',						// maintainability
-				'Squiz/Sniffs/PHP/NonExecutableCodeSniff.php'					// sanity check
-			   );
+    /**
+     * Return a list of external sniffs to include with this standard.
+     *
+     * phpGroupWare uses various sniffs from other coding standards
+     *
+     * @return array
+     */
+    public function getIncludedSniffs()
+    {
+        return array(
+                // Useful Generics
+                'Generic/Sniffs/Files/LineEndingsSniff.php',
+                'Generic/Sniffs/Formatting/MultipleStatementAlignmentSniff.php',
+                'Generic/Sniffs/Formatting/SpaceAfterCastSniff.php',
+                'Generic/Sniffs/Functions/OpeningFunctionBraceBsdAllmanSniff.php',
+                'Generic/Sniffs/Metrics/CyclomaticComplexitySniff.php',
+                'Generic/Sniffs/Metrics/NestingLevelSniff.php',
+                'Generic/Sniffs/NamingConventions/UpperCaseConstantNameSniff.php',
+                'Generic/Sniffs/PHP/DisallowShortOpenTagSniff.php',
+                'Generic/Sniffs/PHP/LowerCaseConstantSniff.php',
+                
+                // The stuff from PEAR we like
+                'PEAR/Sniffs/Commenting/FunctionCommentSniff.php',
+                'PEAR/Sniffs/Commenting/InlineCommentSniff.php',
+                'PEAR/Sniffs/Files/IncludingFileSniff.php',
+                'PEAR/Sniffs/Functions/FunctionCallArgumentSpacingSniff.php',
+                'PEAR/Sniffs/Functions/FunctionCallSignatureSniff.php',
+                'PEAR/Sniffs/Functions/ValidDefaultValueSniff.php',
 
-	}//end getIncludedSniffs()
+                // Some quality and security stuff from Squiz - thanks guys
+                'Squiz/Sniffs/Arrays/ArrayBracketSpacingSniff.php',
+                // 'Squiz/Sniffs/Arrays/ArrayDeclarationSniff.php',
+                // 'Squiz/Sniffs/Commenting/', we need something similar to this that meets our requirements
+                'Squiz/Sniffs/ControlStructures/ElseIfDeclarationSniff.php',
+                'Squiz/Sniffs/Functions/GlobalFunctionSniff.php',
+                'Squiz/Sniffs/PHP/CommentedOutCodeSniff.php',
+                'Squiz/Sniffs/PHP/DisallowCountInLoopsSniff.php',
+                'Squiz/Sniffs/PHP/DisallowMultipleAssignmentsSniff.php',
+                'Squiz/Sniffs/PHP/EvalSniff.php',
+                'Squiz/Sniffs/PHP/InnerFunctionsSniff.php',
+                'Squiz/Sniffs/PHP/NonExecutableCodeSniff.php',
+
+                // And a dash of Zend
+                'Zend/Sniffs/Files/ClosingTagSniff.php'
+               );
+
+    }//end getIncludedSniffs()
 
 }//end class
 ?>
