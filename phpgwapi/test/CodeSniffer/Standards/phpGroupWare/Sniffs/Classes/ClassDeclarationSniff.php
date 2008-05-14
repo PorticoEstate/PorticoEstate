33b383b371f99c677da6d6591171f2cc4c5a9656
dave <dave.hall@skwashd.com>
2008-05-14 08:36:46 +0000

    improved checks - see #199

--- phpgwapi/test/CodeSniffer/Standards/phpGroupWare/Sniffs/Classes/ClassDeclarationSniff.php
+++ phpgwapi/test/CodeSniffer/Standards/phpGroupWare/Sniffs/Classes/ClassDeclarationSniff.php
@@ -48,67 +48,61 @@ class phpGroupWare_Sniffs_Classes_ClassDeclarationSniff implements PHP_CodeSniff
 	}//end register()
 
 
-	/**
-	 * Processes this test, when one of its tokens is encountered.
-	 *
-	 * @param PHP_CodeSniffer_File $phpcsFile The file being scanned.
-	 * @param int                  $stackPtr  The position of the current token in the
-	 *                                        stack passed in $tokens.
-	 *
-	 * @return void
-	 */
-	public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
-	{
-		$tokens = $phpcsFile->getTokens();
+    /**
+     * Processes this test, when one of its tokens is encountered.
+     *
+     * @param PHP_CodeSniffer_File $phpcsFile The file being scanned.
+     * @param int                  $stackPtr  The position of the current token in the
+     *                                        stack passed in $tokens.
+     *
+     * @return void
+     */
+    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    {
+        $tokens = $phpcsFile->getTokens();
 
-		$curlyBrace  = $tokens[$stackPtr]['scope_opener'];
-		$lastContent = $phpcsFile->findPrevious(T_WHITESPACE, ($curlyBrace - 1), $stackPtr, true);
-		$classLine   = $tokens[$lastContent]['line'];
-		$braceLine   = $tokens[$curlyBrace]['line'];
-		if ($braceLine === $classLine)
-		{
-			$error  = 'Opening brace of a ';
-			$error .= $tokens[$stackPtr]['content'];
-			$error .= ' must be on the line after the definition';
-			$phpcsFile->addError($error, $curlyBrace);
-			return;
-		}
-		else if ($braceLine > ($classLine + 1))
-		{
-			$difference  = ($braceLine - $classLine - 1);
-			$difference .= ($difference === 1) ? ' line' : ' lines';
-			$error	      = 'Opening brace of a ';
-			$error	     .= $tokens[$stackPtr]['content'];
-			$error	     .= ' must be on the line following the ';
-			$error	     .= $tokens[$stackPtr]['content'];
-			$error	     .= ' declaration; found '.$difference;
-			$phpcsFile->addError($error, $curlyBrace);
-			return;
-		}
+        $curlyBrace  = $tokens[$stackPtr]['scope_opener'];
+        $lastContent = $phpcsFile->findPrevious(T_WHITESPACE, ($curlyBrace - 1), $stackPtr, true);
+        $classLine   = $tokens[$lastContent]['line'];
+        $braceLine   = $tokens[$curlyBrace]['line'];
+        if ($braceLine === $classLine) {
+            $error  = 'Opening brace of a ';
+            $error .= $tokens[$stackPtr]['content'];
+            $error .= ' must be on the line after the definition';
+            $phpcsFile->addError($error, $curlyBrace);
+            return;
+        } else if ($braceLine > ($classLine + 1)) {
+            $difference  = ($braceLine - $classLine - 1);
+            $difference .= ($difference === 1) ? ' line' : ' lines';
+            $error       = 'Opening brace of a ';
+            $error      .= $tokens[$stackPtr]['content'];
+            $error      .= ' must be on the line following the ';
+            $error      .= $tokens[$stackPtr]['content'];
+            $error      .= ' declaration; found '.$difference;
+            $phpcsFile->addError($error, $curlyBrace);
+            return;
+        }
 
-		if ($tokens[($curlyBrace + 1)]['content'] !== $phpcsFile->eolChar)
-		{
-			$type  = strtolower($tokens[$stackPtr]['content']);
-			$error = "Opening $type brace must be on a line by itself";
-			$phpcsFile->addError($error, $curlyBrace);
-		}
+        if ($tokens[($curlyBrace + 1)]['content'] !== $phpcsFile->eolChar) {
+            $type  = strtolower($tokens[$stackPtr]['content']);
+            $error = "Opening $type brace must be on a line by itself";
+            $phpcsFile->addError($error, $curlyBrace);
+        }
 
-		if ($tokens[($curlyBrace - 1)]['code'] === T_WHITESPACE)
-		{
-			$prevContent = $tokens[($curlyBrace - 1)]['content'];
-			if ($prevContent !== $phpcsFile->eolChar)
-			{
-				$blankSpace = substr($prevContent, strpos($prevContent, $phpcsFile->eolChar));
-				$spaces	    = strlen($blankSpace);
-				if ($spaces !== 4)
-				{
-					$error = "Expected 1 spaces before opening brace; $spaces found";
-					$phpcsFile->addError($error, $curlyBrace);
-				}
-			}
-		}
+        if ($tokens[($curlyBrace - 1)]['code'] === T_WHITESPACE) {
+            $prevContent = $tokens[($curlyBrace - 1)]['content'];
+            if ($prevContent !== $phpcsFile->eolChar) {
+                $blankSpace = substr($prevContent, strpos($prevContent, $phpcsFile->eolChar));
+                $spaces     = strlen($blankSpace);
+                if ($spaces !== 4) {
+                    $error = "Expected 1 tab before opening brace; $spaces found";
+                    $phpcsFile->addError($error, $curlyBrace);
+                }
+            }
+        }
 
-	}//end process()
+    }//end process()
+
 
 }//end class
 
