961b974aa2e99c76bf99625f3c7e48a45832bc5d
dave <dave@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2008-07-16 14:08:11 +0000

    start implementation of new items, stubbed classes - see #360

---
+++ trunk/property/inc/class.so_items.inc.php
@@ -0,0 +1,65 @@
+	<?php
+	/**
+	 * Property - Item Storage Class
+	 *
+	 * @author Dave Hall <skwashd@phpgroupware.org>
+	 * @copyright (c) 2008 Dave Hall http://davehall.com.au
+	 * @license http://www.gnu.org/licenses/gpl.html GNU General Public License Version 3
+	 * @version $Id$
+	 * @package phpgroupware
+	 * @subpackage property
+	 */
+
+	/*
+	   This program is free software: you can redistribute it and/or modify
+	   it under the terms of the GNU General Public License as published by
+	   the Free Software Foundation, either version 3 of the License, or
+	   (at your option) any later version.
+
+	   This program is distributed in the hope that it will be useful,
+	   but WITHOUT ANY WARRANTY; without even the implied warranty of
+	   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	   GNU General Public License for more details.
+
+	   You should have received a copy of the GNU General Public License
+	   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	 */
+
+	/**
+	 * Property - Item Storage Class
+	 *
+	 * @package phpgroupware
+	 * @subpackage property
+	 */
+	class property_so_items
+	{
+		protected $_db;
+
+		public function __construct()
+		{
+		}
+
+		public function add(property_item $item)
+		{
+		}
+
+		public function delete($item_id)
+		{
+		}
+
+		public function edit(property_item $item)
+		{
+		}
+
+		public function find(array_1 $criteria)
+		{
+		}
+
+		public function get($item_id)
+		{
+		}
+
+		protected function _diff(property_item $item)
+		{
+		}
+	}
