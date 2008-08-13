2aa5aab4475c49a9ba2d1bea746470bbd8f0a2bb
sigurd <sigurd@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2008-08-13 16:55:44 +0000

    prototyping

---
+++ branches/dev-sigurd-2/equipo/inc/hook_settings.inc.php
@@ -0,0 +1,20 @@
+<?php
+	/**
+	* phpGroupWare - DEMO: A demo application.
+	*
+	* @author Sigurd Nes <sigurdne@online.no>
+	* @copyright Copyright (C) 2003-2005 Free Software Foundation, Inc. http://www.fsf.org/
+	* @license http://www.gnu.org/licenses/gpl.html GNU General Public License
+	* @internal Development of this application was funded by http://www.bergen.kommune.no/bbb_/ekstern/
+	* @package demo
+	* @subpackage core
+ 	* @version $Id: hook_settings.inc.php 690 2008-02-02 10:11:33Z dave $
+	*/
+
+	//$this->currentapp			= $GLOBALS['phpgw_info']['flags']['currentapp'];
+
+	$yes_and_no = array(
+		'True' => 'Yes',
+		''     => 'No'
+	);
+
