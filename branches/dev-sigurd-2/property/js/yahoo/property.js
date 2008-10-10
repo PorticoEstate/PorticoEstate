7e069babd3bd861c573485f334a9d17a4f7c59ae
cesar <cesar@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2008-10-10 01:05:36 +0000

    modifying property.js customized code  - see #369

--- branches/dev-sigurd-2/property/js/yahoo/property.js
+++ branches/dev-sigurd-2/property/js/yahoo/property.js
@@ -1,133 +1,69 @@
 
 YAHOO.util.Event.addListener(window, "load", function()
 {
-	var Dom = YAHOO.util.Dom;
 	var oSelectedTR;
-	var type_id = YAHOO.util.Dom.get( 'type_id' );
-	var oButtonSearch,oButtonExport = null;
-
 	var menuCB,optionsCB, options_combo_box;
 	var array_options = new Array();
-	var oMenuButton_0, oMenuButton_1, oMenuButton_2, oMenuButton_3;
 
-	//document.getElementById('txt_query').focus();
 	var flag = 0;
+	var flag_update_filter='';
 	var myDataSource,myDataTable, myContextMenu, myPaginator ;
-	var ds;
+	var ds, values_ds;
 	var myrowsPerPage,mytotalRows,ActualValueRowsPerPageDropdown;
-	var values_ds;
+
+	//------------------------------------------------------
+	var type_id = YAHOO.util.Dom.get('type_id');
+	var imput= "txt_query";
+
+	//define SelectButton
+ 	var oMenuButton_0, oMenuButton_1, oMenuButton_2, oMenuButton_3;
+ 	var selectsButtons = [
+	{order:0, var_URL:'cat_id',name:'btn_cat_id',style:'categorybutton',dependiente:''},
+	{order:1, var_URL:'district_id',name:'btn_district_id',style:'districtbutton',dependiente:2},
+	{order:2, var_URL:'part_of_town_id',name:'btn_part_of_town_id',style:'partOFTownbutton',dependiente:''},
+	{order:3, var_URL:'filter', name:'btn_owner_id',style:'ownerIdbutton',dependiente:''}
+	]
+
+	// define buttons
+	//var oNormalButton_0, oNormalButton_1, oNormalButton_2;
+	var normalButtons = [
+	{order:0, name:'btn_search', funct:"onSearchClick"},
+	{order:1, name:'btn_new', funct:"onDownloadClick"},
+	{order:2, name:'btn_export', funct:"onDownloadClick"}
+	]
+
+	//var oNormalButton_0;
+	var textImput = [
+	{order:0, name:'txt_query'}
+	]
+
+ /********************************************************************************
+ *
+ */
+this.initial_focus = function(control)
+{
+	YAHOO.util.Dom.get(control).focus();
+}
 
  /********************************************************************************
  *
  */
 this.filter_data = function(query)
 {
-	document.getElementById('txt_query').value = query;
+	//document.getElementById(imput).value = query;
+	YAHOO.util.Dom.get(imput).value = query;
 	path_values.query = query;
 	execute_ds();
 }
-
-
- /********************************************************************************
- * create a array whith values strValues (..#../..#). Necesary for selected nested
- */
-this.create_array_values_list = function(stValues)
-  {
-   var temp1,temp2,temp3 = new Array();
-
-   temp1 = stValues.split('/');
-   for(i=0 ; i < temp1.length -1 ; i++ ) // -1 because la string has a '/' at last
-   {
-    temp2 = temp1[i].split('#');
-    temp3[i] = new Array();
-    for(j=0 ; j < temp2.length ; j++ )
-    {
-     temp3[i][j]=temp2[j];
-    }
-   }
-   return temp3;
-}
-
-
- /********************************************************************************
- * stValues:  values of select control, separate whit / and #
- * source: indicate the variable-name passed in the URL by GET
- */
-  this.create_menu_list = function(stValues,source)
-{
-   var temp1, temp2, MenuButtonMenu = new Array();
-   temp1 = stValues.split('/');
-   for(i=0 ; i < temp1.length -1 ; i++ ) // -1 because the string has a '/' at last
-   {
-    temp2 = temp1[i].split('#');
-    temp2.push(source);
-    //temp2.push(i); se usara para el check
-    var obj_temp = {id: temp2[3], text: temp2[1], value: temp2[0], onclick: { fn: onMenuItemClick , obj: temp2} };
-    /*if(i==0)
-     obj_temp.checked = true;*/
-    MenuButtonMenu.push(obj_temp);
-
- }
-   return MenuButtonMenu;
-   }
-
- /********************************************************************************
- * p_oItem: values passed
- * p_oItem[0]: id
- * p_oItem[1]: texto
- * p_oItem[2]:variable-name GET
- * p_oItem[3]:order option of the select
- */
-   this.onMenuItemClick = function(p_sType, p_aArgs, p_oItem)
-   {
-	    if(p_oItem[2]=='cat_id')
-	    {
-		     //assign label to control selected
-		     oMenuButton_0.set("label", ("<em>" + p_oItem[1] + "</em>"));
-		     //use field ID for put the value selected
-		     oMenuButton_0.set("value", p_oItem[0]);
-		     //assign filter-values
-		     path_values.cat_id = p_oItem[0];
-	    }
-	    if(p_oItem[2]=='district_id'){
-		     oMenuButton_1.set("label", ("<em>" + p_oItem[1] + "</em>"));
-		     oMenuButton_1.set("value", p_oItem[0]);
-		     path_values.district_id = p_oItem[0];
-		     //set default-value for 'part_of_town'
-		     oMenuButton_2.set("value", (array_options[2][0][0]));
-		     oMenuButton_2.set("label", ("<em>" + array_options[2][0][1]+"</em>"));
-
-	     }
-	    if(p_oItem[2]=='part_of_town_id'){
-		     oMenuButton_2.set("label", ("<em>" + p_oItem[1] + "</em>"));
-		     oMenuButton_2.set("value", p_oItem[0]);
-		     path_values.part_of_town_id = p_oItem[0];
-	     }
-	    if(p_oItem[2]=='filter'){
-		     oMenuButton_3.set("label", ("<em>" + p_oItem[1] + "</em>"));
-		     oMenuButton_3.set("value", p_oItem[0]);
-		     path_values.filter = p_oItem[0];
-
-	     }
-
-	    //los valores de 'path_values' ya estan actualizados no es necesario verificar
-	    execute_ds();
-
-	   	//Update select PART OF TOWN
-	    oMenuButton_2.getMenu().clearContent();
-	    oMenuButton_2.getMenu().itemData = create_menu_list (values_ds.hidden.dependent[0].value,'part_of_town_id');;
-	    oMenuButton_2.set("value",values_ds.hidden.dependent[0].id);
-
-  }
 
  /********************************************************************************
  *
  */
   this.onSearchClick = function()
   {
-
         //no es necesario actualizar los valores actuales de path_value. Este es global y siempre esta actualizado
-        path_values.query = document.getElementById('txt_query').value;
+        //path_values.query = document.getElementById(imput).value;
+        path_values.query = YAHOO.util.Dom.get(imput).value;
 
 		execute_ds();
 
@@ -137,63 +73,110 @@ this.create_array_values_list = function(stValues)
  */
    this.onDownloadClick = function()
    {
+		var actuall_funct = path_values.menuaction;
 		//modify the "function download" in path_values
 		path_values.menuaction='property.uilocation.download';
 		ds_download = phpGWLink('index.php',path_values);
 		//return to "function index"
-		path_values.menuaction='property.uilocation.index';
+		path_values.menuaction=actuall_funct;
 		window.open(ds_download,'window');
    }
 
-   this.onNewClick = function()
+ /********************************************************************************
+ * create a array whith values strValues (..#../..#). Necesary for selected nested
+ */
+this.create_array_values_list = function(stValues)
+  {
+   var temp1,temp2,temp3 = new Array();
+
+   temp1 = stValues.split('/');
+   for(var n=0 ; n < temp1.length -1 ; n++ ) // -1 because la string has a '/' at last
    {
-		sUrl = java_edit;
-		window.open(sUrl,'_self');
+    temp2 = temp1[n].split('#');
+    temp3[n] = new Array();
+    for(var j=0 ; j < temp2.length ; j++ )
+    {
+     temp3[n][j]=temp2[j];
+    }
+   }
+   return temp3;
+}
+
+ /********************************************************************************
+ * p_oItem: values passed
+ * p_oItem[0]: id of opcion-select in database
+ * p_oItem[1]: texto of opcion-select in database
+ * p_oItem[2]:order option of the select
+ */
+   this.onMenuItemClick = function(p_sType, p_aArgs, p_oItem)
+   {
+		 var control = eval("oMenuButton_"+p_oItem[2]);
+	     control.set("label", ("<em>" + p_oItem[1] + "</em>"));
+	     control.set("value", p_oItem[0]);
+	     eval("path_values."+selectsButtons[p_oItem[2]].var_URL+"='"+p_oItem[0]+"'")
+
+		// tiene dependiente asociado?
+	     if(selectsButtons[p_oItem[2]].dependiente!='')
+	     {
+	    	control = eval("oMenuButton_"+selectsButtons[p_oItem[2]].dependiente);
+	     	control.set("label", ("<em>" + array_options[selectsButtons[p_oItem[2]].dependiente][0][1] + "</em>"));
+	     	control.set("value", array_options[selectsButtons[p_oItem[2]].dependiente][0][0]);
+	     	eval("path_values."+selectsButtons[selectsButtons[p_oItem[2]].dependiente].var_URL+"=''");  //empty
+	     	flag_update_filter = selectsButtons[p_oItem[2]].dependiente;
+	     }
+
+	    //los valores de 'path_values' ya estan actualizados no es necesario verificar
+	    execute_ds();
+
+  }
+
+
+
+ /********************************************************************************
+ * stValues:  values of select control, separate whit / and #
+ * order: indicate the id of Combo box
+ */
+  this.create_menu_list = function(stValues,order)
+	{
+	   var temp1, temp2, MenuButtonMenu = new Array();
+	   temp1 = stValues.split('/');
+	   for(var k=0 ; k < temp1.length -1 ; k++ ) // -1 because the string has a '/' at last
+	   {
+		    temp2 = temp1[k].split('#');
+		    temp2.push(order);
+		    var obj_temp = {id: '', text: temp2[1], value: temp2[0], onclick: { fn: onMenuItemClick , obj: temp2} };
+		    MenuButtonMenu.push(obj_temp);
+	   }
+       return MenuButtonMenu;
    }
 
  /********************************************************************************
  *
  */
+
+
+
+
   this.init_filter = function()
   {
-    //create button
-     oButtonSearch = new YAHOO.widget.Button("btn_search");
-     oButtonSearch.on("click", onSearchClick);
+	//create button
+	for(var p=0; p<normalButtons.length; p++)
+	{
+			var botton_tmp = new YAHOO.widget.Button(normalButtons[p].name);
+			botton_tmp.on("click", eval(normalButtons[p].funct));
+			eval("oNormalButton_"+p+" = botton_tmp");
+	}
 
-     oButtonExport = new YAHOO.widget.Button("btn_export");
-     oButtonExport.on("click", onDownloadClick);
-
-	var oButtonNew = new YAHOO.widget.Button("btn_new");
-	oButtonNew.on("click", onNewClick);
-
-	options_combo_box = values_combo_box[0];
-	//cat_id es el nombre del URL variable
-    optionsCB = create_menu_list(options_combo_box.value,'cat_id');
-    array_options[0] = create_array_values_list(options_combo_box.value);
-    //id es for css style
-    menuCB = { type: "menu", label:"<em>"+ array_options[0][0][1]+"</em>", id: "categorybutton", value:"", menu: optionsCB};
-    //name for la button HTML
-    oMenuButton_0 = new YAHOO.widget.Button("btn_cat_id", menuCB);
-
-
-    options_combo_box = values_combo_box[1];
-	var optionsCB = create_menu_list(options_combo_box.value,'district_id');
-    array_options[1] = create_array_values_list(options_combo_box.value);
-    var menuCB = { type: "menu", label:"<em>"+ array_options[1][0][1]+"</em>", id: "districtbutton", value:"", menu: optionsCB};
-    oMenuButton_1 = new YAHOO.widget.Button("btn_district_id", menuCB);
-
-	options_combo_box = values_combo_box[2];
-	var optionsCB = create_menu_list(options_combo_box.value,'part_of_town_id');
-    array_options[2] = create_array_values_list(options_combo_box.value);
-    var menuCB = { type: "menu", label:"<em>"+ array_options[2][0][1]+"</em>", id: "partOFTownbutton", value:"", menu: optionsCB};
-    oMenuButton_2 = new YAHOO.widget.Button("btn_part_of_town_id", menuCB);
-
-	options_combo_box = values_combo_box[3];
-	var optionsCB = create_menu_list(options_combo_box.value,'filter');
-    array_options[3] = create_array_values_list(options_combo_box.value);
-    var menuCB = { type: "menu", label:"<em>"+ array_options[3][0][1]+"</em>", id: "ownerIdbutton", value:"", menu: optionsCB};
-    oMenuButton_3 = new YAHOO.widget.Button("btn_owner_id", menuCB);
-
+	//create filters
+	for(var i=0; i<selectsButtons.length; i++)
+	{
+		options_combo_box = values_combo_box[selectsButtons[i].order];
+		 optionsCB = create_menu_list(options_combo_box.value,selectsButtons[i].order);
+	    array_options[selectsButtons[i].order] = create_array_values_list(options_combo_box.value);
+	    menuCB = { type: "menu", label:"<em>"+ array_options[selectsButtons[i].order][0][1]+"</em>", id: selectsButtons[i].style, value:"", menu: optionsCB};
+	    var tmp = new YAHOO.widget.Button(selectsButtons[i].name, menuCB)
+		eval("oMenuButton_"+selectsButtons[i].order+" = tmp");
+	}
 
  }
 
@@ -240,10 +223,10 @@ this.create_array_values_list = function(stValues)
 
     if(oTarget.tagName != "TD")
     {
-     oTarget = Dom.getAncestorByTagName(oTarget, "td");
+     oTarget = YAHOO.util.Dom.getAncestorByTagName(oTarget, "td");
     }
 
-    oSelectedTR = Dom.getAncestorByTagName(oTarget, "tr");
+    oSelectedTR = YAHOO.util.Dom.getAncestorByTagName(oTarget, "tr");
     oSelectedTR.style.backgroundColor  = 'blue' ;
              oSelectedTR.style.color = "white";
              YAHOO.util.Dom.addClass(oSelectedTR, prefixSelected);
@@ -257,7 +240,7 @@ this.create_array_values_list = function(stValues)
     if (this.getRoot() == this && oSelectedTR) {
      oSelectedTR.style.backgroundColor  = "" ;
           oSelectedTR.style.color = "";
-             Dom.removeClass(oSelectedTR, prefixSelected);
+             YAHOO.util.Dom.removeClass(oSelectedTR, prefixSelected);
       }
    }
  /********************************************************************************
@@ -292,11 +275,9 @@ this.create_array_values_list = function(stValues)
                               p_myDataTable.deleteRow(elRow);
                           }
                           break;
-                        case 3:     // New
+                        case 3:     // Filter
                             var oRecord = p_myDataTable.getRecord(elRow);
-                            sUrl = java_edit;
-                            window.open(sUrl,'_self');
-             				break;
+             break;
                     }
                 }
             }
@@ -319,7 +300,9 @@ this.create_array_values_list = function(stValues)
  */
 	this.buildQuery = function(strQuery)
 	{
-		path_values.query = document.getElementById('txt_query').value;
+		//path_values.query = document.getElementById(imput).value;
+		path_values.query = YAHOO.util.Dom.get(imput).value;
+
 		execute_ds();
 	}
 /******************************************************************************
@@ -358,9 +341,11 @@ this.create_array_values_list = function(stValues)
 						eval('values_ds ='+o.responseText);
 						if(flag==0){
 							init_datatable();
+							init_filter();
 						}
 						else{
 						 	update_datatable();
+						 	update_filter();
 						}
 		 			},
 		  			failure: function(o) {window.alert('Server or your connection is death.')},
@@ -377,6 +362,11 @@ this.create_array_values_list = function(stValues)
  */
 
 
+
+
+/********************************************************************************
+ *
+ */
 	this.init_datatable = function()
 	{
 
@@ -429,7 +419,7 @@ this.create_array_values_list = function(stValues)
 			generateRequest        : buildQueryString,
 			paginationEventHandler : YAHOO.widget.DataTable.handleDataSourcePagination,
 			paginator              : myPaginator,
-			sortedBy			   : {key:"loc1", dir:YAHOO.widget.DataTable.CLASS_ASC}, // arguments necesary for paginator
+			sortedBy			   : {key:"anywhere", dir:YAHOO.widget.DataTable.CLASS_ASC}, // arguments necesary for paginator
 
 		};
 
@@ -439,37 +429,37 @@ this.create_array_values_list = function(stValues)
 		// Override function for custom server-side sorting
 		myDataTable.sortColumn = function(oColumn) {
 
-				var sDir = "asc"
-				if(oColumn.key === this.get("sortedBy").key) {
-					sDir = (this.get("sortedBy").dir === YAHOO.widget.DataTable.CLASS_ASC) ?
-							"desc" : "asc";
-				}
-				//URL-vars adicionales que se agregaran al actual ds
-				var addToRequest = "&start=0&order="+oColumn.source+"&sort="+sDir;
-				//sea actualiza el liveDAta del Datasource con los actuales valores de los combos y txtboxs
-				myDataTable.getDataSource().liveData=ds;
+					var sDir = "asc"
+					if(oColumn.key === this.get("sortedBy").key) {
+						sDir = (this.get("sortedBy").dir === YAHOO.widget.DataTable.CLASS_ASC) ?
+								"desc" : "asc";
+					}
+					//URL-vars adicionales que se agregaran al actual ds
+					var addToRequest = "&start=0&order="+oColumn.source+"&sort="+sDir;
+					//sea actualiza el liveDAta del Datasource con los actuales valores de los combos y txtboxs
+					myDataTable.getDataSource().liveData=ds;
 
-				// Create callback for data request
-				var oCallback3 = {
-					success: this.onDataReturnInitializeTable,
-					failure: this.onDataReturnInitializeTable,
-					scope: this,
-					argument: {
-						sorting: {
-							key: oColumn.key,
-							dir: (sDir === "asc") ? YAHOO.widget.DataTable.CLASS_ASC : YAHOO.widget.DataTable.CLASS_DESC
+					// Create callback for data request
+					var oCallback3 = {
+						success: this.onDataReturnInitializeTable,
+						failure: this.onDataReturnInitializeTable,
+						scope: this,
+						argument: {
+							sorting: {
+								key: oColumn.key,
+								dir: (sDir === "asc") ? YAHOO.widget.DataTable.CLASS_ASC : YAHOO.widget.DataTable.CLASS_DESC
+								}
 							}
 						}
-					}
-				try{
-					myDataTable.getDataSource().sendRequest(addToRequest, oCallback3)}
-				catch(e){
-					alert(e);
-				}
+					 try{
+						myDataTable.getDataSource().sendRequest(addToRequest, oCallback3)}
+					 catch(e){
+						alert(e);
+					 }
 
-			myPaginator.setPage(1,true);
+				      myPaginator.setPage(1,true);
 
-		};
+			        };
 
 
 	   myContextMenu = new YAHOO.widget.ContextMenu("mycontextmenu", {trigger:myDataTable.getTbodyEl()});
@@ -518,34 +508,51 @@ this.create_array_values_list = function(stValues)
 
 this.update_datatable = function()
 	{
-				     //delete values of datatable
-				     var length = myDataTable.getRecordSet().getLength();
-				     if(length)
-				     {
-				     	myDataTable.deleteRows(0,length);
-				     }
+     //delete values of datatable
+     var length = myDataTable.getRecordSet().getLength();
+     if(length)
+     {
+     	myDataTable.deleteRows(0,length);
+     }
 
-				     //obtain records of the last DS and add to datatable
-				     var record = values_ds.records;
-				     var newTotalRecords = values_ds.totalRecords;
+     //obtain records of the last DS and add to datatable
+     var record = values_ds.records;
+     var newTotalRecords = values_ds.totalRecords;
 
-				     myPaginator.setPage(1,true);
-				     if(record.length)
-				     {
-				     	myDataTable.addRows(record);
-				     }
+     myPaginator.setPage(1,true);
+     if(record.length)
+     {
+     	myDataTable.addRows(record);
+     }
 
-				     //update paginator with news values
-				     myPaginator.setTotalRecords(newTotalRecords,true);
-				     myPaginator.updateOnChange=true;
+     //update paginator with news values
+     myPaginator.setTotalRecords(newTotalRecords,true);
+     myPaginator.updateOnChange=true;
 	}
+
+/****************************************************************************************
+*
+*/
+
+this.update_filter = function()
+{
+ if (flag_update_filter !='')
+ {
+ 		 var filter_tmp = eval("oMenuButton_"+flag_update_filter);
+
+ 		filter_tmp.getMenu().clearContent();
+	    filter_tmp.getMenu().itemData = create_menu_list (values_ds.hidden.dependent[0].value,selectsButtons[flag_update_filter].order);
+	    filter_tmp.set("value",values_ds.hidden.dependent[0].id);
+	    flag_update_filter = '';
+ }
+}
 
 //----------------------------------------------------------------------------------------
 
+  this.initial_focus(imput);
   eval("var path_values = {"+base_java_url+"}");
-  YAHOO.widget.DataTable.Formatter.myCustom = this.myCustomFormatter;
   this.execute_ds();
-  init_filter();
+
 
  });
 
