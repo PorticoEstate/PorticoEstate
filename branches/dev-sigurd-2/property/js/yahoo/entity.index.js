accdac0f8b22d396ef1ee6f5b948545217697ea9
alejandro <alejandro@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2008-10-15 23:37:57 +0000

    creating entity.index.js - js file for uientity  - see #369

---
+++ branches/dev-sigurd-2/property/js/yahoo/entity.index.js
@@ -0,0 +1,53 @@
+//--------------------------------------------------------
+// Declaration of location.index vars
+//--------------------------------------------------------
+	//define SelectButton
+ 	var oMenuButton_0, oMenuButton_1, oMenuButton_2, oMenuButton_3;
+ 	var selectsButtons = [
+	{order:0, var_URL:'cat_id',name:'btn_cat_id',style:'categorybutton',dependiente:''},
+	{order:1, var_URL:'district_id',name:'btn_district_id',style:'districtbutton',dependiente:''},
+	{order:2, var_URL:'status',name:'btn_status_id',style:'partOFTownbutton',dependiente:''},
+	{order:3, var_URL:'filter', name:'btn_user_id',style:'ownerIdbutton',dependiente:''}
+	]
+
+	// define buttons
+	var oNormalButton_0, oNormalButton_1, oNormalButton_2;
+	var normalButtons = [
+	{order:0, name:'btn_search', funct:"onSearchClick"},
+	{order:1, name:'btn_new', funct:"onNewClick"},
+	{order:2, name:'btn_export', funct:"onDownloadClick"}
+	]
+
+	// define Text buttons
+	var textImput = [
+	{order:0, name:'txt_query'}
+	]
+
+	// define the hidden column in datatable
+	var config_values = {
+	column_hidden : 1
+	}
+
+
+//----------------------------------------------------------
+	YAHOO.util.Event.addListener(window, "load", function()
+	{
+		var loader = new YAHOO.util.YUILoader();
+		loader.addModule({
+			name: "anyone", //module name; must be unique
+			type: "js", //can be "js" or "css"
+		    fullpath: property_js //'property_js' have the path for property.js, is render in HTML
+		    });
+
+		loader.require("anyone");
+
+		//Insert JSON utility on the page
+
+	    loader.insert();
+	});
+
+
+
+
+
+
