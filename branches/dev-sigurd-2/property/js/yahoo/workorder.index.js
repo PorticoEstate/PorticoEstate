4c0b69adac264b4a2f2ea51397451e052e497f14
alejandro <alejandro@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2008-10-24 22:46:16 +0000

    creating workorder.index.js - js file to initialize workorder module  - see #369

---
+++ branches/dev-sigurd-2/property/js/yahoo/workorder.index.js
@@ -0,0 +1,63 @@
+//--------------------------------------------------------
+// Declaration of workorder.index vars
+//--------------------------------------------------------
+	//define SelectButton
+ 	var oMenuButton_0, oMenuButton_1, oMenuButton_2, oMenuButton_3;
+ 	var selectsButtons = [
+	{order:0, var_URL:'cat_id',name:'btn_cat_id',style:'categorybutton',dependiente:''},
+	{order:1, var_URL:'status_id',name:'btn_status_id',style:'districtbutton',dependiente:''},
+	{order:2, var_URL:'wo_hour_cat_id',name:'btn_wo_hour_cat_id',style:'partOFTownbutton',dependiente:''},
+	{order:3, var_URL:'filter', name:'btn_user_id',style:'ownerIdbutton',dependiente:''}
+	]
+
+	// define buttons
+	var oNormalButton_0, oNormalButton_1, oNormalButton_2;
+	var normalButtons = [
+	{order:0, name:'btn_search', funct:"onSearchClick"},
+	{order:1, name:'btn_new', funct:"onNewClick"},
+	{order:2, name:'btn_export', funct:"onDownloadClick"}
+	]
+
+	 // define Text buttons
+	 var textImput = [
+	 {var_URL:'query', name:'txt_query'},
+	 {var_URL:'search_vendor', name:'txt_search_vendor'}
+	 ]
+
+	// define the hidden column in datatable
+	var config_values = {
+	 column_hidden : [7,15,16,17], //orden de la columna a ocultar en el datatable
+	 date_search : 1 //if search has link "Data search"
+	 };
+
+	this.init_particular_setting = function()
+	{
+
+		// seteo del focus
+		YAHOO.util.Dom.get(textImput[0].name).value = path_values.query;
+		YAHOO.util.Dom.get(textImput[0].name).focus();
+
+	}
+	
+//----------------------------------------------------------
+	YAHOO.util.Event.addListener(window, "load", function()
+	{
+		var loader = new YAHOO.util.YUILoader();
+		loader.addModule({
+			name: "anyone", //module name; must be unique
+			type: "js", //can be "js" or "css"
+		    fullpath: property_js //'property_js' have the path for property.js, is render in HTML
+		    });
+
+		loader.require("anyone");
+
+		//Insert JSON utility on the page
+
+	    loader.insert();
+	});
+
+
+
+
+
+
