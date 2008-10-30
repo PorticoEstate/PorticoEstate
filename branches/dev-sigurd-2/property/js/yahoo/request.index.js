336b9663d55af5ffb77d73046a52885756fb6023
nelson <nelson@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2008-10-30 16:50:41 +0000

    creating request.index.js - js file to initialize request module

---
+++ branches/dev-sigurd-2/property/js/yahoo/request.index.js
@@ -0,0 +1,60 @@
+//--------------------------------------------------------
+// Declaration of request.index vars
+//--------------------------------------------------------
+	//define SelectButton
+ 	var oMenuButton_0, oMenuButton_1, oMenuButton_2, oMenuButton_3;
+ 	var selectsButtons = [
+	{order:0, var_URL:'cat_id',name:'btn_cat_id',style:'categorybutton',dependiente:''},
+	{order:1, var_URL:'status_id',name:'btn_status_id',style:'districtbutton',dependiente:''},
+	{order:2, var_URL:'filter', name:'btn_user_id',style:'ownerIdbutton',dependiente:''}
+	]
+
+	// define buttons
+	var oNormalButton_0, oNormalButton_1, oNormalButton_2;
+	var normalButtons = [
+	{order:0, name:'btn_search', funct:"onSearchClick"},
+	{order:1, name:'btn_new', funct:"onNewClick"},
+	{order:2, name:'btn_export', funct:"onDownloadClick"}
+	]
+
+	 // define Text buttons
+	 var textImput = [
+	 {var_URL:'query', name:'txt_query'}
+	 ]
+
+	// define the hidden column in datatable
+	var config_values = {
+	 column_hidden : [11,12,13]
+	 };
+
+	this.init_particular_setting = function()
+	{
+
+		// seteo del focus
+		YAHOO.util.Dom.get(textImput[0].name).value = path_values.query;
+		YAHOO.util.Dom.get(textImput[0].name).focus();
+
+	}
+	
+//----------------------------------------------------------
+	YAHOO.util.Event.addListener(window, "load", function()
+	{
+		var loader = new YAHOO.util.YUILoader();
+		loader.addModule({
+			name: "anyone", //module name; must be unique
+			type: "js", //can be "js" or "css"
+		    fullpath: property_js //'property_js' have the path for property.js, is render in HTML
+		    });
+
+		loader.require("anyone");
+
+		//Insert JSON utility on the page
+
+	    loader.insert();
+	});
+
+
+
+
+
+
