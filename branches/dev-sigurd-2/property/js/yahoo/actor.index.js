e74f1452110dcf4b9def18518d20ac62e8b4bced
nelson <nelson@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2008-11-06 23:02:25 +0000

    modifying actor.index.js - new definition of TextBoxs - see #369

--- branches/dev-sigurd-2/property/js/yahoo/actor.index.js
+++ branches/dev-sigurd-2/property/js/yahoo/actor.index.js
@@ -16,10 +16,10 @@
 	{order:1, name:'btn_new', funct:"onNewClick"}
 	]
 
-	 // define Text buttons
-	 var textImput = [
-	 {var_URL:'query', name:'txt_query'}
-	 ]
+	// define Text buttons
+	var textImput = [
+	{order:0, name:'query',	id:'txt_query'}
+	]
 
 	// define the hidden column in datatable
 	var config_values = {
@@ -28,11 +28,10 @@
 
 	this.init_particular_setting = function()
 	{
-
-		// seteo del focus
-		YAHOO.util.Dom.get(textImput[0].name).value = path_values.query;
-		YAHOO.util.Dom.get(textImput[0].name).focus();
-
+		// necesary when do "enter" in search botton.
+		YAHOO.util.Dom.get(textImput[0].id).value = path_values.query;
+		// focus
+		YAHOO.util.Dom.get(textImput[0].id).focus();
 	}
 	
 //----------------------------------------------------------
