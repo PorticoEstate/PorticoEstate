211949629f5d8126336f4d28b82ce40cbc26adf3
alejandro <alejandro@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2008-11-17 23:34:48 +0000

    creating tts.index.js - Modifying particular javascript file
    - see #369

--- branches/dev-sigurd-2/property/js/yahoo/tts.index.js
+++ branches/dev-sigurd-2/property/js/yahoo/tts.index.js
@@ -5,9 +5,9 @@
  	var oMenuButton_0, oMenuButton_1, oMenuButton_2, oMenuButton_3;
  	var selectsButtons = [
 	{order:0, var_URL:'cat_id',name:'btn_cat_id',style:'categorybutton',dependiente:''},
-	{order:1, var_URL:'status_id',name:'btn_status_id',style:'districtbutton',dependiente:''},
-	{order:2, var_URL:'wo_hour_cat_id',name:'btn_hour_category_id',style:'partOFTownbutton',dependiente:''},
-	{order:3, var_URL:'filter', name:'btn_user_id',style:'ownerIdbutton',dependiente:''}
+	{order:1, var_URL:'district_id',name:'btn_district_id',style:'districtbutton',dependiente:''},
+	{order:2, var_URL:'status_id',name:'btn_status_id',style:'partOFTownbutton',dependiente:''},
+	{order:3, var_URL:'user_id', name:'btn_user_id',style:'ownerIdbutton',dependiente:''}
 	];
 
 	// define buttons
@@ -19,31 +19,29 @@
 	];
 
 	// define Text buttons
-	var textImput = [
-	{order:0, name:'txt_query'}
-	];
+		var textImput = [
+		{order:0, name:'query',	id:'txt_query'}
+		]
+
 
 	// define the hidden column in datatable
 	var config_values = {
-	 column_hidden : 1, //orden de la columna a ocultar en el datatable
+	 column_hidden : [4], //orden de la columna a ocultar en el datatable
 	 date_search : 1 //if search has link "Data search"
 	 };
 
 	this.init_particular_setting = function()
 	{
-
-		// seteo del focus
-		YAHOO.util.Dom.get(textImput[0].name).value = path_values.query;
-		YAHOO.util.Dom.get(textImput[0].name).focus();
-
+		// necesary when do "enter" in search botton.
+		YAHOO.util.Dom.get(textImput[0].id).value = path_values.query;
+		//YAHOO.util.Dom.get(textImput[0].id).focus();
 	}
 
 
 
 
 //----------------------------------------------------------
-	//YAHOO.util.Event.addListener(window, "load", function()
-	YAHOO.util.Event.onDOMReady(function ()
+	YAHOO.util.Event.addListener(window, "load", function()
 	{
 		var loader = new YAHOO.util.YUILoader();
 		loader.addModule({
@@ -58,36 +56,7 @@
 
 	    loader.insert();
 
-	    //YAHOO.util.Event.onDOMReady(prueba);
-
 	});
-
-
-
-/*this.prueba = function()
-	{
-			var myTableConfig = {
-			initialRequest         : '',//la primera vez ya viene ordenado, por la columna respectiva y solo 15 registros
-			generateRequest        : buildQueryString,
-			paginationEventHandler : YAHOO.widget.DataTable.handleDataSourcePagination,
-			paginator              : myPaginator,
-			sortedBy			   : {key:"anywhere", dir:YAHOO.widget.DataTable.CLASS_ASC}, // arguments necesary for paginator
-			formatRow			   : myRowFormatter
-
-		};
-
-		var myRowFormatter = function(elTr, oRecord)
-		{
-    		if (oRecord.getData('Quantity') < 40)
-    		{
-        		Dom.addClass(elTr, 'mark');
-    		}
-    		Dom.addClass(elTr, 'mark');
-    		return true;
-		};
-
-	}*/
-
 
 
 
