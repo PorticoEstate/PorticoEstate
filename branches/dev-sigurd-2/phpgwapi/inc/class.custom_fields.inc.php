a1b4bfd65b36c8442b0c9b7b44d4afae84872284
sigurd <sigurd@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2008-12-01 08:40:12 +0000

    Fix for adding contacts as custom field: see #401

--- branches/dev-sigurd-2/phpgwapi/inc/class.custom_fields.inc.php
+++ branches/dev-sigurd-2/phpgwapi/inc/class.custom_fields.inc.php
@@ -333,7 +333,7 @@
 
 			$receipt['id'] = $values['id'];
 
-			if ( !$values['precision_'] )
+			if ( !$values['precision_'] > 0)
 			{
 				$precision = $this->_translate_datatype_precision($values['datatype']);
 				if ( $precision )
@@ -1467,6 +1467,6 @@
 			{
 				return 0;
 			}
-			$ret = $datatype_precision[$datatype];
+			return $datatype_precision[$datatype];
 		}
 	}
