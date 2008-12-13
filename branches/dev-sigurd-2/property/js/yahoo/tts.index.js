f713fd5174e8e1b879892eb41ca9f251f153e3b5
alejandro <alejandro@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2008-12-13 00:06:24 +0000

    modifying tts.index.js - Deleting some useless code
    - see #369

--- branches/dev-sigurd-2/property/js/yahoo/tts.index.js
+++ branches/dev-sigurd-2/property/js/yahoo/tts.index.js
@@ -25,8 +25,8 @@
 
 	var toolTips =
 	[
-		{name:'status', title:'', description:''},
-		{name:'btn_export', title:'download', description:'Download table to your browser'}
+		{name:'status', title:'', description:'',ColumnDescription:'status'},
+		{name:'btn_export', title:'download', description:'Download table to your browser',ColumnDescription:''}
 	]
 
 	var deactivateT =
@@ -36,32 +36,38 @@
 
 
 	// define the hidden column in datatable
-	var config_values = {
-	 column_hidden : [], //orden de la columna a ocultar en el datatable
-	 date_search : 1 //if search has link "Data search"
-	 };
+	var config_values =
+	{
+		date_search : 0, //if search has link "Data search"
+		footer_datatable : 0
+	}
 
 	this.particular_setting = function()
 	{
 		if(flag_particular_setting=='init')
 		{
-			//nothing
+			//locate (asign ID) to datatable
+			tableYUI = YAHOO.util.Dom.getElementsByClassName("yui-dt-data","tbody")[0].parentNode;
+			tableYUI.setAttribute("id","tableYUI");
+			//focus initial
+			oMenuButton_0.focus();
 		}
 		else if(flag_particular_setting=='update')
 		{
-			//nothing
+			//reset empty values for update PERIOD
+		   	path_values.voucher_id_for_period = '';
+		   	path_values.period = '';
+		   	path_values.currentPage = '';
+		   	path_values.start = '';
 		}
-
-		//--focus for txt_query---
-		YAHOO.util.Dom.get(textImput[0].id).value = path_values.query;
-		YAHOO.util.Dom.get(textImput[0].id).focus();
 	}
 
 
 
 
 //----------------------------------------------------------
-	YAHOO.util.Event.addListener(window, "load", function()
+	//YAHOO.util.Event.addListener(window, "load", function()
+	YAHOO.util.Event.onDOMReady(function()
 	{
 		YAHOO.util.Dom.getElementsByClassName('toolbar','div')[0].style.display = 'none';
 		var loader = new YAHOO.util.YUILoader();
