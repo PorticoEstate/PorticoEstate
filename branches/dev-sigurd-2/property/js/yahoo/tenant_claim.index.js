dd2f63855d59af3c9b34ece995be4ae8bb0a516e
alejandro <alejandro@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2008-12-19 16:04:35 +0000

    modifying tenant_claim.index.js - Creating particular JS file
    - see #369

---
+++ branches/dev-sigurd-2/property/js/yahoo/tenant_claim.index.js
@@ -0,0 +1,72 @@
+//--------------------------------------------------------
+// Declaration of location.index vars
+//--------------------------------------------------------
+	//define SelectButton
+ 	var oMenuButton_0, oMenuButton_1;
+ 	var selectsButtons = [
+	{order:0, var_URL:'cat_id',name:'btn_cat_id',style:'categorybutton',dependiente:''},
+	{order:1, var_URL:'status',name:'btn_status_id',style:'districtbutton',dependiente:''}
+	];
+
+	// define buttons
+	var oNormalButton_0;
+	var normalButtons = [
+	{order:0, name:'btn_search', funct:"onSearchClick"},
+	{order:1, name:'btn_new', funct:"onNewClick"}
+	];
+
+	// define Text buttons
+	var textImput = [
+		{order:0, name:'query',id:'txt_query'}
+	];
+
+var toolTips =
+	[
+		{name:'btn_export', title:'download', description:'Download table to your browser',ColumnDescription:''}
+	]
+
+
+	// define the hidden column in datatable
+	var config_values =
+	{
+		date_search : 0, //if search has link "Data search"
+		footer_datatable : 0
+	}
+
+	this.particular_setting = function()
+	{
+		if(flag_particular_setting=='init')
+		{
+			oMenuButton_0.focus();
+		}
+		else if(flag_particular_setting=='update')
+		{
+		}
+	}
+
+
+
+//----------------------------------------------------------
+	YAHOO.util.Event.addListener(window, "load", function()
+	{
+		YAHOO.util.Dom.getElementsByClassName('toolbar','div')[0].style.display = 'none';
+
+		var loader = new YAHOO.util.YUILoader();
+		loader.addModule({
+			name: "anyone", //module name; must be unique
+			type: "js", //can be "js" or "css"
+		    fullpath: property_js //'property_js' have the path for property.js, is render in HTML
+		    });
+
+		loader.require("anyone");
+
+		//Insert JSON utility on the page
+
+	    loader.insert();
+	});
+
+
+
+
+
+
