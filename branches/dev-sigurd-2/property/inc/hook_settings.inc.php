4c2047e2b729aadaec2140a56c97108ea0ea14cc
sigurd <sigurd@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2009-01-13 20:53:12 +0000

    Remove navbar as user preference - see #369

--- branches/dev-sigurd-2/property/inc/hook_settings.inc.php
+++ branches/dev-sigurd-2/property/inc/hook_settings.inc.php
@@ -208,4 +208,5 @@
 	create_input_box('Height of textarea','textarearows','Height of textarea in forms');
 
 	create_select_box('show horisontal menues','horisontal_menus',array('no' => 'No','yes' => 'Yes'),'Horisontal menues are shown in top of page');
+	create_select_box('remove navbar','nonavbar',array('no' => 'No','yes' => 'Yes'),'Navigation bar is removed');
 	create_select_box('Tabel export format','export_format',array('excel' => 'Excel','csv' => 'CSV', 'ods' => 'ODS'),'Choose which format to export from the system for tables');
