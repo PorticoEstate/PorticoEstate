686fe898901c55bef4e4fc91d74876390fc5276c
alejandro <alejandro@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2009-02-06 22:05:06 +0000

    creating project.edit.js - creating particular js file for edit project
    - see #369

--- branches/dev-sigurd-2/property/js/yahoo/project.edit.js
+++ branches/dev-sigurd-2/property/js/yahoo/project.edit.js
@@ -1,45 +1,28 @@
-var myDataSource,myDataTable, myContextMenu, myPaginator ;
+var myDataSource,myDataTable, myContextMenu;
 var tableYUI;
+var  myPaginator_0,myPaginator_1,myPaginator_2;
+var  myDataTable_0,myDataTable_1,myDataTable_2;
+
+YAHOO.widget.DataTable.formatLink = function(elCell, oRecord, oColumn, oData)
+{
+  	elCell.innerHTML = "<a href="+datatable[0][0]["edit_action"]+"&id="+oData+">" + oData + "</a>";
+};
 
 
-YAHOO.util.Event.addListener(window, "load", function() {
 
-this.getSumPerPage = function(name_column,round)
+this.myParticularRenderEvent = function(paginator,datatable)
 	{
-		//range actual of rows in datatable
-		begin = end = 0;
-		if( (myPaginator.getPageRecords()[1] - myPaginator.getPageRecords()[0] + 1 ) == myDataTable.getRecordSet().getLength() )
-		//click en Period or ComboBox. (RecordSet start in 0)
-		{
-			begin	= 0;
-			end		= myPaginator.getPageRecords()[1] - myPaginator.getPageRecords()[0];
-
-		}
-		else
-		//click en Paginator
-		{
-			begin	= myPaginator.getPageRecords()[0];
-			end		= myPaginator.getPageRecords()[1];
-		}
-
-		//get sumatory of column AMOUNT
-		tmp_sum = 0;
-		for(i = begin; i <= end; i++)
-		{
-			tmp_sum = tmp_sum + parseFloat(myDataTable.getRecordSet().getRecords(0)[i].getData(name_column));
-		}
-
-		return tmp_sum = YAHOO.util.Number.format(tmp_sum, {decimalPlaces:round, decimalSeparator:",", thousandsSeparator:" "});
+		this.addFooterDatatable(paginator,datatable);
 	}
 
 /********************************************************************************
 	 *
 	 */
-  	this.addFooterDatatable = function()
+  	this.addFooterDatatable = function(paginator,datatable)
   	{
   		//call getSumPerPage(name of column) in property.js
-  		tmp_sum1 = getSumPerPage('budget',2);
-  		tmp_sum2 = getSumPerPage('calculation',2);
+  		tmp_sum1 = getSumPerPage('budget',2,paginator,datatable);
+  		tmp_sum2 = getSumPerPage('calculation',2,paginator,datatable);
 
   		if(typeof(tableYUI)=='undefined')
   		{
@@ -54,33 +37,10 @@ this.getSumPerPage = function(name_column,round)
 		//Create ROW
 		newTR = document.createElement('tr');
 
-		newTD = document.createElement('td');
-		newTD.style.borderTop="1px solid #000000";
-		newTD.appendChild(document.createTextNode('Sum'));
-		newTR.appendChild(newTD);
-
-		newTD = document.createElement('td');
-		newTD.style.borderTop="1px solid #000000";
-		newTD.appendChild(document.createTextNode(tmp_sum1));
-		newTD.setAttribute("style","text-align:right;border-top:1px solid black;");
-		newTR.appendChild(newTD);
-
-		newTD = document.createElement('td');
-		newTD.style.borderTop="1px solid #000000";
-		newTD.appendChild(document.createTextNode(tmp_sum2));
-		newTD.setAttribute("style","text-align:right;border-top:1px solid black;");
-		newTR.appendChild(newTD);
-
-		newTD = document.createElement('td');
-		newTD.style.borderTop="1px solid #000000";
-		newTD.appendChild(document.createTextNode(''));
-		newTR.appendChild(newTD);
-
-		newTD = document.createElement('td');
-		newTD.style.borderTop="1px solid #000000";
-		newTD.colSpan = 2;
-		newTD.appendChild(document.createTextNode(''));
-		newTR.appendChild(newTD);
+		td_sum('Sum');
+		td_sum(tmp_sum1);
+		td_sum(tmp_sum2);
+		td_empty(3);
 
 		myfoot = tableYUI.createTFoot();
 		myfoot.setAttribute("id","myfoot");
@@ -88,77 +48,16 @@ this.getSumPerPage = function(name_column,round)
 	}
 
 
- /********************************************************************************
- *
- */
-	this.init_datatable = function()
-	{
-		data = {workorder: [data_values]};
-		var myDataSource = new YAHOO.util.DataSource(data.workorder[0]);
-
-		YAHOO.widget.DataTable.formatLink = function(elCell, oRecord, oColumn, oData) {
-                var id = oData;
-                elCell.innerHTML = "<a href="+edit_action+"&id="+id+">" + id + "</a>";
-            };
-
-
-		var myColumnDefs = [
-            {key:"workorder_id", label:"Workorder", sortable:true, resizeable:true, formatter:YAHOO.widget.DataTable.formatLink},
-            {key:"budget", label:"Budget", sortable:true, resizeable:true},
-            {key:"calculation", label:"Calculation", sortable:true, resizeable:true},
-            {key:"charge_tenant", sortable:true, resizeable:true, hidden:true},
-            {key:"vendor_name", label:"Vendor", sortable:true, resizeable:true},
-            {key:"status", label:"Status", sortable:true, resizeable:true}
-        ];
-
-
-        myDataSource.responseType = YAHOO.util.DataSource.TYPE_JSARRAY;
-        myDataSource.responseSchema = {
-            fields: [
-            			{key:"workorder_id"},
-            			{key:"budget"},
-            			{key:"calculation"},
-            			{key:"charge_tenant"},
-            			{key:"status"},
-            			{key:"vendor_name"}
-            		]
-        };
-
-		var container = YAHOO.util.Dom.getElementsByClassName( 'datatable-container' , 'div' );
-
-		myPaginatorConfig = {
-								containers			: ['paging'],
-								totalRecords		: total_records,
-								pageLinks			: 10,
-								rowsPerPage			: 1, //MAXIMO el PHPGW me devuelve 15 valor configurado por preferencias
-								pageReportTemplate	: "Showing items {startRecord} - {endRecord} of {totalRecords}"
-							}
-		myPaginator = new YAHOO.widget.Paginator(myPaginatorConfig);
-
-		var myTableConfig = {
-							paginator			: myPaginator
-		};
-
-		myDataTable = new YAHOO.widget.DataTable(container[0], myColumnDefs, myDataSource, myTableConfig);
-
-		myDataTable.subscribe("renderEvent", addFooterDatatable);
-
-		return {
-			ds: myDataSource,
-			dt: myDataTable
-			};
-	}
-
-
-		/********************************************************************************
- *
- */
-	this.init_datatable2 = function()
-	{
-		data = {history: [record_history]};
-		var myDataSource = new YAHOO.util.DataSource(data.history[0]);
-
-		var myColumnDefs = [
+var myColumnDefs = new Array();
+ 		 myColumnDefs[0] = [
+           {key:"workorder_id", label:"Workorder", sortable:true, resizeable:true, formatter:YAHOO.widget.DataTable.formatLink},
+           {key:"budget", label:"Budget", sortable:true, resizeable:true},
+           {key:"calculation", label:"Calculation", sortable:true, resizeable:true},
+           {key:"charge_tenant", sortable:true, resizeable:true, hidden:true},
+           {key:"vendor_name", label:"Vendor", sortable:true, resizeable:true},
+           {key:"status", label:"Status", sortable:true, resizeable:true}
+       ];
+        myColumnDefs[1] = [
             {key:"value_date", label:"Date", sortable:true, resizeable:true},
             {key:"value_user", label:"User", sortable:true, resizeable:true},
             {key:"value_action", label:"Action", sortable:true, resizeable:true},
@@ -166,27 +65,18 @@ this.getSumPerPage = function(name_column,round)
         ];
 
 
-        myDataSource.responseType = YAHOO.util.DataSource.TYPE_JSARRAY;
-        myDataSource.responseSchema = {
-            fields: [
-            			{key:"value_date"},
-            			{key:"value_user"},
-            			{key:"value_action"},
-            			{key:"value_new_value"}
-            		]
-        };
 
-		var container = YAHOO.util.Dom.getElementsByClassName( 'datatable-container2' , 'div' );
+YAHOO.util.Event.addListener(window, "load", function()
+{
+	var loader = new YAHOO.util.YUILoader();
+	loader.addModule({
+		name: "anyone",
+		type: "js",
+	    fullpath: property_js
+	    });
 
-		myDataTable2 = new YAHOO.widget.DataTable(container[0], myColumnDefs, myDataSource);
-
-		return {
-			ds: myDataSource,
-			dt: myDataTable2
-			};
-	}
-
-    this.init_datatable();
-	this.init_datatable2();
-
+	loader.require("anyone");
+    loader.insert();
 });
+
+
