06d26fd9f466ba7ef1a5cce798e6b8a4c3a0bbf3
cesar <cesar@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2009-02-10 00:05:32 +0000

    commit improve property2.js - add validations - delete unnecessary variables - see #369

--- branches/dev-sigurd-2/property/js/yahoo/property2.js
+++ branches/dev-sigurd-2/property/js/yahoo/property2.js
@@ -1,11 +1,14 @@
-this.getSumPerPage = function(name_column,round,paginator,datatable)
+	var myDataSource,myDataTable, myContextMenu,tableYUI;
+
+/********************************************************************************/
+
+	this.getSumPerPage = function(name_column,round,paginator,datatable)
 	{
 		begin = end = 0;
 		if( (paginator.getPageRecords()[1] - paginator.getPageRecords()[0] + 1 ) == datatable.getRecordSet().getLength() )
 		{
 			begin	= 0;
 			end		= paginator.getPageRecords()[1] - paginator.getPageRecords()[0];
-
 		}
 		else
 		{
@@ -22,9 +25,8 @@ this.getSumPerPage = function(name_column,round,paginator,datatable)
 		return tmp_sum = YAHOO.util.Number.format(tmp_sum, {decimalPlaces:round, decimalSeparator:",", thousandsSeparator:" "});
 	}
 
-/********************************************************************************
- *
- */
+/********************************************************************************/
+
   	this.td_empty = function(colspan)
   	{
 		newTD = document.createElement('td');
@@ -33,9 +35,8 @@ this.getSumPerPage = function(name_column,round,paginator,datatable)
 		newTD.appendChild(document.createTextNode(''));
 		newTR.appendChild(newTD);
   	}
- /********************************************************************************
- *
- */
+ /********************************************************************************/
+
   	this.td_sum = function(sum)
   	{
 		newTD = document.createElement('td');
@@ -47,24 +48,14 @@ this.getSumPerPage = function(name_column,round,paginator,datatable)
 		newTD.appendChild(document.createTextNode(sum));
 		newTR.appendChild(newTD);
   	}
-
-
-this.myRenderEvent = function()
-	{
-		myParticularRenderEvent();
-	}
-
-
- /********************************************************************************
- *
- */
+ /********************************************************************************/
  	this.init_datatable = function(data,container,pager,myColumnDefs,num)
 	{
 		myDataSource = new YAHOO.util.DataSource(data[0]["values"]);
         myDataSource.responseType = YAHOO.util.DataSource.TYPE_JSARRAY;
 
-        var fields = new Array();
-		for(var i=0; i < myColumnDefs.length;i++)
+        fields = new Array();
+		for(i=0; i < myColumnDefs.length;i++)
 		{
 			fields[i] = myColumnDefs[i].key;
 		}
@@ -81,12 +72,12 @@ this.myRenderEvent = function()
 									containers			: pager,
 									totalRecords		: data[0]["total_records"],
 									pageLinks			: 10,
-									rowsPerPage			: 1
+									rowsPerPage			: 3
 								}
 
 			eval("myPaginator_"+num+" = new YAHOO.widget.Paginator(myPaginatorConfig)");
 
-			var myTableConfig = { paginator	: eval("myPaginator_"+num)};
+			myTableConfig = { paginator	: eval("myPaginator_"+num)};
 			eval("myDataTable_" + num + " = new YAHOO.widget.DataTable(container, myColumnDefs, myDataSource, myTableConfig)");
 		}
 		else
@@ -95,20 +86,20 @@ this.myRenderEvent = function()
 		}
 
 		eval("myDataTable_" + num).subscribe("renderEvent", function(){
-			myParticularRenderEvent(eval("myPaginator_"+num),eval("myDataTable_"+num));
+			myParticularRenderEvent();
 		});
 
-
-		return {
-			ds: myDataSource,
-			dt: myDataTable
-			};
 	}
-
-var div   = YAHOO.util.Dom.getElementsByClassName("datatable-container" , "div" );
-var pager = YAHOO.util.Dom.getElementsByClassName("paging" , "div" );
-
-for(var j=0;j<datatable.length;j++)
-{
-	this.init_datatable(datatable[j],div[j],pager[j],myColumnDefs[j],j);
-}
+ 	
+ /********************************************************************************/
+ 	
+	for(j=0;j<datatable.length;j++)
+	{
+		if(YAHOO.util.Dom.inDocument("datatable-container_"+j))
+		{
+			pager = YAHOO.util.Dom.get("paging_"+j);		
+			div   = YAHOO.util.Dom.get("datatable-container_"+j);
+			
+			this.init_datatable(datatable[j],div,pager,myColumnDefs[j],j);
+		}
+	}
