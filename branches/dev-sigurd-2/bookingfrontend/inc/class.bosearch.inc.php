a559aa285a3fc7f6ab66fa04b9d8daa9821cc8ec
hannes.magnusson <hannes.magnusson@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2009-04-22 12:56:12 +0000

    Make it possible to only search for building/resource/...

--- branches/dev-sigurd-2/bookingfrontend/inc/class.bosearch.inc.php
+++ branches/dev-sigurd-2/bookingfrontend/inc/class.bosearch.inc.php
@@ -13,32 +13,35 @@
 		
 		function search($searchterm)
 		{
-			$bui_result = $this->sobuilding->read(array("query"=>$searchterm));
-			foreach($bui_result['results'] as &$bui)
-			{
-				$bui['type'] = "building";
-				$bui['link'] = $GLOBALS['phpgw']->link('/index.php', array('menuaction' => 'bookingfrontend.uibuilding.index', 'id' => $bui['id']));
-			}
-			$org_result = $this->soorganization->read(array("query"=>$searchterm));
-			foreach($org_result['results'] as &$org)
-			{
-				$org['type'] = "organization";
-				$org['link'] = $GLOBALS['phpgw']->link('/index.php', array('menuaction' => 'bookingfrontend.uibuilding.index', 'id' => $org['id']));
-			}
-			$res_result = $this->soresource->read(array("query"=>$searchterm));
-			foreach($res_result['results'] as &$res)
-			{
-				$res['type'] = "resource";
-				$res['link'] = $GLOBALS['phpgw']->link('/index.php', array('menuaction' => 'bookingfrontend.uibuilding.index', 'id' => $res['id']));
-			}
+			$type = phpgw::get_var('type', 'GET');
+            $bui_result = $org_result = $res_result = array();
+
+            if (!$type || $type == "building") {
+                $bui_result = $this->sobuilding->read(array("query"=>$searchterm));
+                foreach($bui_result['results'] as &$bui)
+                {
+                    $bui['type'] = "building";
+                    $bui['link'] = $GLOBALS['phpgw']->link('/index.php', array('menuaction' => 'bookingfrontend.uibuilding.show', 'id' => $bui['id']));
+                }
+            }
+            if (!$type || $type == "organization") {
+                $org_result = $this->soorganization->read(array("query"=>$searchterm));
+                foreach($org_result['results'] as &$org)
+                {
+                    $org['type'] = "organization";
+                    $org['link'] = $GLOBALS['phpgw']->link('/index.php', array('menuaction' => 'bookingfrontend.uiorganization.show', 'id' => $org['id']));
+                }
+            }
+            if(!$type || $type == "resource") {
+                $res_result = $this->soresource->read(array("query"=>$searchterm));
+                foreach($res_result['results'] as &$res)
+                {
+                    $res['type'] = "resource";
+                    $res['link'] = $GLOBALS['phpgw']->link('/index.php', array('menuaction' => 'bookingfrontend.uiresource.show', 'id' => $res['id']));
+                }
+            }
 			$final_array = array_merge_recursive($bui_result, $org_result, $res_result);
-			$final_array['total_records_sum']	=	(	$final_array['total_records'][0] +
-														$final_array['total_records'][1] +
-														$final_array['total_records'][2]);
-			
-				#echo("<pre>");
-				#print_r($final_array);
-				#echo("</pre>");
+			$final_array['total_records_sum']	=	array_sum((array)$final_array['total_records']);
 			
 			return $final_array;
 		}
