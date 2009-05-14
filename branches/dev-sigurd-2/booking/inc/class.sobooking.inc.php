c9b69d922bc626cef88ce6d4140723f4c029e0ef
jonas.borgstrom <jonas.borgstrom@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2009-05-14 13:34:11 +0000

    bkbooking: Adding collision checks to the booking validator

--- branches/dev-sigurd-2/booking/inc/class.sobooking.inc.php
+++ branches/dev-sigurd-2/booking/inc/class.sobooking.inc.php
@@ -64,14 +64,39 @@
 			// FIXME: Validate: Season from <= date, season to >= date
 			// FIXME: Validate: booking from/to
 			// Make sure to_ > from_
-			if(!$errors)
+			$booking_id = $entity['id'] ? $entity['id'] : -1;
+			$allocation_id = $entity['allocation_id'] ? $entity['allocation_id'] : -1;
+			$from_ = new DateTime($entity['from_']);
+			$to_ = new DateTime($entity['to_']);
+			$start = $from_->format('Y-m-d H:i');
+			$end = $to_->format('Y-m-d H:i');
+			if($from_ > $to_)
 			{
-				$from_ = date_parse($entity['from_']);
-				$to_ = date_parse($entity['to_']);
-				if($from_ > $to_)
-				{
-					$errors['from_'] = 'Invalid from date';
-				}
+				$errors['from_'] = 'Invalid from date';
+			}
+			$rids = join(',', array_map("intval", $entity['resources']));
+			// Check if we overlap with any existing allocation
+			$this->db->query("SELECT a.id FROM bb_allocation a 
+								WHERE a.id<>$allocation_id AND 
+								a.id IN (SELECT allocation_id FROM bb_allocation_resource WHERE resource_id IN ($rids)) AND
+								((a.from_ >= '$start' AND a.from_ < '$end') OR 
+					 			 (a.to_ > '$start' AND a.to_ <= '$end') OR 
+					 			 (a.from_ < '$start' AND a.to_ > '$end'))", __LINE__, __FILE__);
+			if($this->db->next_record())
+			{
+				$errors['allocation'] = lang('Overlaps with existing allocation');
+			}
+			
+			// Check if we overlap with any existing booking
+			$this->db->query("SELECT b.id FROM bb_booking b 
+								WHERE b.id<>$booking_id AND 
+								b.id IN (SELECT booking_id FROM bb_booking_resource WHERE resource_id IN ($rids)) AND
+								((b.from_ >= '$start' AND b.from_ < '$end') OR 
+					 			 (b.to_ > '$start' AND b.to_ <= '$end') OR 
+					 			 (b.from_ < '$start' AND b.to_ > '$end'))", __LINE__, __FILE__);
+			if($this->db->next_record())
+			{
+				$errors['booking'] = lang('Overlaps with existing booking2');
 			}
 			return $errors;
 		}
