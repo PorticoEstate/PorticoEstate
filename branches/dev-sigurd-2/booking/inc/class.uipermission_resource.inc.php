85741014aee886c5822aa6a56cfcecd2fda4df13
joe.siponen <joe.siponen@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2009-05-15 09:15:11 +0000

    bkbooking: Correctly highlighted menu for permissions

--- branches/dev-sigurd-2/booking/inc/class.uipermission_resource.inc.php
+++ branches/dev-sigurd-2/booking/inc/class.uipermission_resource.inc.php
@@ -6,7 +6,7 @@
 		public function __construct()
 		{
 			parent::__construct();
-			#self::set_active_menu('booking::buildings::resources::documents');
+			self::set_active_menu('booking::buildings::resources::permissions');
 		}
 		
 		protected function get_parent_pathway(array $forDocumentData)
