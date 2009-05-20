76c4318fec11ef2e0036c74a3a48fb139b32bc62
roy.solberg <roy.solberg@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2009-05-20 13:41:55 +0000

    Rental: added composite and unit tables to setup

--- branches/dev-sigurd-2/rental/setup/setup.inc.php
+++ branches/dev-sigurd-2/rental/setup/setup.inc.php
@@ -1,26 +1,26 @@
 <?php
 	$setup_info['rental']['name'] = 'rental';
-	$setup_info['rental']['version'] = '0.0.1';
+	$setup_info['rental']['version'] = '0.0.2';
 	$setup_info['rental']['app_order'] = 51;
 	$setup_info['rental']['tables'] = array(
-		'phpgw_rental_tenant',									// Tenants
-		'phpgw_rental_contract',								// Contracts, has tenants and a rental object
-		'phpgw_rental_composite',								// Rental object, an aggregation of rental units or areas
-		'phpgw_rental_contract_price_item',			// Price items from the price book tied to a contract
-		'phpgw_rental_bill_price_item',					// Price items from the price book tied to a past bill
-		'phpgw_rental_bill',										// A sent bill tied to a contract
-		'phpgw_rental_contract_tenant',					// Link table between tenants and contracts
-		'phpgw_rental_contract_metadata_item',	// Custom field storage for contracts
-		'phpgw_rental_unit',										// Link table between property register and rental objects or composites
-		'phpgw_rental_comment',									// Comments to rental composites and tenants
+	//	'phpgw_rental_tenant',									// Tenants
+	//	'phpgw_rental_contract',								// Contracts, has tenants and a rental object
+		'rental_composite',								// Rental object, an aggregation of rental units or areas
+	//	'phpgw_rental_contract_price_item',			// Price items from the price book tied to a contract
+	//	'phpgw_rental_bill_price_item',					// Price items from the price book tied to a past bill
+	//	'phpgw_rental_bill',										// A sent bill tied to a contract
+	//	'phpgw_rental_contract_tenant',					// Link table between tenants and contracts
+	//	'phpgw_rental_contract_metadata_item',	// Custom field storage for contracts
+		'rental_unit',										// Link table between property register and rental objects or composites
+	//	'phpgw_rental_comment',									// Comments to rental composites and tenants
 		// Admin tables
-		'phpgw_rental_contract_status',
-		'phpgw_rental_billing_term',
-		'phpgw_rental_contract_type',
-		'phpgw_rental_price_item',
-		'phpgw_rental_tenant_type',
-		'phpgw_rental_custom_field_type',
-		'phpgw_rental_rental_object_type'
+	//	'phpgw_rental_contract_status',
+	//	'phpgw_rental_billing_term',
+	//	'phpgw_rental_contract_type',
+	//	'phpgw_rental_price_item',
+	//	'phpgw_rental_tenant_type',
+	//	'phpgw_rental_custom_field_type',
+	//	'phpgw_rental_rental_object_type'
 	);
 	$setup_info['rental']['enable'] = 1;
 	$setup_info['rental']['app_group']	= 'office';
--- branches/dev-sigurd-2/rental/setup/tables_update.inc.php
+++ branches/dev-sigurd-2/rental/setup/tables_update.inc.php
@@ -1,25 +1,44 @@
 <?php
 	$test[] = '0.0.1';
-	function booking_upgrade0_0_1()
+	function rental_upgrade0_0_1()
 	{
 		$GLOBALS['phpgw_setup']->oProc->m_odb->transaction_begin();
-		/*$GLOBALS['phpgw_setup']->oProc->DropTable('phpgw_rental');*/
 		$GLOBALS['phpgw_setup']->oProc->CreateTable(
-			'rental_objects', array(
+			'rental_composite', array(
 				'fd' => array(
-					'id' => array('type' => 'auto', 'nullable' => false),
-					'name' => array('type' => 'varchar','precision' => '50','nullable' => False),
-					'homepage' => array('type' => 'varchar','precision' => '50','nullable' => False)
+					'composite_id' => 		array('type' => 'auto', 'nullable' => false),
+					'name' => 				array('type' => 'varchar','precision' => '45','nullable' => false),
+					'description' => 		array('type' => 'text'),
+					'is_active' => 			array('type' => 'bool','nullable' => false,'default' => 'true'),
+					'address_1' =>			array('type' => 'varchar','precision' => '255'),
+					'address_2' =>			array('type' => 'varchar','precision' => '255'),
+					'house_number' =>		array('type' => 'varchar','precision' => '255'),
+					'postcode' =>			array('type' => 'varchar','precision' => '255'),
+					'place' =>				array('type' => 'varchar','precision' => '255'),
+					'has_custom_address' =>	array('type' => 'bool','nullable' => false,'default' => 'false'),
 				),
-				'pk' => array('id'),
+				'pk' => array('composite_id'),
 				'fk' => array(),
+				'ix' => array(),
+				'uc' => array()
+			),
+			'rental_unit', array(
+				'fd' => array(
+					'composite_id' => 		array('type' => 'int', 'nullable' => false),
+					'location_id' => 		array('type' => 'int', 'nullable' => false)
+				),
+				'pk' => array('composite_id','location_id'),
+				'fk' => array(
+					'rental_composite' => array( 'composite_id' => 'composite_id'),
+					'fm_locations' => array( 'location_id' => 'id')
+				),
 				'ix' => array(),
 				'uc' => array()
 			)
 		);
 		if($GLOBALS['phpgw_setup']->oProc->m_odb->transaction_commit())
 		{
-			$GLOBALS['setup_info']['booking']['currentver'] = '0.0.2';
-			return $GLOBALS['setup_info']['booking']['currentver'];
+			$GLOBALS['setup_info']['rental']['currentver'] = '0.0.2';
+			return $GLOBALS['setup_info']['rental']['currentver'];
 		}
 	}
