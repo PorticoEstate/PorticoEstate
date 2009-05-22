28903360908ed3055b332072be88a0124bb9f61e
roy.solberg <roy.solberg@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2009-05-22 10:46:28 +0000

    Rental: More test data

--- branches/dev-sigurd-2/rental/setup/default_records.inc.php
+++ branches/dev-sigurd-2/rental/setup/default_records.inc.php
@@ -5,3 +5,21 @@
  * $oProc->query("sql_statement");
  * 
  */
+ 
+$oProc->query("INSERT INTO rental_composite (name,description) VALUES ('Herdla fuglereservat','Pip pip')");
+$oProc->query("INSERT INTO rental_composite (name,description) VALUES ('Fløibanen','Tut tut')");
+$oProc->query("INSERT INTO rental_composite (name,description) VALUES ('Perle og Bruse','')");
+$oProc->query("INSERT INTO rental_composite (name,description,is_active) VALUES ('Store Lungegårdsvannet','',false)");
+$oProc->query("INSERT INTO rental_composite (name,description,address_1,address_2,house_number,postcode,place,has_custom_address) VALUES ('Beddingen','Der Bouvet e','Solheimsgaten','Inngang B','15','5058','BERGEN',true)");
+$oProc->query("INSERT INTO rental_unit VALUES (1,1)");
+$oProc->query("INSERT INTO rental_unit VALUES (1,2)");
+$oProc->query("INSERT INTO rental_unit VALUES (1,6)");
+$oProc->query("INSERT INTO rental_unit VALUES (1,10)");
+$oProc->query("INSERT INTO rental_unit VALUES (2,4)");
+$oProc->query("INSERT INTO rental_unit VALUES (2,5)");
+$oProc->query("INSERT INTO rental_unit VALUES (3,6)");
+$oProc->query("INSERT INTO rental_unit VALUES (3,10)");
+$oProc->query("INSERT INTO rental_unit VALUES (4,14)");
+$oProc->query("INSERT INTO rental_unit VALUES (4,16)");
+$oProc->query("INSERT INTO rental_unit VALUES (5,20)");
+$oProc->query("INSERT INTO rental_unit VALUES (5,22)");
