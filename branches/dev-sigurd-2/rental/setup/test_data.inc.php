0266f2bce509275418bfe405f8f022f1250e312a
roy.solberg <roy.solberg@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2009-05-22 10:34:38 +0000

    Rental: UTF-8

--- branches/dev-sigurd-2/rental/setup/test_data.inc.php
+++ branches/dev-sigurd-2/rental/setup/test_data.inc.php
@@ -6,7 +6,7 @@
  * 
  */
 $oProc->query("INSERT INTO rental_composite (name,description) VALUES ('Herdla fuglereservat','Pip pip')");
-$oProc->query("INSERT INTO rental_composite (name,description) VALUES ('Fl¿ibanen','Tut tut')");
+$oProc->query("INSERT INTO rental_composite (name,description) VALUES ('FlÃ¸ibanen','Tut tut')");
 $oProc->query("INSERT INTO rental_composite (name,description) VALUES ('Perle og Bruse','')");
-$oProc->query("INSERT INTO rental_composite (name,description,is_active) VALUES ('Store LungegŒrdsvannet','',false)");
+$oProc->query("INSERT INTO rental_composite (name,description,is_active) VALUES ('Store LungegÃ¥rdsvannet','',false)");
 $oProc->query("INSERT INTO rental_composite (name,description,address_1,address_2,house_number,postcode,place,has_custom_address) VALUES ('Beddingen','Der Bouvet e','Solheimsgaten','Inngang B','15','5058','BERGEN',true)");
