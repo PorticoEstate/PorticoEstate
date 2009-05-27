6dbda9e8dc0109c3568fb7890ac5178d4356d075
roy.solberg <roy.solberg@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2009-05-27 06:44:08 +0000

    Rental: Added prototype of the rental module.

---
+++ branches/dev-sigurd-2/rental/misc/prototype/includes/layoutscript.inc.php
@@ -0,0 +1,65 @@
+<div id="bottom1">
+</div>
+
+<script>
+
+(function() {
+    var Dom = YAHOO.util.Dom,
+        Event = YAHOO.util.Event;
+
+    Event.onDOMReady(function() {
+        var layout = new YAHOO.widget.Layout({
+            units: [
+                { position: 'top', height: 24, body: 'top1'},
+                { position: 'bottom', height: 24, body: 'bottom1' },
+                { position: 'left', header: 'Menu', width: 200, resize: true, body: 'left1', gutter: '3px', collapseSize: 50, scroll: true, animate: true },
+                { position: 'center', header: 'Main View', body: 'center1', gutter: '3px', collapse: true, close: true, collapseSize: 50, scroll: true, animate: true }
+            ]
+        });
+        layout.on('render', function() {
+            layout.getUnitByPosition('left').on('close', function() {
+                closeLeft();
+            });
+        });
+        layout.render();
+        Event.on('tLeft', 'click', function(ev) {
+            Event.stopEvent(ev);
+            layout.getUnitByPosition('left').toggle();
+        });
+        Event.on('tRight', 'click', function(ev) {
+            Event.stopEvent(ev);
+            layout.getUnitByPosition('right').toggle();
+        });
+        Event.on('padRight', 'click', function(ev) {
+            Event.stopEvent(ev);
+            var pad = prompt('CSS gutter to apply: ("2px" or "2px 4px" or any combination of the 4 sides)', layout.getUnitByPosition('right').get('gutter'));
+            layout.getUnitByPosition('right').set('gutter', pad);
+        });
+        var closeLeft = function() {
+            var a = document.createElement('a');
+            a.href = '#';
+            a.innerHTML = 'Add Left Unit';
+            Dom.get('closeLeft').parentNode.appendChild(a);
+
+            Dom.setStyle('tLeft', 'display', 'none');
+            Dom.setStyle('closeLeft', 'display', 'none');
+            Event.on(a, 'click', function(ev) {
+                Event.stopEvent(ev);
+                Dom.setStyle('tLeft', 'display', 'inline');
+                Dom.setStyle('closeLeft', 'display', 'inline');
+                a.parentNode.removeChild(a);
+                layout.addUnit(layout.get('units')[3]);
+                layout.getUnitByPosition('left').on('close', function() {
+                    closeLeft();
+                });
+            });
+        };
+        Event.on('closeLeft', 'click', function(ev) {
+            Event.stopEvent(ev);
+            layout.getUnitByPosition('left').close();
+        });
+    });
+
+
+})();
+</script>
