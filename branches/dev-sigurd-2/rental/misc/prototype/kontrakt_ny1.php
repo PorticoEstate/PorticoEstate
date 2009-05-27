f7c10b3847a9a67c9355bf87e8d3e6c811519960
roy.solberg <roy.solberg@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2009-05-27 06:31:03 +0000

    Rental: Added prototype of the rental module.

---
+++ branches/dev-sigurd-2/rental/misc/prototype/kontrakt_ny1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>Bergen kommune leiemodul</title>
+    <? include('includes/head.inc.php') ?>
+</head>
+
+<body class="yui-skin-sam">
+  <? include('includes/sitetitle.inc.php') ?>
+  <? include('includes/menumarkup.inc.php') ?>
+
+  <!--- Content area -->
+  <div id="center1">
+    <a href="kontrakt_ny2.php"><img src="images/content/kontrakt_leietaker.png" alt="Leietaker" /></a>
+    <h2>Klikk på bildet for å komme videre i prosessen</h2>
+  </div>
+  <!-- End content area -->
+
+  <? include('includes/layoutscript.inc.php') ?>
+</body>
+</html>
