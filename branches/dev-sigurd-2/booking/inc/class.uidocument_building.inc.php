e25d4df9b82b6acb39aa3e8b716835e4ccb0ab84
joe.siponen <joe.siponen@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2009-06-09 12:24:55 +0000

    bkbooking: Removed cause of recursion in uidocument for building

--- branches/dev-sigurd-2/booking/inc/class.uidocument_building.inc.php
+++ branches/dev-sigurd-2/booking/inc/class.uidocument_building.inc.php
@@ -6,9 +6,6 @@
 		public function __construct()
 		{
 			parent::__construct();
-			
-			self::process_booking_unauthorized_exceptions();
-			
 			self::set_active_menu('booking::buildings::documents');
 		}
 		
