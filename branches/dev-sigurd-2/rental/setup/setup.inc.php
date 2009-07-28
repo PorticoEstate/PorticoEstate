2cc09e998e6be67e3a8cad7a3db1b17fb4e3ce20
roy.solberg <roy.solberg@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2009-07-28 08:00:51 +0000

    Rental: Added rental_contract_price_item to list of tables.

--- branches/dev-sigurd-2/rental/setup/setup.inc.php
+++ branches/dev-sigurd-2/rental/setup/setup.inc.php
@@ -8,7 +8,7 @@
 		'rental_contract_composite',			// Connection between contracts and composites
 		'rental_contract_party',				// Link table between tenants and contracts
 		'rental_composite',								// Rental object, an aggregation of rental units or areas
-	//	'rental_contract_price_item',			// Price items from the price book tied to a contract
+		'rental_contract_price_item',			// Price items from the price book tied to a contract
 	//	'rental_bill_price_item',					// Price items from the price book tied to a past bill
 	//	'rental_bill',										// A sent bill tied to a contract
 	//	'rental_contract_metadata_item',	// Custom field storage for contracts
