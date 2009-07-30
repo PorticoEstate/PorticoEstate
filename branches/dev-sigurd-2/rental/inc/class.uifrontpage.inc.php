77fafd72d1fc8ee875eabf4c2b83536c85953362
yngve.espelid <yngve.espelid@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2009-07-30 14:07:09 +0000

    Rental: Frontpage work, lists in windows, settings saved in preferences

--- branches/dev-sigurd-2/rental/inc/class.uifrontpage.inc.php
+++ branches/dev-sigurd-2/rental/inc/class.uifrontpage.inc.php
@@ -6,22 +6,37 @@
 		public $public_functions = array
 		(
 			'index'	=> true,
+			'query' => true
 		);
 
 		public function __construct()
 		{
-      parent::__construct();
+      		parent::__construct();
 			self::set_active_menu('rental::frontpage');
 		}
 
+		public function query()
+		{
+			$type = phpgw::get_var('type');
+			
+			switch($type){
+				case 'save_panel_settings':
+					$panel = phpgw::get_var('name');
+					$visibility = phpgw::get_var('visibility');
+					$x = phpgw::get_var('x');
+					$y = phpgw::get_var('y');
+					$panel_config = array($visibility,$x,$y);
+					$GLOBALS['phpgw']->preferences->account_id=$GLOBALS['phpgw_info']['user']['account_id'];
+					$GLOBALS['phpgw']->preferences->read();
+					$GLOBALS['phpgw']->preferences->add('rental','rental_frontpage_panel_'.$panel,$panel_config,'user');
+					$GLOBALS['phpgw']->preferences->save_repository();
+					break;
+			}
+		}
+		
 		public function index()
 		{
 			$this->render('frontpage.php');
-		}
-		
-		public function query()
-		{
-			
 		}
 	}
 ?>
--- branches/dev-sigurd-2/rental/templates/base/frontpage.php
+++ branches/dev-sigurd-2/rental/templates/base/frontpage.php
@@ -7,55 +7,137 @@
 
 
 YAHOO.util.Event.addListener(window, "load", function() {
-
-	
-
-	var workingOnContracts = new YAHOO.widget.Panel("workingOnContracts",{});
-	workingOnContracts.visible = true;
-	var executiveOfficerOnContracts = new YAHOO.widget.Panel("executiveOfficerOnContracts",{});
-	executiveOfficerOnContracts.visible = true;
-	var endingContracts = new YAHOO.widget.Panel("endingContracts",{});
-	endingContracts.visible = true;
-	var availableComposites = new YAHOO.widget.Panel("availableComposites",{});
-	availableComposites.visible = true;
-
-	
-	
-	var toggleVisibility = function(event){
-		if(this.visible){
-			this.hide();
-			this.visible = false;
+	var panels = new Array();
+	createPanel = function(element,visibility,x,y){
+		var panel = new YAHOO.widget.Panel(element + "_panel",{close: false, constraintoviewport: true});
+		panel.name = element;
+		panels.push(panel);
+		var toggleVisibility = function(event){
+			if(this.visible){
+				this.hide();
+				this.visible = false;
+			}
+			else{
+				this.show();
+				this.visible = true;
+			}
 		}
-		else{
-			this.show();
-			this.visible = true;
+		var button = new YAHOO.widget.Button(element + "_button", {type: "checkbox", onclick: {fn:toggleVisibility, scope: panel}, checked: visibility});
+
+		if(x != 0 && y != 0){
+			
+			panel.moveTo(x,y);
+		}
+		
+		panel.render();
+
+		if(visibility){
+			panel.visible = true;
+			panel.show();
+		} else {
+			panel.visible = false;
+			panel.hide();
 		}
 	}
 
-	var button1 = new YAHOO.widget.Button('toggleWorkingOnContracts',{type: "checkbox", onclick: {fn:toggleVisibility, scope: workingOnContracts}}); 
-	var button2 = new YAHOO.widget.Button('toggleExecutiveOfficerOnContracts',{type: "checkbox", onclick: {fn:toggleVisibility, scope: executiveOfficerOnContracts}}); 
-	var button3 = new YAHOO.widget.Button('toggleEndingContracts',{type: "checkbox", onclick: {fn:toggleVisibility, scope: endingContracts}}); 
-	var button4 = new YAHOO.widget.Button('toggleAvailableComposites',{type: "checkbox",onclick: {fn:toggleVisibility, scope: availableComposites}});
-	var button5 = new YAHOO.widget.Button('saveSetup');
 	
-	workingOnContracts.render();
-	executiveOfficerOnContracts.render();
-	endingContracts.render();
-	availableComposites.render();
+	
+
+	<?php 
+	$panels = array('workingOnContracts','executiveOfficerOnContracts','endingContracts','availableComposites');
+	$GLOBALS['phpgw']->preferences->account_id=$GLOBALS['phpgw_info']['user']['account_id'];
+	$preferences = $GLOBALS['phpgw']->preferences->read();
+		//var_dump($preferences);
+	foreach($panels as $panel){
+		
+		$config = $preferences['rental']['rental_frontpage_panel_'.$panel];
+		//var_dump($config);
+		if(isset($config))
+		{
+	?>
+		createPanel('<?= $panel ?>',<?= $config[0] ?>,<?= $config[1] ?>,<?= $config[2]?>);
+	<?php
+		}
+		else
+		{
+	?>
+		createPanel('<?= $panel ?>',true,0,0);	
+	<?php 
+		}
+	}
+	?>
+
+	
+	
+	var savePanelConfigurations = function(event){
+		var pans = this;
+		var success = true;
+		var getCumulativeOffset = function (obj) {
+		    var left, top;
+		    left = top = 0;
+		    if (obj.offsetParent) {
+		        do {
+		            left += obj.offsetLeft;
+		            top  += obj.offsetTop;
+		        } while (obj = obj.offsetParent);
+		    }
+		    return {
+		        x : left,
+		        y : top
+		    };
+		};
+		
+		for(var i=0; i<pans.length; i++){
+			var p = pans[i];
+
+			var ajaxFailure = function(event){
+				success = false;
+			}
+						
+			var position = getCumulativeOffset(p.element);
+			var request = YAHOO.util.Connect.asyncRequest(
+				'GET', 
+				'<?= html_entity_decode(self::link(array('menuaction' => 'rental.uifrontpage.query','type' => 'save_panel_settings'))) ?>' + 
+					'&name=' + p.name + '&visibility=' + p.visible + '&x=' + position.x + '&y=' + position.y,
+				{ 
+					failure: ajaxFailure
+				}
+			);
+		}
+
+		var element = document.getElementById('messageHolder');
+		
+		if(success){
+			element.innerHTML ='<p class="message">Oppsettet ble lagret</p>';
+		} else {
+			element.innerHTML ='<p class="error">Oppsettet ble ikke lagret</p>';
+		}
+		
+	}
+
+	new YAHOO.widget.Button('saveSetup',{onclick: {fn:savePanelConfigurations, scope: panels}});
+	
 });
 	
 </script>
-<h1><img src="<?= RENTAL_TEMPLATE_PATH ?>images/32x32/places/user-desktop.png" /> <?= lang('rental_dashboard_title') ?></h1>
+<h1><img src="<?= RENTAL_TEMPLATE_PATH ?>images/32x32/places/user-desktop.png" /> <?= lang('rental_dashboard_title') ?></h1> 
 
-<div>
-<button type="button" id="toggleWorkingOnContracts"><?= lang('rental_frontpage_working_on') ?></button>
-<button type="button" id="toggleExecutiveOfficerOnContracts"><?= lang('rental_frontpage_executive_officer_for') ?></button> 
-<button type="button" id="toggleEndingContracts"><?= lang('rental_frontpage_contracts_under_dismissal') ?></button> 
-<button type="button" id="toggleAvailableComposites"><?= lang('rental_frontpage_available_composites') ?></button>
-<button type="button" id="saveSetup"><?= lang('rental_frontpage_save_setup') ?></button> 
+<fieldset>
+	<h3><?= lang('rental_frontpage_panels') ?></h3>
+	<button type="button" id="workingOnContracts_button"><?= lang('rental_frontpage_working_on') ?></button>
+	<button type="button" id="executiveOfficerOnContracts_button"><?= lang('rental_frontpage_executive_officer_for') ?></button> 
+	<button type="button" id="endingContracts_button"><?= lang('rental_frontpage_contracts_under_dismissal') ?></button> 
+	<button type="button" id="availableComposites_button"><?= lang('rental_frontpage_available_composites') ?></button>
+	<button type="button" id="saveSetup"><?= lang('rental_frontpage_save_setup') ?></button> 
+	
+</fieldset>
+
+
+<div id="messageHolder">
+
 </div>
 
-<div id="workingOnContracts"> 
+<div id="workingOnContracts_panel"> 
     <div class="hd"><h3><?= lang('rental_frontpage_working_on') ?></h3></div> 
     <div class="bd">
     	<?php 
@@ -72,7 +154,7 @@ YAHOO.util.Event.addListener(window, "load", function() {
 		?>
     </div> 
 </div> 
-<div id="executiveOfficerOnContracts"> 
+<div id="executiveOfficerOnContracts_panel"> 
 	<div class="hd"><h3><?= lang('rental_frontpage_executive_officer_for') ?></h3></div> 
     <div class="bd">
 		<?php 
@@ -88,7 +170,7 @@ YAHOO.util.Event.addListener(window, "load", function() {
 		?>
 	</div>
 </div> 
-<div id="endingContracts"> 
+<div id="endingContracts_panel"> 
 	<div class="hd"><h3><?= lang('rental_frontpage_contracts_under_dismissal') ?></h3></div> 
     <div class="bd">
 		<?php 
@@ -104,7 +186,7 @@ YAHOO.util.Event.addListener(window, "load", function() {
 		?>
 	</div>
 </div>
-<div id="availableComposites"> 
+<div id="availableComposites_panel"> 
 	<div class="hd"><h3><?= lang('rental_frontpage_available_composites') ?></h3></div> 
     <div class="bd">
 	<?php 
@@ -115,5 +197,3 @@ YAHOO.util.Event.addListener(window, "load", function() {
 	?>
 	</div>
 </div>
-
-
