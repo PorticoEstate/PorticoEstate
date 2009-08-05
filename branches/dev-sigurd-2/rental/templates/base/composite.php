19704113587d74118435b8aa321f0a1c98dd4545
roy.solberg <roy.solberg@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2009-08-05 08:04:24 +0000

    Rental: Bugfix: Composite form didn't save changes.

--- branches/dev-sigurd-2/rental/templates/base/composite.php
+++ branches/dev-sigurd-2/rental/templates/base/composite.php
@@ -122,7 +122,7 @@
 				<div class="form-buttons">
 					<?php
 						if ($editable) {
-							echo '<input type="submit" name="save" value="' . lang('rental_rc_save') . '"/>';
+							echo '<input type="submit" name="save_composite" value="' . lang('rental_rc_save') . '"/>';
 							echo '<a class="cancel" href="' . $cancel_link . '">' . lang('rental_rc_cancel') . '</a>';
 						} else {
 							echo '<a class="cancel" href="' . $cancel_link . '">' . lang('rental_rc_back') . '</a>';
