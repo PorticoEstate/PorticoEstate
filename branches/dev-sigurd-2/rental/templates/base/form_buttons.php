ed8dc2dd9736bad142fe5bbf0a434764fde6df05
roy.solberg <roy.solberg@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2009-08-13 10:57:47 +0000

    Rental: I18N fix.

--- branches/dev-sigurd-2/rental/templates/base/form_buttons.php
+++ branches/dev-sigurd-2/rental/templates/base/form_buttons.php
@@ -1,7 +1,7 @@
 <div class="form-buttons">
 	<?php
 		if ($editable) {
-			echo '<input type="submit" name="save" value="' . lang('rental_common_party') . '"/>';
+			echo '<input type="submit" name="save" value="' . lang('rental_common_save') . '"/>';
 			echo '<a class="cancel" href="' . $cancel_link . '">' . lang('rental_common_cancel') . '</a>';
 		} else {
 			echo '<a class="cancel" href="' . $cancel_link . '">' . lang('rental_common_back') . '</a>';
