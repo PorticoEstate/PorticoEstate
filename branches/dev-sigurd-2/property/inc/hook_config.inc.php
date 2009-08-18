dcedb762c86132e1402db1290791989d8f76679f
sigurd <sigurd@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2009-08-18 13:46:23 +0000

    Config groups for simplified interface

---
+++ branches/dev-sigurd-2/property/inc/hook_config.inc.php
@@ -0,0 +1,56 @@
+<?php
+	/**
+	* Property - configuration hook
+	*
+	* @author Sigurd Nes <sigurdne@online.no>
+	* @copyright Copyright (C) 2000-2009 Free Software Foundation, Inc. http://www.fsf.org/
+	* @license http://www.gnu.org/licenses/gpl.html GNU General Public License
+	* @package phpgroupware
+	* @subpackage property
+	* @category hooks
+	* @version $Id$
+	*/
+
+	/*
+	   This program is free software: you can redistribute it and/or modify
+	   it under the terms of the GNU General Public License as published by
+	   the Free Software Foundation, either version 2 of the License, or
+	   (at your option) any later version.
+
+	   This program is distributed in the hope that it will be useful,
+	   but WITHOUT ANY WARRANTY; without even the implied warranty of
+	   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	   GNU General Public License for more details.
+
+	   You should have received a copy of the GNU General Public License
+	   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	 */
+
+
+	/**
+	* Get HTML checkbox with groups that are candidates for simplified tts interface
+	*
+	* @param $config
+	* @return string HTML checkboxes to be placed in a table
+	*/
+	function fmttssimple_group($config)
+	{
+		$groups = $GLOBALS['phpgw']->accounts->get_list('groups');
+		$simple_group = isset($config['fmttssimple_group']) ? $config['fmttssimple_group'] : array();
+		$out = '';
+		foreach ( $groups as $group => $label)
+		{
+			$checked = '';
+			if ( in_array($group, $simple_group))
+			{
+				$checked = ' checked';
+			}
+
+			$out .=  <<<HTML
+				<tr><td><input type="checkbox" name="newsettings[fmttssimple_group][]" value="{$group}" {$checked}><label>{$label}</label></td></tr>
+HTML;
+		}
+		return $out;
+	}
+
+
