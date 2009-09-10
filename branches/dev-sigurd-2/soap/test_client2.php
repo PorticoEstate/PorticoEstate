d7cf6ffa26ba5054f555451564e9a68bf92401f0
sigurd <sigurd@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2009-09-10 20:24:46 +0000

    more on soap

---
+++ branches/dev-sigurd-2/soap/test_client2.php
@@ -0,0 +1,81 @@
+<?php
+/**************************************************************************\
+* phpGroupWare - XML-RPC Test App                                          *
+* http://www.phpgroupware.org                                              *
+* --------------------------------------------                             *
+*  This program is free software; you can redistribute it and/or modify it *
+*  under the terms of the GNU General Public License as published by the   *
+*  Free Software Foundation; either version 2 of the License, or (at your  *
+*  option) any later version.                                              *
+\**************************************************************************/
+
+/* $Id: index.php 690 2008-02-02 10:11:33Z dave $ */
+
+	$phpgw_info = array();
+	$GLOBALS['phpgw_info']['flags'] = array
+	(
+		'disable_template_class' => true,
+		'login'                  => true,
+		'currentapp'             => 'login',
+		'noheader'               => true
+	);
+
+	include('../header.inc.php');
+
+	$location = "http://{$_SERVER['HTTP_HOST']}" . parse_url($GLOBALS['phpgw_info']['server']['webserver_url'], PHP_URL_PATH) . '/soap2.php';
+
+	$client = new SoapClient(null, array(
+		'location'		=> $location,
+		'uri'			=> "urn://www.sigurd.testing/soap",
+		'soap_version'	=> SOAP_1_2,
+		'trace'			=> 1,
+		'login'			=> 'mylogin',
+		'password'		=> 'secret'
+ 	));
+
+	$login_data = array
+	(
+		'domain'	=> 'default',
+		'username'	=> 'anonymous',
+		'password'	=> 'anonymous1'
+	);
+
+
+	$return = $client->__soapCall("system_login",array($login_data));
+_debug_array($return);
+
+	$return = $client->__soapCall("displayheaders",array());
+_debug_array($return);
+
+
+	$GLOBALS['phpgw_info']['flags'] = array
+	(
+		'currentapp'	=> 'soap',
+		'noheader'		=> True,
+		'noappheader'	=> True,
+		'nonavbar'		=> True
+	);
+
+	$return = $client->__soapCall("hello",array("world"));
+_debug_array($return);
+	$return = $client->__soapCall("system_listApps",array());
+_debug_array($return);
+	$return = $client->__soapCall("system_logout",$login_data);
+_debug_array($return);
+
+/*
+
+	echo("\nReturning value of __soapCall() call: ".$return);
+
+	echo("\nDumping request headers:\n" 
+		.$client->__getLastRequestHeaders());
+
+	echo("\nDumping request:\n".$client->__getLastRequest());
+
+	echo("\nDumping response headers:\n"
+		.$client->__getLastResponseHeaders());
+
+	echo("\nDumping response:\n".$client->__getLastResponse());
+*/
+	$GLOBALS['phpgw']->common->phpgw_exit();
+?>
