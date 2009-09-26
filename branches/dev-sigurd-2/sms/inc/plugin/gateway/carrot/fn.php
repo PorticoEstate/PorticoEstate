c968e2a707295e22e65be63f535b143a8337ae8e
sigurd <sigurd@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2009-09-26 09:07:25 +0000

    Prepare for incoming SMS as mail

--- branches/dev-sigurd-2/sms/inc/plugin/gateway/carrot/fn.php
+++ branches/dev-sigurd-2/sms/inc/plugin/gateway/carrot/fn.php
@@ -154,9 +154,76 @@ return;
 		    return;
 		}
 
+
+		function check_for_new_mail()
+		{
+			if(!isset($this->carrot_param['email_user']) || ! $this->carrot_param['email_user'])
+			{
+			    throw new Exception('Email user not defined');			
+			}
+
+			$account_id = $GLOBALS['phpgw']->accounts->name2id($this->carrot_param['email_user']);
+
+			$GLOBALS['phpgw_info']['user']['account_id'] = $account_id;
+			$GLOBALS['phpgw']->preferences->account_id = $account_id;
+			$pref = $GLOBALS['phpgw']->preferences->read();
+			$GLOBALS['phpgw_info']['user']['preferences']['felamimail'] = isset($pref['felamimail']) ? $pref['felamimail'] : '';
+
+			$boPreferences  = CreateObject('felamimail.bopreferences');
+			$boPreferences->setProfileActive(true,2); //2 for selected user
+			$bofelamimail	= CreateObject('felamimail.bofelamimail');
+
+			$connectionStatus = $bofelamimail->openConnection();
+			$headers = $bofelamimail->getHeaders('INBOX', 1, $maxMessages = 15, $sort = 0, $_reverse = 1, $_filter = array('string' => '', 'type' => 'quick', 'status' => 'unseen'));
+
+			$sms = array();
+			$j = 0;
+			if (isset($headers['header']) && is_array($headers['header']))
+			{
+				foreach ($headers['header'] as $header)
+				{
+					if(!$header['seen'])
+					{
+						$sms[$j]['uid'] = $header['uid'];
+						$sms[$j]['message'] = utf8_encode($header['subject']);
+						$bodyParts = $bofelamimail->getMessageBody($header['uid']);
+						$sms[$j]['message'] .= "\n";
+						for($i=0; $i<count($bodyParts); $i++ )
+						{
+							$sms[$j]['message'] .= utf8_encode($bodyParts[$i]['body']) . "\n";
+						}
+
+						$sms[$j]['message'] = substr($sms[$j]['message'],0,160);
+						$j++;
+					}
+				}
+			}
+
+			foreach($sms as $entry)
+			{
+				$sms_datetime	= $entry[''];
+				$sms_sender		= $entry[''];
+				$target_code	= $entry[''];
+				$message		= $entry['message'];
+				
+				if (!parent::setsmsincomingaction($sms_datetime,$sms_sender,$target_code,$message))
+				{
+					$bofelamimail->flagMessages($_flag = 'unread', array($entry['uid']));
+				}			
+			}
+
+			if($connectionStatus == 'true')
+			{
+				$bofelamimail->closeConnection();
+			}
+		}
+
+
 		function gw_set_incoming_action()
 		{
-return; //for now...
+			$this->check_for_new_mail();
+			
+return;
 		    $handle = @opendir($this->carrot_param[path] . "/cache/smsd");
 		    while ($sms_in_file = @readdir($handle))
 		    {
