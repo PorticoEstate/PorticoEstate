098251ba829c231ce924d96279823c492d4c840a
espen.liland <espen.liland@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2009-09-29 11:06:34 +0000

    Rental: New methods for getting, populating, etc. on party objects.

--- branches/dev-sigurd-2/rental/inc/class.soparty.inc.php
+++ branches/dev-sigurd-2/rental/inc/class.soparty.inc.php
@@ -21,6 +21,185 @@ class rental_soparty extends rental_socommon
 		return self::$so;
 	}
 
+
+    protected function get_query(string $sort_field, boolean $ascending, string $search_for, string $search_type, array $filters, boolean $return_count)
+	{
+		$clauses = array('1=1');
+
+		//Add columns to this array to include them in the query
+		$columns = array();
+
+        if($ascending != null && $sort_field != null) {
+            $sort = $this->marshal($sort_field,'field');
+            $dir = $ascending ? 'ASC' : 'DESC';
+            $order = $sort ? "ORDER BY $sort $dir": '';
+        }
+		if($search_for)
+		{
+			$query = $this->marshal($search_for,'string');
+			$like_pattern = "'%".$search_for."%'";
+			$like_clauses = array();
+			switch($search_type){
+				case "id":
+                    $like_clauses[] = "party.id = $search_for";
+					break;
+                case "name":
+                    $like_clauses[] = "party.first_name $this->like $like_pattern";
+                    $like_clauses[] = "party.last_name $this->like $like_pattern";
+                    $like_clauses[] = "party.company_name $this->like $like_pattern";
+                    break;
+                case "address":
+                    $like_clauses[] = "party.address_1 $this->like $like_pattern";
+                    $like_clauses[] = "party.address_2 $this->like $like_pattern";
+                    $like_clauses[] = "party.postal_code $this->like $like_pattern";
+                    $like_clauses[] = "party.place $this->like $like_pattern";
+                    break;
+                case "ssn":
+                    $like_clauses[] = "party.personal_identification_number = $search_for";
+                    break;
+                case "organisation_number":
+                    $like_clauses[] = "party.organisation_number = $search_for";
+                    break;
+                case "account":
+                    $like_clauses[] = "party.agresso_id = $search_for";
+				case "all":
+					$like_clauses[] = "party.first_name $this->like $like_pattern";
+                    $like_clauses[] = "party.last_name $this->like $like_pattern";
+                    $like_clauses[] = "party.company_name $this->like $like_pattern";
+					$like_clauses[] = "party.address_1 $this->like $like_pattern";
+                    $like_clauses[] = "party.address_2 $this->like $like_pattern";
+                    $like_clauses[] = "party.postal_code $this->like $like_pattern";
+                    $like_clauses[] = "party.place $this->like $like_pattern";
+					$like_clauses[] = "party.personal_identification_number = $search_for";
+                    $like_clauses[] = "party.organisation_number = $search_for";
+                    $like_clauses[] = "party.agresso_id = $search_for";
+                    $like_clauses[] = "party.comment $this->like $like_pattern";
+					$like_clauses[] = "party.reskontro = $like_pattern";
+					break;
+			}
+
+
+			if(count($like_clauses))
+			{
+				$clauses[] = '(' . join(' OR ', $like_clauses) . ')';
+			}
+		}
+
+		$filter_clauses = array();
+
+
+		// All parties with contracts of type X
+		/*if(isset($filters['party_type']))
+		{
+			$party_type = $this->marshal($filters['party_type'],'int');
+            $filter_clauses[] = "SELECT $columns
+					FROM rental_party
+					LEFT JOIN (
+						SELECT party_id, contract_id, location_id FROM rental_contract_party rcp
+						LEFT JOIN (
+							SELECT id, location_id FROM rental_contract
+						) c
+						ON (c.id = rcp.contract_id)
+					) contracts
+					ON (rental_party.id = contracts.party_id OR contracts.contract_id IS NULL)
+					WHERE location_id = $party_type";
+		}*/
+
+		/*
+		 * Contract status is defined by the dates in each contract compared to the target date (default today):
+		 * - contracts under planning:
+		 * the start date is larger (in the future) than the target date, or start date is undefined
+		 * - active contracts:
+		 * the start date is smaller (in the past) than the target date, and the end date is undefined (running) or
+		 * larger (fixed) than the target date
+		 * - under dismissal:
+		 * the start date is smaller than the target date,
+		 * the end date is larger than the target date, and
+		 * the end date substracted the contract type notification period is smaller than the target date
+		 * - ended:
+		 * the end date is smaller than the target date
+		 
+
+		if(isset($filters['contracts_for_billing']))
+		{
+			$sql = "SELECT months FROM rental_billing_term WHERE id = {$billing_term_id}";
+			$result = $this->db->query($sql);
+			if(!$result)
+			{
+				return;
+			}
+			if(!$this->db->next_record())
+			{
+				return;
+			}
+			$months = $this->unmarshal($this->db->f('months', true), 'int');
+			$timestamp_end = strtotime("{$year}-{$month}-01"); // The first day in the month to bill for
+			$timestamp_start = strtotime("-{$$months} months", $timestamp_end); // The first day of the period to bill for
+			$timestamp_end = strtotime('+1 month', $timestamp_end); // The first day in the month after the one to bill for
+
+			$timestamp_start = strtotime("{$year}-{$month}-01");
+
+			$filter_clauses[] = "contract.location_id = {$contract_type_location_id}";
+			$filter_clauses[] = "contract.term_id = {$billing_term_id}";
+			$filter_clauses[] = "date_start < $timestamp_end";
+			$filter_clauses[] = "(date_end IS NULL OR date_end >= {$timestamp_start})";
+			$filter_clauses[] = "billing_start <= {$timestamp_end}";
+
+			$specific_ordering = 'contract.billing_start DESC, contract.date_start DESC, contract.date_end DESC';
+			$order = $order ? $order.' '.$specific_ordering : "ORDER BY {$specific_ordering}";
+		}*/
+
+		if(count($filter_clauses))
+        {
+			$clauses[] = join(' AND ', $filter_clauses);
+		}
+
+		$condition =  join(' AND ', $clauses);
+
+		if($return_count) // We should only return a count
+		{
+			$cols = 'COUNT(DISTINCT(rental_party.id)) AS count';
+		}
+		else
+		{
+			$columns[] = 'party.id AS party_id';
+			$columns[] = 'party.agresso_id';
+			$columns[] = 'party.personal_identification_number AS pid';
+			$columns[] = 'party.first_name';
+			$columns[] = 'party.last_name';
+			$columns[] = 'party.title';
+			$columns[] = 'party.company_name';
+			$columns[] = 'party.department';
+			$columns[] = 'party.organisation_number AS orgno';
+			$columns[] = 'party.address_1';
+			$columns[] = 'party.address_2';
+			$columns[] = 'party.postal_code';
+			$columns[] = 'party.place';
+			$columns[] = 'party.phone';
+			$columns[] = 'party.fax';
+			$columns[] = 'party.email';
+			$columns[] = 'party.url';
+			$columns[] = 'party.account_number';
+			$columns[] = 'party.reskontro';
+			$columns[] = 'party.location_id';
+			$cols = implode(',',$columns);
+		}
+
+
+		$tables = "rental_party party";
+		//$join_contract_type = 	$this->left_join.' rental_contract_responsibility type ON (type.location_id = contract.location_id)';
+		//$join_parties = $this->left_join.' rental_contract_party c_t ON (contract.id = c_t.contract_id) LEFT JOIN rental_party party ON (c_t.party_id = party.id)';
+		//$join_composites = 		$this->left_join." rental_contract_composite c_c ON (contract.id = c_c.contract_id) {$this->left_join} rental_composite composite ON c_c.composite_id = composite.id";
+		//$join_last_edited = $this->left_join.' rental_contract_last_edited last_edited ON (contract.id = last_edited.contract_id)';
+		//$joins = $join_contract_type.' '.$join_parties.' '.$join_composites.' '.$join_last_edited;
+//        echo '<pre>';
+//        print_r("SELECT {$cols} FROM {$tables} WHERE {$condition} {$order}");
+//        echo '</pre>';
+		return "SELECT {$cols} FROM {$tables} WHERE {$condition} {$order}";
+	}
+
+
+
 	/**
 	 * Get single party
 	 *
@@ -80,7 +259,7 @@ class rental_soparty extends rental_socommon
 	 * @param $filters array of custom filters
 	 * @param $count boolean value, true if only count, false if regular query
 	 * @return list of rental_party objects
-	 */
+	 
 	function get_party_array($start = 0, $results = 1000, $sort = 'id', $dir = 'asc', $query = null, $search_option = null, $filters = array(), $count = false)
 	{
 		// If no count query, create ORDER BY condition
@@ -104,7 +283,8 @@ class rental_soparty extends rental_socommon
 		// but gets one or more types from the contracts it is associated to.
 		// So if this filter is set we need to do some joining to check what contracts this
 		// party is tied to.
-		if ((isset($filters['party_type']) && $filters['party_type'] != 'all') || isset($filters['contract_id'])) {
+		if ((isset($filters['party_type']) && $filters['party_type'] != 'all') || isset($filters['contract_id']))
+        {
 				
 			// Join the contracts (many to many) so we can search for contract types, only
 			// include parties that actually have contracts
@@ -192,7 +372,7 @@ class rental_soparty extends rental_socommon
 			$parties[] = $party;
 		}
 		return $parties;
-	}
+	}*/
 
 	/**
 	 * Function for adding a new party to the database. Updates the party object.
@@ -200,7 +380,7 @@ class rental_soparty extends rental_socommon
 	 * @param rental_party $party the party to be added
 	 * @return bool true if successful, false otherwise
 	 */
-	function add(rental_party &$party)
+	function add(&$party)
 	{
 		// Insert a new party
 		$q ="INSERT INTO ".$this->table_name." (is_active) VALUES (true)";
@@ -296,42 +476,42 @@ class rental_soparty extends rental_socommon
 	 * Update the database values for an existing party object.
 	 *
 	 * @param $party the party to be updated
-	 * @return boolean	true if successful, false otherwise
+	 * @return boolean true if successful, false otherwise
 	 */
 	function update($party)
 	{
 		$id = intval($party->get_id());
 		$values = array(
-			'personal_identification_number = \'' . $party->get_personal_identification_number() . '\'',
-			'first_name = \'' . $party->get_first_name() . '\'',
-			'last_name = \'' . $party->get_last_name() . '\'',
-			'title = \'' . $party->get_title() . '\'',
-			'company_name = \'' . $party->get_company_name() . '\'',
-			'department = \'' . $party->get_department() . '\'',
-			'address_1 = \'' . $party->get_address_1() . '\'',
-			'address_2 = \'' . $party->get_address_2() . '\'',
-			'postal_code = \'' . $party->get_postal_code() . '\'',
-			'place = \'' . $party->get_place() . '\'',
-			'phone = \'' . $party->get_phone() . '\'',
-			'fax = \'' . $party->get_fax() . '\'',
-			'email = \'' . $party->get_email() . '\'',
-			'url = \'' . $party->get_url() . '\'',
-			'account_number = \'' . $party->get_account_number() . '\'',
-			'reskontro = \'' . $party->get_reskontro() . '\'',
-			'is_active = \'' . ($party->is_active() ? 'true' : 'false') . '\'',
-			'comment = \'' . $party->get_comment() . '\''
+			'personal_identification_number = ' . $this->marshal($party->get_personal_identification_number(), 'string'),
+			'first_name = '     . $this->marshal($party->get_first_name(), 'string'),
+			'last_name =  '     . $this->marshal($party->get_last_name(), 'string'),
+			'title = '          . $this->marshal($party->get_title(), 'string'),
+			'company_name = '   . $this->marshal($party->get_company_name(), 'string'),
+			'department = '     . $this->marshal($party->get_department(), 'string'),
+			'address_1 = '      . $this->marshal($party->get_address_1(), 'string'),
+			'address_2 = '      . $this->marshal($party->get_address_2(), 'string'),
+			'postal_code = '    . $this->marshal($party->get_postal_code(), 'string'),
+			'place = '          . $this->marshal($party->get_place(), 'string'),
+			'phone = '          . $this->marshal($party->get_phone(), 'string'),
+			'fax = '            . $this->marshal($party->get_fax(), 'string'),
+			'email = '          . $this->marshal($party->get_email(), 'string'),
+			'url = '            . $this->marshal($party->get_url(), 'string'),
+			'account_number = ' . $this->marshal($party->get_account_number(), 'string'),
+			'reskontro = '      . $this->marshal($party->get_reskontro(), 'string'),
+			'is_active = '      . $this->marshal(($party->is_active() ? 'true' : 'false'), 'boolean'),
+			'comment = '        . $this->marshal($party->get_comment(), 'string')
 			);
 
-			$result = $this->db->query('UPDATE ' . $this->table_name . ' SET ' . join(',', $values) . " WHERE id=$id", __LINE__,__FILE__);
+        $result = $this->db->query('UPDATE ' . $this->table_name . ' SET ' . join(',', $values) . " WHERE id=$id", __LINE__,__FILE__);
 
-			if(isset($result))
-			{
-				return true;
-			}
-			else
-			{
-				return false;
-			}
+        if(isset($result))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
 	}
 	
 	
@@ -356,5 +536,38 @@ class rental_soparty extends rental_socommon
 		return $parties;
 	}
 
+    protected function get_id_field_name()
+    {
+        return 'party_id';
+    }
+    protected function populate(int $party_id, &$party)
+    {
+        if($party == null) {
+            $party = new rental_party((int) $party_id);
+
+            $party->set_account_number( $this->unmarshal($this->db->f('account_number'), 'string'));
+            $party->set_address_1(      $this->unmarshal($this->db->f('address_1'), 'string'));
+            $party->set_address_2(      $this->unmarshal($this->db->f('address_2'), 'string'));
+            $party->set_agresso_id(     $this->unmarshal($this->db->f('agresso_id'), 'string'));
+            $party->set_comment(        $this->unmarshal($this->db->f('comment'), 'string'));
+            $party->set_company_name(   $this->unmarshal($this->db->f('company_name'), 'string'));
+            $party->set_department(     $this->unmarshal($this->db->f('department'), 'string'));
+            $party->set_email(          $this->unmarshal($this->db->f('email'), 'string'));
+            $party->set_fax(            $this->unmarshal($this->db->f('fax'), 'string'));
+            $party->set_first_name(     $this->unmarshal($this->db->f('first_name'), 'string'));
+            $party->set_is_active(      $this->unmarshal($this->db->f('is_active'), 'boolean'));
+            $party->set_last_name(      $this->unmarshal($this->db->f('last_name'), 'string'));
+            $party->set_location_id(    $this->unmarshal($this->db->f('location_id'), 'int'));
+            $party->set_pid(            $this->unmarshal($this->db->f('personal_identification_number'), 'string'));
+            $party->set_phone(          $this->unmarshal($this->db->f('phone'), 'string'));
+            $party->set_place(          $this->unmarshal($this->db->f('place'), 'string'));
+            $party->set_postal_code(    $this->unmarshal($this->db->f('postal_code'), 'int'));
+            $party->set_reskontro(      $this->unmarshal($this->db->f('reskontro'), 'string'));
+            $party->set_title(          $this->unmarshal($this->db->f('title'), 'string'));
+            $party->set_url(            $this->unmarshal($this->db->f('url'), 'string'));
+        }
+
+        return $party;
+    }
 }
 ?>
