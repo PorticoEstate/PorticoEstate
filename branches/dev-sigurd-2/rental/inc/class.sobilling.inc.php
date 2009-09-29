7153dbf4ef150082f5a2357db22abcb791371eb5
roy.solberg <roy.solberg@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2009-09-29 06:56:19 +0000

    Rental: Refactoring.

--- branches/dev-sigurd-2/rental/inc/class.sobilling.inc.php
+++ branches/dev-sigurd-2/rental/inc/class.sobilling.inc.php
@@ -16,7 +16,53 @@ class rental_sobilling extends rental_socommon
 		return self::$so;
 	}
 	
-	public function add(rental_billing &$billing)
+	protected function get_query(string $sort_field, boolean $ascending, string $search_for, string $search_type, array $filters, boolean $return_count)
+	{
+		$clauses = array('1=1');
+		if(isset($filters[$this->get_id_field_name()]))
+		{
+			$filter_clauses[] = "{$this->marshal($this->get_id_field_name(),'field')} = {$this->marshal($filters[$this->get_id_field_name()],'int')}";
+		}
+		if(count($filter_clauses))
+		{
+			$clauses[] = join(' AND ', $filter_clauses);
+		}
+		$condition =  join(' AND ', $clauses);
+
+		$tables = "rental_billing";
+		$joins = "	{$this->left_join} rental_unit ON (rental_composite.id = rental_unit.composite_id)";
+		if($return_count) // We should only return a count
+		{
+			$cols = 'COUNT(DISTINCT(id)) AS count';
+		}
+		else
+		{
+			$cols = 'id, total_sum, success, timestamp_start, timestamp_stop, location_id, term_id, year, month';
+		}
+		$dir = $ascending ? 'ASC' : 'DESC';
+		$order = $sort_field ? "ORDER BY {$this->marshal($sort_field, 'field')} $dir ": '';
+		return "SELECT {$cols} FROM {$tables} {$joins} WHERE {$condition} {$order}";
+	}
+	
+	protected function populate(int $billing_id, &$billing)
+	{
+		if($billing == null)
+		{
+			$billing = new rental_billing($this->db->f('id', true), $this->db->f('location_id', true), $this->db->f('term_id', true), $this->db->f('year', true), $this->db->f('month', true));
+			$billing->set_success($this->db->f('success', true));
+			$billing->set_total_sum($this->db->f('total_sum', true));
+			$billing->set_timestamp_start($this->db->f('timestamp_start', true));
+			$billing->set_timestamp_stop($this->db->f('timestamp_stop', true));
+		}
+		return $billing;
+	}
+	
+	protected function get_id_field_name()
+	{
+		return 'id';
+	}
+	
+	public function add(&$billing)
 	{
 		$values = array
 		(
@@ -40,7 +86,7 @@ class rental_sobilling extends rental_socommon
 		return $receipt;
 	}
 	
-	public function update(rental_billing &$billing)
+	public function update($billing)
 	{
 		$values = array(
 			'total_sum = ' . $this->marshal($billing->get_total_sum(), 'float'),
@@ -53,30 +99,6 @@ class rental_sobilling extends rental_socommon
 			'month = ' . $this->marshal($billing->get_month(), 'int')
 		);
 		$result = $this->db->query("UPDATE {$this->table_name} SET " . join(',', $values) . " WHERE id={$billing->get_id()}", __LINE__,__FILE__);
-	}
-	
-	/**
-	 * Returns all billing jobs.
-	 * 
-	 * @return rental_billing objects, empty array if noone found, never
-	 * null.
-	 */
-	public function get_billings()
-	{
-		$billings = array();
-		$query = "SELECT " . join(',', array_keys($this->fields)) . " FROM {$this->table_name} ORDER BY timestamp_start DESC";
-		if($this->db->query($query, __LINE__,__FILE__))
-		{
-			while($this->db->next_record()){
-				$billing = new rental_billing($this->db->f('id', true), $this->db->f('location_id', true), $this->db->f('term_id', true), $this->db->f('year', true), $this->db->f('month', true));
-				$billing->set_success($this->db->f('success', true));
-				$billing->set_total_sum($this->db->f('total_sum', true));
-				$billing->set_timestamp_start($this->db->f('timestamp_start', true));
-				$billing->set_timestamp_stop($this->db->f('timestamp_stop', true));
-				$billings[] = $billing;
-			}
-		}
-		return $billings;
 	}
 	
 	/**
