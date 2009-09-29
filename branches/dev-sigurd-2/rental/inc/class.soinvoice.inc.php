3d779b781aa00e036cedf60808b827d499a3b4ca
roy.solberg <roy.solberg@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2009-09-29 14:05:40 +0000

    Rental: WIP with billing.

--- branches/dev-sigurd-2/rental/inc/class.soinvoice.inc.php
+++ branches/dev-sigurd-2/rental/inc/class.soinvoice.inc.php
@@ -25,7 +25,7 @@ class rental_soinvoice extends rental_socommon
 	
 	protected function get_query(string $sort_field, boolean $ascending, string $search_for, string $search_type, array $filters, boolean $return_count)
 	{
-			$clauses = array('1=1');
+		$clauses = array('1=1');
 		if(isset($filters[$this->get_id_field_name()]))
 		{
 			$filter_clauses[] = "{$this->marshal($this->get_id_field_name(),'field')} = {$this->marshal($filters[$this->get_id_field_name()],'int')}";
@@ -60,12 +60,14 @@ class rental_soinvoice extends rental_socommon
 		if($invoice == null)
 		{
 			$invoice = new rental_invoice($this->db->f('id', true), $this->db->f('billing_id', true), $contract_id, $this->db->f('timestamp_created', true), $this->db->f('timestamp_start', true), $this->db->f('timestamp_end', true), $this->db->f('total_sum', true));
+			$invoice->set_party_id($this->unmarshal($this->db->f('party_id'),'int'));
 		}
 		return $invoice;
 	}
 	
 	public function add(&$invoice)
 	{
+		var_dump($invoice);
 		$values = array
 		(
 			$this->marshal($invoice->get_contract_id(), 'int'),
@@ -76,12 +78,12 @@ class rental_soinvoice extends rental_socommon
 			$this->marshal($invoice->get_timestamp_end(), 'int'),
 			$this->marshal($invoice->get_total_sum(), 'float')
 		);
-		$query ="INSERT INTO ".$this->table_name." (" . join(',', array_keys(array_slice($this->fields, 1))) . ") VALUES (" . join(',', $values) . ")";
+		$query ="INSERT INTO rental_invoice(contract_id, billing_id, party_id, timestamp_created, timestamp_start, timestamp_end, total_sum) VALUES (" . join(',', $values) . ")";
 		$receipt = null;
 		if($this->db->query($query))
 		{
 			$receipt = array();
-			$receipt['id'] = $this->db->get_last_insert_id($this->table_name, 'id');
+			$receipt['id'] = $this->db->get_last_insert_id('rental_invoice', 'id');
 			$invoice->set_id($receipt['id']);
 		}
 		return $receipt;
@@ -98,7 +100,7 @@ class rental_soinvoice extends rental_socommon
 			'timestamp_end = '		. $this->marshal($invoice->get_timestamp_end(), 'int'),
 			'total_sum = '			. $this->marshal($invoice->get_total_sum(), 'float')
 		);
-		$result = $this->db->query('UPDATE ' . $this->table_name . ' SET ' . join(',', $values) . " WHERE id=" . $invoice->get_id(), __LINE__,__FILE__);
+		$result = $this->db->query('UPDATE rental_invoice SET ' . join(',', $values) . " WHERE id=" . $invoice->get_id(), __LINE__,__FILE__);
 	}
 	
 }
