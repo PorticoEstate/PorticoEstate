fc9263061a15e6fa3978c9dd19bd50492d118a53
terje.tjervaag <terje.tjervaag@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2009-10-05 16:59:21 +0000

    Rental: More work on Facilit import.

--- branches/dev-sigurd-2/rental/inc/class.uiimport.inc.php
+++ branches/dev-sigurd-2/rental/inc/class.uiimport.inc.php
@@ -41,10 +41,18 @@
 			
 			$row = 1;
 			
+			// TODO: Don't read first line
+			
+			// How to connect objects?  Might be possible to store 
+			// the created rental_ objects in an associative array keyed by their old ID
+			
 			/*
 			 * Read parties
 			 */
+			$parties = array();
+			
 			$handle = fopen($path . "/u_PersonForetak.csv", "r");
+			
 			while (($data = fgetcsv($handle, 1000, ",", "'")) !== false) {
 				
 				$party = new rental_party();
@@ -59,21 +67,24 @@
 					$party->set_first_name($this->decode($data[2]));
 				}
 				
+				// Contact information
 				$party->set_address_1($this->decode($data[3]));
 				$party->set_address_2($this->decode($data[4]));
 				$party->set_postal_code($this->decode($data[5]));
 				$party->set_mobile_phone($this->decode($data[7]));
 				$party->set_phone($this->decode($data[8]));
 				
-				$party->set_company_name($this->decode($data[9]));
-				$party->set_department($this->decode($data[10]));
+				// Company information
+				$party->set_company_name($this->decode($data[10]));
+				$party->set_department($this->decode($data[11]));
 				
-				$party->set_account_number($this->decode($data[12]));
+				// Account number.  Can be a variety of things.  TODO: check this out.  4 digits at least on internal.
+				$party->set_account_number($this->decode($data[14]));
 				
-				$party->set_reskontro($this->decode($data[xx]));
+				$party->set_reskontro($this->decode($data[23]));
 				
 				// Fødselsnr/Foretaksnr/AgressoID
-				$pin = $this->decode($data[23]);
+				$pin = $this->decode($data[24]);
 				if (strlen($pin) == 11) {
 					// Fødselsnummer
 					$party->set_personal_identification_number($pin);
@@ -86,49 +97,138 @@
 					$party->set_agresso_id($pin);
 				}
 				
-				$party->set_comment($this->decode($data[3]));
+				$party->set_comment($this->decode($data[26]));
 				
 				
 				// Store party
-				// rental_soparty::get_instance()->store($party);
+				rental_soparty::get_instance()->store($party);
+				
+				// Add party to collection of parties keyed by its facilit ID so we can refer to it later.
+				$facilit_id = $data[17];
+				$parties[$facilit_id] = $party;
 			}
 			fclose($handle);
 			
 			/*
 			 * Read composites/areas
 			 */
+			$composites = array();
 			$handle = fopen($path . "/u_Leieobjekt.csv", "r");
 			while (($data = fgetcsv($handle, 1000, ",", "'")) !== false) {
 				$composite = new rental_composite();
 				
-				// TODO: What is location id here?
-				$location_id = (int)phpgw::get_var('location_id');
-				// TODO: Have to parse loc1 to xxxx-xx-xx-xx...?
+				// Convert location code to the correct format, xxxx-xx-xx-xx...
 				$loc1 = $this->decode($data[1]);
-				$composite->add_new_unit(new rental_property($loc1, $location_id));
+				$loc1 = $this->format_location_code($loc1);
+				
+				$composite->add_new_unit(new rental_unit(null, -1, $loc1));
 				
 				$composite->set_description($this->decode($data[3]));
 				$composite->set_name($this->decode($data[6]));
 				$composite->set_custom_address_1($this->decode($data[7]));
-				// TODO: Check export, this field doesn't seem to get exported?
-				//$composite->set_custom_address_2($this->decode($data[8]));
-				$composite->set_custom_postcode($this->decode($data[7]));
+				$composite->set_custom_postcode($this->decode($data[8]));
 				
-				$composite->set_is_active($data[20] == "1");
+				$composite->set_is_active($data[19] == "1");
 				
-				// TODO: Store composite
+				// Store composite
+				rental_socomposite::get_instance()->store($composite);
+				
+				// Add composite to collection of composite so we can refer to it later.
+				$composites[$data[0]] = $composite;
 			}
 			fclose($handle);
 			
 			/*
+			 * Read rental object-to-contract link table
+			 */
+			$rentalobject_to_contract = array();
+			$handle = fopen($path . "/u_Leieobjekt_Kontrakt.csv", "r");
+			while (($data = fgetcsv($handle, 1000, ",", "'")) !== false) {
+				// Array keyed by contract id.  Assuming 1-1 even for link table.
+				$rentalobject_to_contract[$data[1]] = $data[0];
+			}
+			
+			/*
 			 * Read contracts
 			 */
+			$contracts = array();
+			
 			$handle = fopen($path . "/u_Kontrakt.csv", "r");
+			
 			while (($data = fgetcsv($handle, 1000, ",", "'")) !== false) {
-				$contract = new rental_contract();				
+				$contract = new rental_contract();
+				
+				// Add rental composite to contract
+				$composite_id = $rentalobject_to_contract[$data[0]];
+				$contract->add_composite($composites[$composite_id]);
+				
+				// TODO: link this with previously imported rental party. 
+				$personId = $this->decode($data[2]);
+				
+				$date_start = $this->decode($data[3]);
+				$date_end = $this->decode($data[4]);
+				
+				$contract->set_contract_date(new rental_contract_date(strtotime($date_start), strtotime($date_end)));
+				
+				$contract->set_old_contract_id($this->decode($data[5]));
+				
+				$term = $data[10];
+				switch ($term) {
+					case 1: // Monthly
+						$contract->set_term_id(1);
+						break;
+					case 2: // Quarterly
+						$contract->set_term_id(4);
+						break;
+					case 3: // Yearly
+						$contract->set_term_id(2);
+						break;
+				}
+				
+				// What period the prices are calculated from.  4=month, 8=year
+				$price_period = $data[14];
+				
+				$contract->set_billing_start_date(strtotime($this->decode($data[16])));
+				
+				// Deres ref.
+				$contract->set_invoice_header($this->decode($data[17]));
+				
+				$contract->set_comment($this->decode($data[18]));
+				
+				// Ansvar/Tjenestested: F.eks: 080400.13000
+				$ansvar_tjeneste = $this->decode($data[26]);
+				$ansvar_tjeneste_components = split(".", $ansvar_tjeneste);
+				$contract->set_responsibility_id($ansvar_tjeneste_components[0]);
+				$contract->set_service_id($ansvar_tjeneste_components[1]);
+				// TODO: Check other types of contracts.  The above is correct for internal
+				
+				// Store contract
+				rental_socontract::get_instance()->store($contract);
+				
+				$contracts[$data[0]] = $contract;
 			}
 			fclose($handle);
 			
+		}
+		
+		protected function format_location_code($value)
+		{
+			$length = strlen($value);
+			$i = 0;
+			
+			$result = array();
+			
+			while ($i < $length) {
+				if ($i == 0) {
+					$result[] = substr($value, $i, 4);
+					$i += 4;
+				} else {
+					$result[] = substr($value, $i, 2);
+					$i += 2;
+				}
+			}
+			
+			return join("-", $result);
 		}
 		
 		protected function decode($value)
