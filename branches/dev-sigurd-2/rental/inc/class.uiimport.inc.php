1ca4645d7132434d203103d460b0e891fda87279
terje.tjervaag <terje.tjervaag@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2009-10-08 08:01:41 +0000

    Rental: Further work on Facilit import.

--- branches/dev-sigurd-2/rental/inc/class.uiimport.inc.php
+++ branches/dev-sigurd-2/rental/inc/class.uiimport.inc.php
@@ -1,6 +1,9 @@
 <?php
 	phpgw::import_class('rental.uicommon');
 	phpgw::import_class('rental.soparty');
+	phpgw::import_class('rental.socomposite');
+	phpgw::import_class('rental.socontract');
+	phpgw::import_class('rental.sounit');
 	
 	include_class('rental', 'contract', 'inc/model/');
 	include_class('rental', 'party', 'inc/model/');
@@ -9,6 +12,20 @@
 
 	class rental_uiimport extends rental_uicommon
 	{
+		const DELIMITER = ",";
+		const ENCLOSING = "'";
+		
+		// List of messages, warnings and errors to be displayed to the user after the import
+		protected $messages;
+		protected $warnings;
+		protected $errors;
+		
+		// File system path to import folder on server
+		protected $path;
+		
+		// Label on the import button. Changes as we step through the import process.
+		protected $import_button_label;
+		
 		public $public_functions = array
 		(
 			'index'	=> true
@@ -27,38 +44,138 @@
 
 		public function index()
 		{
+			// Set the submit button label to its initial state
+			$this->import_button_label = "Start import";
+			
 			$path = phpgw::get_var("facilit_path");
 			if ($path) {
+				$this->path = $path;
+				$messages = array();
+				$warnings = array();
+				$errors = array();
+				setlocale(LC_ALL, 'no_NO');
 				$result = $this->import($path);
 			}
-			$this->render('facilit_import.php', array('importresult', $result));
-			$this->render('permission_denied.php',array('error' => lang('permission_denied_view_contract')));
+			
+			$this->render('facilit_import.php', array(
+				'messages' => $this->messages,
+				'warnings' => $this->warnings,
+				'errors' => $this->errors, 
+				'button_label' => $this->import_button_label));
 		}
 		
-		public function import($path)
+		/**
+		 * Import Facilit data to Portico Estate's rental module
+		 * The function assumes CSV files have been uploaded to a location on the server reachable by the
+		 * web server user.  The CSV files must correspond to the table names from Facilit, as exported
+		 * from Access.  Field should be enclosed in single quotes and separated by comma.  The CSV files
+		 * must contain the column headers on the first line.
+		 * 
+		 * @return unknown_type
+		 */
+		public function import()
 		{
-			setlocale(LC_ALL, 'no_NO');
-			
-			$row = 1;
-			
 			// TODO: Don't read first line
 			
-			// How to connect objects?  Might be possible to store 
-			// the created rental_ objects in an associative array keyed by their old ID
+			// TODO: Fill error messages in sub functions
 			
-			/*
-			 * Read parties
-			 */
+			// Import rental parties
+			if (!phpgwapi_cache::session_get('rental', 'facilit_parties')) {
+				phpgwapi_cache::session_set('rental', 'facilit_parties', $this->import_parties());
+				$this->import_button_label = "Continue to import composites";
+				return;
+			}
+			
+			// Import composites and units
+			if (!phpgwapi_cache::session_get('rental', 'facilit_composites')) {
+				phpgwapi_cache::session_set('rental', 'facilit_composites', $this->import_composites());
+				$this->import_button_label = "Continue to import composite-to-contract link table";
+				return;
+			}
+			
+			// Import composite to contract link table.  Assumes 1-1 link.
+			if (!phpgwapi_cache::session_get('rental', 'facilit_rentalobject_to_contract')) {
+				phpgwapi_cache::session_set('rental', 'facilit_rentalobject_to_contract', $this->import_rentalobject_to_contract());
+				$this->import_button_label = "Continue to import contracts";
+				return;
+			}
+			
+			// Import contracts
+			if (!phpgwapi_cache::session_get('rental', 'facilit_contracts')) {
+				$composites = phpgwapi_cache::session_get('rental', 'facilit_composites');
+				phpgwapi_cache::session_set('rental', 'facilit_contracts', $this->import_contracts($composites));
+				$this->import_button_label = "Continue to import price items";
+				return;
+			}
+			
+			// Import price items
+			if (!phpgwapi_cache::session_get('rental', 'facilit_price_items')) {
+				$contracts = phpgwapi_cache::session_get('rental', 'facilit_contracts');
+				phpgwapi_cache::session_set('rental', 'facilit_price_items', $this->import_price_items($contracts));
+				$this->import_button_label = "Import done"; // Not really - events will be after this
+				return;
+			}
+			
+			// We're done with the import, so clear all session variables so we're ready for a new one
+			phpgwapi_cache::session_clear('rental', 'facilit_parties');
+			phpgwapi_cache::session_clear('rental', 'facilit_composites');
+			phpgwapi_cache::session_clear('rental', 'facilit_rentalobject_to_contract');
+			phpgwapi_cache::session_clear('rental', 'facilit_contracts');
+			phpgwapi_cache::session_clear('rental', 'facilit_price_items');
+		}
+		
+		protected function import_price_items($contracts)
+		{
+			// Read priselementdetaljkontrakt list first so we can create our complete price items in the next loop
+			// This is an array keyed by the main price item ID
+			$detail_price_item = array();
+			
+			$handle = fopen($path . "/u_PriselementDetaljKontrakt.csv", "r");
+			
+			// Read the first line to get the headers out of the way
+			$this->getcsv($handle);
+			
+			while (($data = $this->getcsv($handle)) !== false) {
+				$detail_price_item[$data[1]] = array(
+					'price' => $data[2],
+					'amount' => $data[3],
+					'date_start' => strtotime($this->decode($data[4])),
+					'date_end' => strtotime($this->decode($data[5]))
+				);
+			}
+			
+			fclose($handle);
+		
+			$handle = fopen($path . "/u_PrisElementKontrakt.csv", "r");
+			
+			// Read the first line to get the headers out of the way
+			$this->getcsv($handle);
+			
+			while (($data = $this->getcsv($handle)) !== false) {
+				// Create new admin price item if one doesn't exist in the admin price list
+				// Add new price item to contract with correct reference from the $contracts array
+				// Remember fields from detail price item.
+			}
+			
+			fclose($handle);
+		}
+		
+		protected function import_parties()
+		{
 			$parties = array();
 			
-			$handle = fopen($path . "/u_PersonForetak.csv", "r");
+			// Open the Facilit file containing rental parties
+			$handle = fopen($this->path . "/u_PersonForetak.csv", "r");
 			
-			while (($data = fgetcsv($handle, 1000, ",", "'")) !== false) {
-				
+			// Read the first line to get the headers out of the way
+			$this->getcsv($handle);
+			
+			$counter = 1;
+			// Loop through each line of the file, parsing CSV data to a php array
+			while (($data = $this->getcsv($handle)) !== false) {
+				// Create a new rental party we can fill with info from this line from the file
 				$party = new rental_party();
-				
-				// TODO: Where is the PersonForetak_id in the export?
-				
+
 				// Fill in first/last name if applicable, otherwise use Foretaksnavn
 				if ($this->is_null($data[0])) {
 					$party->set_first_name($this->decode($data[0]));
@@ -100,28 +217,34 @@
 				$party->set_comment($this->decode($data[26]));
 				
 				
-				// Store party
-				rental_soparty::get_instance()->store($party);
-				
-				// Add party to collection of parties keyed by its facilit ID so we can refer to it later.
-				$facilit_id = $data[17];
-				$parties[$facilit_id] = $party;
+				// Store party and log message
+				if (rental_soparty::get_instance()->store($party)) {
+					// Add party to collection of parties keyed by its facilit ID so we can refer to it later.
+					$facilit_id = $data[17];
+					$parties[$facilit_id] = $party->get_id();
+					
+					$this->messages[] = "Successfully added party " . $party->get_last_name() . ", " . $party->get_first_name() . " (" . $party->get_id() . ")";
+				} else {
+					$this->errors[] = "Failed to store party " . $party->get_last_name() . ", " . $party->get_first_name();
+				}
 			}
+			
 			fclose($handle);
 			
-			/*
-			 * Read composites/areas
-			 */
+			return $parties;
+		}
+		
+		protected function import_composites()
+		{
 			$composites = array();
-			$handle = fopen($path . "/u_Leieobjekt.csv", "r");
-			while (($data = fgetcsv($handle, 1000, ",", "'")) !== false) {
+			
+			$handle = fopen($this->path . "/u_Leieobjekt.csv", "r");
+			
+			// Read the first line to get the headers out of the way
+			$this->getcsv($handle);
+			
+			while (($data = $this->getcsv($handle)) !== false) {
 				$composite = new rental_composite();
-				
-				// Convert location code to the correct format, xxxx-xx-xx-xx...
-				$loc1 = $this->decode($data[1]);
-				$loc1 = $this->format_location_code($loc1);
-				
-				$composite->add_new_unit(new rental_unit(null, -1, $loc1));
 				
 				$composite->set_description($this->decode($data[3]));
 				$composite->set_name($this->decode($data[6]));
@@ -131,37 +254,62 @@
 				$composite->set_is_active($data[19] == "1");
 				
 				// Store composite
-				rental_socomposite::get_instance()->store($composite);
-				
-				// Add composite to collection of composite so we can refer to it later.
-				$composites[$data[0]] = $composite;
+				if (rental_socomposite::get_instance()->store($composite)) {
+					// Convert location code to the correct format, xxxx-xx-xx-xx...
+					$loc1 = $this->decode($data[1]);
+					$loc1 = $this->format_location_code($loc1);
+					echo 'Adding unit with location code ' . $loc1 . '\n';
+					// Add units only if composite stored ok.
+					rental_sounit::get_instance()->add(new rental_unit(null, $composite->get_id(), $loc1));
+					
+					// Add composite to collection of composite so we can refer to it later.
+					$composites[$data[0]] = $composite->get_id();
+					
+					$this->messages[] = "Successfully added composite " . $composite->get_name() . " (" . $composite->get_id() . ")";
+				} else {
+					$this->errors[] = "Failed to store composite " . $composite->get_name();
+				}
 			}
+			
 			fclose($handle);
 			
-			/*
-			 * Read rental object-to-contract link table
-			 */
+			return $composites;
+		}
+		
+		protected function import_rentalobject_to_contract()
+		{
 			$rentalobject_to_contract = array();
-			$handle = fopen($path . "/u_Leieobjekt_Kontrakt.csv", "r");
-			while (($data = fgetcsv($handle, 1000, ",", "'")) !== false) {
+			
+			$handle = fopen($this->path . "/u_Leieobjekt_Kontrakt.csv", "r");
+			
+			// Read the first line to get the headers out of the way
+			$this->getcsv($handle);
+			
+			$first_line = fgetcsv($handle, 0, ",", "'");
+			
+			while (($data = $this->getcsv($handle)) !== false) {
 				// Array keyed by contract id.  Assuming 1-1 even for link table.
 				$rentalobject_to_contract[$data[1]] = $data[0];
 			}
+			
 			fclose($handle);
 			
-			/*
-			 * Read contracts
-			 */
+			$this->messages[] = "Successfully imported " . count($rentalobject_to_contract) . " contract links";
+			
+			return $rentalobject_to_contract;
+		}
+		
+		protected function import_contracts($composites)
+		{
 			$contracts = array();
 			
-			$handle = fopen($path . "/u_Kontrakt.csv", "r");
+			$handle = fopen($this->path . "/u_Kontrakt.csv", "r");
 			
-			while (($data = fgetcsv($handle, 1000, ",", "'")) !== false) {
+			// Read the first line to get the headers out of the way
+			$this->getcsv($handle);
+			
+			while (($data = $this->getcsv($handle)) !== false) {
 				$contract = new rental_contract();
-				
-				// Add rental composite to contract
-				$composite_id = $rentalobject_to_contract[$data[0]];
-				$contract->add_composite($composites[$composite_id]);
 				
 				// TODO: link this with previously imported rental party. 
 				$personId = $this->decode($data[2]);
@@ -188,6 +336,11 @@
 				
 				// What period the prices are calculated from.  4=month, 8=year
 				$price_period = $data[14];
+				if ($price_period == 4) {
+					// The price period is month.  We ignore this but print a warning.
+					// TODO: What to use as reference here?  Currently using K-number
+					$this->warnings[] = "Price period of contract " . $this->decode($data[5]) . " is month.  Ignored.";
+				}
 				
 				$contract->set_billing_start_date(strtotime($this->decode($data[16])));
 				
@@ -203,25 +356,47 @@
 				$contract->set_service_id($ansvar_tjeneste_components[1]);
 				// TODO: Check other types of contracts.  The above is correct for internal
 				
-				// Store contract
-				rental_socontract::get_instance()->store($contract);
+				// Set the location ID according to what the user selected
+				$contract->set_location_id(phpgw::get_var("location_id"));
 				
-				$contracts[$data[0]] = $contract;
+				// Store contract
+				if (rental_socontract::get_instance()->store($contract)) {
+					$contracts[$data[0]] = $contract->get_id();
+					
+					// Add rental composite to contract
+					//$composite_id = $rentalobject_to_contract[$data[0]];
+					//$composite = new rental_composite($composites[$composite_id]);
+					$contract->add_composite($contract->get_id(), $rentalobject_to_contract[$data[0]]);	
+			
+					$this->messages[] = "Successfully added contract for property " . $contract->get_composite_name() . " (" . $contract->get_id() . ")";
+				} else {
+					$this->errors[] = "Failed to store contract " . $this->decode($data[5]);
+				}
 			}
+			
 			fclose($handle);
 			
-			/*
-			 * Price items
-			 */
-			$handle = fopen($path . "/u_PriselementKontrakt.csv", "r");
-			while (($data = fgetcsv($handle, 1000, ",", "'")) !== false) {
-				// TODO: Have to combine this with PrisElementDetaljKontrakt
-				
-				// Create new price item if one doesn't exist in the admin price list
-			}
-			
+			return $contracts;
 		}
 		
+		/**
+		 * Read the next line from the given file handle and parse it to CSV according to the rules set up
+		 * in the class constants DELIMITER and ENCLOSING.  Returns FALSE like getcsv on EOF.
+		 * 
+		 * @param file-handle $handle
+		 * @return array of values from the parsed csv line
+		 */
+		protected function getcsv($handle)
+		{
+			return fgetcsv($handle, 1000, self::DELIMITER, self::ENCLOSING);
+		}
+		
+		/**
+		 * Format a given location code according to the rule xxxx-xx-xx-xx...
+		 * 
+		 * @param string $value A location code as a continuous string without - delimiters
+		 * @return string The formatted location code
+		 */
 		protected function format_location_code($value)
 		{
 			$length = strlen($value);
@@ -231,9 +406,11 @@
 			
 			while ($i < $length) {
 				if ($i == 0) {
+					// The four first characters should be in one group
 					$result[] = substr($value, $i, 4);
 					$i += 4;
 				} else {
+					// .. after that it's all 2 characters per group
 					$result[] = substr($value, $i, 2);
 					$i += 2;
 				}
@@ -242,19 +419,34 @@
 			return join("-", $result);
 		}
 		
+		/**
+		 * Convert from the locale encoding to UTF-8 encoding and escape single quotes
+		 * 
+		 * @param string $value The value to convert
+		 * @return string
+		 */
 		protected function decode($value)
 		{
 			$converted = mb_convert_encoding($value, 'UTF-8');
-			if ($converted == "<NULL>") {
+			if ($this->is_null(trim($converted))) {
 				return null;
 			}
 			
-			return $converted;
+			// Escape single quotes
+			// TODO: This is a DB problem and doesn't belong here
+			return str_replace("'", "\'", $converted);
 		}
 		
+		/**
+		 * Test a value for null according to several formats that can exist in the export.
+		 * Returns true if the value is null according to these rules, false otherwise.
+		 * 
+		 * @param string $value The value to test
+		 * @return bool
+		 */
 		protected function is_null($value)
 		{
-			return (trim($data[0]) != "" && $data != "<NULL>");
+			return (trim($data[0]) != "" && $data != "<NULL>" && $data != "''");
 		}
 	}
 ?>
--- branches/dev-sigurd-2/rental/templates/base/facilit_import.php
+++ branches/dev-sigurd-2/rental/templates/base/facilit_import.php
@@ -13,5 +13,59 @@
 
 <form action="index.php?menuaction=rental.uiimport.index" method="post">
 	<label for="path">Path to facilit dump:</label> <input type="text" name="facilit_path" id="facilit_path" value="/home/notroot/FacilitExport" /><br />
-	<input type="submit" value="Start import" />
+	<label for="path">Location for the imported contracts:</label>
+	<select name="location_id" id="location_id">
+		<?php
+		$types = rental_socontract::get_instance()->get_fields_of_responsibility();
+		foreach($types as $id => $label)
+		{
+
+			$names = $this->locations->get_name($id);
+			if($names['appname'] == $GLOBALS['phpgw_info']['flags']['currentapp'])
+			{
+				if($this->hasPermissionOn($names['location'],PHPGW_ACL_ADD))
+				{
+				?>
+					<option 
+						value="<?php echo $id ?>"
+					>
+						<?php echo lang($label) ?>
+					</option>
+				<?php
+				}
+			}
+		}
+		?>
+	</select><br />
+	<input type="submit" value="<?php echo $button_label; ?>" />
+	
+	<?php if ($messages) { ?>
+	<ul class="errors">
+	<?php
+		foreach ($messages as $message) {
+			echo '<li>' . $message . '</li>';
+		}
+	?>
+	</ul>
+	<?php } ?>
+	
+	<?php if ($warnings) { ?>
+	<ul class="warnings">
+	<?php
+		foreach ($warnings as $warning) {
+			echo '<li>' . $warning . '</li>';
+		}
+	?>
+	</ul>
+	<?php } ?>
+	
+	<?php if ($errors) { ?>
+	<ul class="errors">
+	<?php
+		foreach ($errors as $error) {
+			echo '<li>' . $error . '</li>';
+		}
+	?>
+	</ul>
+	<?php } ?>
 </form>
