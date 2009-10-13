9fb1b1d1866128bac32e23047e0d0ed3f5a1687e
roy.solberg <roy.solberg@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2009-10-13 19:44:29 +0000

    Rental: Fixed sql typo.

--- branches/dev-sigurd-2/rental/inc/class.socontract.inc.php
+++ branches/dev-sigurd-2/rental/inc/class.socontract.inc.php
@@ -197,7 +197,7 @@ class rental_socontract extends rental_socommon
 			$filter_clauses[] = "billing_start <= {$timestamp_end}";
 			
 			$specific_ordering = 'invoice.timestamp_end DESC, contract.billing_start DESC, contract.date_start DESC, contract.date_end DESC';
-			$order = $order ? $order.' '.$specific_ordering : "ORDER BY {$specific_ordering}";
+			$order = $order ? $order.', '.$specific_ordering : "ORDER BY {$specific_ordering}";
 		}
 		
 		if(count($filter_clauses))
