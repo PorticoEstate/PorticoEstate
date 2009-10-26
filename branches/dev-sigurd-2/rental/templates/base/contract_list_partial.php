4037bf7070809152372b4d57848be8bba0001fba
espen.liland <espen.liland@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2009-10-26 09:10:32 +0000

    Rental: Added change listener to date field on contract list.

--- branches/dev-sigurd-2/rental/templates/base/contract_list_partial.php
+++ branches/dev-sigurd-2/rental/templates/base/contract_list_partial.php
@@ -172,3 +172,18 @@
 
 <div id="<?php echo $list_id ?>_container" class="datatable_container"></div>
 <div id="<?php echo $list_id ?>_paginator" class="paginator"></div>
+
+<script type="text/javascript">
+var datestatus = document.getElementById('date_status');
+if(datestatus.addEventListener) {
+    datestatus.addEventListener('change', date_event, false);
+}
+
+function date_event() {
+    var hidden_date = document.getElementById('date_status_hidden');
+    if(hidden_date != undefined) {
+        var date = datestatus.value.split("/");
+        hidden_date.value = date[2]+"-"+date[1]+"-"+date[0];
+    }
+}
+</script>
