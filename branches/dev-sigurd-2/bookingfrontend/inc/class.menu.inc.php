7d35044c1dbd4583980a22130a756b905e9e7bdd
sigurd <sigurd@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2010-01-24 12:14:21 +0000

    More on per application admin

--- branches/dev-sigurd-2/bookingfrontend/inc/class.menu.inc.php
+++ branches/dev-sigurd-2/bookingfrontend/inc/class.menu.inc.php
@@ -8,7 +8,8 @@
 
 			$menus = array();
 
-			if ( isset($GLOBALS['phpgw_info']['user']['apps']['admin']) )
+			if ( $GLOBALS['phpgw']->acl->check('run', phpgwapi_acl::READ, 'admin')
+			|| $GLOBALS['phpgw']->acl->check('admin', phpgwapi_acl::ADD, 'bookingfrontend'))
 			{
 				$menus['admin'] = array
 				(
