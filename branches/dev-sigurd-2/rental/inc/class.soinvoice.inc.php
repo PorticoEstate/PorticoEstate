be198a00822b99a51c0c98ef61b754b54b55cce9
yngve.espelid <yngve.espelid@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2010-01-27 08:47:01 +0000

    Rental: do not show invoices if the the billing run is deleted

--- branches/dev-sigurd-2/rental/inc/class.soinvoice.inc.php
+++ branches/dev-sigurd-2/rental/inc/class.soinvoice.inc.php
@@ -33,6 +33,7 @@ class rental_soinvoice extends rental_socommon
 		if(isset($filters['contract_id']))
 		{
 			$filter_clauses[] = "rental_invoice.contract_id = {$this->marshal($filters['contract_id'],'int')}";
+			$filter_clauses[] = "rental_billing.deleted IS FALSE";
 		}
 		if(isset($filters['billing_id']))
 		{
@@ -50,6 +51,7 @@ class rental_soinvoice extends rental_socommon
 		$joins .= "	{$this->left_join} rental_contract_party ON (rental_contract_party.contract_id = rental_invoice.contract_id)";
 		$joins .= "	{$this->left_join} rental_party party ON (rental_contract_party.party_id = party.id)";
 		$joins .= " {$this->left_join} rental_contract contract ON (contract.id = rental_invoice.contract_id)";
+		$joins .= " {$this->left_join} rental_billing ON (rental_billing.id = rental_invoice.billing_id)";
 		$order = '';
 		if($return_count) // We should only return a count
 		{
@@ -57,7 +59,7 @@ class rental_soinvoice extends rental_socommon
 		}
 		else
 		{
-			$cols = 'rental_invoice.id, rental_invoice.contract_id, billing_id, rental_invoice.party_id, timestamp_created, timestamp_start, timestamp_end, total_sum, total_area, header, rental_invoice.account_in, rental_invoice.account_out, rental_invoice.service_id, rental_invoice.responsibility_id, rental_invoice.project_id, rental_composite.name AS composite_name, party.identifier AS party_identifier, party.first_name AS party_first_name, party.last_name AS party_last_name, party.title AS party_title, party.company_name AS party_company_name, party.department AS party_department, party.address_1 AS party_address_1, party.address_2 AS party_address_2, party.postal_code AS party_postal_code, party.place AS party_postal_code, party.phone AS party_phone, party.mobile_phone AS party_mobile_phone, party.fax AS party_fax, party.email AS party_email, party.url AS party_url, party.account_number AS party_account_number, party.reskontro AS party_reskontro, party.location_id AS party_location_id, party.is_inactive as party_in_active, contract.old_contract_id';
+			$cols = 'rental_invoice.id, rental_invoice.contract_id, billing_id, rental_invoice.party_id, timestamp_created, rental_invoice.timestamp_start, timestamp_end, rental_invoice.total_sum, total_area, header, rental_invoice.account_in, rental_invoice.account_out, rental_invoice.service_id, rental_invoice.responsibility_id, rental_invoice.project_id, rental_composite.name AS composite_name, party.identifier AS party_identifier, party.first_name AS party_first_name, party.last_name AS party_last_name, party.title AS party_title, party.company_name AS party_company_name, party.department AS party_department, party.address_1 AS party_address_1, party.address_2 AS party_address_2, party.postal_code AS party_postal_code, party.place AS party_postal_code, party.phone AS party_phone, party.mobile_phone AS party_mobile_phone, party.fax AS party_fax, party.email AS party_email, party.url AS party_url, party.account_number AS party_account_number, party.reskontro AS party_reskontro, party.location_id AS party_location_id, party.is_inactive as party_in_active, contract.old_contract_id';
 			$dir = $ascending ? 'ASC' : 'DESC';
 			if($sort_field == null || $sort_field == '') // Sort field no set
 			{
