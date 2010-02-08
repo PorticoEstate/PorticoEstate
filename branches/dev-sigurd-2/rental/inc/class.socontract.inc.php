29aca104d921a771ba188fd892592fb4702bb069
yngve.espelid <yngve.espelid@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2010-02-08 07:45:54 +0000

    Rental: update contracts for billing query to collect contracts with no billing start

--- branches/dev-sigurd-2/rental/inc/class.socontract.inc.php
+++ branches/dev-sigurd-2/rental/inc/class.socontract.inc.php
@@ -223,7 +223,7 @@ class rental_socontract extends rental_socommon
 			$filter_clauses[] = "contract.term_id = {$billing_term_id}";
 			$filter_clauses[] = "contract.date_start < $timestamp_end";
 			$filter_clauses[] = "(contract.date_end IS NULL OR contract.date_end >= {$timestamp_start})";
-			$filter_clauses[] = "contract.billing_start < {$timestamp_end}";
+			$filter_clauses[] = "(contract.billing_start IS NULL OR contract.billing_start < {$timestamp_end})";
 			
 			$specific_ordering = 'invoice.timestamp_end DESC, contract.billing_start DESC, contract.date_start DESC, contract.date_end DESC';
 			$order = $order ? $order.', '.$specific_ordering : "ORDER BY {$specific_ordering}";
