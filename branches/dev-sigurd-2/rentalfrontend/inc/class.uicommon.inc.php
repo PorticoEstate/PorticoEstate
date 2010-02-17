c3020b808b7953cb07ac340a99f2711973d07ef8
espen.liland <espen.liland@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2010-02-17 14:46:09 +0000

    rentalfrontend: Cleaned up render_template()

--- branches/dev-sigurd-2/rentalfrontend/inc/class.uicommon.inc.php
+++ branches/dev-sigurd-2/rentalfrontend/inc/class.uicommon.inc.php
@@ -10,25 +10,7 @@
          */
         public static function render_template($tpl, $data)
 		{
-            $tmpl_search_path = array();
-            $tmpl_search_path[] = PHPGW_SERVER_ROOT . '/phpgwapi/templates/base';
-            $tmpl_search_path[] = PHPGW_SERVER_ROOT . '/phpgwapi/templates/' . $GLOBALS['phpgw_info']['server']['template_set'];
-            $tmpl_search_path[] = PHPGW_SERVER_ROOT . '/' . $GLOBALS['phpgw_info']['flags']['currentapp'] . '/templates/base';
-
-			$GLOBALS['phpgw']->xslttpl->set_output('html');
-
-            // Loop through search paths
-            foreach($tmpl_search_path as $path) {
-                $filename = $path . '/' . $tpl . '.xsl';
-
-                // If found, apply to xslfiles register and break loop
-                if (file_exists($filename))
-                {
-                    $GLOBALS['phpgw']->xslttpl->xslfiles[$tpl] = $filename;
-                    break;
-                }
-            }
-            
+            $GLOBALS['phpgw']->xslttpl->add_file(array($tpl.'.xsl'));
 			$GLOBALS['phpgw']->xslttpl->set_var('phpgw', array('data' => $data));
 		}
     }
