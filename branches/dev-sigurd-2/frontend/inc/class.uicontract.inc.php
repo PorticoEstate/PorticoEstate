9e0c5ce12b4e8bfd2af284b6d3fc327ba6a1d0cd
yngve.espelid <yngve.espelid@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2010-03-17 08:27:40 +0000

    Rental: contract view

--- branches/dev-sigurd-2/frontend/inc/class.uicontract.inc.php
+++ branches/dev-sigurd-2/frontend/inc/class.uicontract.inc.php
@@ -8,8 +8,7 @@ class frontend_uicontract extends frontend_uifrontend
 {
 
 	public $public_functions = array(
-            'index'     => true,
-            'show'      => true
+            'index'     => true
 	);
 
 	public function __construct()
@@ -31,191 +30,61 @@ class frontend_uicontract extends frontend_uifrontend
 		// Holds the contract object (for use in this function only), if any
 		$contract = null;
 
+		// Request parameter: the user wants to view details about anther contract
 		$new_contract = phpgw::get_var('contract_id');
+
+		// The current state of the contract view of this user's session
 		$this->contract_state = phpgwapi_cache::session_get('frontend', 'contract_state');
 
+		// If the user visits the contract tab for the first time...
 		if(!isset($this->contract_state))
 		{
+			//... retrieve the first contract bound to the location (e.g. building)
 			$contract = frontend_borental::get_first_contract_per_location($this->header_state['selected']);
-            if(is_object($contract))
-            {
-                $this->contract_state['selected'] = $contract->get_id();
-            }
+			if(is_object($contract))
+			{
+				//... and set this contract as selected
+				$this->contract_state['selected'] = $contract->get_id();
+			}
+
+			//... then store this contract on the session
 			$this->contract_state['contract'] = $contract;
 			phpgwapi_cache::session_set('frontend', 'contract_state', $this->contract_state);
 		}
-			
+
+		// If the user wants to view another contract connected to this location
 		if(isset($new_contract))
 		{
-			//check to see if contract exist
+			//... first check to see if contract exist
 			$exist = frontend_borental::contract_exist_per_location($new_contract,$this->header_state['selected']);
 			if($exist)
 			{
+				// ... and if it exist set the identifier as selected and update the contract session state
 				$this->contract_state['selected'] = $new_contract;
 				$this->contract = rental_socontract::get_instance()->get_single($new_contract);
 				$this->contract_state['contract'] = $this->contract;
 				phpgwapi_cache::session_set('frontend', 'contract_state', $this->contract_state);
 			}
 		}
-
-		//prepare datatable
-		$datatable['config']['allow_allrows'] = true;
-		$datatable['config']['base_java_url'] = "menuaction:'frontend.uicontract.view'";
 		
 		$contracts_per_location = phpgwapi_cache::session_get('frontend', 'contracts_per_location');
-		
-		$j = 0;
+		$contract_for_selection = array();
 		foreach($contracts_per_location[$this->header_state['selected']] as $contract)
 		{
-			
-				$datatable['rows']['row'][$j]['column'][0]['name']		= 'id';
-				$datatable['rows']['row'][$j]['column'][0]['value']	= $contract->get_id();
-
-				$datatable['rows']['row'][$j]['column'][1]['name']		= 'old_contract_id';
-				$datatable['rows']['row'][$j]['column'][1]['value']	= $contract->get_old_contract_id();	
-				
-				$datatable['rows']['row'][$j]['column'][2]['name']		= 'status';
-				$datatable['rows']['row'][$j]['column'][2]['value']	= $contract->get_contract_status();	
-				$j++;
+			$contract_for_selection[] = $contract->serialize();
 		}
-		
-		 $parameters = array
-                (
-                'parameter' => array
-                (
-                    array
-                    (
-                        'name'		=> 'contract_id',
-                        'source'	=> 'id'
-                    ),
-                )
-            );
 
-            $datatable['rowactions']['action'][] = array(
-                'my_name' 			=> 'view',
-                'statustext' 	=> lang('view the contract'),
-                'text'			=> lang('view'),
-                'action'		=> $GLOBALS['phpgw']->link('/index.php',array
-                (
-                'menuaction'	=> 'frontend.uicontract.index'
-                )),
-                'parameters'	=> $parameters
-            );
-            
-            $datatable['headers']['header'][0]['formatter'] 	= "''";
-            $datatable['headers']['header'][0]['visible'] 	= false;
-            $datatable['headers']['header'][0]['name'] 		= 'id';
-            $datatable['headers']['header'][0]['sortable']	= false;
-            
-            $datatable['headers']['header'][1]['formatter'] 	= "''";
-             $datatable['headers']['header'][1]['visible'] 	= true;
-            $datatable['headers']['header'][1]['name'] 		= 'old_contract_id';
-            $datatable['headers']['header'][1]['text'] 		= lang('old_contract_id');
-            $datatable['headers']['header'][1]['sortable']	= false;
-            
-            $datatable['headers']['header'][2]['formatter'] 	= "''";
-             $datatable['headers']['header'][2]['visible'] 	= true;
-            $datatable['headers']['header'][2]['name'] 		= 'status';
-            $datatable['headers']['header'][2]['text'] 		= lang('old_contract_id');
-            $datatable['headers']['header'][1]['sortable']	= false;
-            
-//path for property.js
-            $datatable['property_js'] = $GLOBALS['phpgw_info']['server']['webserver_url']."/property/js/yahoo/property.js";
-            
-	// Pagination and sort values
-            $datatable['pagination']['records_start'] 	= (int)$bo->start;
-            $datatable['pagination']['records_limit'] 	= $GLOBALS['phpgw_info']['user']['preferences']['common']['maxmatchs'];
-            if($dry_run)
-            {
-                $datatable['pagination']['records_returned'] = $GLOBALS['phpgw_info']['user']['preferences']['common']['maxmatchs'];
-            }
-            else
-            {
-                $datatable['pagination']['records_returned']= count($contracts_per_location[$this->header_state['selected']]);
-            }
-            $datatable['pagination']['records_total'] 	= count($contracts_per_location[$this->header_state['selected']]);
-
-            $datatable['sorting']['order'] 	= phpgw::get_var('order', 'string'); // Column
-
-            $appname		= lang('contracts');
-            $function_msg	= lang('list contract');
-
-           
-                $datatable['sorting']['order']			= phpgw::get_var('order', 'string'); // name of column of Database
-                $datatable['sorting']['sort'] 			= phpgw::get_var('sort', 'string'); // ASC / DESC
-            
-
-        //-- BEGIN----------------------------- JSON CODE ------------------------------
-            //values for Pagination
-            $json = array(
-                'recordsReturned' 	=> $datatable['pagination']['records_returned'],
-                'totalRecords' 		=> (int)$datatable['pagination']['records_total'],
-                'startIndex' 		=> $datatable['pagination']['records_start'],
-                'sort'				=> $datatable['sorting']['order'],
-                'dir'				=> $datatable['sorting']['sort'],
-                'records'			=> array()
-            );
-
-            // values for datatable
-            if(is_array($datatable['rows']['row']))
-            {
-                foreach( $datatable['rows']['row'] as $row )
-                {
-                    $json_row = array();
-                    foreach( $row['column'] as $column)
-                    {
-                        $json_row[$column['name']] = $column['value'];
-                    }
-                    $json['records'][] = $json_row;
-                }
-            }
-
-            // right in datatable
-            if(is_array($datatable['rowactions']['action']))
-            {
-                $json['rights'] = $datatable['rowactions']['action'];
-            }
-
-            if( phpgw::get_var('phpgw_return_as') == 'json' )
-            {
-                return $json;
-            }
-
-
-            $datatable['json_data'] = json_encode($json);
-//-------------------- JSON CODE ----------------------
-            
-            
 		$data = array
-		(
-		//'msgbox_data'   => $GLOBALS['phpgw']->common->msgbox($GLOBALS['phpgw']->common->msgbox_data($msglog)),
+                (
+                //'msgbox_data'   => $GLOBALS['phpgw']->common->msgbox($GLOBALS['phpgw']->common->msgbox_data($msglog)),
 				'header' =>$this->header_state,
 				'tabs' => $this->tabs,
-				'contracts'      => array('datatable' => $datatable)
-				//'contract'		=> $this->contract_state['contract']->serialize()
-		);
-			
+                'selected_contract' =>  $this->contract_state['selected'],
+				'contracts'      => array('select' => $contract_for_selection),
+                'contract'		=> $this->contract_state['contract']->serialize()
+                );
+                	
 		$GLOBALS['phpgw']->xslttpl->set_var('phpgw',array('app_data' => $data));
 		$GLOBALS['phpgw']->xslttpl->add_file(array('frontend','contract','datatable'));
-	}
-
-	/**
-	 * TODO
-	 */
-	public function show()
-	{
-		$sotts = CreateObject('property.botts');
-
-		/*$json = $sotts->read(array(
-		 'query' => '1101-01-01-01-105'
-
-		 ));*/
-		$sotts->query = '1101-01-01-01-105';
-		$json = $sotts->read();
-
-		print_r($json);
-
-
-
 	}
 }
--- branches/dev-sigurd-2/frontend/templates/base/contract.xsl
+++ branches/dev-sigurd-2/frontend/templates/base/contract.xsl
@@ -1,16 +1,38 @@
 <xsl:template match="contracts" xmlns:php="http://php.net/xsl">
-    <table cellpadding="2" cellspacing="2" width="95%" align="center">
-        <xsl:choose>
-            <xsl:when test="msgbox_data != ''">
-                <tr>
-                    <td align="left" colspan="3">
-                        <xsl:call-template name="msgbox"/>
-                    </td>
-                </tr>
-            </xsl:when>
-        </xsl:choose>
-    </table>
-
-    <xsl:apply-templates select="datatable" />
+    <xsl:variable name="form_action"><xsl:value-of select="form_action"/></xsl:variable>
+    <div class="yui-navset" id="ticket_tabview">
+        <xsl:value-of disable-output-escaping="yes" select="tabs" />
+        <div class="yui-content">
+            <div class="toolbar-container">
+                <div class="toolbar">
+                    <xsl:apply-templates select="datatable/actions" />
+                </div>
+            </div>
+        </div>
+    </div>
+	<div id="contract_selector">
+        <form action="index.php?menuaction=frontend.uicontract.index" method="post">
+            <label>Velg kontrakt</label>
+            <br/>
+            <select name="contract_id" size="7" onchange="this.form.submit();">
+            	
+            	<xsl:for-each select="child::*">
+            		<xsl:choose>
+            			<xsl:when test="id = //selected_contract">
+            				<option value="{id}" selected="selected"><xsl:value-of select="old_contract_id"/></option>
+            			</xsl:when>
+            			<xsl:otherwise>
+            				<option value="{id}"><xsl:value-of select="old_contract_id"/></option>
+            			</xsl:otherwise>
+            		</xsl:choose>
+            	</xsl:for-each>
+            	
+            </select>
+        </form>
+    </div>
 </xsl:template>
+
+<xsl:template match="contract" xmlns:php="http://php.net/xsl">
+	<xsl:value-of select="date_start"/>
+</xsl:template>	
 
