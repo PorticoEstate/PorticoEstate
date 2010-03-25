e78d8bf30430bebfbdc1090ac34ac19572be8ec1
sigurd <sigurd@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2010-03-25 09:11:25 +0000

    odt2xhtml updated to latest and moved to api

---
+++ branches/dev-sigurd-2/phpgwapi/inc/class.odt2xhtml.inc.php
@@ -0,0 +1,53 @@
+<?php
+	/**
+	 * phpGroupWare
+	 *
+	 * @author Sigurd Nes <sigurdne@online.no>
+	 * @copyright Copyright (C) 2007,2008 Free Software Foundation, Inc. http://www.fsf.org/
+	 * @license http://www.fsf.org/licenses/gpl.html GNU General Public License
+	 * @package phpgroupware
+	 * @subpackage phpgwapi
+	 * @category utilities
+ 	 * @version $Id: class.pclzip.inc.php 2588 2009-04-14 11:00:02Z sigurd $
+	 */
+
+	/*
+	   This program is free software: you can redistribute it and/or modify
+	   it under the terms of the GNU General Public License as published by
+	   the Free Software Foundation, either version 2 of the License, or
+	   (at your option) any later version.
+
+	   This program is distributed in the hope that it will be useful,
+	   but WITHOUT ANY WARRANTY; without even the implied warranty of
+	   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	   GNU General Public License for more details.
+
+	   You should have received a copy of the GNU General Public License
+	   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	 */
+
+	/**
+	* Document me!
+	*
+	* @package phpgwapi
+	* @subpackage utilities
+	*/
+	if ( !isset($GLOBALS['phpgw_info']['server']['temp_dir'])  
+			|| !is_dir($GLOBALS['phpgw_info']['server']['temp_dir']) )
+	{
+		if ( substr(PHP_OS, 3) == 'WIN' )
+		{
+			$GLOBALS['phpgw_info']['server']['temp_dir'] = 'c:/temp';
+		}
+		else
+		{
+			$GLOBALS['phpgw_info']['server']['temp_dir'] = '/tmp/';
+		}
+	}
+
+	/**
+	* Include the odt2xhtml class
+	* @see odt2xhtml
+	*/
+
+	require_once PHPGW_API_INC . '/odt2xhtml/index.php5';
