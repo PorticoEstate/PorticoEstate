17493da5c9eb7606a389b5d227c2a6acb03d2d7e
yngve.espelid <yngve.espelid@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2010-04-19 11:36:08 +0000

    Frontend: database query update

--- branches/dev-sigurd-2/frontend/inc/class.bofellesdata.inc.php
+++ branches/dev-sigurd-2/frontend/inc/class.bofellesdata.inc.php
@@ -124,7 +124,7 @@
          * @param int $number the result unit number
          */
         public function get_organisational_unit_name(int $number) {
-        	$sql = "SELECT V_ORG_ENHET.ORG_NAVN FROM V_ORG_ENHET WHERE V_ORG_ENHET.ENHET_ID = $number";
+        	$sql = "SELECT V_ORG_ENHET.ORG_NAVN FROM V_ORG_ENHET WHERE V_ORG_ENHET.RESULTATENHET = $number";
         	$db = $this->get_db();
         	$db->query($sql,__LINE__,__FILE__);
         	if($db->num_rows() > 0)
@@ -133,7 +133,7 @@
         	} 
         	else
         	{
-        		return "Enhet har ingen navn";
+        		return lang('no_name_organisational_unit');
         	}
         	//return "No name";
         }
