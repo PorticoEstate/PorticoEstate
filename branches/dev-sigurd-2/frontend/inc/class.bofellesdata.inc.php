bbac94e5614c8656575434592bd19ceb1a6e2b4d
yngve.espelid <yngve.espelid@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2010-04-23 09:32:35 +0000

    Frontend: debugging

--- branches/dev-sigurd-2/frontend/inc/class.bofellesdata.inc.php
+++ branches/dev-sigurd-2/frontend/inc/class.bofellesdata.inc.php
@@ -137,8 +137,7 @@
          */
         public function get_organisational_unit_name($number) {
         	$sql = "SELECT V_ORG_ENHET.ORG_NAVN FROM V_ORG_ENHET WHERE V_ORG_ENHET.RESULTATENHET = $number";
-        	var_dump($sql);
-        	/*$db = $this->get_db();
+        	$db = $this->get_db();
         	$db->query($sql,__LINE__,__FILE__);
         	if($db->num_rows() > 0)
         	{
@@ -148,7 +147,7 @@
         	else
         	{
         		return lang('no_name_organisational_unit');
-        	}*/
+        	}
         	//return "No name";
         }
     }
