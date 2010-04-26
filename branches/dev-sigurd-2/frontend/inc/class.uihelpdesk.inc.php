5220236a906db63b5904f742d0d1b2cb94a7ae51
yngve.espelid <yngve.espelid@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2010-04-26 11:59:09 +0000

    Frontend: debugging left click

--- branches/dev-sigurd-2/frontend/inc/class.uihelpdesk.inc.php
+++ branches/dev-sigurd-2/frontend/inc/class.uihelpdesk.inc.php
@@ -228,14 +228,17 @@
 					array('menuaction'	=> 'frontend.uihelpdesk.view','id' => )
 				);*/
 			
-			$function_view_ticket = "";
-			$function_view_ticket .= "console.log(data);";
+			//$function_view_ticket = "";
+			//$function_view_ticket .= "console.log(data);";
 			//$function_view_ticket .= "var id = data.getData('id');";
 			//$function_view_ticket .= "console.log(id);";
 			//$function_view_ticket .= "console.log('/index.php?menuaction=frontend.uihelpdesk.view&id=' + id);";
 			//$function_view_ticket .= "document.location = '/index.php?menuaction=frontend.uihelpdesk.view&id=' + id;";
-			$datatable['exchange_values'] = $function_view_ticket;
+			//$datatable['exchange_values'] = $function_view_ticket;
 
+			$datatable['exchange_values'] = 'console.log(data);';
+			$datatable['valida'] = '';
+			
 			unset($parameters);
 			for ($i = 0 ; $i < $count_uicols_name ; $i++)
 			{
