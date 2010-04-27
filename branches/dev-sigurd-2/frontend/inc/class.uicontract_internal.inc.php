4da7b481577ff2bf7f6a27a529ff758516a2d151
yngve.espelid <yngve.espelid@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2010-04-27 09:27:11 +0000

    Frontend: tab update

--- branches/dev-sigurd-2/frontend/inc/class.uicontract_internal.inc.php
+++ branches/dev-sigurd-2/frontend/inc/class.uicontract_internal.inc.php
@@ -9,7 +9,7 @@ class frontend_uicontract_internal extends frontend_uicontract
 		$this->contract_state_identifier = "contract_state";
 		$this->contracts_per_location_identifier = "contracts_per_location";
 		$this->form_url = "index.php?menuaction=frontend.uicontract_internal.index";
-		phpgwapi_cache::user_set('frontend','tab',$GLOBALS['phpgw']->locations->get_id('frontend','.rental.contract_in'), $GLOBALS['phpgw_info']['user']['account_id']);
+		phpgwapi_cache::user_set('frontend','tab',$GLOBALS['phpgw']->locations->get_id('frontend','.rental.contract_internal'), $GLOBALS['phpgw_info']['user']['account_id']);
 		parent::__construct();
 	}
 }
