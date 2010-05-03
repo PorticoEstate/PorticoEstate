6ecef72c2c3bff89415a16dfa835a90c7a575706
yngve.espelid <yngve.espelid@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2010-05-03 08:13:46 +0000

    Frontend: delegates

---
+++ branches/dev-sigurd-2/frontend/inc/class.uidelegates.inc.php
@@ -0,0 +1,55 @@
+<?php
+    phpgw::import_class('frontend.uifrontend');
+
+	class frontend_uidelegates extends frontend_uifrontend
+	{	
+		public $public_functions = array
+			(
+			'index'				=> true,
+			'add_delegate'		=> true,
+			'remove_delegate'	=> true
+		);
+
+		public function __construct()
+		{
+			phpgwapi_cache::user_set('frontend','tab',$GLOBALS['phpgw']->locations->get_id('frontend','.delegates'), $GLOBALS['phpgw_info']['user']['account_id']);
+			parent::__construct();	
+		}
+		
+		
+
+		public function index()
+		{
+			$delegates = frontend_bofrontend::get_delegates(null);
+			$data = array (
+				'msgbox_data'   => $GLOBALS['phpgw']->common->msgbox($GLOBALS['phpgw']->common->msgbox_data($msglog)),
+				'header' 		=>	$this->header_state,
+				'tabs' 			=> 	$this->tabs,
+				'delegate_data' => 	array (
+					'delegate' => $delegates
+				)
+			);
+			$GLOBALS['phpgw']->xslttpl->set_var('phpgw',array('app_data' => $data));
+			$GLOBALS['phpgw']->xslttpl->add_file(array('frontend','delegate'));
+		}
+		
+		public function add_delegate()
+		{
+			/*
+			 * (0). Assume that user is a phpgw user
+			 * 1. Add access to frontend
+			 * 2. Add access to frontend areas (helpdesk, contracts, ...)
+			 * 3. Insert delegate user_name and unit leader in database
+			 */
+			
+		}
+		
+		public function remove_delegate()
+		{
+			$account_id = phpgw::get_var('account_id'); 
+			$owner_id = phpgw::get_var('owner_id'); 
+			
+			frontend_bofrontend::remove_delegate($account_id,$owner_id);
+			$GLOBALS['phpgw']->redirect_link('/index.php', array('menuaction' => 'frontend.uidelegates.index'));
+		}
+	}
