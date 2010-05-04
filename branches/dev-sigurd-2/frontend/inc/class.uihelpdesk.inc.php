da3dcf3b824ff7bdf96a6b0c516ec09bdc19fcdb
yngve.espelid <yngve.espelid@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2010-05-04 09:47:28 +0000

    Frontend: helpdesk

--- branches/dev-sigurd-2/frontend/inc/class.uihelpdesk.inc.php
+++ branches/dev-sigurd-2/frontend/inc/class.uihelpdesk.inc.php
@@ -354,12 +354,10 @@
 			$msglog = phpgwapi_cache::session_get('frontend','msgbox');
 			phpgwapi_cache::session_clear('frontend','msgbox');
 			
-			$datatable['msgbox_data'] = $GLOBALS['phpgw']->common->msgbox($GLOBALS['phpgw']->common->msgbox_data($msglog));
-			
 			$data = array(
 				'header' 		=> $this->header_state,
 				'tabs'			=> $this->tabs,
-				'helpdesk' 		=> array('datatable' => $datatable),
+				'helpdesk' 		=> array('datatable' => $datatable, 'msgbox_data' => $GLOBALS['phpgw']->common->msgbox($GLOBALS['phpgw']->common->msgbox_data($msglog))),
 				'lightbox_name'	=> lang('add ticket')
 			);
 
