6dd81866e23a8dba1e4b26bc377910e05c44d9f7
sigurd <sigurd@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2010-05-24 19:24:08 +0000

    new xml functions

---
+++ branches/dev-sigurd-2/phpgwapi/inc/class.xmlhelper.inc.php
@@ -0,0 +1,154 @@
+<?php
+	/**
+	* Original work by 'djdykes' http://snipplr.com/users/djdykes/
+	* @package phpgwapi
+	* @version $Id: class.xmlhelper.inc.php 4908 2010-02-24 20:49:33Z sigurd $
+	*/
+
+	class phpgwapi_xmlhelper
+	{
+		/**
+		 * The main function for converting to an XML document.
+		 * Pass in a multi dimensional array and this recrusively loops through and builds up an XML document.
+		 *
+		 * @param array $data
+		 * @param string $rootNodeName - what you want the root node to be - defaultsto data.
+		 * @param SimpleXMLElement $xml - should only be used recursively
+		 * @return string XML
+		 */
+		public static function toXML( $data, $rootNodeName = 'ResultSet', &$xml=null )
+		{
+			if ( is_null( $xml ) )
+			{
+				$xml = simplexml_load_string("<?xml version='1.0' encoding='utf-8'?><$rootNodeName />");
+				
+			}
+			// loop through the data passed in.
+			foreach( $data as $key => $value )
+			{
+				// no numeric keys in our xml please!
+				if ( is_numeric( $key ) )
+				{
+					$numeric = 1;
+					$key = $rootNodeName;
+				}
+
+				// delete any char not allowed in XML element names
+				$key = preg_replace('/[^a-z0-9\-\_\.\:]/i', '', $key);
+
+				// if there is another array found recrusively call this function
+				if ( is_array( $value ) )
+				{
+					$node = phpgwapi_xmlhelper::is_assoc( $value ) || $numeric ? $xml->addChild( $key ) : $xml;
+
+					// recrusive call.
+					if ( $numeric )
+					{
+						$key = 'anon';
+					}
+					phpgwapi_xmlhelper::toXml( $value, $key, $node );
+				}
+				else
+				{
+					// add single node.
+					$value = htmlspecialchars( $value , ENT_QUOTES, 'UTF-8');
+					$xml->addChild( $key, $value );
+				}
+			}
+
+			// pass back as XML
+			return $xml->asXML();
+
+		// if you want the XML to be formatted, use the below instead to return the XML
+			//$doc = new DOMDocument('1.0');
+			//$doc->preserveWhiteSpace = false;
+			//$doc->loadXML( $xml->asXML() );
+			//$doc->formatOutput = true;
+			//return $doc->saveXML();
+		}
+
+
+		/**
+		 * Convert an XML document to a multi dimensional array
+		 * Pass in an XML document (or SimpleXMLElement object) and this recrusively loops through and builds a representative array
+		 *
+		 * @param string $xml - XML document - can optionally be a SimpleXMLElement object
+		 * @return array ARRAY
+		 */
+		public static function toArray( $xml )
+		{
+			if ( is_string( $xml ) ) $xml = new SimpleXMLElement( $xml );
+			$children = $xml->children();
+			if ( !$children ) return (string) $xml;
+			$arr = array();
+			foreach ( $children as $key => $node )
+			{
+				$node = phpgwapi_xmlhelper::toArray( $node );
+
+				// support for 'anon' non-associative arrays
+				if ( $key == 'anon' ) $key = count( $arr );
+
+				// if the node is already set, put it into an array
+				if ( isset( $arr[$key] ) )
+				{
+					if ( !is_array( $arr[$key] ) || $arr[$key][0] == null ) $arr[$key] = array( $arr[$key] );
+					{
+						$arr[$key][] = $node;
+					}
+				}
+				else
+				{
+					$arr[$key] = $node;
+				}
+			}
+			return $arr;
+		}
+
+		// determine if a variable is an associative array
+		// Returns true if array has elements with non-numeric keys
+		public static function is_assoc($array)
+		{
+			return (is_array($array) && (0 !== count(array_diff_key($array, array_keys(array_keys($array)))) || count($array)==0));
+		}
+
+		
+		/**
+		* Not for use with big dataset
+		*/
+		public static function xml2assoc($xmldata)
+		{
+			$xml = new XMLReader();
+			$xml->xml($xmldata);
+			$assoc = phpgwapi_xmlhelper::_xml2assoc($xml);
+			$xml->close();
+			return $assoc;
+		}
+		
+		protected static function _xml2assoc($xml)
+		{
+			$tree = null;
+			while($xml->read())
+			{
+				switch ($xml->nodeType)
+				{
+					case XMLReader::END_ELEMENT:
+						return $tree;
+					case XMLReader::ELEMENT:
+						$node = array('tag' => $xml->name, 'value' => $xml->isEmptyElement ? '' : phpgwapi_xmlhelper::_xml2assoc($xml));
+						if($xml->hasAttributes)
+						{
+							while($xml->moveToNextAttribute())
+							{
+								$node['attributes'][$xml->name] = $xml->value;
+							}
+						}
+						$tree[] = $node;
+					break;
+					case XMLReader::TEXT:
+					case XMLReader::CDATA:
+						$tree .= $xml->value;
+				}
+			}
+			return $tree;
+		}
+	}
