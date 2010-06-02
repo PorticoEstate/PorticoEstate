3c99faad79e629a558d097d019a6b8a3f5ae471e
yngve.espelid <yngve.espelid@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2010-06-02 13:57:01 +0000

    Frontend: identify result units by database id

--- branches/dev-sigurd-2/frontend/inc/class.uifrontend.inc.php
+++ branches/dev-sigurd-2/frontend/inc/class.uifrontend.inc.php
@@ -94,7 +94,7 @@
 						//Creating a temporary array holding the single organisational unit in query
 						$org_unit_ids = array(
 							array(
-								"ORG_UNIT_ID" => 1,
+								"ORG_UNIT_ID" => $this_>get_org_enhet_id($param_selected_org_unit),
 								"ORG_NAME" => frontend_bofellesdata::get_instance()->get_organisational_unit_name($param_selected_org_unit),
 								"UNIT_ID" => $param_selected_org_unit
 							)
@@ -360,6 +360,18 @@
 				if($unit_id == $org_unit['UNIT_ID'])
 				{
 					return true;
+				}
+			}
+			return false;
+		}
+		
+		function get_org_enhet_id($result_unit_number)
+		{
+			foreach($org_units as $org_unit)
+			{
+				if($unit_id == $org_unit['UNIT_ID'])
+				{
+					return $org_unit['ORG_UNIT_ID'];
 				}
 			}
 			return false;
