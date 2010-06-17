c0a09408071a817ec0766b54eee818bc2aa2d176
yngve.espelid <yngve.espelid@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2010-06-17 11:20:59 +0000

    Rental: removed old javascript stuff

--- branches/dev-sigurd-2/rental/templates/base/sync_party_list_org_id.php
+++ branches/dev-sigurd-2/rental/templates/base/sync_party_list_org_id.php
@@ -2,33 +2,10 @@
 	include("common.php");
 ?>
 
-
-
-<script type="text/javascript">
-
-	YAHOO.util.Event.addListener(
-		'ctrl_add_rental_party',
-		'click',
-		function(e)
-		{
-            YAHOO.util.Event.stopEvent(e);
-            window.location = 'index.php?menuaction=rental.uiparty.add';
-        }
-   );
-</script>
-
 <?php echo rental_uicommon::get_page_error($error) ?>
 <?php echo rental_uicommon::get_page_message($message) ?>
 
 <h1><img src="<?php echo RENTAL_TEMPLATE_PATH ?>images/32x32/x-office-address-book.png" /> <?php echo lang('sync_parties_fellesdata_id') ?></h1>
-
-<fieldset>
-	<input type="submit" name="ctrl_sync_rental_party" id="ctrl_sync_rental_party" value="<?php echo lang('f_sync_party') ?>" />
-</fieldset>
-
-<p>
-Synkroniser kontraktsparter allerede tilknyttet en organgisasjonsenhet i Fellesdata
-</p>
 
 <?php
 	$list_form = true;
