ac176bdfeb5df7a9b44057ae9bd4d88e9afe00fc
sigurd <sigurd@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2010-07-24 16:41:21 +0000

    Property: update compiling server to latest

--- branches/dev-sigurd-2/property/tutorials/property/examples/install.apache.sh
+++ branches/dev-sigurd-2/property/tutorials/property/examples/install.apache.sh
@@ -54,8 +54,8 @@ APACHE="httpd-2.2.15"
 #  * @var               string PHP, PHPTAR
 #  * Download: http://httpd.apache.org/
 #  */
-PHPTAR="php-5.3.3RC2.tar.bz2"
-PHP="php-5.3.3RC2"
+PHPTAR="php-5.3.3.tar.bz2"
+PHP="php-5.3.3"
 
 #/**
 #  * Name of the EACCELERATOR tarball e.g eaccelerator-0.9.5.tar.bz2
