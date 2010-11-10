39bc6c8629bf88773ebf94cf845be2f6ce893d3a
joakim_hop <joakim_hop@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2010-11-10 11:19:05 +0000

    rental:  updated contracts list for regulations

--- trunk/rental/templates/base/contracts_for_regulation_list.php
+++ trunk/rental/templates/base/contracts_for_regulation_list.php
@@ -2,22 +2,56 @@
 	include("common.php");
 ?>
 
-
-<h1><img src="<?php echo RENTAL_TEMPLATE_PATH ?>images/32x32/text-x-generic.png" /> <?php echo lang('contracts_for_regulation') ?></h1>
-
-<?php
-
-$list_form = false; 
-$list_id = 'contracts_for_adjustment';
-$url_add_on = '&amp;type='.$list_id.'&amp;id='.$adjustment_id;
-$editable = false;
-$extra_cols = array(
-	array("key" => "type", "label" => lang('responsibility'), "index" => 3),
-	array("key" => "composite", "label" => lang('composite'), "sortable"=>'true', "index" => 4),
-	array("key" => "party", "label" => lang('party'), "sortable"=>'true', "index" => 5),
-	array("key" => "adjustment_interval", "label" => lang('adjustment_interval'), "sortable"=>false),
-	array("key" => "adjustment_share", "label" => lang('adjustment_share'), "sortable"=>false),
-	array("key" => "adjustment_year", "label" => lang('adjustment_year'), "sortable"=>false)
-);
-include('contract_list_partial.php'); 
-?>
+<div class="identifier-header">
+	<h1><img src="<?php echo RENTAL_TEMPLATE_PATH ?>images/32x32/text-x-generic.png" /> <?php echo lang('contracts_for_regulation') ?></h1>
+	<div style="float: left; width: 50%;">
+		<button onclick="javascript:window.location.href ='<?php echo $cancel_link;?>;'">&laquo;&nbsp;<?php echo lang('regulation_back');?></button><br/>
+		<label for="name"><?php echo lang('field_of_responsibility') ?></label>
+		<?php echo lang(rental_socontract::get_instance()->get_responsibility_title($adjustment->get_responsibility_id())); ?>
+		<br/>
+		<label for="adjustment_type"><?php echo lang('adjustment_type')?></label>
+		<?php
+			if($adjustment->get_adjustment_type()){
+				echo lang($adjustment->get_adjustment_type());
+			}
+			else{
+				echo lang('none');
+			}
+		?>
+		 <br/>
+		<label for="percent"><?php echo lang('percent') ?></label>
+		<?php echo $adjustment->get_percent(). "%"; ?>
+		<br/>
+		<label for="interval"><?php echo lang('interval') ?></label>
+		<?php echo $adjustment->get_interval().' '.lang('year'); ?>
+		<br/>
+		<label for="adjustment_date"><?php echo lang('adjustment_date') ?></label>
+		<?php
+			$adjustment_date = $adjustment->get_adjustment_date() ? date($GLOBALS['phpgw_info']['user']['preferences']['common']['dateformat'], $adjustment->get_adjustment_date()) : '-';
+			echo $adjustment_date;
+		?>
+		<br/>
+		<?php if($adjustment->is_executed()){?>
+			<label for="is_executed"><?php echo lang("adjustment_is_executed")?></label>
+		<?php }else{?>
+			<label for="is_executed"><?php echo lang("adjustment_is_not_executed")?></label>
+		<?php }?>
+	</div>
+	<div style="float: left; width: 100%;">
+	<?php
+		$list_form = false; 
+		$list_id = 'contracts_for_adjustment';
+		$url_add_on = '&amp;type='.$list_id.'&amp;id='.$adjustment_id;
+		$editable = false;
+		$extra_cols = array(
+			array("key" => "type", "label" => lang('responsibility'), "index" => 3),
+			array("key" => "composite", "label" => lang('composite'), "sortable"=>'true', "index" => 4),
+			array("key" => "party", "label" => lang('party'), "sortable"=>'true', "index" => 5),
+			array("key" => "adjustment_interval", "label" => lang('adjustment_interval'), "sortable"=>false),
+			array("key" => "adjustment_share", "label" => lang('adjustment_share'), "sortable"=>false),
+			array("key" => "adjustment_year", "label" => lang('adjustment_year'), "sortable"=>false)
+		);
+		include('contract_list_partial.php'); 
+	?>
+	</div>
+</div>
