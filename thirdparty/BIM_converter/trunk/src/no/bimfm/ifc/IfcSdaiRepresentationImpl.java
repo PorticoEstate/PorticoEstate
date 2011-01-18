1e196624f726d63b6b3678b15b5cd0c3928db056
peturbjorn <peturbjorn@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2011-01-18 15:22:10 +0000

    Added logging

--- thirdparty/BIM_converter/trunk/src/no/bimfm/ifc/IfcSdaiRepresentationImpl.java
+++ thirdparty/BIM_converter/trunk/src/no/bimfm/ifc/IfcSdaiRepresentationImpl.java
@@ -5,11 +5,15 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import jsdai.lang.SdaiException;
 import jsdai.lang.SdaiSession;
 import jsdai.lang.SdaiTransaction;
 
 public class IfcSdaiRepresentationImpl implements IfcSdaiRepresentation{
+	private Logger logger = LoggerFactory.getLogger("no.bimfm.ifc.IfcSdaiRepresentationImpl");
 	protected SdaiSession session = null;
 	protected SdaiTransaction transaction =null;
 	protected Properties properties = new java.util.Properties();
@@ -20,19 +24,24 @@ public class IfcSdaiRepresentationImpl implements IfcSdaiRepresentation{
 	public IfcSdaiRepresentationImpl(String propertiesFileNameArg) {
 		InputStream inStream;
 		try {
+			logger.debug("Opening following properties file:{}", Thread.currentThread().getContextClassLoader().getResource(propertiesFileNameArg).toString());
 			inStream =  Thread.currentThread().getContextClassLoader().getResourceAsStream(propertiesFileNameArg);
 			if(inStream == null) {
+				logger.error("Properties file not found!!");
 				throw new FileNotFoundException();
 			}
 			properties.load(inStream);
 			SdaiSession.setSessionProperties(properties);
 		} catch (FileNotFoundException e1) {
+			logger.error("Properties file not found!!");
 			e1.printStackTrace();
 			throw new IfcSdaiException("File not found!", e1);
 		} catch (IOException e) {
+			logger.error("IO Exception!!");
 			e.printStackTrace();
 			throw new IfcSdaiException("IO error!", e);
 		} catch (SdaiException e) {
+			logger.error("Sdai Exception!!");
 			e.printStackTrace();
 			throw new IfcSdaiException("Sdai error!", e);
 		}
