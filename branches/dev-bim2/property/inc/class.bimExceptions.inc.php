fb430aebadf3a5f0bd8f756180ce06dfd08e9ccc
peturbjorn <peturbjorn@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2011-01-27 08:18:17 +0000

    Added exceptions

--- branches/dev-bim2/property/inc/class.bimExceptions.inc.php
+++ branches/dev-bim2/property/inc/class.bimExceptions.inc.php
@@ -26,3 +26,30 @@ class ModelExistsException extends Exception
         return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
     }
 }
+class ModelDoesNotExistException extends Exception
+{
+    public function __construct($message, Exception $previous = null) {
+        parent::__construct($message, 0, $previous);
+    }
+    public function __toString() {
+        return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
+    }
+}
+class IncompleteItemException extends Exception
+{
+    public function __construct($message, $code = 0) {
+        parent::__construct($message, $code);
+    }
+    public function __toString() {
+        return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
+    }
+}
+class BimDataException extends Exception
+{
+    public function __construct($message, Exception $previous = null) {
+        parent::__construct($message, 0, $previous);
+    }
+    public function __toString() {
+        return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
+    }
+}
