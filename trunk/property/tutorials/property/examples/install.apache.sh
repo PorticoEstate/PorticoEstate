9132b521a9166c4f46a8ec192d6c3380a5ed9cce
sigurdne <sigurdne@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2011-01-30 13:41:13 +0000

    property: update to latest

--- trunk/property/tutorials/property/examples/install.apache.sh
+++ trunk/property/tutorials/property/examples/install.apache.sh
@@ -54,8 +54,8 @@ APACHE="httpd-2.2.17"
 #  * @var               string PHP, PHPTAR
 #  * Download: http://httpd.apache.org/
 #  */
-PHPTAR="php-5.3.4.tar.bz2"
-PHP="php-5.3.4"
+PHPTAR="php-5.3.5.tar.bz2"
+PHP="php-5.3.5"
 
 #/**
 #  * Name of the EACCELERATOR tarball e.g eaccelerator-0.9.5.tar.bz2
