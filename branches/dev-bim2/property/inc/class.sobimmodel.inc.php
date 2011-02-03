fbc8cf3603fcfe487a63d9bd1c458dfca7452900
peturbjorn <peturbjorn@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2011-02-03 19:22:50 +0000

    moved to separate class.

--- branches/dev-bim2/property/inc/class.sobimmodel.inc.php
+++ branches/dev-bim2/property/inc/class.sobimmodel.inc.php
@@ -1,6 +1,8 @@
 <?php
 
 phpgw::import_class('property.bimExceptions');
+phpgw::import_class('property.bimmodel');
+phpgw::import_class('property.bimmodelinformation');
 
 /*
  * @see sobimmodel_impl
@@ -199,79 +201,5 @@ class sobimmodel_impl implements sobimmodel
 		$this->modelId = $id;
 	}
 }
-class BimModel {
-	private $databaseId;
-	private $name;
-	private $creationDate;
-	private $fileSize;
-	private $fileName;
-	private $usedItemCount;
-	private $vfsFileId;
-	private $used;
-	 
-	function __construct($databaseId = null, $name = null, $creationDate = null, $fileSize = null,$fileName= null,$usedItemCount= null, $vfsFileId = null) {
-		$this->databaseId = (int)$databaseId;
-		$this->name = $name;
-		$this->creationDate =  $creationDate;
-		$this->fileSize  = $fileSize;
-		$this->fileName = $fileName;
-		$this->usedItemCount = $usedItemCount;
-		if($usedItemCount && $usedItemCount > 0) {
-			$this->used =  true;
-		} else {
-			$this->used =  false;
-		}
-		
-	}
-	function getDatabaseId() {
-		return $this->databaseId;
-	}
-	function setDatabaseId($databaseId) {
-		$this->databaseId = $databaseId;
-	}
-	function getName() {
-		return $this->name;
-	}
-	function setName($item) {
-		$this->name = $item;
-	}
-	function getCreationDate() {
-		return $this->creationDate;
-	}
-	function setCreationDate($item) {
-		$this->creationDate = $item;
-	}
-	function getFileSize() {
-		return $this->fileSize;
-	}
-	function setFileSize($item) {
-		$this->fileSize = $item;
-	}
-	function getFileName() {
-		return $this->fileName;
-	}
-	function setFileName($item) {
-		$this->fileName = $item;
-	}
-	function getUsedItemCount() {
-		return $this->usedItemCount;
-	}
-	function setUsedItemCount($item) {
-		$this->usedItemCount = $item;
-	}
-	function getVfsFileId() {
-		return $this->vfsFileId;
-	}
-	function setVfsFileId($item) {
-		$this->vfsFileId = $item;
-	}
-	
-	function getUsed() {
-		if($this->used) {
-			return $this->used;
-		} else {
-			return (bool)($this->usedItemCount > 0);
-		}
-	}
-}
+
 
