0f9bfabd38c2de5b823d25629d9a2b2fce7bcbf0
"lars.nestas" <"lars.nestas"@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2011-02-21 11:45:09 +0000

    print and pdf for contracts

---
+++ trunk/rental/templates/base/pdf/rental_contract_hybler.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Melding om inn/utflytting - Hybler</title>
+<link rel="stylesheet" type="text/css" href="css/contract.css" />
+</head>
+<body>
+<img src="http://www.nordlandssykehuset.no/getfile.php/NLSH_bilde%20og%20filarkiv/Internett/NLSH_logo_siste.jpg%20%28352x58%29.jpg" alt="Nordlanssykehuset logo" />
+<h1>Melding om inn/utflytting - Hybler</h1>
+
+
+<div class="two_column">
+
+<dl class="left_column">
+	<dt><input type="checkbox" disabled="disabled" />&nbsp Innflytting</dt>
+	<dd>&nbsp</dd>
+	<dt>Navn:</dt>
+	<dd>....</dd>
+	<dt>Fnr.:</dt>
+	<dd>...</dd>
+	<dt>Adresse:</dt>
+	<dd>...</dd>
+	<dt>Tildelt bolig:</dt>
+	<dd>...</dd>
+</dl>
+
+
+<dl class="right_column">
+	<dt><input type="checkbox" disabled="disabled" />&nbsp Utflytting</dt>
+	<dd>&nbsp</dd>
+	<dt>Stilling:</dt>
+	<dd>....</dd>
+	<dt>Avd.:</dt>
+	<dd>...</dd>
+	<dt>Innflytting-dato:</dt>
+	<dd>...</dd>
+	<dt>Utflytting-dato:</dt>
+	<dd>...</dd>
+</dl>
+</div>
+
+
+<div class="one_column">
+<dl class="checkbox_list">
+	<dt><input type="checkbox" disabled="disabled" /></dt>
+	<dd>Lever nøkler etter utflytting til vaktmesters postkasse i postkasserommet</dd>
+	<dt><input type="checkbox" disabled="disabled" /></dt>
+	<dd>Underrett vaktmester vedr. eventuelle mangler/skader</dd>
+	<dt><input type="checkbox" disabled="disabled" /></dt>
+	<dd>Har du tjenestetelefon – meld fra til personalkontoret (ikke Telenor)</dd>
+</dl>
+</div>
+
+<div class="one_column">
+
+<table>
+	<tr>
+		<td>Husleie uten møbler</td>
+		<td>Kr.:</td>
+		<td>....</td>
+		<td>Pr.mnd.</td>
+	</tr>
+	<tr>
+		<td>Husleie med møbler</td>
+		<td>Kr.:</td>
+		<td>....</td>
+		<td>Pr.mnd.</td>
+	</tr>
+	<tr>
+		<td>Utstyr</td>
+		<td>Kr.:</td>
+		<td>....</td>
+		<td>Pr.mnd.</td>
+	</tr>
+	<tr>
+		<td>Sengetøy/Håndduker</td>
+		<td>Kr.:</td>
+		<td>....</td>
+		<td>Pr.mnd.</td>
+	</tr>
+	<tr>
+		<td>Husleie</td>
+		<td>Kr.:</td>
+		<td>....</td>
+		<td>Pr.mnd.</td>
+	</tr>
+	<tr>
+		<td>1 mnd forskudd</td>
+		<td>Kr.:</td>
+		<td>....</td>
+		<td>Pr.mnd.</td>
+	</tr>
+</table>
+</div>
+
+
+<div class="one_column">
+<p>Merknader: <strong>Boligen (hybelen) skal ved flytting være ryddet og rengjort.</strong></p>
+</div>
+<p></p>
+
+<div class="one_column">
+<p>Dato:</p>
+<table>
+	<tr>
+		<td align="center">
+		<p class="sign">Underskrift leietaker</p>
+		</td>
+		<td align="center">
+		<p class="sign">Underskrift vaktmester</p>
+		</td>
+	</tr>
+</table>
+</div>
+
+
+<p>Kopi:</p>
+<p><input type="checkbox" disabled="disabled" />Personalkontoret</p>
+<p><input type="checkbox" disabled="disabled" />Lønningskontoret</p>
+</body>
+</html>
