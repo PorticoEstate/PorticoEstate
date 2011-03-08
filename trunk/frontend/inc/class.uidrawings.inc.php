73a5f453b0b79b341c5adbb7ca6d7c4adf11f17d
sigurdne <sigurdne@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2011-03-08 15:15:43 +0000

    drawings

--- trunk/frontend/inc/class.uidrawings.inc.php
+++ trunk/frontend/inc/class.uidrawings.inc.php
@@ -42,19 +42,79 @@
 
 		public function __construct()
 		{
+			phpgwapi_cache::session_set('frontend','tab',$GLOBALS['phpgw']->locations->get_id('frontend','.drawings'));
 			parent::__construct();
+			$this->location_code = $this->header_state['selected_location'];
 		}
 
 		public function index()
 		{
+			$doc_type = 17;
+			$allrows = true;
+			$sodocument	= CreateObject('property.sodocument');
+
+			$document_list = $sodocument->read_at_location(array('start' => $this->start,'query' => $this->query,'sort' => $this->sort,'order' => $this->order,
+				'filter' => $this->filter,'location_code' => $this->location_code,'doc_type' => $doc_type, 'allrows' => $allrows));
+			$this->total_records = $this->so->total_records;
+_debug_array($document_list);
+_debug_array($this->location_code);
+
+			//----------------------------------------------datatable settings--------
+
+			$content = array();
+			foreach($invoices as $entry)
+			{
+				$content[] = array
+				(
+					'document_id'			=> $entry['document_id'],
+					'document_name'			=> $entry['document_name'],
+					'link'					=> $entry['link'],
+					'title'					=> $entry['title'],
+					'doc_type'				=> $entry['doc_type'],
+					'document_date'			=> $GLOBALS['phpgw']->common->show_date($entry['document_date'],$GLOBALS['phpgw_info']['user']['preferences']['common']['dateformat']),
+				);	
+			}
+
+			$datavalues[0] = array
+			(
+				'name'					=> "0",
+				'values' 				=> json_encode($content),
+				'total_records'			=> count($content),
+				'edit_action'			=> json_encode($GLOBALS['phpgw']->link('/index.php',array('menuaction'=> 'property.uidocument.view_file'))),
+				'is_paginator'			=> 1,
+				'footer'				=> 0
+			);
+
+
+			$myColumnDefs[0] = array
+			(
+				'name'		=> "0",
+				'values'	=>	json_encode(array(	array('key' => 'document_name','label'=>lang('name'),'sortable'=>true,'resizeable'=>true),
+													array('key' => 'document_id','label'=>lang('id'),'sortable'=>false,'resizeable'=>true,'formatter'=>'YAHOO.widget.DataTable.formatLink'),
+													array('key' => 'title','label'=>lang('title'),'sortable'=>false,'resizeable'=>true),
+													array('key' => 'doc_type','label'=>lang('type'),'sortable'=>true,'resizeable'=>true),
+													array('key' => 'document_date','label'=>lang('date'),'sortable'=>true,'resizeable'=>true)
+													))
+			);
+
+			//----------------------------------------------datatable settings--------
+
+
+
+
 			$data = array
 			(
-				'header' =>$this->header_state,
-				'tabs' => $this->tabs,
-				'drawings'      => lang('not_implemented')
+				'property_js'			=> json_encode($GLOBALS['phpgw_info']['server']['webserver_url']."/property/js/yahoo/property2.js"),
+				'datatable'				=> $datavalues,
+				'myColumnDefs'			=> $myColumnDefs,
+				'header'				=>$this->header_state,
+				'tabs'					=> $this->tabs,
+				'drawings'				=> lang('not_implemented')
 			);
 			
 	      	$GLOBALS['phpgw']->xslttpl->set_var('phpgw',array('app_data' => $data));
         	$GLOBALS['phpgw']->xslttpl->add_file(array('frontend','drawings'));
+			$GLOBALS['phpgw']->js->validate_file( 'yahoo', 'project.edit', 'frontend' );
+
 		}
     }
