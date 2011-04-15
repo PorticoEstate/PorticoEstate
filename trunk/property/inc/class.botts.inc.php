b7bd01d9a29d5a79a6015139362ecf80d59603b9
sigurdne <sigurdne@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2011-04-15 11:50:32 +0000

    property: remove debug

--- trunk/property/inc/class.botts.inc.php
+++ trunk/property/inc/class.botts.inc.php
@@ -528,7 +528,7 @@
 			}
 
 			$status_text = $this->get_status_text();
-_debug_array($status_text);
+
 			$ticket['status_name'] = $status_text[$ticket['status']];
 			$ticket['user_lid']=$GLOBALS['phpgw']->accounts->id2name($ticket['user_id']);
 			$ticket['category_name']=ucfirst($this->get_category_name($ticket['cat_id']));
