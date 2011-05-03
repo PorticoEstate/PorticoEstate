6eea3767fb122f47c32d1dfe2ce332b0c7527f37
Sigurd Nes <sigurdne@users.noreply.github.com>
2011-05-03 20:03:26 +0000

    Merge 7249:7250 from trunk

--- bookingfrontend/inc/class.uibooking.inc.php
+++ bookingfrontend/inc/class.uibooking.inc.php
@@ -592,9 +592,10 @@
 				$system_message['phone'] = ' ';
 				$system_message['email'] = ' ';
 				$system_message['title'] = lang('Cancelation of booking from')." ".$booking['group_name'];
-                 
-                $system_message['message'] = $system_message['message']."\n\n".lang('To cancel booking use this link')." - <a href='".self::link(array('menuaction' => 'booking.uibooking.delete','id' => $booking['id'], 'outseason' => $outseason, 'recurring' => $recurring, 'repeat_until' => $repeat_until, 'field_interval' => $field_interval, 'delete_allocation' => $delete_allocation))."'>".lang('Delete')."</a>";
 
+                $link = self::link(array('menuaction' => 'booking.uibooking.delete','id' => $booking['id'], 'outseason' => $outseason, 'recurring' => $recurring, 'repeat_until' => $repeat_until, 'field_interval' => $field_interval, 'delete_allocation' => $delete_allocation));
+                $link = mb_strcut($link,16,strlen($link));
+                $system_message['message'] = $system_message['message']."\n\n".lang('To cancel booking use this link')." - <a href='".$link."'>".lang('Delete')."</a>";
 				$receipt = $this->system_message_bo->add($system_message);
 				$this->redirect(array('menuaction' =>  'bookingfrontend.uibuilding.schedule', 'id' => $system_message['building_id']));
 
