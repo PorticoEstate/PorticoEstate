96c81dabbd7c0bcc194e00e5372a5969df14a128
Sigurd Nes <sigurdne@users.noreply.github.com>
2011-11-14 22:39:00 +0000

    Property: more on EAV

--- property/inc/class.uiadmin_entity.inc.php
+++ property/inc/class.uiadmin_entity.inc.php
@@ -533,7 +533,6 @@
 			{
 				$entry['location_id'] = $GLOBALS['phpgw']->locations->get_id($this->type_app[$this->type], ".{$this->type}.{$entity_id}.{$entry['id']}");
 			}			
-
 			$uicols['name'][0]	= 'location_id';
 			$uicols['descr'][0]	= 'location_id';
 			$uicols['name'][1]	= 'id';
@@ -546,8 +545,8 @@
 			$uicols['descr'][4]	= lang('Prefix');
 			$uicols['name'][5]	= 'entity_id';
 			$uicols['descr'][5]	= lang('id');
-			$uicols['name'][5]	= 'is_eav';
-			$uicols['descr'][5]	= lang('is_eav');
+			$uicols['name'][6]	= 'is_eav';
+			$uicols['descr'][6]	= lang('is_eav');
 			$j = 0;
 			$count_uicols_name = count($uicols['name']);
 
