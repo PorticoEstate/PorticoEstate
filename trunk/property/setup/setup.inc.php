c024ab198fedb0f387ea2d33dd004dd4553d1b17
sigurdne <sigurdne@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2011-12-23 11:37:50 +0000

    property: move config values for invoices

--- trunk/property/setup/setup.inc.php
+++ trunk/property/setup/setup.inc.php
@@ -12,7 +12,7 @@
 	*/
 
 	$setup_info['property']['name']			= 'property';
-	$setup_info['property']['version']		= '0.9.17.629';
+	$setup_info['property']['version']		= '0.9.17.630';
 	$setup_info['property']['app_order']	= 8;
 	$setup_info['property']['enable']		= 1;
 	$setup_info['property']['app_group']	= 'office';
