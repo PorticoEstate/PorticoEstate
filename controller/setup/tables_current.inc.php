2bbd2f453cf8de98bde81f8a12a8ab7aa8f73a6f
Sigurd Nes <sigurdne@users.noreply.github.com>
2012-01-25 14:47:55 +0000

    controller: configurable status

--- controller/setup/tables_current.inc.php
+++ controller/setup/tables_current.inc.php
@@ -206,5 +206,19 @@
                 'fk' => array('controller_check_item' => array('check_item_id' => 'id')),
                 'ix' => array(),
                 'uc' => array()
+		),
+		'controller_check_item_status' => array(
+			'fd' => array(
+				'id' => array('type' => 'auto','nullable' => False),
+				'name' => array('type' => 'varchar','precision' => '50','nullable' => False),
+				'open' => array('type' => 'int','precision' => '2','nullable' => True),
+				'closed' => array('type' => 'int','precision' => '2','nullable' => True),
+				'pending' => array('type' => 'int','precision' => '2','nullable' => True),
+				'sorting' => array('type' => 'int','precision' => '4','nullable' => True)
+			),
+			'pk' => array('id'),
+			'ix' => array(),
+			'ix' => array(),
+			'uc' => array()
 		)
 	);
