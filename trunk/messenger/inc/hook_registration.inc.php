177a8848b14243e267369c385f54c19311cc8578
sigurdne <sigurdne@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2012-04-02 13:50:06 +0000

    registration

--- trunk/messenger/inc/hook_registration.inc.php
+++ trunk/messenger/inc/hook_registration.inc.php
@@ -1,15 +1,9 @@
 <?php
 	// Note: This is temp sample data, I will make a config option for it soon.
 
-	global $reg_info;
-
-	$message['to']      = $reg_info['lid'];
-	$message['subject'] = 'Welcome!';
-	$message['content'] = '
-
-Thanks for signing up for your new account, blah blah blah blah
-
-';
+	$message['to']      = $account_lid;
+	$message['subject'] = lang('Welcome');
+	$message['content'] = $this->config['messenger_welcome_message'];
 
 	$so = createobject('messenger.somessenger');
 	$so->send_message($message,True);
