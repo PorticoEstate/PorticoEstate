510d29fb410bf40939f1ddfdcf6a8c8e6bbd9fbc
Sigurd Nes <sigurdne@users.noreply.github.com>
2012-04-03 07:48:18 +0000

    registration

--- messenger/inc/hook_registration.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-	// Note: This is temp sample data, I will make a config option for it soon.
-
-	$message['to']      = $account_lid;
-	$message['subject'] = lang('Welcome');
-	$message['content'] = $this->config['messenger_welcome_message'];
-
-	$so = createobject('messenger.somessenger');
-	$so->send_message($message,True);
