a16c4000028f7520f6aae8250a5b470dca7faf48
andvare <alexander.stevenson@redpill-linpro.com>
2012-04-11 12:39:29 +0000

    Added support for changing internal and external billing sequential numbers through Administrator interface

--- booking/inc/class.uisettings.inc.php
+++ booking/inc/class.uisettings.inc.php
@@ -1,5 +1,6 @@
 <?php
 	phpgw::import_class('booking.uicommon');
+	phpgw::import_class('booking.uicommon');
 
 	class booking_uisettings extends booking_uicommon
 	{
@@ -19,9 +20,13 @@
 			$config	= CreateObject('phpgwapi.config','booking');
 			$config->read();
 
+			// Define internal and external billing sequence number values
+			$internal_value = null;
+			$external_value = null;
+
 			if($_SERVER['REQUEST_METHOD'] == 'POST')
 			{
-				foreach($_POST as $dim => $value)
+				foreach($_POST['config_data'] as $dim => $value)
 				{
 					if (strlen(trim($value)) > 0)
 					{
@@ -33,8 +38,31 @@
 					}
 				}
 				$config->save_repository();
+
+				// Save internal and external sequential numbers to database
+				if( isset( $_POST['billing'] ) ) {
+					$internal_value = intval( $_POST['billing']['internal'] );
+					$external_value = intval( $_POST['billing']['external'] );
+					$GLOBALS['phpgw']->db->query( "UPDATE bb_billing_sequential_number_generator SET value=" . $internal_value . " WHERE name='internal'" );
+					$GLOBALS['phpgw']->db->query( "UPDATE bb_billing_sequential_number_generator SET value=" . $external_value . " WHERE name='external'" );
+				}
 			}
-			
-			self::render_template('settings', array('config_data' =>$config->config_data));
+
+			// Load internal and external sequential numbers from database (if not already set) and insert into $billing
+			if( is_null( $internal_value ) || is_null( $external_value ) ) {
+				$db = $GLOBALS['phpgw']->db;
+				$db->query( "SELECT name, value FROM bb_billing_sequential_number_generator WHERE name='internal' OR name='external' LIMIT 2" );
+				while( $db->next_record() ) {
+					if( !strcmp( $db->f( 'name', false ), "internal" ) ) {
+						$internal_value = intval( $db->f( 'value', false ) );
+					} else if( !strcmp( $db->f( 'name', false ), "external" ) ) {
+						$external_value = intval( $db->f( 'value', false ) );
+					}
+	 			}
+			}
+			$billing = array( 'internal' => $internal_value,
+				'external' => $external_value );
+
+			self::render_template('settings', array('config_data' =>$config->config_data, 'billing' => $billing ) );
 		}
 	}
