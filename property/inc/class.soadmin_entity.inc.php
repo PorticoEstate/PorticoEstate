dbbc5c15155957e76235db2e2cbb735da4d0b0c9
Sigurd Nes <sigurdne@users.noreply.github.com>
2012-06-04 13:07:30 +0000

    property: typo

--- property/inc/class.soadmin_entity.inc.php
+++ property/inc/class.soadmin_entity.inc.php
@@ -319,7 +319,7 @@
 			$this->db2->query($sql,__LINE__,__FILE__);
 			$this->total_records = $this->db2->num_rows();
 
-			$category = array();
+			$categories = array();
 			while ($this->db2->next_record())
 			{
 				$id	= $this->db2->f('id');
