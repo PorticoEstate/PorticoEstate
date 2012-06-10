0f8daa841ac9cf6855b359699b1c1be77cda3a1b
sigurdne <sigurdne@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2012-06-10 15:24:10 +0000

    property: configurable column

--- trunk/property/inc/class.uitts.inc.php
+++ trunk/property/inc/class.uitts.inc.php
@@ -884,8 +884,10 @@
 			$uicols['descr'][]	= lang('bgcolor');
 			$uicols['name'][] = 'subject';
 			$uicols['descr'][]	= lang('subject');
+/*
 			$uicols['name'][] = 'location_code';
 			$uicols['descr'][]	= lang('location code');
+*/
 			$location_types = execMethod('property.soadmin_location.select_location_type');
 			$level_assigned = isset($this->bo->config->config_data['list_location_level']) && $this->bo->config->config_data['list_location_level'] ? $this->bo->config->config_data['list_location_level'] : array();
 
