119f8fbc01618ca631a4f18ba1be3c6b1f49fb17
Sigurd Nes <sigurdne@users.noreply.github.com>
2012-06-20 11:35:09 +0000

    move a local function from booking to api and move functions from outside the uicommon-class

--- booking/inc/class.uicommon.inc.php
+++ booking/inc/class.uicommon.inc.php
@@ -1,93 +1,6 @@
 <?php
 	phpgw::import_class('phpgwapi.yui');
 	
-	/**
-	 * Cherry pick selected values into a new array
-	 * 
-	 * @param array $array    input array
-	 * @param array $keys     array of keys to pick
-	 *
-	 * @return array containg values from $array for the keys in $keys.
-	 */
-	function extract_values($array, $keys, $options = array())
-	{
-		static $default_options = array(
-			'prefix' => '',
-			'suffix' => '', 
-			'preserve_prefix' => false,
-			'preserve_suffix' => false
-		);
-		
-		$options = array_merge($default_options, $options);
-		
-		$result = array();
-		foreach($keys as $write_key)
-		{
-			$array_key = $options['prefix'].$write_key.$options['suffix'];
-			if(isset($array[$array_key])) {
-				$result[($options['preserve_prefix'] ? $options['prefix'] : '').$write_key.($options['preserve_suffix'] ? $options['suffix'] : '')] = $array[$array_key];
-			}
-		}
-		return $result;
-	}
-	
-	function array_set_default(&$array, $key, $value)
-	{
-		if(!isset($array[$key])) $array[$key] = $value;
-	}
-
-	/**
-	 * Reformat an ISO timestamp into norwegian format
-	 * 
-	 * @param string $date    date
-	 *
-	 * @return string containg timestamp in norwegian format
-	 */
-	function pretty_timestamp($date)
-	{
-		if (empty($date)) return "";
-		
-		if(is_array($date) && is_object($date[0]) && $date[0] instanceof DOMNode)
-		{
-			$date = $date[0]->nodeValue;
-		}
-		preg_match('/([0-9]{4})-([0-9]{2})-([0-9]{2})( ([0-9]{2}):([0-9]{2}))?/', $date, $match);
-
-		$dateformat = $GLOBALS['phpgw_info']['user']['preferences']['common']['dateformat'];
-		if($match[4]) 
-		{
-			$dateformat .= ' H:i';
-			$timestamp = mktime($match[5], $match[6], 0, $match[2], $match[3], $match[1]);
-		}
-		else
-		{
-			$timestamp = mktime(0, 0, 0, $match[2], $match[3], $match[1]);
-		}
-		$text = date($dateformat,$timestamp);
-			
-		return $text;
-	}
-
-	/**
-	 * Generates a javascript translator object/hash for the specified fields.
-	 */
-	function js_lang()
-	{
-		$keys = func_get_args();
-		$strings = array();
-		foreach($keys as $key) { $strings[$key] = is_string($key) ? lang($key) : call_user_func_array('lang', $key); }
-		return json_encode($strings);
-	}
-	
-	/**
-	 * Creates an array of translated strings.
-	 */
-	function lang_array()
-	{
-		$keys = func_get_args();
-		foreach($keys as &$key) $key = lang($key);
-		return $keys;
-	}
 	
 	abstract class booking_uicommon
 	{
@@ -137,6 +50,17 @@
 			}
 		}
 		
+
+		/**
+		 * Creates an array of translated strings.
+		 */
+		function lang_array()
+		{
+			$keys = func_get_args();
+			foreach($keys as &$key) $key = lang($key);
+			return $keys;
+		}
+
 		private function get_ui_session_key() {
 			return $this->ui_session_key;
 		}
@@ -348,9 +272,9 @@
 							
 			$data['yui_booking_i18n'] = array(
 				'Calendar' => array(
-					'WEEKDAYS_SHORT' => json_encode(lang_array('Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa')),
-					'WEEKDAYS_FULL' => json_encode(lang_array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday')),
-					'MONTHS_LONG' => json_encode(lang_array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December')),
+					'WEEKDAYS_SHORT' => json_encode($this->lang_array('Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa')),
+					'WEEKDAYS_FULL' => json_encode($this->lang_array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday')),
+					'MONTHS_LONG' => json_encode($this->lang_array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December')),
 				),
 				'DataTable' => array(
 					'MSG_EMPTY' => json_encode(lang('No records found.')),
