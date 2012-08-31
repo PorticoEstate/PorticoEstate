795d200a7d6cf697ccddbd443348868bf8af27d8
erikhl <erikhl@gmail.com>
2012-08-31 10:09:39 +0000

    whitespace-fix (formatting)

--- activitycalendarfrontend/inc/class.menu.inc.php
+++ activitycalendarfrontend/inc/class.menu.inc.php
@@ -2,26 +2,26 @@
 
 class activitycalendarfrontend_menu {
 
-  function get_menu() {
-    $incoming_app = $GLOBALS['phpgw_info']['flags']['currentapp'];
-    $GLOBALS['phpgw_info']['flags']['currentapp'] = 'activitycalendarfrontend';
+	function get_menu() {
+		$incoming_app = $GLOBALS['phpgw_info']['flags']['currentapp'];
+		$GLOBALS['phpgw_info']['flags']['currentapp'] = 'activitycalendarfrontend';
 
-    $menus = array();
+		$menus = array();
 
-    if ($GLOBALS['phpgw']->acl->check('run', phpgwapi_acl::READ, 'admin')
-            || $GLOBALS['phpgw']->acl->check('admin', phpgwapi_acl::ADD, 'activitycalendarfrontend')) {
-      $menus['admin'] = array
-          (
-          'index' => array
-              (
-              'text' => lang('Configuration'),
-              'url' => $GLOBALS['phpgw']->link('/index.php', array('menuaction' => 'admin.uiconfig.index', 'appname' => 'activitycalendarfrontend'))
-          ),
-      );
-    }
+		if ($GLOBALS['phpgw']->acl->check('run', phpgwapi_acl::READ, 'admin')
+				|| $GLOBALS['phpgw']->acl->check('admin', phpgwapi_acl::ADD, 'activitycalendarfrontend')) {
+			$menus['admin'] = array
+				(
+				'index' => array
+					(
+					'text' => lang('Configuration'),
+					'url' => $GLOBALS['phpgw']->link('/index.php', array('menuaction' => 'admin.uiconfig.index', 'appname' => 'activitycalendarfrontend'))
+				),
+			);
+		}
 
-    $GLOBALS['phpgw_info']['flags']['currentapp'] = $incoming_app;
-    return $menus;
-  }
+		$GLOBALS['phpgw_info']['flags']['currentapp'] = $incoming_app;
+		return $menus;
+	}
 
 }
