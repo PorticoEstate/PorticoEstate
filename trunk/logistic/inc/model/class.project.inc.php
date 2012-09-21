69571e19997a6401f0007f0db0c40dd336d57191
erikhl <erikhl@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2012-09-21 08:40:27 +0000

    Initial add of logistic module

---
+++ trunk/logistic/inc/model/class.project.inc.php
@@ -0,0 +1,32 @@
+<?php
+	/**
+	* phpGroupWare - logistic: a part of a Facilities Management System.
+	*
+	* @author Erik Holm-Larsen <erik.holm-larsen@bouvet.no>
+	* @copyright Copyright (C) 2011,2012 Free Software Foundation, Inc. http://www.fsf.org/
+	* This file is part of phpGroupWare.
+	*
+	* phpGroupWare is free software; you can redistribute it and/or modify
+	* it under the terms of the GNU General Public License as published by
+	* the Free Software Foundation; either version 2 of the License, or
+	* (at your option) any later version.
+	*
+	* phpGroupWare is distributed in the hope that it will be useful,
+	* but WITHOUT ANY WARRANTY; without even the implied warranty of
+	* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	* GNU General Public License for more details.
+	*
+	* You should have received a copy of the GNU General Public License
+	* along with phpGroupWare; if not, write to the Free Software
+	* Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+	*
+	* @license http://www.gnu.org/licenses/gpl.html GNU General Public License
+	* @internal Development of this application was funded by http://www.bergen.kommune.no/
+	* @package property
+	* @subpackage logistic
+ 	* @version $Id$
+	*/
+
+ include_class('logistic', 'model' , 'inc/model')
+
+?>
---
+++ trunk/logistic/setup/tables_current.inc.php
@@ -0,0 +1,128 @@
+<?php
+
+$phpgw_baseline = array(
+		'lg_project_type' => array(
+				'fd' => array(
+						'id' => array('type' => 'auto', 'precision' => 4, 'nullable' => false),
+						'name' => array('type' => 'varchar', 'precision' => '255', 'nullable' => false)
+				),
+				'pk' => array('id'),
+				'fk' => array(),
+				'ix' => array(),
+				'uc' => array()
+		),
+		'lg_project' => array(
+				'fd' => array(
+						'id' => array('type' => 'auto', 'precision' => 4, 'nullable' => false),
+						'name' => array('type' => 'varchar', 'precision' => '255', 'nullable' => false),
+						'project_type_id' => array('type' => 'int', 'precision' => 4, 'nullable' => false),
+						'description' => array('type' => 'text', 'nullable' => false)
+				),
+				'pk' => array('id'),
+				'fk' => array('lg_project_type' => array('project_type_id' => 'id')),
+				'ix' => array(),
+				'uc' => array()
+		),
+		'lg_activity' => array(
+				'fd' => array(
+						'id' => array('type' => 'auto', 'precision' => 4, 'nullable' => false),
+						'parent_activity_id' => array('type' => 'int', 'precision' => 4, 'nullable' => true),
+						'name' => array('type' => 'varchar', 'precision' => '255', 'nullable' => false),
+						'project_id' => array('type' => 'int', 'precision' => 4, 'nullable' => false),
+						'start_date' => array('type' => 'int', 'precision' => 4, 'nullable' => true),
+						'end_date' => array('type' => 'int', 'precision' => 4, 'nullable' => true),
+						'responsible_user_id' => array('type' => 'int', 'precision' => 4, 'nullable'=> false),
+						'update_user' => array('type' => 'int', 'precision' => 4, 'nullable' => false),
+						'update_date' => array('type' => 'int', 'precision' => 4, 'nullable' => false)
+				),
+				'pk' => array('id'),
+				'fk' => array(
+						'lg_project' => array('project_id' => 'id'),
+						'lg_activity' => array('parent_activity_id' => 'id')
+				),
+				'ix' => array('name'),
+				'uc' => array()
+		),
+		'lg_requirement' => array(
+				'fd' => array(
+						'id' => array('type' => 'auto', 'precision' => 4, 'nullable' => false),
+						'activity_id' => array('type' => 'int', 'precision' => 4, 'nullable' => false),
+						'date_from' => array('type' => 'int', 'precision' => 4, 'nullable' => false),
+						'date_to' => array('type' => 'int', 'precision' => 4, 'nullable' => false)
+				),
+				'pk' => array('id'),
+				'fk' => array('lg_activity' => array('activity_id' => 'id')),
+				'ix' => array(),
+				'uc' => array()
+		),
+		'lg_requirement_resource_type' => array(
+				'fd' => array(
+						'id' => array('type' => 'auto', 'precision' => 4, 'nullable' => false),
+						'requirement_id' => array('type' => 'int', 'precision' => 4, 'nullable' => false),
+						'resource_type_id' => array('type' => 'int', 'precision' => 4, 'nullable' => false),
+						'no_of_elements' => array('type' => 'int', 'precision' => 4, 'nullable' => true)
+				),
+				'pk' => array('id'),
+				'fk' => array(
+						'lg_requirement' => array('requirement_id' => 'id'),
+						'fm_bim_type' => array('resource_type_id' => 'id')
+				),
+				'ix' => array(),
+				'uc' => array()
+		),
+		'lg_requirement_resource_allocation' => array(
+				'fd' => array(
+						'id' => array('type' => 'auto', 'precision' => 4, 'nullable' => false),
+						'requirement_id' => array('type' => 'int', 'precision' => 4, 'nullable' => false),
+						'article_id' => array('type' => 'int', 'precision' => 4, 'nullable' => false),
+						'type' => array('type' => 'int', 'precision' => 4, 'nullable' => false)
+				),
+				'pk' => array('id'),
+				'fk' => array(
+						'lg_requirement' => array('requirement_id' => 'id'),
+						'fm_bim_item' => array('article_id' => 'id', 'type' => 'type')
+				),
+				'ix' => array(),
+				'uc' => array()
+		),
+		'lg_bim_item_type_requirement' => array(
+				'fd' => array(
+						'id' => array('type' => 'auto', 'precision' => 4, 'nullable' => false),
+						'location_id' => array('type' => 'int', 'precision' => 4, 'nullable' => false),
+						'attribute_name' => array('type' => 'varchar', 'precision' => 255, 'nullable' => false),
+						'attribute_type' => array('type' => 'varchar', 'precision' => 255, 'nullable' => false),
+						'project_type_id' => array('type' => 'int', 'precision' => 4, 'nullable' => true)
+				),
+				'pk' => array('id'),
+				'fk' => array(
+						'lg_project_type' => array('project_type_id' => 'id'),
+						'phpgw_locations' => array('location_id' => 'location_id')
+				),
+				'ix' => array(),
+				'uc' => array()
+		),
+		'lg_requirement_value' => array(
+				'fd' => array(
+						'id' => array('type' => 'auto', 'precision' => 4, 'nullable' => false),
+						'type_requirement_id' => array('type' => 'int', 'precision' => 4, 'nullable' => false),
+						'value' => array('type' => 'varchar', 'precision' => '255', 'nullable' => true)
+				),
+				'pk' => array('id'),
+				'fk' => array('lg_bim_item_type_requirement' => array('type_requirement_id' => 'id')),
+				'ix' => array(),
+				'uc' => array()
+		),
+		'lg_calendar' => array(
+				'fd' => array(
+						'id' => array('type' => 'auto', 'precision' => 4, 'nullable' => false),
+						'location_id' => array('type' => 'int', 'precision' => 4, 'nullable' => false),
+						'item_id' => array('type' => 'int', 'precision' => 4, 'nullable' => false),
+						'type' => array('type' => 'int', 'precision' => 4, 'nullable' => false),
+						'tracking' => array('type' => 'varchar', 'precision' => '255', 'nullable' => true)
+				),
+				'pk' => array('id'),
+				'fk' => array('fm_bim_item' => array('item_id' => 'id', 'type' => 'type')),
+				'ix' => array(),
+				'uc' => array()
+		)
+);
