8d3b02fb4933397b93fde6692d7b1b6a023c8abd
Sigurd Nes <sigurdne@users.noreply.github.com>
2012-09-28 19:08:25 +0000

    logistic : propset svn:keywords "Revision Author Id"

--- logistic/inc/class.uiproject.inc.php
+++ logistic/inc/class.uiproject.inc.php
@@ -25,7 +25,7 @@
 	 * @internal Development of this application was funded by http://www.bergen.kommune.no/
 	 * @package property
 	 * @subpackage logistic
-	 * @version $Id:$
+	 * @version $Id$
 	 */
 	phpgw::import_class('phpgwapi.uicommon');
 	phpgw::import_class('logistic.soproject');
--- logistic/templates/base/project_type_item.xsl
+++ logistic/templates/base/project_type_item.xsl
@@ -1,4 +1,4 @@
-<!-- $Id:$ -->
+<!-- $Id$ -->
 <!-- item  -->
 
 <xsl:template match="data" xmlns:php="http://php.net/xsl">
