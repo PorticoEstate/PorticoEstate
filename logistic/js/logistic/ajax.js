972f9b5ceabdced7b636f71039d291b7422319cb
vator <torstein.vadla@gmail.com>
2012-10-02 12:17:22 +0000

    Logistic: Handler for selecting new parent activity

--- logistic/js/logistic/ajax.js
+++ logistic/js/logistic/ajax.js
@@ -1,32 +1,24 @@
 $(document).ready(function(){
-	alert("!");
+
 	$("#select_activity").change(function () {
-		
-		alert("!");
-		/*
-		 var location_code = $(this).val();
-		 var thisForm = $(this).parents("form");
+		 var activity_id = $(this).val();
+		 var thisForm = $(this).closest("form");
+ 		 var project_id = $(thisForm).find("input[name=project_id]").val();
+		 
+		 /*
+		 
 
 		 var period_type = $(thisForm).find("input[name='period_type']").val();
 		 var year = $(thisForm).find("input[name='year']").val();
 		 var month = $(thisForm).find("input[name='month']").val();
 		 
-		 if(location_code != "" & period_type == 'view_month')
-		 {
-			 var oArgs = {menuaction:'controller.uicalendar.view_calendar_for_month'};
-			 var baseUrl = phpGWLink('index.php', oArgs, false);
-			 var requestUrl = baseUrl + "&location_code=" + location_code + "&year=" + year + "&month=" + month;
+		 var oArgs = {menuaction:'controller.uicalendar.view_calendar_for_month'};
+		 var baseUrl = phpGWLink('index.php', oArgs, false);
+		 var requestUrl = baseUrl + "&location_code=" + location_code + "&year=" + year + "&month=" + month;
 			 
-			 window.location.href = requestUrl;
-		 }
-		 else if(location_code != "" & period_type == 'view_year')
-		 {
-			 var oArgs = {menuaction:'controller.uicalendar.view_calendar_for_year'};
-			 var baseUrl = phpGWLink('index.php', oArgs, false);
-			 var requestUrl = baseUrl +  "&location_code=" + location_code + "&year=" + year;
-			 
-			 window.location.href = requestUrl;
-		 }
+		 window.location.href = requestUrl;
+		
+		 
 		 */
    });	
 });
