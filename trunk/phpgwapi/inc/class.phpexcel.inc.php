6abeff3a7b39bdf1c3759a5ae315cd43cc41ca94
sigurdne <sigurdne@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2012-10-07 17:05:16 +0000

    API: add PHPExcel

---
+++ trunk/phpgwapi/inc/class.phpexcel.inc.php
@@ -0,0 +1,41 @@
+<?php
+	/**
+	 * phpGroupWare
+	 *
+	 * @author Sigurd Nes <sigurdne@online.no>
+	 * @copyright Copyright (C) 2012 Free Software Foundation, Inc. http://www.fsf.org/
+	 * @license http://www.fsf.org/licenses/gpl.html GNU General Public License
+	 * @package phpgroupware
+	 * @subpackage phpgwapi
+	 * @category utilities
+ 	 * @version $Id$
+	 */
+
+	/*
+	   This program is free software: you can redistribute it and/or modify
+	   it under the terms of the GNU General Public License as published by
+	   the Free Software Foundation, either version 2 of the License, or
+	   (at your option) any later version.
+
+	   This program is distributed in the hope that it will be useful,
+	   but WITHOUT ANY WARRANTY; without even the implied warranty of
+	   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	   GNU General Public License for more details.
+
+	   You should have received a copy of the GNU General Public License
+	   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	 */
+
+	/**
+	* Document me!
+	*
+	* @package phpgwapi
+	* @subpackage utilities
+	*/
+
+	/**
+	* Include the PHPExcel class
+	* @see PHPExcel
+	*/
+	require_once PHPGW_API_INC . '/phpexcel/PHPExcel.php';
+
