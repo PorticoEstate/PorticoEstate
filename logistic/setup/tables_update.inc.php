92617ba88dacf7856f7d66cc5ff53170ace6d08b
vator <torstein.vadla@gmail.com>
2012-10-15 07:58:09 +0000

    Logistic: Added column cust_attrib_id to lg_requirement_value and removed type_requirement_id

--- logistic/setup/tables_update.inc.php
+++ logistic/setup/tables_update.inc.php
@@ -15,3 +15,23 @@
 		}
 	}
 	
+	$test[] = '0.0.2';
+	function logistic_upgrade0_0_2()
+	{
+		$GLOBALS['phpgw_setup']->oProc->m_odb->transaction_begin();
+
+		$GLOBALS['phpgw_setup']->oProc->DropColumn('lg_requirement_value', array(), 'type_requirement_id');
+		
+		$GLOBALS['phpgw_setup']->oProc->AddColumn('lg_requirement_value','cust_attribute_id',array(
+			'type' => 'int',
+			'precision' => 4,
+			'nullable' => False
+		));
+	
+		if($GLOBALS['phpgw_setup']->oProc->m_odb->transaction_commit())
+		{
+			$GLOBALS['setup_info']['logistic']['currentver'] = '0.0.3';
+			return $GLOBALS['setup_info']['logistic']['currentver'];
+		}
+	}
+	
