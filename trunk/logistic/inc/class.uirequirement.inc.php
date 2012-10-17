89c11fba21b859e3106a1c89dc8014607dbfb00f
vator <vator@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2012-10-17 08:46:31 +0000

    Logistic: Added view method for requirement values

--- trunk/logistic/inc/class.uirequirement.inc.php
+++ trunk/logistic/inc/class.uirequirement.inc.php
@@ -37,6 +37,7 @@
 
 	include_class('logistic', 'requirement');
 	phpgw::import_class('phpgwapi.datetime');
+	phpgw::import_class('phpgwapi.jquery');
 
 	class uirequirement extends phpgwapi_uicommon
 	{
@@ -54,6 +55,7 @@
 			'edit' => true,
 			'view' => true,
 			'add_requirement_values' => true,
+			'view_requirement_values' => true,
 			'get_custom_attributes' => true
 		);
 
@@ -83,6 +85,8 @@
 				'dir' => phpgw::get_var('dir'),
 				'filters' => $filters
 			);
+			
+			$activity_id = phpgw::get_var('activity_id');
 
 			if ($GLOBALS['phpgw_info']['user']['preferences']['common']['maxmatchs'] > 0)
 			{
@@ -106,7 +110,7 @@
 
 			//Retrieve a contract identifier and load corresponding contract
 			$project_id = phpgw::get_var('project_id');
-
+			
 			$exp_param = phpgw::get_var('export');
 			$export = false;
 			if (isset($exp_param))
@@ -117,13 +121,12 @@
 
 			//Retrieve the type of query and perform type specific logic
 			$query_type = phpgw::get_var('type');
-			//var_dump($query_type);
 
 			switch ($query_type)
 			{
 				default: // ... all composites, filters (active and vacant)
 					phpgwapi_cache::session_set('logistic', 'requirement_query', $search_for);
-					//$filters = array('project_type' => phpgw::get_var('project_type'));
+					$filters = array('activity' => $activity_id);
 					$result_objects = $this->so->get($start_index, $num_of_objects, $sort_field, $sort_ascending, $search_for, $search_type, $filters);
 					$object_count = $this->so->get_count($search_for, $search_type, $filters);
 					break;
@@ -168,7 +171,8 @@
 			self::add_javascript('phpgwapi', 'yahoo', 'datatable.js');
 			phpgwapi_yui::load_widget('datatable');
 			phpgwapi_yui::load_widget('paginator');
-
+			$activity_id = phpgw::get_var('activity_id');
+			
 			$data = array(
 				'datatable_name'	=> lang('requirement'),
 				'form' => array(
@@ -187,7 +191,7 @@
 					),
 				),
 				'datatable' => array(
-					'source' => self::link(array('menuaction' => 'logistic.uirequirement.index', 'phpgw_return_as' => 'json')),
+					'source' => self::link(array('menuaction' => 'logistic.uirequirement.index', 'activity_id' => $activity_id, 'phpgw_return_as' => 'json')),
 					'field' => array(
 						array(
 							'key' => 'id',
@@ -287,13 +291,6 @@
 				$requirement->set_no_of_items( phpgw::get_var('no_of_items') );
 				$requirement->set_location_id( phpgw::get_var('location_id') );
 			
-				/*
-				$entity_id = phpgw::get_var('entity_id');
-				$category_id = phpgw::get_var('category_id');
-				
-				$location_id = $GLOBALS['phpgw']->locations->get_id('property',".entity.{$entity_id}.{$category_id}");
-				*/
-
 				if(phpgw::get_var('start_date','string') != '')
 				{
 					$start_date_ts = phpgwapi_datetime::date_to_timestamp( phpgw::get_var('start_date','string') );
@@ -313,11 +310,12 @@
 				{
 					$requirement->set_end_date(0);
 				}
-				
+					
 				$user_id = $GLOBALS['phpgw_info']['user']['id'];
 				$requirement->set_create_user($user_id);
 				
 				$requirement_id = $this->so->store($requirement);
+					
 				$GLOBALS['phpgw']->redirect_link('/index.php', array('menuaction' => 'logistic.uirequirement.add_requirement_values', 'requirement_id' => $requirement_id));
 			}
 			else if (isset($_POST['cancel_requirement']))
@@ -400,7 +398,7 @@
 		public function add_requirement_values()
 		{
 			$requirement_id = phpgw::get_var('requirement_id');
-
+			
 			if ($requirement_id && is_numeric($requirement_id))
 			{
 				$requirement = $this->so->get_single($requirement_id);
@@ -435,7 +433,7 @@
 					$requirement_id = $this->so_requirement_value->store($requirement_value);	
 				}
 	
-				$GLOBALS['phpgw']->redirect_link('/index.php', array('menuaction' => 'logistic.uirequirement.add_requirement_values', 'requirement_id' => $requirement_id));
+				$GLOBALS['phpgw']->redirect_link('/index.php', array('menuaction' => 'logistic.uirequirement.view_requirement_values', 'requirement_id' => $requirement_id));
 			}
 			else
 			{
@@ -462,12 +460,66 @@
 					$data['activity'] = $activity;
 				}
 				
-				$GLOBALS['phpgw']->jqcal->add_listener('start_date');
-				$GLOBALS['phpgw']->jqcal->add_listener('end_date');
-
+				phpgwapi_jquery::load_widget('core');
+				
 				self::add_javascript('logistic', 'logistic', 'requirement.js');
-				self::render_template_xsl(array('requirement/requirement_tabs', 'requirement/requirement_values'), $data);	
+				self::render_template_xsl(array('requirement/requirement_tabs', 'requirement/requirement_values'), $data);
 			}
+		}
+		
+		public function view_requirement_values()
+		{
+			$requirement_id = phpgw::get_var('requirement_id');
+
+			if ($requirement_id && is_numeric($requirement_id))
+			{
+				$requirement = $this->so->get_single($requirement_id);
+			}
+			else
+			{
+				$GLOBALS['phpgw']->redirect_link('/index.php', array('menuaction' => 'logistic.uirequirement.edit'));
+			}
+			
+			$filters = array('requirement_id' => $requirement_id);
+			$requirement_values_array = $this->so_requirement_value->get($start_index, $num_of_objects, $sort_field, $sort_ascending, $search_for, $search_type, $filters);
+	
+			$custom	= createObject('phpgwapi.custom_fields');
+	
+			foreach($requirement_values_array as $requirement_value)
+			{
+				$location_id = $requirement->get_location_id(); 
+		
+				$loc_arr = $GLOBALS['phpgw']->locations->get_name($location_id);
+				$entity_arr = explode('.',$loc_arr['location']);
+
+				$entity_id = $entity_arr[2];
+				$cat_id = $entity_arr[3];
+				$cust_attribute_id = $requirement_value->get_cust_attribute_id();
+									
+				$attrib_data = $custom->get('property', ".entity.{$entity_id}.{$cat_id}", $cust_attribute_id);
+
+
+				$requirement_attributes_array[] = array(
+																									"id" 							=> $requirement_value->get_id(),
+																									"value" 					=> $requirement_value->get_value(),
+																									"operator" 				=> $requirement_value->get_operator(), 
+																									"cust_attribute" 	=> $attrib_data
+																								);
+			}
+
+			print_r($requirement_attributes_array);
+			
+			$tabs = $this->make_tab_menu($requirement_id);
+			
+			$data = array
+			(
+				'tabs'													=> $GLOBALS['phpgw']->common->create_tabs($tabs, 1),
+				'view'													=> "requirement_values",
+				'requirement' 									=> $requirement,
+				'requirement_attributes_array'	=> $requirement_attributes_array
+			);
+			
+			self::render_template_xsl(array('requirement/requirement_tabs', 'requirement/requirement_values'), $data);	
 		}
 		
 		public function get_custom_attributes($location_id, $activity_id){
@@ -519,16 +571,16 @@
 				$requirement = $this->so->get_single($requirement_id);
 				
 				$tabs[] = array(
-							'label' => "1: " . lang('Details'),
+							'label' => "1: " . lang('Requirement details'),
 							'link'  => $GLOBALS['phpgw']->link('/index.php', array('menuaction' => 'controller.uirequirement.edit', 
 																				   'id' => $requirement->get_id()))
 						);
 			}else{
 				$tabs = array( 
 						   array(
-							'label' => "1: " . lang('Details')
+							'label' => "1: " . lang('Requirement details')
 						), array(
-							'label' => "2: " . lang('Add_requirement_values')
+							'label' => "2: " . lang('Add constraints')
 						));
 			}
 			
