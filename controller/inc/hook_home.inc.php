b06e4726a08cedb8eb75093c49c79b5401f27b5e
vator <torstein.vadla@gmail.com>
2013-01-02 09:20:31 +0000

    Controller: Removed comment out code for including jquery for this module

--- controller/inc/hook_home.inc.php
+++ controller/inc/hook_home.inc.php
@@ -82,7 +82,6 @@
 	$styling .= "\n".'<!-- END checklist info -->'."\n";
 	echo $styling;
 	
-//	echo "<script src='controller/js/controller/jquery.js'></script>";
 	echo "<script src='phpgwapi/js/jquery/js/jquery-1.7.2.min.js'></script>";
 	
 	$script = "<script>";
