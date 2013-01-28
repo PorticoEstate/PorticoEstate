6c7d042ce40adecd670d5fa5cbb6beca5c1f68cc
sigurdne <sigurdne@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2013-01-28 12:50:39 +0000

    logistic:  get all candidates for dropdown

--- trunk/logistic/inc/class.uiactivity.inc.php
+++ trunk/logistic/inc/class.uiactivity.inc.php
@@ -444,7 +444,7 @@
 
 			$accounts = $GLOBALS['phpgw']->acl->get_user_list_right(PHPGW_ACL_READ, 'run', 'logistic');
 
-			$activities = $this->so->get();
+			$activities = $this->so->get(0, 0, 'name', true, null, null, null, true);
 
 			if($activity_id)
 			{
