f05c2ab1baf67de4afc3101c7399799497c187cc
vator <vator@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2013-02-14 14:23:54 +0000

    Functionality for validating controller item case

--- trunk/controller/js/controller/ajax.js
+++ trunk/controller/js/controller/ajax.js
@@ -533,8 +533,7 @@ $(document).ready(function(){
 	// Display submit button on click
 	$("#frm_add_check_list").live("click", function(e){
 		var thisForm = $(this);
-		var submitBnt = $(thisForm).find("input[type='submit']");
-		$(submitBnt).removeClass("not_active");
+		var submitBnt = $(thisForm).find("input[type='submit']");		$(submitBnt).removeClass("not_active");
 	});
 
 	
