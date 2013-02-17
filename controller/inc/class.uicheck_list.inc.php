388f19fb8bce2f0f589578c28b1425387510dbe1
vator <torstein.vadla@gmail.com>
2013-02-17 14:36:55 +0000

    Deleted function view_cases_for_check_list

--- controller/inc/class.uicheck_list.inc.php
+++ controller/inc/class.uicheck_list.inc.php
@@ -66,9 +66,7 @@
 										'save_check_list' 					=> true,
 										'edit_check_list' 					=> true,
 										'view_control_info' 				=> true,
-										'view_cases_for_check_list'	=> true,
 										'print_check_list'					=> true,
-										'add_case'									=> true,
 										'view_control_details'			=> true,
 										'view_control_items'				=> true,
 										'get_check_list_info'				=> true, 
@@ -284,6 +282,7 @@
       
 			self::add_javascript('controller', 'controller', 'custom_ui.js');
 			self::add_javascript('controller', 'controller', 'ajax.js');
+      self::add_javascript('controller', 'controller', 'check_list.js');
       
       self::render_template_xsl(array('check_list/add_check_list', 'check_list/fragments/nav_control_plan', 
                                       'check_list/fragments/check_list_top_section', 'check_list/fragments/check_list_menu', 
@@ -360,6 +359,7 @@
 
 			self::add_javascript('controller', 'controller', 'custom_ui.js');
 			self::add_javascript('controller', 'controller', 'ajax.js');
+      self::add_javascript('controller', 'controller', 'check_list.js');
 			
 			self::render_template_xsl(array('check_list/fragments/check_list_menu', 'check_list/fragments/nav_control_plan', 
                                       'check_list/fragments/check_list_top_section', 'check_list/edit_check_list', 
@@ -456,74 +456,6 @@
 					$this->add_check_list($check_list);
 				}
 			}
-		}
-		
-		function view_cases_for_check_list()
-		{
-			$check_list_id = phpgw::get_var('check_list_id');
-			
-			$check_list = $this->so->get_single($check_list_id);
-			$control = $this->so_control->get_single($check_list->get_control_id());
-				
-			$component_id = $check_list->get_component_id();
-
-			if($component_id > 0)
-			{
-				$location_id = $check_list->get_location_id();
-				$component_id = $check_list->get_component_id();
-				
-				$component_arr = execMethod('property.soentity.read_single_eav', array('location_id' => $location_id, 'id' => $component_id));
-				$short_desc = execMethod('property.soentity.get_short_description', array('location_id' => $location_id, 'id' => $component_id));
-    		
-				$component = new controller_component();
-				$component->set_location_code( $component_arr['location_code'] );
-    		$component->set_xml_short_desc( $short_desc );
-				$component_array = $component->toArray();
-				
-				$type = 'component';
-				$building_location_code = $this->location_finder->get_building_location_code($component_arr['location_code']);
-			}
-			else
-			{
-				$location_code = $check_list->get_location_code();
-				$location_array = execMethod('property.bolocation.read_single', array('location_code' => $location_code));
-				$type = 'location';
-				$level = $this->location_finder->get_location_level($location_code);
-			}	
-			
-			$level = $this->location_finder->get_location_level($location_code);
-			$year = date("Y", $check_list->get_deadline());
-			$month = date("n", $check_list->get_deadline());
-							
-			$user_role = true;
-
-			// Fetches buildings on property
-			$buildings_on_property = $this->location_finder->get_buildings_on_property($user_role, $location_code, $level);
-      
-			$data = array
-			(
-				'control' 								=> $control,
-				'check_list' 							=> $check_list,
-        'buildings_on_property' 	=> $buildings_on_property,
-        'location_array'					=> $location_array,
-				'component_array'					=> $component_array,
-				'type' 										=> $type,
-				'current_year' 						=> $year,
-				'current_month_nr' 				=> $month,
-				'building_location_code' 	=> $building_location_code,
-				'location_level' 					=> $level,
-				'current_year' 						=> $year,
-				'current_month_nr' 				=> $month
-			);
-
-			phpgwapi_jquery::load_widget('core');
-			self::add_javascript('controller', 'controller', 'custom_ui.js');
-			self::add_javascript('controller', 'controller', 'ajax.js');
-      self::add_javascript('controller', 'controller', 'case.js');
-			
-			self::render_template_xsl(array('check_list/fragments/check_list_menu', 'check_list/fragments/nav_control_plan', 
-                                      'check_list/fragments/check_list_top_section', 'check_list/view_cases_for_check_list', 
-                                      'check_list/fragments/select_buildings_on_property'), $data);
 		}
 		
 		function view_control_info()
