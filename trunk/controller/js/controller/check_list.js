d64c02343da74c889a3290560faaa8a9f6fd4a5c
vator <vator@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2013-02-17 15:05:05 +0000

    Deleted function view_cases_for_check_list

--- trunk/controller/js/controller/check_list.js
+++ trunk/controller/js/controller/check_list.js
@@ -73,40 +73,43 @@ $(document).ready(function(){
 	});
   
   // UPDATE CHECKLIST STATUS
-	$("#frm-update-check-list-status").live("click", function(e){
+	$("#update-check-list-status").live("submit", function(e){
+    e.preventDefault();
+    
 		var thisForm = $(this);
 		var submitBnt = $(thisForm).find("input[type='submit']");
 		var requestUrl = $(thisForm).attr("action");
 		
-		var check_list_id = $("#check_list_id").val();
+     $.ajax({
+			  type: 'POST',
+			  url: requestUrl + "&" + $(thisForm).serialize(),
+			  success: function(data) {
+				  if(data){
+	    			  var jsonObj = jQuery.parseJSON(data);
+		    		
+	    			  if(jsonObj.status == "saved"){
+		    			  var submitBnt = $(thisForm).find("input[type='submit']");
+		    			  $(submitBnt).val("Lagret");	
+		    			  
+		    			  clear_form( thisForm );
+			      				  
+		    			  // Changes text on save button back to original
+		    			  window.setTimeout(function() {
+		    				  if( type == "control_item_type_2")
+		    					  $(submitBnt).val('Lagre måling');
+		    				  else
+		    					  $(submitBnt).val('Lagre sak');
+		    				  
+							$(submitBnt).addClass("not_active");
+		    			  }, 1000);
+
+		    			  $(thisForm).delay(1500).slideUp(500, function(){
+		    				  $(thisForm).parents("ul.expand_list").find("h4 img").attr("src", "controller/images/arrow_right.png");  
+		    			  });
+					  }
+				  }
+				}
+		});
 			
-		var statusFieldVal = $("#status").val();
-		var statusRow = $("#status").closest(".row");
-		var plannedDateVal = $("#planned_date").val();
-		var plannedDateRow = $("#planned_date").closest(".row");
-		var completedDateVal = $("#completed_date").val();
-		var completedDateRow = $("#completed_date").closest(".row");
-		
-		$(thisForm).find('.input_error_msg').remove();
-		
-		// Checks that COMPLETE DATE is set if status is set to DONE 
-		if(statusFieldVal == 1 & completedDateVal == ''){
-			e.preventDefault();
-			// Displays error message above completed date
-			$(completedDateRow).before("<div class='input_error_msg'>Vennligst angi når kontrollen ble utført</div>");
-    	}
-		else if(statusFieldVal == 0 && completedDateVal != ''){
-			e.preventDefault();
-			// Displays error message above completed date
-			$(statusRow).before("<div class='input_error_msg'>Vennligst endre status til utført eller slett utførtdato</div>");
-		}
-		else if(statusFieldVal == 0 & plannedDateVal == ''){
-			e.preventDefault();
-			// Displays error message above planned date
-			if( !$(plannedDateRow).prev().hasClass("input_error_msg") )
-			{
-			  $(plannedDateRow).before("<div class='input_error_msg'>Vennligst endre status for kontroll eller angi planlagtdato</div>");	
-			}
-		}	
 	});
 });
