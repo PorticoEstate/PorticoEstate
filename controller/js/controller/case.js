43d781cf0b6c2b3a3f68a0138cd3de6e58c88e14
vator <torstein.vadla@gmail.com>
2013-02-17 18:49:40 +0000

    Deleted function view_cases_for_check_list

--- controller/js/controller/case.js
+++ controller/js/controller/case.js
@@ -256,29 +256,15 @@ $(document).ready(function(){
 	});	
   
   $("#choose-building-on-property.view-cases").change(function () {
-		 var location_code = $(this).val();
-		 var thisForm = $(this).parents("form");
+     var location_code = $(this).val();
+		 var thisForm = $(this).closest("form");
 
-		 var period_type = $(thisForm).find("input[name='period_type']").val();
-		 var year = $(thisForm).find("input[name='year']").val();
-		 var month = $(thisForm).find("input[name='month']").val();
-		 
-		 if(location_code != "" & period_type == 'view_month')
-		 {
-			 var oArgs = {menuaction:'controller.uicalendar.view_calendar_for_month'};
-			 var baseUrl = phpGWLink('index.php', oArgs, false);
-			 var requestUrl = baseUrl + "&location_code=" + location_code + "&year=" + year + "&month=" + month;
-			 
-			 window.location.href = requestUrl;
-		 }
-		 else if(location_code != "" & period_type == 'view_year')
-		 {
-			 var oArgs = {menuaction:'controller.uicalendar.view_calendar_for_year'};
-			 var baseUrl = phpGWLink('index.php', oArgs, false);
-			 var requestUrl = baseUrl +  "&location_code=" + location_code + "&year=" + year;
-			 
-			 window.location.href = requestUrl;
-		 }
+     var url = location.href;
+console.log(url);
+     
+		 $(thisForm).find("input[name='location_code']").val(location_code);
+     console.log(location_code);
+    $(thisForm).submit();
     });
   
 });
