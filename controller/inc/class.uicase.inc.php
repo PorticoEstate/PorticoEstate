12f1a9ee9a36f33777d91440e3ea8631b3b6a409
Sigurd Nes <sigurdne@users.noreply.github.com>
2013-02-18 15:16:00 +0000

    controller: missing js

--- controller/inc/class.uicase.inc.php
+++ controller/inc/class.uicase.inc.php
@@ -171,6 +171,7 @@
 			self::add_javascript('controller', 'controller', 'custom_ui.js');
 			self::add_javascript('controller', 'controller', 'ajax.js');
             self::add_javascript('controller', 'controller', 'case.js');
+            self::add_javascript('controller', 'controller', 'check_list.js');
 			
 			self::render_template_xsl(array('check_list/fragments/check_list_menu', 'check_list/fragments/nav_control_plan', 
                                       'check_list/fragments/check_list_top_section', 'case/add_case', 
