79efa82776a80e43564b6e51d62f5c16c8d192c7
sigurdne <sigurdne@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2013-08-01 15:53:30 +0000

    test ajax menu

--- trunk/property/js/yahoo/test.menu.js
+++ trunk/property/js/yahoo/test.menu.js
@@ -1,29 +1,7 @@
-
-
-function get_html (sUrl)
-{
-	document.getElementById('html_content').src = sUrl;
-
-		var callback_2 = {
-			success: function(oResponse)
-			{
-				var oResults = oResponse.responseText;
-				document.getElementById("html_content").innerHTML = oResults;
-			},
-
-			failure: function(oResponse) {
-			},
-		};
-
-	YAHOO.util.Connect.asyncRequest('GET', sUrl, callback_2);
-
-}
-
-
 //an anonymous function wraps our code to keep our variables
 //in function scope rather than in the global namespace:
 (function() {
-//	var tree;
+	var tree;
 
 	function loadNodeData(node, fnLoadComplete)  {
 		//We'll load node data based on what we get back when we
@@ -34,14 +12,9 @@ function get_html (sUrl)
 		//server.  In our success handler, we'll build our new children
 		//and then return fnLoadComplete back to the tree.
 
-		//Get the node's label and urlencode it; this is the word/s
-		//on which we'll search for related words:
-		//var nodeLabelElId = encodeURI(node.labelElId);
-
-
-		var mapping_str = node.labelElId
-		mapping_id = mapping_str.replace('ygtvlabelel','');
+		mapping_id = node.index;
 		var app = mapping[mapping_id]['name'];
+		//var app = node.data['app'];
 
 		//prepare URL for XHR request:
 		var oArgs = {menuaction:'phpgwapi.menu.get_local_menu_ajax',node:app};
@@ -53,7 +26,6 @@ function get_html (sUrl)
 			//if our XHR call is successful, we want to make use
 			//of the returned data and create child nodes.
 			success: function(oResponse) {
-			//	YAHOO.log("XHR transaction was successful.", "info", "example");
 
 				var oResults = eval("(" + oResponse.responseText + ")");
 
@@ -71,35 +43,35 @@ function get_html (sUrl)
 						if (!titles[title]) {
 							titles[title] = true;
 
-							var myobj = {label: title, href:url}//,target:"_self" };
+							var myobj = {
+								label: title,
+								href:url,
+								data:{app: app + '|' + key}
+							}//,target:"_self" };
 							tempNode = new YAHOO.widget.TextNode( myobj, node, false);
 
-							var mapping_str = tempNode.labelElId
-							mapping_id = mapping_str.replace('ygtvlabelel','');
-							mapping[mapping_id] = {id: mapping_id, name: app + '|' + key, expanded: false, highlight: false};
+							mapping_id = tempNode.index;
+							mapping[mapping_id] = {id: mapping_id, name: app + '|' + key, expanded: false, highlight: false, is_leaf:is_leaf};
 
-							//----------
-							var parent_mapping_str = node.labelElId
-							parent_mapping_id = parent_mapping_str.replace('ygtvlabelel','');
+
+							parent_mapping_id = node.index;
 
 							myobj['parent'] = parent_mapping_id;
 							myobj['id'] = mapping_id;
 							myobj['isLeaf'] = is_leaf;
 							myobj['expanded'] = false;
-//							proxy_data.push(myobj);
 
-//console.log(myobj);
 							proxy_data[mapping_id] =  myobj;
 							if(parent_mapping_id)
 							{
-								proxy_data[parent_mapping_id]['expanded'] = true;
+				//				proxy_data[parent_mapping_id]['expanded'] = true;
 								mapping[parent_mapping_id]['expanded'] = true;
 							}
 
 							// we can tell the tree node that this is a leaf node so
 							// that it doesn't try to dynamically load children.
 
-					 
+
 							 if(is_leaf)
 							 {
 							 	tempNode.isLeaf = is_leaf;
@@ -116,12 +88,8 @@ function get_html (sUrl)
 						}
 					}
 
-console.log(proxy_data);
-console.log(mapping);
-
-					sessionStorage.cached_menu_tree_data = JSON.stringify(proxy_data);
-					sessionStorage.cached_mapping = JSON.stringify(mapping);
-
+					sessionStorage.cached_menu_tree_data = YAHOO.lang.JSON.stringify(proxy_data);
+					sessionStorage.cached_mapping = YAHOO.lang.JSON.stringify(mapping);
 				}
 
 				//When we're done creating child nodes, we execute the node's
@@ -135,7 +103,6 @@ console.log(mapping);
 			//fire the TreeView callback and let the Tree
 			//proceed with its business.
 			failure: function(oResponse) {
-				YAHOO.log("Failed to process XHR transaction.", "info", "example");
 				oResponse.argument.fnLoadComplete();
 			},
 
@@ -176,130 +143,138 @@ console.log(mapping);
 
 		if(typeof(Storage)!=="undefined")
 		{
-//			alert('Yes! localStorage and sessionStorage support!');
 			cached_menu_tree_data = sessionStorage.cached_menu_tree_data;
 			cached_mapping = sessionStorage.cached_mapping;
-
-//alert(cached_menu_tree_data);
 	 	}
 		else
 		{
 			alert('Sorry! No web storage support..');
 		}
 
-//		console.log(menu_tree_data);
-
 		if(cached_mapping)
 		{
-			mapping = JSON.parse(cached_mapping);
+			mapping = YAHOO.lang.JSON.parse(cached_mapping);
 		}
 
 		menu_tree_data = apps;
 		if(cached_menu_tree_data)
 		{
-//alert(cached_menu_tree_data);
-			cached_menu_tree_data = JSON.parse(cached_menu_tree_data);
-	//		proxy_data = cached_menu_tree_data;
+			cached_menu_tree_data = YAHOO.lang.JSON.parse(cached_menu_tree_data);
 			menu_tree_data = buildHierarchy(cached_menu_tree_data);
 		}
 
-	   //create a new tree:
-	   tree = new YAHOO.widget.TreeView("treeDiv1");
+		//create a new tree:
+		tree = new YAHOO.widget.TreeView("MenutreeDiv1");
+//		tree.singleNodeHighlight = true;
+		tree.setNodesProperty('propagateHighlightUp',true);
 
 
-   // Expand and collapse happen prior to the actual expand/collapse,
-    // and can be used to cancel the operation
-    tree.subscribe("expand", function(node) {
-		var mapping_str = node.labelElId
-		var mapping_id = mapping_str.replace('ygtvlabelel','');
-		alert(mapping_id);
- //       mapping[mapping_id]['expanded'] = true;
-//		sessionStorage.cached_mapping = JSON.stringify(mapping);
-        // return false; // return false to cancel the expand
-    });
- 
-    tree.subscribe("collapse", function(node) {
-		var mapping_str = node.labelElId
-		var mapping_id = mapping_str.replace('ygtvlabelel','');
-		alert(mapping_id);
-//        mapping[mapping_id]['expanded'] = false;
-//		sessionStorage.cached_mapping = JSON.stringify(mapping);
-    });
+	//	Expand and collapse happen prior to the actual expand/collapse,
+	//	and can be used to cancel the operation
+		tree.subscribe("expand", function(node) {
+			//var mapping_str = node.labelElId
+			var mapping_id = node.index; //mapping_str.replace('ygtvlabelel','');
+			mapping[mapping_id]['expanded'] = true;
+			sessionStorage.cached_mapping = JSON.stringify(mapping);
+			sessionStorage.cached_menu_tree_data = JSON.stringify(proxy_data);
+		});
 
+		tree.subscribe("collapse", function(node) {
+			//var mapping_str = node.labelElId
+			var mapping_id = node.index; //mapping_str.replace('ygtvlabelel','');
+			mapping[mapping_id]['expanded'] = false;
+			sessionStorage.cached_mapping = JSON.stringify(mapping);
+			sessionStorage.cached_menu_tree_data = JSON.stringify(proxy_data);
+		});
 
-	   //get root node for tree:
-	   var root = tree.getRoot();
+		tree.subscribe('clickEvent',tree.onEventToggleHighlight);
 
-	   //add child nodes for tree; our top level nodes are apps - defined in html
+		//get root node for tree:
+		var root = tree.getRoot();
 
-	   function buildTree(menu_tree_data)
-	   {
-//console.log(menu_tree_data);
-//console.log(mapping);
+		//add child nodes for tree; our top level nodes are apps - defined in html
 
+		function buildTree(menu_tree_data)
+		{
+			//Reset proxy_data
+			proxy_data = ['first_element_is_dummy'];
 			var buildBranch = function(node, branch)
 			{
 				for (var i = 0; i < branch.length; i++)
 				{
-					var tempNode = new YAHOO.widget.TextNode({label:branch[i]['value']['label'], href:branch[i]['value']['href']}, node, mapping[branch[i]['value']['id']]['expanded']);
-					var mapping_str = tempNode.labelElId
-					var mapping_id = mapping_str.replace('ygtvlabelel','');
+
+					var myobj = {
+						label: branch[i]['value']['label'],
+						href: branch[i]['value']['href']
+					//	data:{app: app + '|' + key}
+						}//,target:"_self" };
+
+					var tempNode = new YAHOO.widget.TextNode(myobj, node, mapping[branch[i]['value']['id']]['expanded']);
+
+					if(mapping[branch[i]['value']['id']]['name'] == menu_selection)
+					{
+						tempNode.label = '[' + tempNode.label + ']';
+						tempNode.highlight();
+					}
+
+					var mapping_id = tempNode.index;
 					new_mapping[mapping_id] = mapping[branch[i]['value']['id']];
 					new_mapping[mapping_id]['id'] = mapping_id;
 
-					var parent_mapping_str = node.labelElId
-					parent_mapping_id = parent_mapping_str.replace('ygtvlabelel','');
+					parent_mapping_id = node.index;
 
-/*					new_proxy_data[mapping_id]			= proxy_data[branch[i]['value']['id']];
-					new_proxy_data[mapping_id]['id']	= mapping_id;
-					new_proxy_data[mapping_id]['parent'] = parent_mapping_id;
-*/
+					myobj['parent'] = parent_mapping_id;
+					myobj['id'] = mapping_id;
+					proxy_data[mapping_id] =  myobj;
+
 					if(typeof(branch[i]['children']) != 'undefined' && branch[i]['children'].length)
 					{
 						buildBranch(tempNode, branch[i]['children']);
 					}
 					else
 					{
-						tempNode.isLeaf = branch[i]['value']['isLeaf'];
+						tempNode.isLeaf = mapping[branch[i]['value']['id']]['is_leaf'];
 						tempNode.setDynamicLoad(loadNodeData);
 					}
 				}
 			};
 
-		   var id = 0;
-		   for (var i=0, j=menu_tree_data.length; i<j; i++)
-		   {
-				var myobj = { 
+			for (var i=0, j=menu_tree_data.length; i<j; i++)
+			{
+				var myobj = {
 					label: menu_tree_data[i]['value']['label'],
-					href: menu_tree_data[i]['value']['href']
-					}//,target:"_self" };
-				
-			
+					href: menu_tree_data[i]['value']['href'],
+					enableHighlight: true
+					}
+
+
 				var tempNode = new YAHOO.widget.TextNode(myobj, root, mapping[menu_tree_data[i]['value']['id']]['expanded']);
 
-//				if(mapping[menu_tree_data[i]['value']['id']]['highlight'])
+				if(mapping[menu_tree_data[i]['value']['id']]['highlight'])
 				{
-//					tempNode.highlight();
+					tempNode.highlight();
 				}
 
-				var mapping_str = tempNode.labelElId
-				var mapping_id = mapping_str.replace('ygtvlabelel','');
+				if(mapping[menu_tree_data[i]['value']['id']]['name'] == menu_selection)
+				{
+					tempNode.label = '[' + tempNode.label + ']';
+					tempNode.highlight();
+				}
+
+				var mapping_id = tempNode.index;
 
 				var old_id = menu_tree_data[i]['value']['id'];
 				new_mapping[mapping_id] = mapping[menu_tree_data[i]['value']['id']];
 				new_mapping[mapping_id]['id'] = mapping_id;
-				
+
 				if(typeof(proxy_data[old_id]) != 'undefined')
 				{
 					new_proxy_data[mapping_id] = proxy_data[old_id];
 				}
-				//id = i + 1;
-				
-				id = mapping_id;
+
 				myobj['parent'] = '';
-				myobj['id'] = id;
-				myobj['expanded'] = false;
-				proxy_data[id] =  myobj;
+				myobj['id'] = mapping_id;
+				proxy_data[mapping_id] =  myobj;
 
 				if(typeof(menu_tree_data[i]['children']) != 'undefined' && menu_tree_data[i]['children'].length)
 				{
@@ -309,25 +284,17 @@ console.log(mapping);
 				{
 					tempNode.setDynamicLoad(loadNodeData);
 				}
-		   }
-
-		   //render tree with these toplevel nodes; all descendants of these nodes
-		   //will be generated as needed by the dynamic loader.
-
-		   tree.draw();
-			mapping = new_mapping;
-			if(new_proxy_data.length > 1)
-			{
-				proxy_data = new_proxy_data;
 			}
-	  }
+
+			//render tree with these toplevel nodes; all descendants of these nodes
+			//will be generated as needed by the dynamic loader.
+
+			tree.draw();
+			mapping = new_mapping;
+		}
+
 
 		buildTree(menu_tree_data);
-
-//console.log(mapping);
-
-//----------test -------------
-
 
 		function buildHierarchy(arry) {
 
@@ -338,11 +305,10 @@ console.log(mapping);
 			for (var i = 1, len = arry.length; i < len; ++i)
 			{
 				var item = arry[i],
-				    p = item.parent,
-				    target = !p ? roots : (children[p] || (children[p] = []));
+					p = item.parent,
+					target = !p ? roots : (children[p] || (children[p] = []));
 
 				target.push({ value: item });
-//				target.push(item);
 			}
 
 			// function to recursively build the tree
@@ -350,11 +316,11 @@ console.log(mapping);
 			{
 				if (children[parent.value.id])
 				{
-				    parent.children = children[parent.value.id];
-				    for (var i = 0, len = parent.children.length; i < len; ++i)
-				    {
-				        findChildren(parent.children[i]);
-				    }
+					parent.children = children[parent.value.id];
+					for (var i = 0, len = parent.children.length; i < len; ++i)
+					{
+						findChildren(parent.children[i]);
+					}
 				}
 			};
 
@@ -366,16 +332,9 @@ console.log(mapping);
 			return roots;
 		}
 
-
-// ------test -----------
-
-
 	}
-
-
 
 	//once the DOM has loaded, we can go ahead and set up our tree:
 	YAHOO.util.Event.onDOMReady(init);
-
 
 })();
