c4f489168252ee9beab7757a3dba30c9a4f398af
Sigurd Nes <sigurdne@users.noreply.github.com>
2013-08-11 08:51:29 +0000

    property: typo

--- property/inc/class.soentity.inc.php
+++ property/inc/class.soentity.inc.php
@@ -272,7 +272,7 @@
 							case '=':
 							case 'equal':
 							case 'eq':
-								$_querymethod[]= "xmlexists_('//{$attribute_name}[text() = ''{$condition['value']}'']' PASSING BY REF xml_representation)";
+								$_querymethod[]= "xmlexists('//{$attribute_name}[text() = ''{$condition['value']}'']' PASSING BY REF xml_representation)";
 								break;
 							case 'gt':
 							case '>':
