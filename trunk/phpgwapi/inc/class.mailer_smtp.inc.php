6eaff4e05079a46f35cc780ecb149ccfc719a7cf
sigurdne <sigurdne@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2013-09-16 13:20:16 +0000

    api: add switch for email-logging

--- trunk/phpgwapi/inc/class.mailer_smtp.inc.php
+++ trunk/phpgwapi/inc/class.mailer_smtp.inc.php
@@ -33,10 +33,47 @@
 			$this->PluginDir = PHPGW_INCLUDE_ROOT . '/phpgwapi/inc/phpmailer/';
 			$this->Host = $GLOBALS['phpgw_info']['server']['smtp_server'];
 			$this->Port = isset($GLOBALS['phpgw_info']['server']['smtp_port']) ? $GLOBALS['phpgw_info']['server']['smtp_port'] : 25;
-			$this->SMTPAuth = isset($GLOBALS['phpgw_info']['server']['smtpAuth']) && $GLOBALS['phpgw_info']['server']['smtpAuth'] == yes ? true : false;
 			$this->SMTPSecure = isset($GLOBALS['phpgw_info']['server']['smtpSecure']) ? $GLOBALS['phpgw_info']['server']['smtpSecure'] : '';
-			$this->Username = isset($GLOBALS['phpgw_info']['server']['smtpUser']) ? $GLOBALS['phpgw_info']['server']['smtpUser'] : '';
-			$this->Password =  isset($GLOBALS['phpgw_info']['server']['smtpPassword']) ? $GLOBALS['phpgw_info']['server']['smtpPassword'] : '';
 			$this->Version = 'custom - phpGroupWare 1.73';
+			$this->CharSet = 'utf-8';
+ 
+			if ( isset($GLOBALS['phpgw_info']['server']['smtpAuth']) && $GLOBALS['phpgw_info']['server']['smtpAuth'] == 'yes' )
+			{
+				$this->SMTPAuth	= true;
+				$this->Username = isset($GLOBALS['phpgw_info']['server']['smtpUser']) ? $GLOBALS['phpgw_info']['server']['smtpUser'] : '';
+				$this->Password =  isset($GLOBALS['phpgw_info']['server']['smtpPassword']) ? $GLOBALS['phpgw_info']['server']['smtpPassword'] : '';
+			}
+			
+		    /**
+		     * SMTP class debug output mode.
+		     * Options: 0 = off, 1 = commands, 2 = commands and data
+		     * @type int
+		     */
+
+			if ( isset($GLOBALS['phpgw_info']['server']['SMTPDebug']) )
+			{
+			    $this->SMTPDebug = (int)$GLOBALS['phpgw_info']['server']['SMTPDebug'];
+			}
+
+		    /**
+    		 * The function/method to use for debugging output.
+     		 * Options: 'echo', 'html' or 'error_log'
+    		 * @type string
+    		 * @see SMTP::$Debugoutput
+    		 */
+
+			if ( isset($GLOBALS['phpgw_info']['server']['Debugoutput']) && $GLOBALS['phpgw_info']['server']['smtpAuth'] != 'echo' )
+			{
+   				switch ($GLOBALS['phpgw_info']['server']['Debugoutput'])
+   				{
+   					case 'html':
+	    				$this->Debugoutput =  'html';
+	    				break;
+   					case 'errorlog':
+	    				$this->Debugoutput =  'error_log';
+	    				break;
+	    			default:
+   				}
+   			}
 		}
 	}
