4a17f02c21b4f8deeeb5e3c3083fd206e4a24c35
sigurdne <sigurdne@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2013-12-27 08:49:03 +0000

    Merge 11562:11572 from trunk

--- branches/Version-1_0-branch/property/inc/cron/default/import_oppdatering_av_bestilling_fra_agresso_bkb.php
+++ branches/Version-1_0-branch/property/inc/cron/default/import_oppdatering_av_bestilling_fra_agresso_bkb.php
@@ -24,7 +24,7 @@
 	* @internal Development of this application was funded by http://www.bergen.kommune.no/bbb_/ekstern/
 	* @package property
 	* @subpackage import
- 	* @version $Id: import_oppdatering_av_bestilling_fra_agresso_bkb.php 11456 2013-11-13 17:34:50Z sigurdne $
+ 	* @version $Id$
 	*/
 
 	/**
@@ -32,142 +32,29 @@
 	 * @package property
 	 */
 
+	include_class('property', 'cron_parent', 'inc/cron/');
 
-	class  import_oppdatering_av_bestilling_fra_agresso_bkb
+	class  import_oppdatering_av_bestilling_fra_agresso_bkb extends property_cron_parent
 	{
 		var	$function_name = 'import_oppdatering_av_bestilling_fra_agresso_bkb';
 		var $debug = true;
 		protected $updated_tickects = array();
 		protected $receipt = array();
 
-
 		function __construct()
 		{
-			$this->sotts			= CreateObject('property.sotts');
-			$this->db				= & $GLOBALS['phpgw']->db;
-			$this->join				= & $this->db->join;
-			$this->left_join		= & $this->db->left_join;
-			$this->like				= & $this->db->like;
+			parent::__construct();
 
+			$this->function_name = get_class($this);
+			$this->sub_location = lang('ticket');
+			$this->function_msg	= 'Importer rapport fra Agresso for oppdatering av meldinger';
+
+			$this->sotts			= CreateObject('property.sotts');
 			$this->config			= CreateObject('admin.soconfig',$GLOBALS['phpgw']->locations->get_id('property', '.invoice'));
 			$this->send				= CreateObject('phpgwapi.send');
 		}
 
-		function pre_run($data = array())
-		{
-			if(isset($data['enabled']) && $data['enabled']==1)
-			{
-				$confirm	= true;
-				$cron		= true;
-			}
-			else
-			{
-				$confirm	= phpgw::get_var('confirm', 'bool', 'POST');
-				$execute	= phpgw::get_var('execute', 'bool', 'GET');
-			}
-
-			if( isset($data['debug']) && $data['debug'] )
-			{
-				$this->debug = true;
-			}
-			else
-			{
-				$this->debug	= phpgw::get_var('debug', 'bool');
-			}
-
-			if ($confirm)
-			{
-				$this->execute($cron);
-			}
-			else
-			{
-				$this->confirm($execute=false);
-			}
-		}
-
-		function confirm($execute='')
-		{
-			$link_data = array
-			(
-				'menuaction'	=> 'property.custom_functions.index',
-				'function'		=> $this->function_name,
-				'execute'		=> $execute,
-				'debug'			=> $this->debug
-			);
-
-
-			if(!$execute)
-			{
-				$lang_confirm_msg 	= lang('do you want to perform this action');
-			}
-
-			$lang_yes			= lang('yes');
-
-			$GLOBALS['phpgw']->xslttpl->add_file(array('confirm_custom'));
-
-			$msgbox_data = $GLOBALS['phpgw']->common->msgbox_data($this->receipt);
-
-			$data = array
-			(
-				'msgbox_data'			=> $GLOBALS['phpgw']->common->msgbox($msgbox_data),
-				'done_action'			=> $GLOBALS['phpgw']->link('/index.php', array('menuaction'=>'property.uiasync.index')),
-				'run_action'			=> $GLOBALS['phpgw']->link('/index.php',$link_data),
-				'message'				=> $this->receipt['message'],
-				'lang_confirm_msg'		=> $lang_confirm_msg,
-				'lang_yes'				=> $lang_yes,
-				'lang_yes_statustext'	=> 'Importer rapport fra Agresso for oppdatering av meldinger',
-				'lang_no_statustext'	=> 'tilbake',
-				'lang_no'				=> lang('no'),
-				'lang_done'				=> 'Avbryt',
-				'lang_done_statustext'	=> 'tilbake'
-			);
-
-			$appname		= lang('location');
-			$function_msg	= 'Importer rapport fra Agresso for oppdatering av meldinger';
-			$GLOBALS['phpgw_info']['flags']['app_header'] = lang('property') . ' - ' . $appname . ': ' . $function_msg;
-			$GLOBALS['phpgw']->xslttpl->set_var('phpgw',array('confirm' => $data));
-			$GLOBALS['phpgw']->xslttpl->pp();
-		}
-
-		private function alert_assigned()
-		{
-
-			$updated_tickects = array_keys($this->updated_tickects);
-			
-			foreach ($updated_tickects as $id)
-			{
-				$this->db->query("SELECT assignedto FROM fm_tts_tickets WHERE id= '{$id}'",__LINE__,__FILE__);
-				$this->db->next_record();
-				$assignedto	= $this->db->f('assignedto');
-				$this->send_notification($assignedto, $id);
-			}
-		}
-
-		private function send_notification($assignedto = 0, $id = 0)
-		{
-			if (!isset($GLOBALS['phpgw_info']['server']['smtp_server']) || !$GLOBALS['phpgw_info']['server']['smtp_server'])
-			{
-				return;
-			}
-
-			$subject = 'Melding er oppdatert fra Agresso';
-			$from = "Ikke svar<IkkeSvar@Bergen.kommune.no>";
-			$prefs = $this->bocommon->create_preferences('property', $assignedto);
-			if(isset($prefs['email']) && $prefs['email'])
-			{
-				$body = '<a href ="' . $GLOBALS['phpgw']->link('/index.php', array('menuaction' => 'property.uitts.view', 'id' => $id),false,true).'">' . lang('Ticket').' #' .$id .'</a>'."\n";
-				try
-				{
-					$rc = $this->send->msg('email',$prefs['email'], $subject, stripslashes($body), '', '', '',$from,'','html');
-				}
-				catch (phpmailerException $e)
-				{
-					$this->receipt['error'][] = array('msg' => $e->getMessage());
-				}
-			}
-		}
-
-		public function execute($cron='')
+		public function execute()
 		{
 			$this->get_files();
 
@@ -241,28 +128,6 @@
 			{
 				$this->receipt['error'][] = array('msg' => "Arkiv katalog '{$dirname}/archive/' ikke er ikke skrivbar - kontakt systemadminstrator for å korrigere");
 			}
-
-			if(!$cron)
-			{
-				$this->confirm($execute=false);
-			}
-
-			$msgbox_data = $GLOBALS['phpgw']->common->msgbox_data($this->receipt);
-
-			$insert_values= array
-			(
-				$cron,
-				date($this->db->datetime_format()),
-				$this->function_name,
-				$this->db->db_addslashes(implode(',',(array_keys($msgbox_data))))
-			);
-
-			$insert_values	= $this->db->validate_insert($insert_values);
-
-			$sql = "INSERT INTO fm_cron_log (cron,cron_date,process,message) "
-					. "VALUES ($insert_values)";
-			$this->db->query($sql,__LINE__,__FILE__);
-
 		}
 
 		protected function get_files()
@@ -348,15 +213,14 @@ die();
 			$file_name = basename($file);
 
 			$fp = fopen($file,'rb');
-			$row = 1;
 
 			while (($data = fgetcsv($handle, 1000, ";")) !== false && $ok == true )
 			{
-				if($row > 1 && stripos( $file_name, 'penger' ) === 0)
+				if( preg_match('/^penger/i', $file_name) )
 				{
 					$ok = $this->update_amount($data);
 				}
-				else if($row > 1 && stripos( $file_name, 'status' ) === 0)
+				else if( preg_match('/^status/i', $file_name) )
 				{
 					$ok = $this->update_status($data);
 				}
@@ -366,7 +230,6 @@ die();
 			fclose($fp);
 
 			return $ok;
-
 		}
 
 		private function update_amount($data)
@@ -443,4 +306,41 @@ die();
 			return $ok;
 		}
 
+		private function alert_assigned()
+		{
+
+			$updated_tickects = array_keys($this->updated_tickects);
+			
+			foreach ($updated_tickects as $id)
+			{
+				$this->db->query("SELECT assignedto FROM fm_tts_tickets WHERE id= '{$id}'",__LINE__,__FILE__);
+				$this->db->next_record();
+				$assignedto	= $this->db->f('assignedto');
+				$this->send_notification($assignedto, $id);
+			}
+		}
+
+		private function send_notification($assignedto = 0, $id = 0)
+		{
+			if (!isset($GLOBALS['phpgw_info']['server']['smtp_server']) || !$GLOBALS['phpgw_info']['server']['smtp_server'])
+			{
+				return;
+			}
+
+			$subject = 'Melding er oppdatert fra Agresso';
+			$from = "Ikke svar<IkkeSvar@Bergen.kommune.no>";
+			$prefs = $this->bocommon->create_preferences('property', $assignedto);
+			if(isset($prefs['email']) && $prefs['email'])
+			{
+				$body = '<a href ="' . $GLOBALS['phpgw']->link('/index.php', array('menuaction' => 'property.uitts.view', 'id' => $id),false,true).'">' . lang('Ticket').' #' .$id .'</a>'."\n";
+				try
+				{
+					$rc = $this->send->msg('email',$prefs['email'], $subject, stripslashes($body), '', '', '',$from,'','html');
+				}
+				catch (phpmailerException $e)
+				{
+					$this->receipt['error'][] = array('msg' => $e->getMessage());
+				}
+			}
+		}
 	}
--- branches/Version-1_0-branch/property/inc/cron/default/synkroniser_med_boei.php
+++ branches/Version-1_0-branch/property/inc/cron/default/synkroniser_med_boei.php
@@ -32,13 +32,19 @@
 	 * @package property
 	 */
 
-	class synkroniser_med_boei
-	{
-		var	$function_name = 'synkroniser_med_boei';
+	include_class('property', 'cron_parent', 'inc/cron/');
 
-		function synkroniser_med_boei()
+	class synkroniser_med_boei extends property_cron_parent
+	{
+
+		function __construct()
 		{
-		//	$this->currentapp		= $GLOBALS['phpgw_info']['flags']['currentapp'];
+			parent::__construct();
+
+			$this->function_name = get_class($this);
+			$this->sub_location = lang('location');
+			$this->function_msg	= 'Synkroniser_med_boei';
+
 			$this->bocommon			= CreateObject('property.bocommon');
 			$this->db           	= $this->bocommon->new_db();
 			$this->join				= $this->db->join;
@@ -70,75 +76,8 @@
 			$this->db_boei2 = clone($this->db_boei);
 		}
 
-		function pre_run($data='')
-		{
-			if($data['enabled']==1)
-			{
-				$confirm	= true;
-				$cron		= true;
-			}
-			else
-			{
-				$confirm	= phpgw::get_var('confirm', 'bool', 'POST');
-				$execute	= phpgw::get_var('execute', 'bool', 'GET');
-			}
 
-			if ($confirm)
-			{
-				$this->execute($cron);
-			}
-			else
-			{
-				$this->confirm($execute=false);
-			}
-		}
-
-
-		function confirm($execute='')
-		{
-			$link_data = array
-			(
-				'menuaction' => 'property.custom_functions.index',
-				'function'	=>$this->function_name,
-				'execute'	=> $execute,
-			);
-
-
-			if(!$execute)
-			{
-				$lang_confirm_msg 	= lang('do you want to perform this action');
-			}
-
-			$lang_yes			= lang('yes');
-
-			$GLOBALS['phpgw']->xslttpl->add_file(array('confirm_custom'));
-
-
-			$msgbox_data = $this->bocommon->msgbox_data($this->receipt);
-
-			$data = array
-			(
-				'msgbox_data'			=> $GLOBALS['phpgw']->common->msgbox($msgbox_data),
-				'done_action'			=> $GLOBALS['phpgw']->link('/admin/index.php'),
-				'run_action'			=> $GLOBALS['phpgw']->link('/index.php',$link_data),
-				'message'				=> $this->receipt['message'],
-				'lang_confirm_msg'		=> $lang_confirm_msg,
-				'lang_yes'				=> $lang_yes,
-				'lang_yes_statustext'	=> lang('Update the category to not active based on if there is only nonactive apartments'),
-				'lang_no_statustext'	=> 'tilbake',
-				'lang_no'				=> lang('no'),
-				'lang_done'				=> 'Avbryt',
-				'lang_done_statustext'	=> 'tilbake'
-			);
-
-			$appname		= lang('location');
-			$function_msg	= 'synkroniser med BOEI';
-			$GLOBALS['phpgw_info']['flags']['app_header'] = lang('property') . ' - ' . $appname . ': ' . $function_msg;
-			$GLOBALS['phpgw']->xslttpl->set_var('phpgw',array('confirm' => $data));
-			$GLOBALS['phpgw']->xslttpl->pp();
-		}
-
-		function execute($cron='')
+		function execute()
 		{
 			set_time_limit(500);
 			$receipt = $this->legg_til_eier_phpgw();
@@ -170,17 +109,13 @@
 			$receipt = $this->update_tenant_name();
 			$this->cron_log($receipt,$cron);
 
-			if(!$cron)
-			{
-				$this->confirm($execute=false);
-			}
 		}
 
-		function cron_log($receipt='',$cron='')
+		function cron_log($receipt='')
 		{
 
 			$insert_values= array(
-				$cron,
+				$this->cron,
 				date($this->db->datetime_format()),
 				$this->function_name,
 				$receipt
@@ -989,4 +924,3 @@
 			return $msg;
 		}
 	}
-
