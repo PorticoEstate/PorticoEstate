51386764bb9a63d8c08f87b59ec12fbc49c71b11
sigurdne <sigurdne@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2014-02-19 08:16:21 +0000

    property: formatting, switch config

--- trunk/property/inc/class.boXport.inc.php
+++ trunk/property/inc/class.boXport.inc.php
@@ -1,5 +1,4 @@
 <?php
-
 	/**
 	 * phpGroupWare - property: a Facilities Management System.
 	 *
@@ -36,10 +35,10 @@
 	{
 
 		var $public_functions = array
-		(
-			'import'		=> true,
-			'export'		=> true,
-			'export_cron'	=> true
+			(
+			'import'		 => true,
+			'export'		 => true,
+			'export_cron'	 => true
 		);
 		var $start;
 		var $query;
@@ -47,58 +46,58 @@
 		var $order;
 		var $filter;
 		var $cat_id;
-		var $use_session = false;
+		var $use_session		 = false;
 
-		function property_boXport( $session = false )
+		function property_boXport($session = false)
 		{
 
 			$GLOBALS['phpgw_info']['flags']['currentapp'] = 'property';
 
-			$this->config = CreateObject( 'phpgwapi.config', 'property' );
+			$this->config = CreateObject('admin.soconfig', $GLOBALS['phpgw']->locations->get_id('property', '.invoice'));
 
-			if ( $session )
+			if($session)
 			{
 				$this->read_sessiondata();
 				$this->use_session = true;
 			}
 
-			$start	 = phpgw::get_var( 'start', 'int', 'REQUEST', 0 );
-			$query	 = phpgw::get_var( 'query' );
-			$sort	 = phpgw::get_var( 'sort' );
-			$order	 = phpgw::get_var( 'order' );
-			$filter	 = phpgw::get_var( 'filter', 'int' );
-			$cat_id	 = phpgw::get_var( 'cat_id', 'int' );
+			$start	 = phpgw::get_var('start', 'int', 'REQUEST', 0);
+			$query	 = phpgw::get_var('query');
+			$sort	 = phpgw::get_var('sort');
+			$order	 = phpgw::get_var('order');
+			$filter	 = phpgw::get_var('filter', 'int');
+			$cat_id	 = phpgw::get_var('cat_id', 'int');
 
-			if ( $start || $start == 0 )
+			if($start || $start == 0)
 			{
 				$this->start = $start;
 			}
-			if ( $query )
+			if($query)
 			{
 				$this->query = $query;
 			}
-			if ( $sort )
+			if($sort)
 			{
 				$this->sort = $sort;
 			}
-			if ( $order )
+			if($order)
 			{
 				$this->order = $order;
 			}
-			if ( $filter )
+			if($filter)
 			{
-				$this->filter	 = $filter;
+				$this->filter = $filter;
 			}
-			$this->cat_id	 = $cat_id;
+			$this->cat_id = $cat_id;
 		}
 
 		function save_sessiondata()
 		{
 
-			if ( $this->use_session )
+			if($this->use_session)
 			{
 				$data = array
-				(
+					(
 					'start'	 => $this->start,
 					'query'	 => $this->query,
 					'sort'	 => $this->sort,
@@ -106,18 +105,18 @@
 					'filter' => $this->filter,
 					'cat_id' => $this->cat_id
 				);
-				if ( $this->debug )
+				if($this->debug)
 				{
 					echo '<br>Save:';
-					_debug_array( $data );
+					_debug_array($data);
 				}
-				$GLOBALS['phpgw']->session->appsession( 'session_data', 'export', $data );
+				$GLOBALS['phpgw']->session->appsession('session_data', 'export', $data);
 			}
 		}
 
 		function read_sessiondata()
 		{
-			$data = $GLOBALS['phpgw']->session->appsession( 'session_data', 'export' );
+			$data = $GLOBALS['phpgw']->session->appsession('session_data', 'export');
 
 			$this->start	 = $data['start'];
 			$this->query	 = $data['query'];
@@ -127,27 +126,27 @@
 			$this->cat_id	 = $data['cat_id'];
 		}
 
-		function select_import_conv( $selected = '' )
+		function select_import_conv($selected = '')
 		{
-			$dir_handle	 = @opendir( PHPGW_SERVER_ROOT . "/property/inc/import/{$GLOBALS['phpgw_info']['user']['domain']}" );
+			$dir_handle	 = @opendir(PHPGW_SERVER_ROOT . "/property/inc/import/{$GLOBALS['phpgw_info']['user']['domain']}");
 			$i			 = 0;
 			$myfilearray = array();
-			while ( $file = readdir( $dir_handle ) )
+			while($file		 = readdir($dir_handle))
 			{
-				if ( (substr( $file, 0, 1 ) != '.') && is_file( PHPGW_SERVER_ROOT . "/property/inc/import/{$GLOBALS['phpgw_info']['user']['domain']}/{$file}" ) )
+				if((substr($file, 0, 1) != '.') && is_file(PHPGW_SERVER_ROOT . "/property/inc/import/{$GLOBALS['phpgw_info']['user']['domain']}/{$file}"))
 				{
 					$myfilearray[$i] = $file;
 					$i++;
 				}
 			}
-			closedir( $dir_handle );
-			sort( $myfilearray );
+			closedir($dir_handle);
+			sort($myfilearray);
 
-			for ( $i = 0; $i < count( $myfilearray ); $i++ )
+			for($i = 0; $i < count($myfilearray); $i++)
 			{
-				$fname		 = preg_replace( '/_/', ' ', $myfilearray[$i] );
+				$fname		 = preg_replace('/_/', ' ', $myfilearray[$i]);
 				$sel_file	 = '';
-				if ( $myfilearray[$i] == $selected )
+				if($myfilearray[$i] == $selected)
 				{
 					$sel_file = 'selected';
 				}
@@ -160,38 +159,38 @@
 				);
 			}
 
-			for ( $i = 0; $i < count( $conv_list ); $i++ )
+			for($i = 0; $i < count($conv_list); $i++)
 			{
-				if ( $conv_list[$i]['selected'] != 'selected' )
+				if($conv_list[$i]['selected'] != 'selected')
 				{
-					unset( $conv_list[$i]['selected'] );
+					unset($conv_list[$i]['selected']);
 				}
 			}
 
 			return $conv_list;
 		}
 
-		function select_export_conv( $selected = '' )
+		function select_export_conv($selected = '')
 		{
-			$dir_handle	 = @opendir( PHPGW_SERVER_ROOT . "/property/inc/export/{$GLOBALS['phpgw_info']['user']['domain']}" );
+			$dir_handle	 = @opendir(PHPGW_SERVER_ROOT . "/property/inc/export/{$GLOBALS['phpgw_info']['user']['domain']}");
 			$i			 = 0;
 			$myfilearray = array();
-			while ( $file = readdir( $dir_handle ) )
+			while($file		 = readdir($dir_handle))
 			{
-				if ( (substr( $file, 0, 1 ) != '.') && is_file( PHPGW_SERVER_ROOT . "/property/inc/export/{$GLOBALS['phpgw_info']['user']['domain']}/{$file}" ) )
+				if((substr($file, 0, 1) != '.') && is_file(PHPGW_SERVER_ROOT . "/property/inc/export/{$GLOBALS['phpgw_info']['user']['domain']}/{$file}"))
 				{
 					$myfilearray[$i] = $file;
 					$i++;
 				}
 			}
-			closedir( $dir_handle );
-			sort( $myfilearray );
+			closedir($dir_handle);
+			sort($myfilearray);
 
-			for ( $i = 0; $i < count( $myfilearray ); $i++ )
+			for($i = 0; $i < count($myfilearray); $i++)
 			{
-				$fname		 = preg_replace( '/_/', ' ', $myfilearray[$i] );
+				$fname		 = preg_replace('/_/', ' ', $myfilearray[$i]);
 				$sel_file	 = '';
-				if ( $myfilearray[$i] == $selected )
+				if($myfilearray[$i] == $selected)
 				{
 					$sel_file = 'selected';
 				}
@@ -204,41 +203,40 @@
 				);
 			}
 
-			for ( $i = 0; $i < count( $conv_list ); $i++ )
+			for($i = 0; $i < count($conv_list); $i++)
 			{
-				if ( $conv_list[$i]['selected'] != 'selected' )
+				if($conv_list[$i]['selected'] != 'selected')
 				{
-					unset( $conv_list[$i]['selected'] );
+					unset($conv_list[$i]['selected']);
 				}
 			}
 
 			return $conv_list;
 		}
 
-		function select_rollback_file( $selected = '' )
+		function select_rollback_file($selected = '')
 		{
-			$this->config->read();
-			$file_catalog = $this->config->config_data['export_path'];
+			$file_catalog = $this->config->config_data['export']['path'];
 
-			$dir_handle	 = @opendir( $file_catalog );
+			$dir_handle	 = @opendir($file_catalog);
 			$i			 = 0;
 			$myfilearray = '';
-			while ( $file		 = readdir( $dir_handle ) )
+			while($file		 = readdir($dir_handle))
 			{
-				if ( (substr( $file, 0, 1 ) != '.') && is_file( "{$file_catalog}/{$file}" ) )
+				if((substr($file, 0, 1) != '.') && is_file("{$file_catalog}/{$file}"))
 				{
 					$myfilearray[$i] = $file;
 					$i++;
 				}
 			}
-			closedir( $dir_handle );
-			@sort( $myfilearray );
+			closedir($dir_handle);
+			@sort($myfilearray);
 
-			for ( $i = 0; $i < count( $myfilearray ); $i++ )
+			for($i = 0; $i < count($myfilearray); $i++)
 			{
-				$fname		 = preg_replace( '/_/', ' ', $myfilearray[$i] );
+				$fname		 = preg_replace('/_/', ' ', $myfilearray[$i]);
 				$sel_file	 = '';
-				if ( $myfilearray[$i] == $selected )
+				if($myfilearray[$i] == $selected)
 				{
 					$sel_file = 'selected';
 				}
@@ -251,24 +249,24 @@
 				);
 			}
 
-			for ( $i = 0; $i < count( $rollback_list ); $i++ )
+			for($i = 0; $i < count($rollback_list); $i++)
 			{
-				if ( $rollback_list[$i]['selected'] != 'selected' )
+				if($rollback_list[$i]['selected'] != 'selected')
 				{
-					unset( $rollback_list[$i]['selected'] );
+					unset($rollback_list[$i]['selected']);
 				}
 			}
 
 			return $rollback_list;
 		}
 
-		function import( $invoice_common, $download )
+		function import($invoice_common, $download)
 		{
 			include (PHPGW_SERVER_ROOT . "/property/inc/import/{$GLOBALS['phpgw_info']['user']['domain']}/{$invoice_common['conv_type']}");
 			$invoice = new import_conv;
 
-			$buffer = $invoice->import( $invoice_common, $download );
-			if ( $download )
+			$buffer = $invoice->import($invoice_common, $download);
+			if($download)
 			{
 				$header	 = $invoice->header;
 				$import	 = $invoice->import;
@@ -281,7 +279,7 @@
 			return $buffer;
 		}
 
-		function export( $data )
+		function export($data)
 		{
 			$conv_type			 = $data['conv_type'];
 			$download			 = $data['download'];
@@ -291,32 +289,31 @@
 			include(PHPGW_SERVER_ROOT . "/property/inc/export/{$GLOBALS['phpgw_info']['user']['domain']}/{$conv_type}");
 			$invoice = new export_conv;
 
-			$buffer = $invoice->overfor( $download, $pre_transfer, $force_period_year );
+			$buffer = $invoice->overfor($download, $pre_transfer, $force_period_year);
 
 			return $buffer;
 		}
 
-		function rollback( $conv_type, $role_back_date, $rollback_file,
-					 $rollback_voucher )
+		function rollback($conv_type, $role_back_date, $rollback_file, $rollback_voucher)
 		{
 			include (PHPGW_SERVER_ROOT . "/property/inc/export/{$GLOBALS['phpgw_info']['user']['domain']}/{$conv_type}");
 			$invoice = new export_conv;
-			$buffer	 = $invoice->RullTilbake( $role_back_date, $rollback_file,
-									 $rollback_voucher );
+			$buffer	 = $invoice->RullTilbake($role_back_date, $rollback_file, $rollback_voucher);
 			return $buffer;
 		}
 
-		function export_cron( $data = array())
+		function export_cron($data = array())
 		{
 			if(!$data)
 			{
-				$data = unserialize(urldecode(phpgw::get_var('data')));
-				$data = phpgw::clean_value($data);
+				$data	 = unserialize(urldecode(phpgw::get_var('data')));
+				$data	 = phpgw::clean_value($data);
 			}
 			_debug_array($data);
-			$receipt = $this->export( $data );
+			$receipt = $this->export($data);
 			{
-				_debug_array( $receipt );
+				_debug_array($receipt);
 			}
 		}
-	}
+
+	}	
