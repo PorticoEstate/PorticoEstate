e3c5fb9749a28d2fcbe2d978c48f82fe46f5dec8
Sigurd Nes <sigurdne@users.noreply.github.com>
2014-05-13 09:19:45 +0000

    API: update phpmailer version

--- phpgwapi/inc/class.mailer_smtp.inc.php
+++ phpgwapi/inc/class.mailer_smtp.inc.php
@@ -34,7 +34,7 @@
 			$this->Host = $GLOBALS['phpgw_info']['server']['smtp_server'];
 			$this->Port = isset($GLOBALS['phpgw_info']['server']['smtp_port']) ? $GLOBALS['phpgw_info']['server']['smtp_port'] : 25;
 			$this->SMTPSecure = isset($GLOBALS['phpgw_info']['server']['smtpSecure']) ? $GLOBALS['phpgw_info']['server']['smtpSecure'] : '';
-			$this->Version = 'custom - phpGroupWare 1.73';
+			$this->Version = 'custom - phpGroupWare 5.2.7';
 			$this->CharSet = 'utf-8';
  
 			if ( isset($GLOBALS['phpgw_info']['server']['smtpAuth']) && $GLOBALS['phpgw_info']['server']['smtpAuth'] == 'yes' )
