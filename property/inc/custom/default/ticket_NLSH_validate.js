ca57c87639d9b19afb5f0f35451ab28a027b9e0f
Sigurd Nes <sigurdne@users.noreply.github.com>
2014-06-29 15:06:35 +0000

    property: test hide field

--- property/inc/custom/default/ticket_NLSH_validate.js
+++ property/inc/custom/default/ticket_NLSH_validate.js
@@ -1,13 +1,22 @@
 
+$(document).ready(function()
+{
+	if (!$("#global_category_id").val() === 20)
+	{
+		document.getElementById('label_feilkoder').style.display = 'none';
+		document.getElementById('id_feilkoder').style.display = 'none';
+	}
+});
+
 function validate_submit()
 {
 	var error = false;
 	var feilkode_id = $("#id_feilkoder").val();
 	var category_id = $("#global_category_id").val();
 	var group_id = $("#global_category_id").val();
-	var status_id = $("#status_id").val();	
+	var status_id = $("#status_id").val();
 
-	if(category_id == 20)
+	if (category_id == 20)
 	{
 		if (!feilkode_id && status_id == 'X')
 		{
@@ -15,7 +24,7 @@ function validate_submit()
 		}
 	}
 
-	if(error)
+	if (error)
 	{
 		alert('Feilkode må velges før meldingen kan avsluttes');
 	}
