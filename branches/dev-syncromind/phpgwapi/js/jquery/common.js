b7837eae64745c1e50b3df9e30c0ae84c4e3c51e
nudrick <nudrick@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2014-10-10 21:02:01 +0000

    add autocomplete jquery helper

--- branches/dev-syncromind/phpgwapi/js/jquery/common.js
+++ branches/dev-syncromind/phpgwapi/js/jquery/common.js
@@ -11,3 +11,43 @@ JqueryPortico.formatLink = function(key, oData) {
 	var link = oData['link'];
 	return '<a href="' + link + '">' + name + '</a>';
 };
+
+JqueryPortico.autocompleteHelper = function(baseUrl, field, hidden, container, label_attr) {
+	$(document).ready(function () 
+	{
+		var oArgs = {menuaction:'property.uicondition_survey.get_users'};
+		var strURL = phpGWLink('index.php', oArgs, true);
+
+		$("#coordinator_name").autocomplete({
+			source: function( request, response ) {
+				$.ajax({
+					url: strURL,
+					dataType: "json",
+					data: {
+						location_name: request.term,
+						phpgw_return_as: "json"
+					},
+					success: function( data ) {
+						response( $.map( data.ResultSet.Result, function( item ) {
+							return {
+								label: item.name,
+								value: item.id
+							}
+						}));
+					}
+				});
+			},
+			focus: function (event, ui) {
+				$(event.target).val(ui.item.label);
+				return false;
+			},
+			minLength: 1,
+			select: function( event, ui ) {
+			  chooseLocation( ui.item.label, ui.item.value);
+			}
+        });
+	});
+
+};
+		
+		
