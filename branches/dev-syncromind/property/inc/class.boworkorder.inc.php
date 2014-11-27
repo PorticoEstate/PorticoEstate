0f658959db89417a147a3216459e8ec585b53a69
psaul <psaul@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2014-11-27 23:23:16 +0000

    class.boworkorder.inc

--- branches/dev-syncromind/property/inc/class.boworkorder.inc.php
+++ branches/dev-syncromind/property/inc/class.boworkorder.inc.php
@@ -504,6 +504,7 @@
 
 		function read($data = array())
 		{
+                        
 			$start_date	= $this->bocommon->date_to_timestamp($data['start_date']);
 			$end_date	= $this->bocommon->date_to_timestamp($data['end_date']);
 
@@ -512,13 +513,34 @@
 				$data['allrows'] = true;
 			}
 
-			$workorder = $this->so->read(array('start' => $this->start,'query' => $this->query,'sort' => $this->sort,'order' => $this->order,
+			/*$workorder = $this->so->read(array('start' => $this->start,'query' => $this->query,'sort' => $this->sort,'order' => $this->order,
 				'filter' => $this->filter,'cat_id' => $this->cat_id,'status_id' => $this->status_id,
 				'wo_hour_cat_id' => $this->wo_hour_cat_id,
 				'start_date'=>$start_date,'end_date'=>$end_date,'allrows'=>$data['allrows'],
 				'b_group'=>$this->b_group,'ecodimb'=>$this->ecodimb, 'paid'=>$this->paid,'b_account' => $this->b_account,
 				'district_id' => $this->district_id,'dry_run'=>$data['dry_run'], 'criteria' => $this->get_criteria($this->criteria_id),
-				'obligation' => $this->obligation, 'filter_year' => $this->filter_year) );
+				'obligation' => $this->obligation, 'filter_year' => $this->filter_year) );*/
+                        $workorder = $this->so->read(array(
+                                    'start' => $data['start'],
+                                    'query' => $data['query'],
+                                    'sort' => $data['sort'],
+                                    'order' => $data['order'],
+                                    'filter' => $this->filter,
+                                    'cat_id' => $this->cat_id,
+                                    'status_id' => $this->status_id,
+                                    'wo_hour_cat_id' => $this->wo_hour_cat_id,
+                                    'start_date'=>$start_date,
+                                    'end_date'=>$end_date,
+                                    'allrows'=>$data['allrows'],
+                                    'b_group'=>$this->b_group,
+                                    'ecodimb'=>$this->ecodimb, 
+                                    'paid'=>$this->paid,
+                                    'b_account' => $this->b_account,
+                                    'district_id' => $this->district_id,
+                                    'dry_run'=>$data['dry_run'], 
+                                    'criteria' => $this->get_criteria($this->criteria_id),
+                                    'obligation' => $this->obligation, 
+                                    'filter_year' => $this->filter_year ) );
 
 			$this->total_records = $this->so->total_records;
 
