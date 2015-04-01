ac4eb77ef9e8220851839e98fd829daee8b330cb
psaul <psaul@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2015-04-01 23:16:02 +0000

    class.soagreement.inc

--- branches/dev-syncromind/property/inc/class.soagreement.inc.php
+++ branches/dev-syncromind/property/inc/class.soagreement.inc.php
@@ -387,6 +387,18 @@
 
 		function read_details($data)
 		{
+            //print_r($data);
+            
+//    [start] => 0
+//    [query] => 
+//    [sort] => asc
+//    [order] => num
+//    [filter] => 
+//    [cat_id] => 
+//    [allrows] => 
+//    [member_id] => 
+//    [agreement_id] => 3
+//            
 			$start			= isset($data['start']) && $data['start'] ? $data['start']:0;
 			$filter			= isset($data['filter']) && $data['filter'] ? $data['filter']:'none';
 			$query 			= isset($data['query']) ? $data['query'] : '';
@@ -479,9 +491,9 @@
 			{
 				$ordermethod = "ORDER BY {$entity_table}.activity_id DESC";
 			}
-
+//            ORDER BY fm_activity_price_index.num asc
 			$sql = "SELECT {$cols} FROM {$paranthesis} {$entity_table} {$joinmethod}";
-
+            
 			$this->uicols	= $uicols;
 
 			$where= 'WHERE';
@@ -495,8 +507,8 @@
 			}
 
 			$sql .= " $filtermethod";
-			//echo $sql;
-
+			//echo $sql.$ordermethod;
+            
 			$this->db2->query($sql,__LINE__,__FILE__);
 			$this->total_records = $this->db2->num_rows();
 			if(!$allrows)
@@ -506,8 +518,9 @@
 			else
 			{
 				$this->db->query($sql . $ordermethod,__LINE__,__FILE__);
+                
 			}
-
+            
 			$dateformat = $GLOBALS['phpgw_info']['user']['preferences']['common']['dateformat'];
 
 			while ($this->db->next_record())
