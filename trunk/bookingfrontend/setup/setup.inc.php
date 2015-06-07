5f59e02df39bad2222001f42463b0ff5eb5c5a1a
sigurdne <sigurdne@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2015-06-07 20:20:15 +0000

    update dependencies

--- trunk/bookingfrontend/setup/setup.inc.php
+++ trunk/bookingfrontend/setup/setup.inc.php
@@ -21,7 +21,7 @@
 
 	$setup_info['bookingfrontend']['depends'][] = array(
 		'appname' => 'booking',
-		'versions' => Array('0.2.10', '0.2.11','0.2.12','0.2.13','0.2.14','0.2.15','0.2.16','0.2.17','0.2.18','0.2.19','0.2.20')
+		'versions' => Array('0.2.20')
 	);
 
 	$setup_info['bookingfrontend']['depends'][] = array(
