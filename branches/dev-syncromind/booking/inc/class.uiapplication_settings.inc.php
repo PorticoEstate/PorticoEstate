ac7e4b21a6de2455dbcf60b8e65f3ed89a09502b
psaul <psaul@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2015-06-25 22:46:13 +0000

    uiapplicattion_settings.php

--- branches/dev-syncromind/booking/inc/class.uiapplication_settings.inc.php
+++ branches/dev-syncromind/booking/inc/class.uiapplication_settings.inc.php
@@ -6,6 +6,7 @@
 		public $public_functions = array
 		(
 			'index'			=>	true,
+            'query'         =>  true,
 		);
 		
 		public function __construct()
@@ -34,7 +35,17 @@
 				}
 				$config->save_repository();
 			}
-			$this->use_yui_editor();
-			self::render_template('application_settings', array('config_data' =>$config->config_data));
+            
+            $tabs = array();
+            $tabs['generic'] = array('label' => lang('Application Settings'), 'link' => '#settings');
+            $active_tab = 'generic';
+            
+            $settings = array();
+            $settings['tabs'] = phpgwapi_jquery::tabview_generate($tabs, $active_tab);
+//			$this->use_yui_editor();
+			self::render_template_xsl('application_settings', array('config_data' =>$config->config_data, 'data' => $settings));
 		}
+        public function query(){
+            
+        }
 	}
