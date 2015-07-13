9e3bd5c282b8a9624973138e6a70107a6e81e00e
psaul <psaul@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2015-07-13 22:56:04 +0000

    class.uipermission_root php

--- branches/dev-syncromind/booking/inc/class.uipermission_root.inc.php
+++ branches/dev-syncromind/booking/inc/class.uipermission_root.inc.php
@@ -211,8 +211,15 @@
 			$this->add_default_display_data($permission);
 			
 			$this->flash_form_errors($errors);
+            
+            $tabs = array();
+            $tabs['generic'] = array('label' => lang('Permission Add'), 'link' => '#permission_add');
+            $active_tab = 'generic';
 
-			self::render_template('permission_root_form', array('permission' => $permission));
+//            $data = array();
+            $permission['tabs'] = phpgwapi_jquery::tabview_generate($tabs, $active_tab);
+
+			self::render_template_xsl('permission_root_form', array('permission' => $permission));
 		}
 		
 		// public function edit()
