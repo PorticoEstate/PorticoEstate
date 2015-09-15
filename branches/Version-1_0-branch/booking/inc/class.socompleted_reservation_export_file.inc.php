a8be5c4c3e2f03d70c0517cbdc95439aba97aa2f
sigurdne <sigurdne@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2015-09-15 07:06:08 +0000

    block sql-injection

--- branches/Version-1_0-branch/booking/inc/class.socompleted_reservation_export_file.inc.php
+++ branches/Version-1_0-branch/booking/inc/class.socompleted_reservation_export_file.inc.php
@@ -225,10 +225,18 @@
 #					}
 				}
 			
-				$this->db_query(
-					join(";\n", $export_conf_updates),
-					__LINE__, __FILE__
-				);
+				/**
+				 * Sigurd: block SQL-injection by not allowing multiple statements in one query
+				 */
+				foreach($export_conf_updates as $sql)
+				{
+					$this->db_query($sql,__LINE__, __FILE__);
+				}
+
+//				$this->db_query(
+//					join(";\n", $export_conf_updates),
+//					__LINE__, __FILE__
+//				);
 			
 				if ($this->db->transaction_commit()) { 
 					return $entity_export_files;
@@ -254,3 +262,4 @@
 			} catch (booking_unattached_storage_object $e) { }
 		}
 	}
+
