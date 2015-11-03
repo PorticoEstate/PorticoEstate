e6e8eef55857fc04e21d038549a289f3c8a93848
sigurdne <sigurdne@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2015-11-03 09:19:02 +0000

    include pure-css in bkbooking template

--- branches/dev-syncromind/phpgwapi/templates/bkbooking/head.inc.php
+++ branches/dev-syncromind/phpgwapi/templates/bkbooking/head.inc.php
@@ -1,4 +1,5 @@
 <?php
+	$GLOBALS['phpgw_info']['server']['no_jscombine']=true;
 	phpgw::import_class('phpgwapi.jquery');
 	phpgw::import_class('phpgwapi.yui');
 	phpgw::import_class('phpgwapi.template_portico');
@@ -54,15 +55,26 @@ JS;
 	phpgwapi_yui::load_widget('layout');
 
 	phpgwapi_yui::load_widget('button');
-	$stylesheets = array
-		(
-			"/phpgwapi/js/yahoo/reset-fonts-grids/reset-fonts-grids.css",
-			"/phpgwapi/js/yahoo/tabview/assets/skins/sam/tabview.css",
-			"/phpgwapi/js/yahoo/resize/assets/skins/sam/resize.css",
-			"/phpgwapi/js/yahoo/layout/assets/skins/sam/layout.css",
-		);
-	$stylesheets[] = "/phpgwapi/js/yahoo/menu/assets/skins/sam/menu.css";
-	$stylesheets[] = "/phpgwapi/js/yahoo/button/assets/skins/sam/button.css";
+
+//	$stylesheets = array
+//		(
+//			"/phpgwapi/js/yahoo/reset-fonts-grids/reset-fonts-grids.css",
+//			"/phpgwapi/js/yahoo/tabview/assets/skins/sam/tabview.css",
+//			"/phpgwapi/js/yahoo/resize/assets/skins/sam/resize.css",
+//			"/phpgwapi/js/yahoo/layout/assets/skins/sam/layout.css",
+//		);
+
+	$stylesheets = array();
+	$stylesheets[] = "/phpgwapi/templates/pure/css/global.css";
+	$stylesheets[] = "/phpgwapi/templates/pure/css/demo_mmenu.css";
+	$stylesheets[] = "/phpgwapi/templates/pure/css/pure-min.css";
+	$stylesheets[] = "/phpgwapi/templates/pure/css/pure-extension.css";
+	$stylesheets[] = "/phpgwapi/templates/pure/css/grids-responsive-min.css";
+
+
+//	$stylesheets[] = "/phpgwapi/js/yahoo/menu/assets/skins/sam/menu.css";
+//	$stylesheets[] = "/phpgwapi/js/yahoo/button/assets/skins/sam/button.css";
+
 	$stylesheets[] = "/phpgwapi/templates/portico/css/base.css";
 	$stylesheets[] = "/phpgwapi/templates/portico/css/{$GLOBALS['phpgw_info']['user']['preferences']['common']['theme']}.css";
 	$stylesheets[] = "/{$app}/templates/base/css/base.css";
