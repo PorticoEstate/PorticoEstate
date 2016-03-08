1a8dab194297d38ffcec1055ed059d647fb72c51
Sigurd Nes <sigurdne@users.noreply.github.com>
2016-03-08 14:48:03 +0000

    Merge 14819:14822 from trunk

--- property/js/portico/wo_hour.index.js
+++ property/js/portico/wo_hour.index.js
@@ -3,17 +3,17 @@ this.local_DrawCallback2 = function (oTable)
 	var api = oTable.api();
 	var data = api.ajax.json();
 
-	$('#value_sum_calculation').html(data.table_sum.value_sum_calculation);
-	$('#sum_deviation').html(data.table_sum.sum_deviation);
-	$('#sum_result').html(data.table_sum.sum_result);
+	$('#value_sum_calculation').html($.number(data.table_sum.value_sum_calculation, 2, ',', '.'));
+	$('#sum_deviation').html($.number(data.table_sum.sum_deviation, 2, ',', '.'));
+	$('#sum_result').html($.number(data.table_sum.sum_result, 2, ',', '.'));
 
-	$('#value_addition_rs').html(data.table_sum.value_addition_rs);
+	$('#value_addition_rs').html($.number(data.table_sum.value_addition_rs, 2, ',', '.'));
 
-	$('#value_addition_percentage').html(data.table_sum.value_addition_percentage);
+	$('#value_addition_percentage').html($.number(data.table_sum.value_addition_percentage, 2, ',', '.'));
 
-	$('#value_sum_tax').html(data.table_sum.value_sum_tax);
+	$('#value_sum_tax').html($.number(data.table_sum.value_sum_tax, 2, ',', '.'));
 
-	$('#value_total_sum').html(data.table_sum.value_total_sum);
+	$('#value_total_sum').html($.number(data.table_sum.value_total_sum, 2, ',', '.'));
 
 
 	var project_id = data.workorder_data.project_id;
