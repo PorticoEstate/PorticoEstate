94a55d491fb218c196381020396f1516db5799f8
sigurdne <sigurdne@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2016-05-13 12:07:10 +0000

    Syncromind: Merge 14936:15027 from trunk

--- branches/dev-syncromind-2/property/inc/class.uijasper.inc.php
+++ branches/dev-syncromind-2/property/inc/class.uijasper.inc.php
@@ -454,6 +454,7 @@
 			$order = phpgw::get_var('order');
 			$draw = phpgw::get_var('draw', 'int');
 			$columns = phpgw::get_var('columns');
+			$export = phpgw::get_var('export', 'bool');
 
 			$params = array(
 				'start' => phpgw::get_var('start', 'int', 'REQUEST', 0),
@@ -461,7 +462,7 @@
 				'query' => $search['value'],
 				'order' => $columns[$order[0]['column']]['data'],
 				'sort' => $order[0]['dir'],
-				'allrows' => phpgw::get_var('length', 'int') == -1,
+				'allrows' => phpgw::get_var('length', 'int') == -1 || $export,
 				'app' => $this->app
 			);
 
@@ -470,7 +471,7 @@
 
 			$values = $this->bo->read($params);
 
-			if (phpgw::get_var('export', 'bool'))
+			if ($export)
 			{
 				return $values;
 			}
