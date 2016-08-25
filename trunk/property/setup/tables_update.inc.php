6c0b6497fb8983e983406c79387e7c4a63236895
sigurdne <sigurdne@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2016-08-25 14:54:30 +0000

    more on property

--- trunk/property/setup/tables_update.inc.php
+++ trunk/property/setup/tables_update.inc.php
@@ -9248,6 +9248,22 @@
 			)
 		);
 
+		$GLOBALS['phpgw_setup']->oProc->query("SELECT DISTINCT account_id, project_id FROM fm_workorder WHERE account_id IS NOT NULL AND account_id != '0'");
+		$GLOBALS['phpgw_setup']->oProc->next_record();
+		$projects = array();
+		while($GLOBALS['phpgw_setup']->oProc->next_record())
+		{
+			$projects[] = array(
+				'id' => $GLOBALS['phpgw_setup']->oProc->f('project_id'),
+				'b_account_id' => $GLOBALS['phpgw_setup']->oProc->f('account_id'),
+			);
+		}
+
+		foreach ($projects as $project)
+		{
+			$GLOBALS['phpgw_setup']->oProc->query("UPDATE fm_project SET b_account_id = '{$project['b_account_id']}' WHERE id = {$project['id']}");
+		}
+
 		if($GLOBALS['phpgw_setup']->oProc->m_odb->transaction_commit())
 		{
 			$GLOBALS['setup_info']['property']['currentver'] = '0.9.17.704';
