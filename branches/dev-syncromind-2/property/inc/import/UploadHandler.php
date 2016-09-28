03bbc62dea5ae5045ee252da9be820213460117d
sigurdne <sigurdne@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2016-09-28 08:40:34 +0000

    uploadhandler: change delete type to POST

--- branches/dev-syncromind-2/property/inc/import/UploadHandler.php
+++ branches/dev-syncromind-2/property/inc/import/UploadHandler.php
@@ -52,7 +52,7 @@ class UploadHandler
             'param_name' => 'files',
             // Set the following option to 'POST', if your server does not support
             // DELETE requests. This is a parameter sent to the client:
-            'delete_type' => 'DELETE',
+            'delete_type' => 'POST',
             'access_control_allow_origin' => '*',
             'access_control_allow_credentials' => false,
             'access_control_allow_methods' => array(
