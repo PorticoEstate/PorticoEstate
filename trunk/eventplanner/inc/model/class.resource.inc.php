cda6e0a6df2d0580f5d7b203cd3fb7664e0fae72
sigurdne <sigurdne@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2016-10-13 14:56:17 +0000

    start work on eventplanner

---
+++ trunk/eventplanner/inc/model/class.resource.inc.php
@@ -0,0 +1,197 @@
+<?php
+	/**
+	 * phpGroupWare - eventplanner: a part of a Facilities Management System.
+	 *
+	 * @author Sigurd Nes <sigurdne@online.no>
+	 * @copyright Copyright (C) 2016 Free Software Foundation, Inc. http://www.fsf.org/
+	 * This file is part of phpGroupWare.
+	 *
+	 * phpGroupWare is free software; you can redistribute it and/or modify
+	 * it under the terms of the GNU General Public License as published by
+	 * the Free Software Foundation; either version 2 of the License, or
+	 * (at your option) any later version.
+	 *
+	 * phpGroupWare is distributed in the hope that it will be useful,
+	 * but WITHOUT ANY WARRANTY; without even the implied warranty of
+	 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	 * GNU General Public License for more details.
+	 *
+	 * You should have received a copy of the GNU General Public License
+	 * along with phpGroupWare; if not, write to the Free Software
+	 * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+	 *
+	 * @license http://www.gnu.org/licenses/gpl.html GNU General Public License
+	 * @internal Development of this application was funded by http://www.bergen.kommune.no/ and Nordlandssykehuset
+	 * @package eventplanner
+	 * @subpackage resource
+	 * @version $Id: $
+	 */
+
+	phpgw::import_class('eventplanner.boresource');
+
+	include_class('phpgwapi', 'model', 'inc/model/');
+
+	class eventplanner_resource extends phpgwapi_model
+	{
+		const STATUS_REGISTERED = 1;
+		const STATUS_PENDING = 2;
+		const STATUS_REJECTED = 3;
+		const STATUS_APPROVED = 4;
+
+		const acl_location = '.resource';
+
+		protected
+			$id,
+			$status,
+			$category_id,
+			$category_name,
+			$date_start,
+			$date_end,
+			$active,
+			$name,
+			$description,
+			$comment,
+			$comments,
+			$comment_input,
+			$entry_date,
+			$executive_officer;
+
+		protected $field_of_responsibility_name;
+
+		public function __construct( int $id = null )
+		{
+			parent::__construct((int)$id);
+			$this->field_of_responsibility_name = self::acl_location;
+		}
+
+		/**
+		 * Implementing classes must return an instance of itself.
+		 *
+		 * @return the class instance.
+		 */
+		public static function get_instance()
+		{
+			return new eventplanner_resource();
+		}
+
+		public static function get_status_list()
+		{
+			return array(
+				self::STATUS_REGISTERED => lang('registered'),
+				self::STATUS_PENDING	=> lang('pending'),
+				self::STATUS_REJECTED => lang('rejected'),
+				self::STATUS_APPROVED	=> lang('approved')
+			);
+		}
+
+
+		public static function get_fields($debug = true)
+		{
+			 $fields = array(
+				'id' => array('action'=> PHPGW_ACL_READ,
+					'type' => 'int',
+					'label' => 'id',
+					'sortable'=> true,
+					'formatter' => 'JqueryPortico.formatLink',
+					),
+				'category_id' => array(
+					'action'=>  PHPGW_ACL_ADD | PHPGW_ACL_EDIT,
+					'type' => 'int',
+					'label' => 'category',
+					),
+				'category_name' => array(
+					'action'=>  PHPGW_ACL_READ,
+					'type' => 'string',
+					'label' => 'category',
+					'join' => array(
+						'table' => 'eventplanner_resource_category',
+						'fkey' => 'category_id',
+						'key' => 'id',
+						'column' => 'name'
+						)
+					),
+				'date_start' => array('action'=> PHPGW_ACL_ADD | PHPGW_ACL_EDIT,
+					'type' => 'date'),
+				'date_end' => array('action'=> PHPGW_ACL_ADD | PHPGW_ACL_EDIT,
+					'type' => 'date'),
+				'active' => array('action'=> PHPGW_ACL_ADD | PHPGW_ACL_EDIT,
+					'type' => 'bool'),
+				'name' => array(
+					'action'=> PHPGW_ACL_ADD | PHPGW_ACL_EDIT,
+					'type' => 'string',
+					'required' => true,
+					'query' => true,
+					'label' => 'name',
+					),
+				'description' => array(
+					'action'=> PHPGW_ACL_ADD | PHPGW_ACL_EDIT,
+					'type' => 'string',
+					'required' => true,
+					'query' => true,
+					'label' => 'description',
+					),
+				'comments' => array(
+					'action'=> PHPGW_ACL_ADD | PHPGW_ACL_EDIT,
+					'type' => 'string',
+					'manytomany' => array(
+						'input_field' => 'comment_input',
+						'table' => 'eventplanner_resource_comment',
+						'key' => 'resource_id',
+						'column' => array('time', 'author', 'comment', 'type'),
+						'order' => array('sort' => 'time', 'dir' => 'ASC')
+					)),
+				'comment' => array(
+					'action'=> PHPGW_ACL_ADD | PHPGW_ACL_EDIT,
+					'type' => 'string',
+					'related' => true,
+					),
+				'executive_officer' => array('action'=> PHPGW_ACL_READ | PHPGW_ACL_ADD | PHPGW_ACL_EDIT,
+					'type' => 'int',
+					'label' => 'executive_officer',
+					'sortable' => true,
+					'history' => true,
+					),
+
+				'entry_date' => array('action'=> PHPGW_ACL_READ | PHPGW_ACL_ADD,
+					'type' => 'int',
+					'label' => 'entry_date',
+					'sortable' => true,
+					),
+				);
+
+			if($debug)
+			{
+				foreach ($fields as $field => $field_info)
+				{
+					if(!property_exists('eventplanner_resource', $field))
+					{
+					   phpgwapi_cache::message_set("$field is missing from model-definition", 'error');
+					}
+
+				}
+			}
+			return $fields;
+		}
+
+		/**
+		 * Implement in subclasses to perform actions on entity before validation
+		 */
+		protected function preValidate( &$entity )
+		{
+		}
+
+		public function serialize()
+		{
+			return self::toArray();
+		}
+
+		public function store()
+		{
+			return eventplanner_boresource::get_instance()->store($this);
+		}
+
+		public function read_single($id)
+		{
+			return eventplanner_boresource::get_instance()->read_single($id, true);
+		}
+	}
