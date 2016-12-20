f8d9c8743849e3d09fdb1923c9a4d3c844719ab5
Sigurd Nes <sigurdne@users.noreply.github.com>
2016-12-20 16:39:37 +0000

    more on rental

--- rental/js/rental/moveout.edit.js
+++ rental/js/rental/moveout.edit.js
@@ -59,7 +59,7 @@ function populateContractParty(contract_id)
 	JqueryPortico.execute_ajax(requestUrl,
 		function (result)
 		{
-		
+
 			$("#executive_officer").html(result.executive_officer);
 			$("#composite").html(result.composite);
 			$("#rented_area").html(result.rented_area);
@@ -78,3 +78,51 @@ function populateContractParty(contract_id)
 		}, data, "POST", "json"
 		);
 }
+
+/*
+$(document).ready(function ()
+{
+
+	var wrapper = document.getElementById("signature-pad"),
+		clearButton = wrapper.querySelector("[data-action=clear]"),
+		saveButton = wrapper.querySelector("[data-action=save]"),
+		canvas = wrapper.querySelector("canvas"),
+		signaturePad;
+
+// Adjust canvas coordinate space taking into account pixel ratio,
+// to make it look crisp on mobile devices.
+// This also causes canvas to be cleared.
+	function resizeCanvas()
+	{
+		// When zoomed out to less than 100%, for some very strange reason,
+		// some browsers report devicePixelRatio as less than 1
+		// and only part of the canvas is cleared then.
+		var ratio = Math.max(window.devicePixelRatio || 1, 1);
+		canvas.width = canvas.offsetWidth * ratio;
+		canvas.height = canvas.offsetHeight * ratio;
+		canvas.getContext("2d").scale(ratio, ratio);
+	}
+
+	window.onresize = resizeCanvas;
+	resizeCanvas();
+
+	signaturePad = new SignaturePad(canvas);
+
+	clearButton.addEventListener("click", function (event)
+	{
+		signaturePad.clear();
+	});
+
+	saveButton.addEventListener("click", function (event)
+	{
+		if (signaturePad.isEmpty())
+		{
+			alert("Please provide signature first.");
+		}
+		else
+		{
+			window.open(signaturePad.toDataURL());
+		}
+	});
+});
+ */
