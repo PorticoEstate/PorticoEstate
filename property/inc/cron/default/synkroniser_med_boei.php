44e031859bd2fd3aca25e4d77e8d5905e9fd414b
Sigurd Nes <sigurdne@users.noreply.github.com>
2017-03-16 17:12:33 +0000

    property: add db_slashes

--- property/inc/cron/default/synkroniser_med_boei.php
+++ property/inc/cron/default/synkroniser_med_boei.php
@@ -181,7 +181,7 @@ SQL;
 					),
 					2 => array
 						(
-						'value' => ($this->db_boei->f('Navn')),
+						'value' => $this->db->db_addslashes($this->db_boei->f('Navn')),
 						'type' => PDO::PARAM_STR
 					),
 					3 => array
@@ -235,7 +235,7 @@ SQL;
 					),
 					2 => array
 						(
-						'value' => ($this->db_boei->f('GateNavn')),
+						'value' => $this->db->db_addslashes($this->db_boei->f('GateNavn')),
 						'type' => PDO::PARAM_STR
 					),
 					3 => array
@@ -433,7 +433,7 @@ SQL;
 					),
 					4 => array
 						(
-						'value' => ($this->db_boei->f('Beskrivelse')),
+						'value' => $this->db->db_addslashes($this->db_boei->f('Beskrivelse')),
 						'type' => PDO::PARAM_STR
 					)
 				);
@@ -532,12 +532,12 @@ SQL;
 					),
 					8 => array
 						(
-						'value' => ($this->db_boei->f('Gatenr')),
+						'value' => $this->db->db_addslashes($this->db_boei->f('Gatenr')),
 						'type' => PDO::PARAM_STR
 					),
 					9 => array
 						(
-						'value' => $this->db_boei->f('Etasje'),
+						'value' => $this->db->db_addslashes($this->db_boei->f('Etasje')),
 						'type' => PDO::PARAM_STR
 					),
 					10 => array
@@ -635,12 +635,12 @@ SQL;
 					),
 					2 => array
 						(
-						'value' => ($this->db_boei->f('Fornavn')),
+						'value' => $this->db->db_addslashes($this->db_boei->f('Fornavn')),
 						'type' => PDO::PARAM_STR
 					),
 					3 => array
 						(
-						'value' => ($this->db_boei->f('Etternavn')),
+						'value' => $this->db->db_addslashes($this->db_boei->f('Etternavn')),
 						'type' => PDO::PARAM_STR
 					),
 					4 => array
