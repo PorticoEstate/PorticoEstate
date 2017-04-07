12c68ec0ae4690ae051f866be8b82808bab37821
sigurdne <sigurdne@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2017-04-07 11:59:07 +0000

    more on helpdesk

--- trunk/phpgwapi/inc/class.historylog.inc.php
+++ trunk/phpgwapi/inc/class.historylog.inc.php
@@ -121,8 +121,8 @@
 					'owner'		=> $GLOBALS['phpgw']->accounts->id2name($this->db->f('history_owner')),
 //					'status'	=> lang($this->types[$this->db->f('history_status')]),
 					'status'	=> str_replace(' ','',$this->db->f('history_status')),
-					'new_value'	=> htmlspecialchars_decode($this->db->f('history_new_value',true)),
-					'old_value'	=> htmlspecialchars_decode($this->db->f('history_old_value',true)),
+					'new_value'	=> $this->db->f('history_new_value',true),
+					'old_value'	=> $this->db->f('history_old_value',true),
 					'datetime'	=> $this->db->from_timestamp($this->db->f('history_timestamp')),
 					'publish'	=> $this->db->f('publish')
 				);
