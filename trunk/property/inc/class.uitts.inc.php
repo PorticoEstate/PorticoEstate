af60e15959c484c8b9ac3ab863cc7c862ce78fc8
sigurdne <sigurdne@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2017-05-01 18:10:43 +0000

    formatting

--- trunk/property/inc/class.uitts.inc.php
+++ trunk/property/inc/class.uitts.inc.php
@@ -1638,7 +1638,7 @@ HTML;
 
 
 
-			$cat_select = $this->cats->formatted_xslt_list(array('select_name' => 'values[cat_id]',	'use_acl' => $this->_category_acl, 'required' => true));
+			$cat_select = $this->cats->formatted_xslt_list(array('select_name' => 'values[cat_id]',	'use_acl' => $this->_category_acl, 'required' => true, 'class' => 'pure-input-1-2'));
 
 			$_cat_list = array();
 			if($this->simple && isset($fmttssimple_categories) && $fmttssimple_categories[1])
@@ -2844,7 +2844,7 @@ HTML;
 //_debug_array($supervisor_email);die();
 			$msgbox_data = $this->bocommon->msgbox_data($receipt);
 			$cat_select = $this->cats->formatted_xslt_list(array('select_name' => 'values[cat_id]',
-				'selected' => $this->cat_id, 'use_acl' => $this->_category_acl, 'required' => true));
+				'selected' => $this->cat_id, 'use_acl' => $this->_category_acl, 'required' => true, 'class' => 'pure-input-1-2'));
 
 			$_ticket_cat_found = false;
 			if (isset($cat_select['cat_list']) && is_array($cat_select['cat_list']))
