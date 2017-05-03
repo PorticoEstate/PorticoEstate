bb873d03366fb761fd74352b798eba07ed5da50f
Sigurd Nes <sigurdne@users.noreply.github.com>
2017-05-03 19:22:42 +0000

    resize

--- phpgwapi/templates/pure/js/mmenu.js
+++ phpgwapi/templates/pure/js/mmenu.js
@@ -130,22 +130,41 @@ $(document).ready(function ()
 
 	$(window).resize(function ()
 	{
+		var width = $(window).width();
+		if (width < 620)
+		{
+			$('.pure-form-aligned').each(function (i, obj)
+			{
+				$(this).removeClass('pure-form-aligned').addClass('pure-form-stacked');
+			});
+			$('.pure-input-1-2').each(function (i, obj)
+			{
+				$(this).removeClass('pure-input-1-2').addClass('pure-input-1');
+			});
+			$('.pure-u-3-4').each(function (i, obj)
+			{
+				$(this).removeClass('pure-u-3-4').addClass('pure-u-5-6');
+			});
+		}
+//		else if (resized == true && width > 620)
+//		{
+//			$('.pure-form-stacked').each(function (i, obj)
+//			{
+//				$(this).removeClass('pure-form-stacked').addClass('pure-form-aligned');
+//			});
+//			$('.pure-input-1').each(function (i, obj)
+//			{
+//				$(this).removeClass('pure-input-1').addClass('pure-input-1-2');
+//			});
+//			$('.pure-u-5-6').each(function (i, obj)
+//			{
+//				$(this).removeClass('pure-u-5-6').addClass('pure-u-3-4');
+//			});
+//			resized = false;
+//
+//		}
 
-		$('.pure-form-aligned').each(function (i, obj)
-		{
-			$(this).removeClass('pure-form-aligned').addClass('pure-form-stacked');
-		});
-		$('.pure-input-1-2').each(function (i, obj)
-		{
-			$(this).removeClass('pure-input-1-2').addClass('pure-input-1');
-		});
-		$('.pure-u-3-4').each(function (i, obj)
-		{
-			$(this).removeClass('pure-u-3-4').addClass('pure-u-5-6');
-		});
-
-	})
-	.resize();//trigger the resize event on page load.
+	}).resize();//trigger the resize event on page load.
 
 });
 
