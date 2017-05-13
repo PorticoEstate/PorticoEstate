e6083e07980144cfa3c61b5787ef6927624dae56
Sigurd Nes <sigurdne@users.noreply.github.com>
2017-05-13 14:17:39 +0000

    Backport diff from Syncromind to trunk

--- property/js/portico/report.edit.js
+++ property/js/portico/report.edit.js
@@ -1,10 +1,11 @@
 $(document).ready(function ()
 {
-	//$('.processing').hide();
+	$('.processing').hide();
+	$('.processing-preview').hide();
 	
 	$('#btn_get_columns').click( function()
 	{
-		var oArgs = {menuaction: 'property.uireport.get_columns'};
+		var oArgs = {menuaction: 'property.uireport.get_column_preview'};
 		var requestUrl = phpGWLink('index.php', oArgs, true);
 		var data = {"dataset_id": $('#cbo_dataset_id').val()};
 
@@ -20,7 +21,7 @@ $(document).ready(function ()
 			dataType: 'json',
 			data: data
 		}).always(function () {
-			//$('.processing').hide();
+			$('.processing').hide();
 		}).done(function (result) {
 			//console.log(result);
 			$('#container_columns').empty();
@@ -28,7 +29,8 @@ $(document).ready(function ()
 			$('#container_order').empty();
 			$('#container_aggregates').empty();
 			
-			build_check_columns(result);
+			$('#container_columns').html(result.columns_preview);
+			
 			if (jsonB !== '')
 			{
 				set_values();
@@ -37,14 +39,108 @@ $(document).ready(function ()
 	});
 	
 	$('#btn_get_columns').click();
+	
+	$('#btn_preview').click( function()
+	{
+		var oArgs = {menuaction: 'property.uireport.preview'};
+		var requestUrl = phpGWLink('index.php', oArgs, true);
+
+		if ($('#cbo_dataset_id').val() == '')
+		{
+			alert('choose dataset');
+			return;
+		}
+		
+		var values = {};
+
+		values['columns'] = {};
+		values['group'] = {};
+		values['order'] = {};
+		values['aggregate'] = {};
+		values['cbo_aggregate'] = {};
+		
+		$('input[name^="columns"]').each(function() {
+
+			if ($(this).is(":checked"))
+			{
+				values['columns'][$(this).val()] = $(this).val();
+			}
+		});
+		
+		var invalid_groups = true;
+		$('input[name="group"]').each(function() {
+
+			if ($(this).is(":checked"))
+			{
+				values['group'][$(this).val()] = $(this).val();
+				invalid_groups = false;
+			}
+		});
+				
+		if (invalid_groups)
+		{
+			alert('Choose group');
+			$('#responsiveTabsGroups').responsiveTabs('activate', 1);
+			return;
+		}
+		
+		$('input[name="order"]').each(function() {
+
+			if ($(this).is(":checked"))
+			{
+				values['order'][$(this).val()] = $(this).val();
+			}
+		});
+	  
+	    var name = '';
+		var invalid_aggregate = true;
+
+		$('input[name^="aggregate"]').each(function() {
+
+			if ($(this).is(":checked"))
+			{
+				name = $(this).val();
+				values['aggregate'][name] = name;
+				invalid_aggregate = false;
+				
+				values['cbo_aggregate'][name] = $('#cbo_' + name).val();
+			}
+		});
+		
+		if (invalid_aggregate)
+		{
+			$('#responsiveTabsGroups').responsiveTabs('activate', 3);
+			alert('Choose COUNT/SUM option');			
+			return;
+		}
+		
+		var data = {"values": values, "dataset_id": $('#cbo_dataset_id').val()};
+		$('.processing-preview').show();
+		$.ajax({
+			type: 'GET',
+			url: requestUrl,
+			dataType: 'json',
+			data: data
+		}).always(function () {
+			$('.processing-preview').hide();
+		}).done(function (result) {
+			$('#container_preview').html(result);
+		});		
+	});
+	
 });
 
 function set_values()
 {
+	$.each(jsonB.columns, function(key, value) 
+	{
+		$("#c_" + value).prop('checked', true);
+		$("#c_" + value).change();
+	});
+	
 	$.each(jsonB.group, function(key, value) 
 	{
-		$("#c_" + key).prop('checked', true);
-		$("#c_" + key).change();
+		$("#g_" + key).prop('checked', true);
 	});	
 	
 	$.each(jsonB.order, function(key, value) 
@@ -62,39 +158,39 @@ function set_values()
 	{
 		$("#cbo_" + key).val(value);
 	});
-	
-	$.each(jsonB.txt_aggregate, function(key, value) 
-	{
-		$("#txt_" + key).val(value);
-	});
 }
 
 function build_check_columns(data)
 {
 	$.each(data, function(key, object) 
 	{
-		$('#container_columns').append('<span style="margin-right:12px;"><input type="checkbox" id="c_'+ object.name +'" value="'+ object.name +'" onchange="build_check_groups(\''+ object.name +'\')"/> ' + object.name + '</span>');
-
 		var combo = build_list_aggregates(object.name, object.type);
-		var text = build_text_aggregates(object.name);
+		//var text = build_text_aggregates(object.name);
 		var check = build_check_aggregates(object.name);
-		var el_1 = '<span style="display:table-row;">'+ check + combo + text + '</span>';
+		var el_1 = '<span style="display:table-row;">'+ check + combo + '</span>';
 		$('#container_aggregates').append(el_1);			
 	});	
 }
 
-function build_check_groups(name)
+function build_check_groups(name, type)
 {
 	if ($("#c_" + name).is(":checked")) 
 	{
-		var el_1 = '<span style="display:block;"><input onclick="return false;" onkeydown="return false;" type="checkbox" name="group['+ name +']" id="g_'+ name +'" value="'+ name +'" checked/>' + name + '</span>';
-		var el_2 = '<span style="display:block;"><input type="checkbox" name="order['+ name +']" id="o_'+ name +'" value="'+ name +'"/>' + name + '</span>';
+		var el_1 = '<span style="display:block;"><input type="radio" name="group" id="g_'+ name +'" value="'+ name +'"/>' + name + '</span>';
+		var el_2 = '<span style="display:block;"><input type="radio" name="order" id="o_'+ name +'" value="'+ name +'"/>' + name + '</span>';
 		$('#container_groups').append(el_1);
 		$('#container_order').append(el_2);
+		
+		var combo = build_list_aggregates(name, type);
+		//var text = build_text_aggregates(name);
+		var check = build_check_aggregates(name);
+		var el_1 = '<span style="display:table-row;">'+ check + combo + '</span>';
+		$('#container_aggregates').append(el_1);
 	} 
 	else {
 		$("#g_" + name).parent().remove();
 		$("#o_" + name).parent().remove();
+		$("#cbo_" + name).parent().parent().remove();
 	}
 }
 
@@ -108,7 +204,7 @@ function build_list_aggregates(name, type)
 {
     var combo = $("<select></select>");  
 	combo.append("<option value='count'>Count</option>");
-	if (type == 'integer')
+	if (type == 'integer' || type == 'numeric')
 	{
 		combo.append("<option value='sum'>Sum</option>");
 	}
@@ -126,9 +222,7 @@ function enabled_disabled_aggregates(name)
 	if ($("#a_" + name).is(":checked")) 
 	{
 		$("#cbo_" + name).prop('disabled', false);
-		$("#txt_" + name).prop('disabled', false);
 	} else {
-		$("#cbo_" + name).prop('disabled', true);
-		$("#txt_" + name).prop('disabled', true);		
+		$("#cbo_" + name).prop('disabled', true);		
 	}
 }
