eacec606a7d95f55e00ff66e5c63707800a91770
sigurdne <sigurdne@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2017-11-22 09:17:37 +0000

    booking: session handling and code formatting

--- trunk/phpgwapi/templates/bkbooking/head.inc.php
+++ trunk/phpgwapi/templates/bkbooking/head.inc.php
@@ -54,7 +54,7 @@ JS;
 	$stylesheets[] = "/phpgwapi/templates/pure/css/pure-min.css";
 	$stylesheets[] = "/phpgwapi/templates/pure/css/pure-extension.css";
 	$stylesheets[] = "/phpgwapi/templates/pure/css/grids-responsive-min.css";
-    $stylesheets[] = "/phpgwapi/js/DataTables/extensions/Responsive/css/responsive.dataTables.min.css";
+	$stylesheets[] = "/phpgwapi/js/DataTables/extensions/Responsive/css/responsive.dataTables.min.css";
 
 	$stylesheets[] = "/phpgwapi/templates/portico/css/base.css";
 	$stylesheets[] = "/phpgwapi/templates/portico/css/{$GLOBALS['phpgw_info']['user']['preferences']['common']['theme']}.css";
@@ -62,7 +62,7 @@ JS;
 	$stylesheets[] = "/{$app}/templates/portico/css/base.css";
 	$stylesheets[] = "/{$app}/templates/portico/css/{$GLOBALS['phpgw_info']['user']['preferences']['common']['theme']}.css";
 	$stylesheets[] = "/phpgwapi/templates/bkbooking/css/frontend.css";
-    $stylesheets[] = "/bookingfrontend/css/bookingfrontend.css";
+	$stylesheets[] = "/bookingfrontend/css/bookingfrontend.css";
 
 	foreach ( $stylesheets as $stylesheet )
 	{
@@ -180,20 +180,21 @@ JS;
 		$robots = '<meta name="robots" content="none">';
 	}
 
-    $test = $GLOBALS['phpgw']->common->get_on_events();
-    $test = str_replace('window.onload = function()','$(document).ready(function()',$test);
-    $test = str_replace("\n}\n","\n})\n",$test);
+	$test = $GLOBALS['phpgw']->common->get_on_events();
+	$test = str_replace('window.onload = function()','$(document).ready(function()',$test);
+	$test = str_replace("\n}\n","\n})\n",$test);
 	$app = lang($app);
 	$tpl_vars = array
 	(
 		'css'			=> $GLOBALS['phpgw']->common->get_css(),
 		'javascript'	=> $GLOBALS['phpgw']->common->get_javascript(),
-		'img_icon'      => $GLOBALS['phpgw']->common->find_image('phpgwapi', 'favicon.ico'),
+		'img_icon'		=> $GLOBALS['phpgw']->common->find_image('phpgwapi', 'favicon.ico'),
+		'site_url'		=> $GLOBALS['phpgw']->link('/bookingfrontend/', array('menuaction'=>'bookingfrontend.uisearch.index')),
 		'site_title'	=> "{$GLOBALS['phpgw_info']['server']['site_title']}",
 		'str_base_url'	=> $GLOBALS['phpgw']->link('/', array(), true),
 		'webserver_url'	=> $webserver_url,
 //		'win_on_events'	=> $GLOBALS['phpgw']->common->get_on_events(),
-        'win_on_events'	=> $test,
+		'win_on_events'	=> $test,
 		'navbar_config' => $_navbar_config,
 		'metainfo_author' => $author,
 		'metainfo_keywords' => $keywords,
@@ -211,23 +212,27 @@ JS;
 #		$tpl_vars['help_url'] = => '#';
 	}
 	$bouser = CreateObject('bookingfrontend.bouser');
-    $org = CreateObject('bookingfrontend.uiorganization');
-    $orgid = $org->get_orgid($bouser->orgnr);
+	$org = CreateObject('bookingfrontend.uiorganization');
+	$orgid = $org->get_orgid($bouser->orgnr);
 	if($bouser->is_logged_in())
 	{
 		$tpl_vars['organization_json'] = json_encode(phpgwapi_cache::session_get($bouser->get_module(), $bouser::ORGARRAY_SESSION_KEY));
 
 		$tpl_vars['change_org_header'] = lang('Change organization');
 
-        if ( $bouser->orgname == '000000000') {
-            $tpl_vars['login_text_org'] = lang('SSN not registred');
-            $tpl_vars['login_text'] = lang('Logout');
-            $tpl_vars['org_url'] = '#';
-        } else {
-        	$tpl_vars['login_text_org'] = $bouser->orgname;
-            $tpl_vars['login_text'] = lang('Logout');
-            $tpl_vars['org_url'] = "/bookingfrontend/?menuaction=bookingfrontend.uiorganization.show&id=".$orgid;
-        }
+		if ( $bouser->orgname == '000000000')
+		{
+			$tpl_vars['login_text_org'] = lang('SSN not registred');
+			$tpl_vars['login_text'] = lang('Logout');
+			$tpl_vars['org_url'] = '#';
+		}
+		else
+		{
+			$tpl_vars['login_text_org'] = $bouser->orgname;
+			$tpl_vars['login_text'] = lang('Logout');
+			$tpl_vars['org_url'] = $GLOBALS['phpgw']->link('/bookingfrontend/', array('menuaction'=>'bookingfrontend.uiorganization.show', 'id'=> $orgid));
+	//		$tpl_vars['org_url'] = "/bookingfrontend/?menuaction=bookingfrontend.uiorganization.show&id=".$orgid;
+		}
 		$tpl_vars['login_text'] = $bouser->orgnr . ' :: ' . lang('Logout');
 		$tpl_vars['login_url'] = 'logout.php';
 	}
