b548931f32863a1fd7735015d753f03183936ad0
sigurdne <sigurdne@5fb7aff4-6320-438a-9c3a-c7f03e575ea1>
2017-12-11 09:06:35 +0000

    translations

--- trunk/booking/inc/class.uibuilding.inc.php
+++ trunk/booking/inc/class.uibuilding.inc.php
@@ -209,7 +209,7 @@
 			self::rich_text_editor('field_description');
 
 			$tabs = array();
-			$tabs['generic'] = array('label' => lang('Building New'), 'link' => '#building_form');
+			$tabs['generic'] = array('label' => lang('new building'), 'link' => '#building_form');
 			$active_tab = 'generic';
 
 			$building['tabs'] = phpgwapi_jquery::tabview_generate($tabs, $active_tab);
