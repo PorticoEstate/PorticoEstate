b9a9acbf0ca201e8cb728afc77f825434e5944c2
Øystein Lygre <oystein.lygre@bouvet.no>
2018-02-02 15:32:02 +0100

    Make several docker-compose setups for different environments

--- phpgwapi/doc/docker/testrunner/run.sh
+++ phpgwapi/doc/docker/testrunner/run.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
+HEADER_INC=/var/www/html/portico/header.inc.php
+HEADER_INC_BACKUP=/var/www/html/portico/header.inc.php.backup
+TEST_HEADER_INC=/var/www/html/portico/phpgwapi/doc/docker/testrunner/test-header.inc.php
 
 #Wait for postgres container
 sleep 5
 
 cd /var/www/html/portico
-
-#Setup web application with header.inc.php for test env.
-rm -rf /var/www/html/portico/header.inc.php
-ln -s /var/www/html/portico/phpgwapi/doc/docker/testrunner/header.inc.php /var/www/html/portico/header.inc.php
-
 composer install
 
+#Backup existing header.inc.php
+if [[ -f $HEADER_INC ]]; then
+  HEADER_INC_EXISTS=true
+  mv $HEADER_INC $HEADER_INC_BACKUP
+fi
+
+#Setup web application with header.inc.php for test env
+ln -s $TEST_HEADER_INC $HEADER_INC
+
+## Run tests
 ./vendor/bin/codecept run
-#rm -rf /var/www/html/portico/header.inc.php
+
+#Restore existing header.inc.php
+rm -rf $HEADER_INC
+if [[ "$HEADER_INC_EXISTS" = true ]]; then
+  mv $HEADER_INC_BACKUP $HEADER_INC
+fi
