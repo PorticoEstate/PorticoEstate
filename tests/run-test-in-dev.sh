80f8fa8ae3477b54b038a939e349668b1255c383
Øystein Lygre <oystein.lygre@bouvet.no>
2018-02-02 22:41:18 +0100

    Improve dev setup of tests

---
+++ tests/run-test-in-dev.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export WEBHOST=localhost
+export WEBPORT=8080
+export DBHOST=localhost
+export DBPORT=5433
+export DBNAME=porticotest
+export DBUSER=portico
+export DBPASSWORD=portico
+export DBDUMP=tests/_data/portico-test.sql
+export SELENIUMHOST=localhost
+
+cd ..
+./vendor/bin/codecept run --debug
