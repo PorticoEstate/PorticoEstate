131650d6e8db24c289110f504e403719822ddcf3
Sigurd Nes <sigurdne@users.noreply.github.com>
2018-03-11 14:36:44 +0100

    helpdesk: use materialize for dropdown

--- helpdesk/inc/class.uitts.inc.php
+++ helpdesk/inc/class.uitts.inc.php
@@ -818,10 +818,10 @@ HTML;
 					'name' => lang('my assigned tickets'),
 					'selected'	=> ((int)$this->user_id < 0  || (int)$filter_tts_assigned_to_me == 1) ? 1 : 0));
 
-				array_unshift($values_combo_box[4], array('id' => '', 'name' => lang('assigned to')));
+//				array_unshift($values_combo_box[4], array('id' => '', 'name' => lang('assigned to')));
 				$combos[] = array(
 					'type' => 'filter',
-//					'multiple'	=> true,
+					'multiple'	=> true,
 					'name' => 'user_id',
 					'extra' => '',
 					'text' => lang('assigned to'),
@@ -911,26 +911,12 @@ HTML;
 				return;
 			}
 
-			if (!empty($GLOBALS['phpgw_info']['user']['preferences']['helpdesk']['refreshinterval']) && (int)$GLOBALS['phpgw_info']['user']['preferences']['helpdesk']['refreshinterval'] > 0)
-			{
-				$refreshinterval = (int) $GLOBALS['phpgw_info']['user']['preferences']['helpdesk']['refreshinterval'] * 1000;
-
-			$js =<<<JS
-				setInterval( function () {
-					var api = oTable.api();
-					api.ajax.reload();
-			}, {$refreshinterval} );
-//			$("#user_id").material_select();
-JS;
-
-				$GLOBALS['phpgw']->js->add_code('', $js, true);
-			}
-
 			phpgwapi_jquery::load_widget('numberformat');
 			self::add_javascript('phpgwapi', 'jquery', 'editable/jquery.jeditable.js');
 			self::add_javascript('phpgwapi', 'jquery', 'editable/jquery.dataTables.editable.js');
 			self::add_javascript('helpdesk', 'portico', 'tts.index.js');
-//			self::add_javascript('phpgwapi', 'materialize', 'js/materialize.min.js');
+			self::add_javascript('phpgwapi', 'materialize', 'js/materialize.min.js');
+			$GLOBALS['phpgw']->css->add_external_file('phpgwapi/js/materialize/css/materialize.css');
 
 			$start_date = urldecode($this->start_date);
 			$end_date = urldecode($this->end_date);
@@ -992,6 +978,38 @@ JS;
 			foreach ($filters as $filter)
 			{
 				array_unshift($data['form']['toolbar']['item'], $filter);
+			}
+
+			$js ='';
+
+			if (!empty($GLOBALS['phpgw_info']['user']['preferences']['helpdesk']['refreshinterval']) && (int)$GLOBALS['phpgw_info']['user']['preferences']['helpdesk']['refreshinterval'] > 0)
+			{
+				$refreshinterval = (int) $GLOBALS['phpgw_info']['user']['preferences']['helpdesk']['refreshinterval'] * 1000;
+
+				$js =<<<JS
+					setInterval( function () {
+						var api = oTable.api();
+						api.ajax.reload();
+				}, {$refreshinterval} );
+JS;
+			}
+
+			foreach ($filters as $filter)
+			{
+				if($filter['type'] == 'filter')
+				{
+					$js .=<<<JS
+
+					$("#{$filter['name']}").hide();
+					$("#{$filter['name']}").material_select();
+JS;
+
+				}
+			}
+
+			if($js)
+			{
+				$GLOBALS['phpgw']->js->add_code('', $js, true);
 			}
 
 			$parameters = array(
