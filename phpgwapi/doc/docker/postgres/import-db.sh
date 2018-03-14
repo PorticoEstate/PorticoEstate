7e9da3a36ef0cb0dcb2f353a826879136208485f
Øystein Lygre <oystein.lygre@gmail.com>
2018-03-14 16:33:55 +0100

    Improve dockersetup and upgrade postgres in docker to 9.6

--- phpgwapi/doc/docker/postgres/import-db.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-psql -h localhost -U postgres -c 'CREATE DATABASE porticotest;'
-psql -h localhost -U postgres -c 'CREATE ROLE portico LOGIN SUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION;'
-psql -h localhost -U postgres portico-empty-start < portico-empty-start.2.sql
