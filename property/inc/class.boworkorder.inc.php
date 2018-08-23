54b50a6efade05636cff96c621646a252d41f041
Sigurd Nes <sigurdne@users.noreply.github.com>
2018-08-23 16:11:06 +0200

    property: add related to order from parent project

--- property/inc/class.boworkorder.inc.php
+++ property/inc/class.boworkorder.inc.php
@@ -630,6 +630,7 @@
 			$events = execMethod('property.soevent.read_at_location', $event_criteria);
 			$workorder['event_id'] = $events ? $events[0]['id'] : '';
 			$workorder['origin_data'] = $this->interlink->get_relation('property', '.project.workorder', $workorder_id, 'origin');
+			$workorder['origin_data'] = array_merge($workorder['origin_data'], $this->interlink->get_relation('property', '.project', $workorder['project_id'], 'origin'));
 			$workorder['target'] = $this->interlink->get_relation('property', '.project.workorder', $workorder_id, 'target');
 
 			return $workorder;
