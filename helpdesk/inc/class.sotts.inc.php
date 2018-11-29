aa1a6473d18cd211281d0f75d2abd6dc30716432
Sigurd Nes <sigurdne@users.noreply.github.com>
2018-11-29 13:22:40 +0100

    property-ticket and helpdesk: filter on user includes group assignment

--- helpdesk/inc/class.sotts.inc.php
+++ helpdesk/inc/class.sotts.inc.php
@@ -322,29 +322,39 @@
 
 			if ($user_id)
 			{
+				$_membership = array();
+				$membership = array(-1);
 				if (is_array($user_id))
 				{
+					$user_ids = array(-1);
 					foreach ($user_id as &$_user_id)
 					{
-						$_user_id = abs($_user_id);
+						if($_user_id < 0)
+						{
+							$_membership = array_merge($_membership ,$GLOBALS['phpgw']->accounts->membership(abs($_user_id)));
+						}
+						$user_ids[] = abs($_user_id);
 					}
-					$filtermethod .= " {$where} assignedto IN (" . implode(', ' ,$user_id) . ')';
-					$where = 'AND';
 				}
 				else if ($user_id > 0)
 				{
-					$filtermethod .= " {$where} (assignedto={$user_id}";
-					$where = 'AND';
-
-					$membership = $GLOBALS['phpgw']->accounts->membership($user_id);
-					array_unshift($membership, array(-1));
-					$filtermethod .= ' OR (assignedto IS NULL AND phpgw_helpdesk_tickets.group_id IN (' . implode(',',array_keys($membership)) . ')))';
+					$user_ids = array((int)abs($user_id));
 				}
 				else if ($user_id < 0)
 				{
-					$filtermethod .= " {$where} assignedto=" . (int)abs($user_id);
-					$where = 'AND';
+					$user_ids = array((int)abs($user_id));
+					$_membership = $GLOBALS['phpgw']->accounts->membership(abs($user_id));
 				}
+
+				foreach ($_membership as $_key => $group_member)
+				{
+					$membership[] = $group_member->id;
+				}
+
+				$filtermethod .= " {$where} (assignedto IN (" . implode(', ' ,$user_ids) . ')';
+				$where = 'AND';
+				$filtermethod .= ' OR (assignedto IS NULL AND phpgw_helpdesk_tickets.group_id IN (' . implode(',',$membership) . ')))';
+
 			}
 
 			if ($reported_by > 0)
