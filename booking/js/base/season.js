8461522d027dad058e33ad260a81644ca2d1f9a2
Sigurd Nes <sigurdne@users.noreply.github.com>
2019-04-25 15:26:37 +0200

    booking: generate proper links in js

--- booking/js/base/season.js
+++ booking/js/base/season.js
@@ -3,9 +3,9 @@ var regulations_select_all = "";
 
 $(document).ready(function ()
 {
-	JqueryPortico.autocompleteHelper('index.php?menuaction=booking.uipermission_root.index_accounts&phpgw_return_as=json&',
+	JqueryPortico.autocompleteHelper(phpGWLink('index.php', {menuaction: 'booking.uipermission_root.index_accounts'}, true),
 		'field_officer_name', 'field_officer_id', 'officer_container');
-	JqueryPortico.autocompleteHelper('index.php?menuaction=booking.uibuilding.index&phpgw_return_as=json&',
+	JqueryPortico.autocompleteHelper(phpGWLink('index.php', {menuaction: 'booking.uibuilding.index'}, true),
 		'field_building_name', 'field_building_id', 'building_container');
 });
 
@@ -53,7 +53,7 @@ if ($.formUtils)
 
 function populateTableChkResources(building_id, selection)
 {
-	var url = 'index.php?menuaction=booking.uiresource.index&sort=name&filter_building_id=' + building_id + '&phpgw_return_as=json&';
+	var url = phpGWLink('index.php', {menuaction: 'booking.uiresource.index', sort: 'name', filter_building_id: building_id, length: -1}, true);
 	var container = 'resources-container';
 	var colDefsResources = [{label: '', object: [{type: 'input', attrs: [
 						{name: 'type', value: 'checkbox'}, {name: 'name', value: 'resources[]'}
