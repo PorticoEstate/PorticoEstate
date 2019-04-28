20a0c0f64880525f32b8592838c9ba86f981b2d3
Sigurd Nes <sigurdne@users.noreply.github.com>
2019-04-28 21:27:07 +0200

    helpdesk: add multiselect

--- helpdesk/inc/class.uitts.inc.php
+++ helpdesk/inc/class.uitts.inc.php
@@ -897,9 +897,10 @@ HTML;
 				$values_combo_box[5] = $this->bo->get_reported_by($this->reported_by);
 				array_unshift($values_combo_box[5], array('id' => $GLOBALS['phpgw_info']['user']['account_id'],
 					'name' => lang('my submitted tickets')));
-				array_unshift($values_combo_box[5], array('id' => '', 'name' => lang('reported by')));
+//				array_unshift($values_combo_box[5], array('id' => '', 'name' => lang('reported by')));
 				$combos[] = array('type' => 'filter',
 					'name' => 'reported_by',
+					'multiple'	=> true,
 					'extra' => '',
 					'text' => lang('reported by'),
 					'list' => $values_combo_box[5]
