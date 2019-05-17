a95d00b98543bcece191d0236a67954b32151f67
Sigurd Nes <sigurdne@users.noreply.github.com>
2019-05-17 20:33:56 +0200

    booking: more on datepicker

--- booking/js/base/application.js
+++ booking/js/base/application.js
@@ -5,9 +5,15 @@ $(document).ready(function ()
 {
 	$("#start_date").change(function ()
 	{
-		if(!$("#end_date").val())
+//		if(!$("#end_date").val())
 		{
+			var temp_start_date =   $( "#start_date" ).datetimepicker('getValue');
+//			console.log(temp_start_date);
 			$("#end_date").val($("#start_date").val());
+
+			$('#end_date').datetimepicker('setOptions', {
+				startDate: new Date(temp_start_date)
+			});
 		}
 	});
 
