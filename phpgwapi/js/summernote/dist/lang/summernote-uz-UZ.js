c37f22c8382fc99867fbfa926eb3391074897815
Sigurd Nes <sigurdne@users.noreply.github.com>
2019-12-29 17:07:11 +0100

    update summernote from upstream

--- phpgwapi/js/summernote/dist/lang/summernote-uz-UZ.js
+++ phpgwapi/js/summernote/dist/lang/summernote-uz-UZ.js
@@ -103,160 +103,122 @@ return /******/ (function(modules) { // webpackBootstrap
 /******/
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 10);
+/******/ 	return __webpack_require__(__webpack_require__.s = 46);
 /******/ })
 /************************************************************************/
 /******/ ({
 
-/***/ 10:
+/***/ 46:
 /***/ (function(module, exports) {
 
 (function ($) {
   $.extend($.summernote.lang, {
-    'cs-CZ': {
+    'uz-UZ': {
       font: {
-        bold: 'Tučné',
-        italic: 'Kurzíva',
-        underline: 'Podtržené',
-        clear: 'Odstranit styl písma',
-        height: 'Výška řádku',
-        strikethrough: 'Přeškrtnuté',
-        size: 'Velikost písma'
+        bold: 'қалин',
+        italic: 'Курсив',
+        underline: 'Белгиланган',
+        clear: 'Ҳарф турларини олиб ташлаш',
+        height: 'Чизиқ баландлиги',
+        name: 'Ҳарф',
+        strikethrough: 'Ўчирилган',
+        subscript: 'Пастки индекс',
+        superscript: 'Юқори индекс',
+        size: 'ҳарф ҳажми'
       },
       image: {
-        image: 'Obrázek',
-        insert: 'Vložit obrázek',
-        resizeFull: 'Původní velikost',
-        resizeHalf: 'Poloviční velikost',
-        resizeQuarter: 'Čtvrteční velikost',
-        floatLeft: 'Umístit doleva',
-        floatRight: 'Umístit doprava',
-        floatNone: 'Neobtékat textem',
-        shapeRounded: 'Shape: Rounded',
-        shapeCircle: 'Shape: Circle',
-        shapeThumbnail: 'Shape: Thumbnail',
-        shapeNone: 'Shape: None',
-        dragImageHere: 'Přetáhnout sem obrázek',
-        dropImage: 'Drop image or Text',
-        selectFromFiles: 'Vybrat soubor',
-        url: 'URL obrázku',
-        remove: 'Remove Image',
-        original: 'Original'
+        image: 'Расм',
+        insert: 'расмни қўйиш',
+        resizeFull: 'Ҳажмни тиклаш',
+        resizeHalf: '50% гача кичрайтириш',
+        resizeQuarter: '25% гача кичрайтириш',
+        floatLeft: 'Чапда жойлаштириш',
+        floatRight: 'Ўнгда жойлаштириш',
+        floatNone: 'Стандарт бўйича жойлашув',
+        shapeRounded: 'Шакли: Юмалоқ',
+        shapeCircle: 'Шакли: Доира',
+        shapeThumbnail: 'Шакли: Миниатюра',
+        shapeNone: 'Шакли: Йўқ',
+        dragImageHere: 'Суратни кўчириб ўтинг',
+        dropImage: 'Суратни кўчириб ўтинг',
+        selectFromFiles: 'Файллардан бирини танлаш',
+        url: 'суратлар URL и',
+        remove: 'Суратни ўчириш'
       },
       video: {
-        video: 'Video',
-        videoLink: 'Odkaz videa',
-        insert: 'Vložit video',
-        url: 'URL videa?',
-        providers: '(YouTube, Vimeo, Vine, Instagram, DailyMotion nebo Youku)'
+        video: 'Видео',
+        videoLink: 'Видеога ҳавола',
+        insert: 'Видео',
+        url: 'URL видео',
+        providers: '(YouTube, Vimeo, Vine, Instagram, DailyMotion или Youku)'
       },
       link: {
-        link: 'Odkaz',
-        insert: 'Vytvořit odkaz',
-        unlink: 'Zrušit odkaz',
-        edit: 'Upravit',
-        textToDisplay: 'Zobrazovaný text',
-        url: 'Na jaké URL má tento odkaz vést?',
-        openInNewWindow: 'Otevřít v novém okně'
+        link: 'Ҳавола',
+        insert: 'Ҳаволани қўйиш',
+        unlink: 'Ҳаволани олиб ташлаш',
+        edit: 'Таҳрир қилиш',
+        textToDisplay: 'Кўринадиган матн',
+        url: 'URL ўтиш учун',
+        openInNewWindow: 'Янги дарчада очиш'
       },
       table: {
-        table: 'Tabulka',
-        addRowAbove: 'Add row above',
-        addRowBelow: 'Add row below',
-        addColLeft: 'Add column left',
-        addColRight: 'Add column right',
-        delRow: 'Delete row',
-        delCol: 'Delete column',
-        delTable: 'Delete table'
+        table: 'Жадвал'
       },
       hr: {
-        insert: 'Vložit vodorovnou čáru'
+        insert: 'Горизонтал чизиқни қўйиш'
       },
       style: {
-        style: 'Styl',
-        p: 'Normální',
-        blockquote: 'Citace',
-        pre: 'Kód',
-        h1: 'Nadpis 1',
-        h2: 'Nadpis 2',
-        h3: 'Nadpis 3',
-        h4: 'Nadpis 4',
-        h5: 'Nadpis 5',
-        h6: 'Nadpis 6'
+        style: 'Услуб',
+        p: 'Яхши',
+        blockquote: 'Жумла',
+        pre: 'Код',
+        h1: 'Сарлавҳа 1',
+        h2: 'Сарлавҳа  2',
+        h3: 'Сарлавҳа  3',
+        h4: 'Сарлавҳа  4',
+        h5: 'Сарлавҳа  5',
+        h6: 'Сарлавҳа  6'
       },
       lists: {
-        unordered: 'Odrážkový seznam',
-        ordered: 'Číselný seznam'
+        unordered: 'Белгиланган рўйҳат',
+        ordered: 'Рақамланган рўйҳат'
       },
       options: {
-        help: 'Nápověda',
-        fullscreen: 'Celá obrazovka',
-        codeview: 'HTML kód'
+        help: 'Ёрдам',
+        fullscreen: 'Бутун экран бўйича',
+        codeview: 'Бошланғич код'
       },
       paragraph: {
-        paragraph: 'Odstavec',
-        outdent: 'Zvětšit odsazení',
-        indent: 'Zmenšit odsazení',
-        left: 'Zarovnat doleva',
-        center: 'Zarovnat na střed',
-        right: 'Zarovnat doprava',
-        justify: 'Zarovnat oboustranně'
+        paragraph: 'Параграф',
+        outdent: 'Орқага қайтишни камайтириш',
+        indent: 'Орқага қайтишни кўпайтириш',
+        left: 'Чап қирғоққа тўғрилаш',
+        center: 'Марказга тўғрилаш',
+        right: 'Ўнг қирғоққа тўғрилаш',
+        justify: 'Эни бўйлаб чўзиш'
       },
       color: {
-        recent: 'Aktuální barva',
-        more: 'Další barvy',
-        background: 'Barva pozadí',
-        foreground: 'Barva písma',
-        transparent: 'Průhlednost',
-        setTransparent: 'Nastavit průhlednost',
-        reset: 'Obnovit',
-        resetToDefault: 'Obnovit výchozí',
-        cpSelect: 'Vybrat'
+        recent: 'Охирги ранг',
+        more: 'Яна ранглар',
+        background: 'Фон  ранги',
+        foreground: 'Ҳарф ранги',
+        transparent: 'Шаффоф',
+        setTransparent: 'Шаффофдай қилиш',
+        reset: 'Бекор қилиш',
+        resetToDefault: 'Стандартга оид тиклаш'
       },
       shortcut: {
-        shortcuts: 'Klávesové zkratky',
-        close: 'Zavřít',
-        textFormatting: 'Formátování textu',
-        action: 'Akce',
-        paragraphFormatting: 'Formátování odstavce',
-        documentStyle: 'Styl dokumentu'
-      },
-      help: {
-        'insertParagraph': 'Insert Paragraph',
-        'undo': 'Undoes the last command',
-        'redo': 'Redoes the last command',
-        'tab': 'Tab',
-        'untab': 'Untab',
-        'bold': 'Set a bold style',
-        'italic': 'Set a italic style',
-        'underline': 'Set a underline style',
-        'strikethrough': 'Set a strikethrough style',
-        'removeFormat': 'Clean a style',
-        'justifyLeft': 'Set left align',
-        'justifyCenter': 'Set center align',
-        'justifyRight': 'Set right align',
-        'justifyFull': 'Set full align',
-        'insertUnorderedList': 'Toggle unordered list',
-        'insertOrderedList': 'Toggle ordered list',
-        'outdent': 'Outdent on current paragraph',
-        'indent': 'Indent on current paragraph',
-        'formatPara': 'Change current block\'s format as a paragraph(P tag)',
-        'formatH1': 'Change current block\'s format as H1',
-        'formatH2': 'Change current block\'s format as H2',
-        'formatH3': 'Change current block\'s format as H3',
-        'formatH4': 'Change current block\'s format as H4',
-        'formatH5': 'Change current block\'s format as H5',
-        'formatH6': 'Change current block\'s format as H6',
-        'insertHorizontalRule': 'Insert horizontal rule',
-        'linkDialog.show': 'Show Link Dialog'
+        shortcuts: 'Клавишларнинг ҳамохҳанглиги',
+        close: 'Ёпиқ',
+        textFormatting: 'Матнни ',
+        action: 'Ҳаркат',
+        paragraphFormatting: 'Параграфни форматлаш',
+        documentStyle: 'Ҳужжатнинг тури',
+        extraKeys: 'Қўшимча имкониятлар'
       },
       history: {
-        undo: 'Krok vzad',
-        redo: 'Krok vpřed'
-      },
-      specialChar: {
-        specialChar: 'SPECIAL CHARACTERS',
-        select: 'Select Special characters'
+        undo: 'Бекор қилиш',
+        redo: 'Қайтариш'
       }
     }
   });
