a94fc51e79e9c1512e32a922d0a95dc16616cf98
Sigurd Nes <sigurdne@users.noreply.github.com>
2020-04-14 15:55:30 +0200

    helpdesk: refactor glider

--- helpdesk/js/portico/tts.view.js
+++ helpdesk/js/portico/tts.view.js
@@ -1,5 +1,4 @@
 /* global CKEDITOR */
-var glider;
 
 var FormatterCenter = function (key, oData)
 {
@@ -132,43 +131,14 @@ this.refresh_files = function ()
 	base_java_url['action'] = 'get_files';
 	var oArgs = base_java_url;
 	var strURL = phpGWLink('index.php', oArgs, true);
-
-	$.ajax({
-		type: 'POST',
-		dataType: 'json',
-		url: strURL,
-		success: function (data)
-		{
-			if (data != null)
-			{
-				var slides = glider.slides.length - 1;
-				for (i = slides; i >= 0; i--)
-				{
-					glider.removeItem(i);
-				}
-
-				var files = data.data;
-				$.each(files, function (k, v)
-				{
-					if (typeof (v.img_url) !== 'undefined' && v.img_url)
-					{
-						var div = document.createElement('div');
-						var img = document.createElement('img');
-						img.setAttribute('data-src', v.img_url.replace(/\&amp;/g, '&'));
-						img.alt = v.file_name;
-						div.appendChild(img);
-						glider.addItem(div);
-					}
-				});
-				if (glider.slides.length > 0)
-				{
-					$('.wrapperForGlider').show();
-					glider.refresh(true);
-					loadImages.call(glider.slides[0]);
-				}
-			}
-		}
-	});
+	try
+	{
+		refresh_glider(strURL);
+	}
+	catch(e)
+	{
+		
+	}
 
 	JqueryPortico.updateinlineTableHelper(oTable2, strURL);
 };
@@ -274,70 +244,7 @@ $(document).ready(function ()
 		$('form').isValid(false, conf_on_load, true);
 	}, 500);
 
-	try
-	{
-
-		glider = new Glider(document.querySelector('.glider'), {
-			slidesToShow: 1,
-			draggable: true,
-			arrows: {
-				prev: '.glider-prev',
-				next: '.glider-next'
-			},
-			easing: function (x, t, b, c, d)
-			{
-				return c * (t /= d) * t + b;
-			},
-			dots: '.dots'
-		});
-
-		document.querySelector('.glider').addEventListener('glider-slide-visible', function (event)
-		{
-			var imgs_to_anticipate = 1;
-			var glider = Glider(this);
-			for (var i = 0; i <= imgs_to_anticipate; ++i)
-			{
-				var index = Math.min(event.detail.slide + i, glider.slides.length - 1),
-					glider = glider;
-				loadImages.call(glider.slides[index], function ()
-				{
-					glider.refresh(true);
-				})
-			}
-		});
-
-		if (glider.slides.length > 0)
-		{
-			$('.wrapperForGlider').show();
-			glider.refresh(true);
-			loadImages.call(glider.slides[0]);
-		}
-
-	}
-	catch (e)
-	{
-
-	}
-
 });
-
-function loadImages(callback)
-{
-	[].forEach.call(this.querySelectorAll('img'), function (img)
-	{
-		var _img = new Image, _src = img.getAttribute('data-src');
-		_img.onload = function ()
-		{
-			img.src = _src;
-			img.classList.add('loaded');
-			callback && callback(img);
-		}
-		if (img.src !== _src)
-		{
-			_img.src = _src;
-		}
-	});
-}
 
 var oArgs = {
 	menuaction: 'helpdesk.uitts.get_on_behalf_of',
