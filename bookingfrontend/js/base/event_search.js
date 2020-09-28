b26d124814c277f135a00d53fbc3c786e107ae64
Andreas Sekkingstad <andsek@hotmail.com>
2020-09-28 22:34:38 +0200

    UT-29 Legger til autocomplete for organisasjoner, inser at det er mulig overlapp i kode mellom eksisterende og dette. Men det fungerer til nå.

--- bookingfrontend/js/base/event_search.js
+++ bookingfrontend/js/base/event_search.js
@@ -1,9 +1,17 @@
 var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 var viewmodel;
+var orgNameList = [];
+var tempList= [];
 
 function AppViewModel() {
     var self = this;
     self.events = ko.observableArray();
+
+    self.removeRecord = function () {
+        self.events.remove(this);
+        self.events.valueHasMutated();
+
+    };
 }
 //"2020-09-28 09:00:00"
 function getDateFormat(from, to) {
@@ -12,7 +20,7 @@ function getDateFormat(from, to) {
     var toDate = new Date(to);
 
     if (fromDate.getDate() === toDate.getDate()) {
-        ret.push(fromDate.getDate())
+        ret.push(fromDate.getDate()+". ")
         ret.push(months[fromDate.getMonth()]);
         return ret;
     } else {
@@ -33,6 +41,7 @@ function getTimeFormat(from, to) {
 
 function setdata(result) {
     for (var i = 0; i < result.length; i++) {
+        orgNameList.push(result[i].org_name);
         var formattedDateAndMonthArr = getDateFormat(result[i].from,result[i].to);
 
         var eventTime = getTimeFormat(result[i].from,result[i].to);
@@ -46,6 +55,8 @@ function setdata(result) {
             location_name: result[i].location_name
         });
     }
+    console.log("test"+viewmodel.events().length);
+
 }
 
 $(document).ready(function () {
@@ -65,5 +76,144 @@ function getUpcomingEvents() {
         error: function (error) {
             console.log(error);
         }
+    });
+}
+
+function sortEvents() {
+
+}
+
+function searchInput() {
+    var inputVal = document.getElementById("eventsearchBoxID").value;
+    console.log(inputVal);
+    if (inputVal === 'undefined' || !inputVal) {
+        viewmodel.events.removeAll();
+        getUpcomingEvents();
+    } else {
+        if (viewmodel.events().length != 0) {
+
+            updatedList = [];
+            for (var i = 0; i < viewmodel.events().length; i++) {
+                if (viewmodel.events()[i].org_name == inputVal) {
+                    console.log(viewmodel.events()[i])
+                    updatedList.push(viewmodel.events()[i]);
+                }
+                console.log("size" + updatedList.length);
+            }
+        }
+
+        viewmodel.events.removeAll();
+        console.log(updatedList);
+        for (var i = 0; i < updatedList.length;i++)
+        viewmodel.events.push({
+            event_name: updatedList[i].event_name,
+            formattedDate: updatedList[i].formattedDate,
+            monthText: updatedList[i].monthText,
+            event_time: updatedList[i].event_time,
+            org_name: updatedList[i].org_name,
+            location_name: updatedList[i].location_name
+        });
+    }
+
+}
+
+function coolfunc() {
+    autocomplete(document.getElementById('eventsearchBoxID'), orgNameList);
+}
+
+function autocomplete(inp, arr) {
+    /*the autocomplete function takes two arguments,
+    the text field element and an array of possible autocompleted values:*/
+    var currentFocus;
+    /*execute a function when someone writes in the text field:*/
+    inp.addEventListener("input", function(e) {
+        var a, b, i, val = this.value;
+        /*close any already open lists of autocompleted values*/
+        closeAllLists();
+        if (!val) { return false;}
+        currentFocus = -1;
+        /*create a DIV element that will contain the items (values):*/
+        a = document.createElement("DIV");
+        a.setAttribute("id", this.id + "autocomplete-list");
+        a.setAttribute("class", "autocomplete-items");
+        /*append the DIV element as a child of the autocomplete container:*/
+        this.parentNode.appendChild(a);
+        /*for each item in the array...*/
+        for (i = 0; i < arr.length; i++) {
+            /*check if the item starts with the same letters as the text field value:*/
+            if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+                /*create a DIV element for each matching element:*/
+                b = document.createElement("DIV");
+                /*make the matching letters bold:*/
+                b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+                b.innerHTML += arr[i].substr(val.length);
+                /*insert a input field that will hold the current array item's value:*/
+                b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+                /*execute a function when someone clicks on the item value (DIV element):*/
+                b.addEventListener("click", function(e) {
+                    /*insert the value for the autocomplete text field:*/
+                    inp.value = this.getElementsByTagName("input")[0].value;
+                    /*close the list of autocompleted values,
+                    (or any other open lists of autocompleted values:*/
+                    closeAllLists();
+                });
+                a.appendChild(b);
+            }
+        }
+    });
+    /*execute a function presses a key on the keyboard:*/
+    inp.addEventListener("keydown", function(e) {
+        var x = document.getElementById(this.id + "autocomplete-list");
+        if (x) x = x.getElementsByTagName("div");
+        if (e.keyCode == 40) {
+            /*If the arrow DOWN key is pressed,
+            increase the currentFocus variable:*/
+            currentFocus++;
+            /*and and make the current item more visible:*/
+            addActive(x);
+        } else if (e.keyCode == 38) { //up
+            /*If the arrow UP key is pressed,
+            decrease the currentFocus variable:*/
+            currentFocus--;
+            /*and and make the current item more visible:*/
+            addActive(x);
+        } else if (e.keyCode == 13) {
+            /*If the ENTER key is pressed, prevent the form from being submitted,*/
+            e.preventDefault();
+            if (currentFocus > -1) {
+                /*and simulate a click on the "active" item:*/
+                if (x) x[currentFocus].click();
+            }
+        }
+    });
+    function addActive(x) {
+        /*a function to classify an item as "active":*/
+        if (!x) return false;
+        /*start by removing the "active" class on all items:*/
+        removeActive(x);
+        if (currentFocus >= x.length) currentFocus = 0;
+        if (currentFocus < 0) currentFocus = (x.length - 1);
+        /*add class "autocomplete-active":*/
+        x[currentFocus].classList.add("autocomplete-active");
+    }
+    function removeActive(x) {
+        /*a function to remove the "active" class from all autocomplete items:*/
+        for (var i = 0; i < x.length; i++) {
+            x[i].classList.remove("autocomplete-active");
+        }
+    }
+    function closeAllLists(elmnt) {
+        /*close all autocomplete lists in the document,
+        except the one passed as an argument:*/
+        var x = document.getElementsByClassName("autocomplete-items");
+        for (var i = 0; i < x.length; i++) {
+            if (elmnt != x[i] && elmnt != inp) {
+                x[i].parentNode.removeChild(x[i]);
+            }
+        }
+    }
+    /*execute a function when someone clicks in the document:*/
+    document.addEventListener("click", function (e) {
+        closeAllLists(e.target);
     });
 }
