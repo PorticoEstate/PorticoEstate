2655a3a29a92213561162f5c67c7cc1e30d8d84c
Sigurd Nes <sigurdne@online.no>
2020-10-19 09:44:30 +0200

    fix calculation of weeknumber

--- phpgwapi/templates/aalesund/js/common.js
+++ phpgwapi/templates/aalesund/js/common.js
@@ -119,8 +119,16 @@ function CreateUrlParams(params)
 
 Date.prototype.getWeek = function ()
 {
-	var onejan = new Date(this.getFullYear(), 0, 1);
-	return Math.ceil((((this - onejan) / 86400000) + onejan.getDay() + 1) / 7);
+//	var onejan = new Date(this.getFullYear(), 0, 1);
+//	return Math.ceil((((this - onejan) / 86400000) + onejan.getDay() + 1) / 7);
+// https://stackoverflow.com/questions/6117814/get-week-of-year-in-javascript-like-in-php
+  var d = new Date(Date.UTC(this.getFullYear(), this.getMonth(), this.getDate()));
+  var dayNum = d.getUTCDay() || 7;
+  d.setUTCDate(d.getUTCDate() + 4 - dayNum);
+  var yearStart = new Date(Date.UTC(d.getUTCFullYear(),0,1));
+  return Math.ceil((((d - yearStart) / 86400000) + 1)/7)
+
+
 };
 
 $(document).ready(function ()
