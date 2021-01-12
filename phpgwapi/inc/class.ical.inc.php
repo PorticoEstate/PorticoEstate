f78406f1b19e642df9dddf44a48ea171f67129b7
Sigurd Nes <sigurdne@online.no>
2021-01-12 17:07:37 +0100

    API: add iCalcreator

---
+++ phpgwapi/inc/class.ical.inc.php
@@ -0,0 +1,51 @@
+<?php
+	/**
+	* phpGroupWare
+	*
+	* @author Sigurd Nes <sigurdne@online.no>
+	* @copyright Copyright (C) 2021 Free Software Foundation, Inc. http://www.fsf.org/
+	* @license http://www.gnu.org/licenses/gpl.html GNU General Public License
+	* @internal Development of this application was funded by http://www.bergen.kommune.no/
+	* @package phpgwapi
+	* @subpackage utilities
+ 	* @version $Id$
+	*
+	*/
+
+
+	require_once PHPGW_API_INC . '/icalcreator/autoload.php';
+	use Kigkonsult\Icalcreator\Vcalendar;
+
+	/**
+	* Document me!
+	*
+	* @package phpgwapi
+	* @subpackage utilities
+	*/
+	class phpgwapi_ical
+	{
+		public $vcalendar;
+
+		public function __construct()
+		{
+			$timezone	 = !empty($GLOBALS['phpgw_info']['user']['preferences']['common']['timezone']) ? $GLOBALS['phpgw_info']['user']['preferences']['common']['timezone'] : 'UTC';
+			$unique_id	 = !empty($GLOBALS['phpgw_info']['server']['site_title']) ? $GLOBALS['phpgw_info']['server']['site_title'] : $GLOBALS['phpgw_info']['server']['system_name'];
+
+			$this->vcalendar = Vcalendar::factory([Vcalendar::UNIQUE_ID => $unique_id,])
+
+				// with calendaring info
+				->setMethod(Vcalendar::PUBLISH)
+				->setXprop(
+					Vcalendar::X_WR_CALNAME,
+					"Calendar Sample"
+				)
+				->setXprop(
+					Vcalendar::X_WR_CALDESC,
+					 "This is a demo calendar"
+				)
+				->setXprop(
+				Vcalendar::X_WR_TIMEZONE,
+				$timezone
+			);
+		}
+	}
