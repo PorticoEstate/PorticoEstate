c1f66618b773202afc09c51a3dde94f2d6533093
sigurdne <sigurdne@online.no>
2021-03-05 17:26:55 +0100

    template selector

--- phpgwapi/templates/bootstrap/head.inc.php
+++ phpgwapi/templates/bootstrap/head.inc.php
@@ -34,15 +34,11 @@
 	$javascripts[]	 = "/phpgwapi/js/popper/popper.min.js";
 	$javascripts[]	 = "/phpgwapi/js/bootstrap/js/bootstrap.min.js";
 
-	if( !isset($GLOBALS['phpgw_info']['flags']['noframework']) && !isset($GLOBALS['phpgw_info']['flags']['nonavbar']) )
+	if( empty($GLOBALS['phpgw_info']['flags']['noframework']) && empty($GLOBALS['phpgw_info']['flags']['nonavbar']) )
 	{
 		$GLOBALS['phpgw_info']['user']['preferences']['common']['sidecontent'] = 'ajax_menu';//ajax_menu|jsmenu
-		if (isset($GLOBALS['phpgw_info']['user']['preferences']['common']['sidecontent']) && $GLOBALS['phpgw_info']['user']['preferences']['common']['sidecontent'] == 'ajax_menu')
-		{
-			phpgwapi_jquery::load_widget('contextMenu');
-			$javascripts[] = "/phpgwapi/templates/bootstrap/js/sidenav.js";
-		}
-
+		phpgwapi_jquery::load_widget('contextMenu');
+		$javascripts[] = "/phpgwapi/templates/bootstrap/js/sidenav.js";
 	}
 
 	$stylesheets = array();
