6cb1f4ca0495e2d994aa67d17b9818a89fbf08b0
sigurdne <sigurdne@online.no>
2021-03-05 15:54:06 +0100

    booking: delete reservation with (not billed) completed reservations

--- booking/inc/class.soevent.inc.php
+++ booking/inc/class.soevent.inc.php
@@ -458,8 +458,18 @@
 			$sql = "DELETE FROM $table_name WHERE event_id = ($id)";
 			$db->query($sql, __LINE__, __FILE__);
 
-			$sql = "DELETE FROM bb_completed_reservation WHERE reservation_id = $id AND reservation_type = 'event' AND export_file_id IS NULL";
+			$sql = "SELECT id FROM bb_completed_reservation WHERE reservation_id = $id AND reservation_type = 'event' AND export_file_id IS NULL";
 			$db->query($sql, __LINE__, __FILE__);
+			$this->db->next_record();
+			$completed_reservation_id = (int)$this->db->f('id');
+			if($completed_reservation_id)
+			{
+				$sql = "DELETE FROM bb_completed_reservation_resource WHERE completed_reservation_id = $completed_reservation_id";
+				$db->query($sql, __LINE__, __FILE__);
+
+				$sql = "DELETE FROM bb_completed_reservation WHERE id = $completed_reservation_id";
+				$db->query($sql, __LINE__, __FILE__);	
+			}
 
 			$table_name = $this->table_name;
 			$sql = "DELETE FROM $table_name WHERE id = ($id)";
