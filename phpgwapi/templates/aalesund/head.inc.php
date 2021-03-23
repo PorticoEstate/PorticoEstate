0d101334202ca564909c045629258c5be231f703
Andreas Sekkingstad <andsek@hotmail.com>
2021-03-23 09:47:37 +0100

    Revert "AK-2 Fjerner arrangement knapp"
    This reverts commit 51feceec3780e82192cccbef194505432ebcfed3.
    
    # Conflicts:
    #	phpgwapi/templates/aalesund/head.tpl

--- phpgwapi/templates/aalesund/head.inc.php
+++ phpgwapi/templates/aalesund/head.inc.php
@@ -290,6 +290,9 @@ JS;
 			<div class="container header-container my_class">
 				<a class="navbar-brand brand-site-title" href="{$site_url}">{$site_title} </a>
 				<a href="{$site_url}"><img class="navbar-brand brand-site-img" src="{$headlogoimg}" alt="{$logo_title}"/></a>
+				<div class="arrangement-link-box">
+					<a class="Arrangement_link" href="{eventsearch_url}">Arrangement</a>
+				</div>
 				<!-- Search Box -->
 				<!--div class="search-container">
 					<form id="navSearchForm" class="search-form">
