da2820829ad9e28642cd4131ca8bdc20e8af3468
Sigurd Nes <sigurdne@online.no>
2021-03-30 13:18:25 +0200

    Ny regel for epost-integrasjon

--- property/inc/cron/default/hent_epost_fra_eksterne_BK_LRS.php
+++ property/inc/cron/default/hent_epost_fra_eksterne_BK_LRS.php
@@ -1381,6 +1381,18 @@
 					$target['id']				 = $ticket_id;
 				}
 			}
+			else if (preg_match("/faktura med feil MVA-beregning/i", $subject))
+			{
+				$message_cat_id	 = 334; // LRS-Regnskap- underkategori: 28 Kostfordeling/prekontering
+				$group_id		 = 4253; //LRS-DRIFT_Regnskap
+				$ticket_id		 = $this->create_ticket($subject, $body, $message_cat_id, $group_id, $sender, $body_type);
+				if ($ticket_id)
+				{
+					$this->receipt['message'][]	 = array('msg' => "Melding #{$ticket_id} er opprettet");
+					$target['type']				 = 'helpdesk';
+					$target['id']				 = $ticket_id;
+				}
+			}
 
 			/**
 			 * Ticket created / updated
