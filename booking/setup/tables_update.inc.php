fcb0055789068dfce02ec69ef609751418533b5f
Sigurd Nes <sigurdne@online.no>
2021-04-23 11:40:47 +0200

    booking: more on billing (organize sub-departments within organization)

--- booking/setup/tables_update.inc.php
+++ booking/setup/tables_update.inc.php
@@ -4952,7 +4952,7 @@
 		}
 	}
 	/**
-	 * Update booking version from 0.2.67 to 0.2.68
+	 * Update booking version from 0.2.68 to 0.2.69
 	 *
 	 */
 	$test[] = '0.2.68';
@@ -4970,6 +4970,27 @@
 		if ($GLOBALS['phpgw_setup']->oProc->m_odb->transaction_commit())
 		{
 			$GLOBALS['setup_info']['booking']['currentver'] = '0.2.69';
+			return $GLOBALS['setup_info']['booking']['currentver'];
+		}
+	}
+
+	/**
+	 * Update booking version from 0.2.69 to 0.2.70
+	 *
+	 */
+	$test[] = '0.2.69';
+	function booking_upgrade0_2_69()
+	{
+		$GLOBALS['phpgw_setup']->oProc->m_odb->transaction_begin();
+
+		$GLOBALS['phpgw_setup']->oProc->AddColumn('bb_completed_reservation','customer_number',array(
+			'type' => 'text',
+			'nullable' => True
+		));
+
+		if ($GLOBALS['phpgw_setup']->oProc->m_odb->transaction_commit())
+		{
+			$GLOBALS['setup_info']['booking']['currentver'] = '0.2.70';
 			return $GLOBALS['setup_info']['booking']['currentver'];
 		}
 	}
