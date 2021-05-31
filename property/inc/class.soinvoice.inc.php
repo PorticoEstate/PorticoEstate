78957c5352f6aec375f9ebf1a21e95eb4f5bc783
sigurdne <sigurdne@online.no>
2021-05-31 22:06:16 +0200

    more on voucher filter

--- property/inc/class.soinvoice.inc.php
+++ property/inc/class.soinvoice.inc.php
@@ -2646,71 +2646,37 @@
 
 			if ($data['janitor_lid'])
 			{
-//				if (stripos($data['janitor_lid'], '*') === 0)
-//				{
-					$data['janitor_lid'] = ltrim($data['janitor_lid'], '*');
-//					$filtermethod		 .= " $where oppsynsigndato IS NULL";
-//					$where				 = 'AND';
-//				}
-
-				$filtermethod		 .= " {$where} oppsynsigndato IS NULL AND saksbehandlerid != '{$data['janitor_lid']}'";
-				$where				 = 'AND';
-
-				$filtermethod	 .= " {$where} oppsynsmannid = '{$data['janitor_lid']}'";
-				$where			 = 'AND';
+				$data['janitor_lid'] = ltrim($data['janitor_lid'], '*');
+				$filtermethod	 .= " {$where} (oppsynsigndato IS NULL AND saksbehandlerid != '{$data['janitor_lid']}'";
+				$filtermethod	 .= " AND oppsynsmannid = '{$data['janitor_lid']}')";
+				$where			 = 'OR';
 			}
 
 			if ($data['supervisor_lid'])
 			{
-//				if (stripos($data['supervisor_lid'], '*') === 0)
-//				{
-					$data['supervisor_lid']	 = ltrim($data['supervisor_lid'], '*');
-//					if($data['supervisor_lid']	== $GLOBALS['phpgw_info']['user']['account_lid'])
-//					{
-//						$filtermethod 	 .= " $where oppsynsigndato IS NOT NULL AND saksigndato IS NULL";
-//					}
-//					else
-//					{
-//						$filtermethod	 .= " $where saksigndato IS NULL";
-//					}
-//
-//					$where					 = 'AND';
-//				}
+				$data['supervisor_lid']	 = ltrim($data['supervisor_lid'], '*');
 
-				$filtermethod 	 .= " {$where} ( oppsynsigndato IS NOT NULL AND saksigndato IS NULL";
-				$filtermethod 	 .= " OR oppsynsmannid = '{$data['supervisor_lid']}' AND oppsynsigndato IS NULL AND saksigndato IS NULL)";
-				$where			 = 'AND';
+				$filtermethod 	 .= " {$where} (( saksbehandlerid = '{$data['supervisor_lid']}' AND oppsynsigndato IS NOT NULL	AND saksigndato IS NULL )
+					OR (saksbehandlerid = '{$data['supervisor_lid']}' AND oppsynsigndato IS NULL AND saksigndato IS NULL)
+					OR (oppsynsmannid = '{$data['supervisor_lid']}' AND saksbehandlerid = '{$data['supervisor_lid']}' AND oppsynsigndato IS NULL AND saksigndato IS NULL ))";
 
-				$filtermethod	 .= " {$where} saksbehandlerid = '{$data['supervisor_lid']}'";
-				$where			 = 'AND';
+				$where			 = 'OR';
 			}
 
 			if ($data['budget_responsible_lid'])
 			{
-//				if (stripos($data['budget_responsible_lid'], '*') === 0)
-//				{
-					$data['budget_responsible_lid']	 = ltrim($data['budget_responsible_lid'], '*');
-//					if($data['budget_responsible_lid']	== $GLOBALS['phpgw_info']['user']['account_lid'])
-//					{
-//						$filtermethod					 .= " $where saksigndato IS NOT NULL AND budsjettsigndato IS NULL";
-//					}
-//					else
-//					{
-//						$filtermethod					 .= " $where budsjettsigndato IS NULL";
-//					}
-//
-//					$where							 = 'AND';
-//				}
+				$data['budget_responsible_lid']	 = ltrim($data['budget_responsible_lid'], '*');
+				$filtermethod		.= " $where (saksigndato IS NOT NULL AND budsjettsigndato IS NULL";
+				$filtermethod	 .= " AND budsjettansvarligid = '{$data['budget_responsible_lid']}')";
+				$where	= 'AND';
 
-				$filtermethod					 .= " $where saksigndato IS NOT NULL AND budsjettsigndato IS NULL";
-				$where							 = 'AND';
-
-				$filtermethod	 .= " {$where} budsjettansvarligid = '{$data['budget_responsible_lid']}'";
-				$where			 = 'AND';
 			}
+
 
 			if ($data['query'])
 			{
+				$where =  'WHERE';
+				$filtermethod = '';
 				switch ($data['criteria'])
 				{
 					case 'voucher_id':
