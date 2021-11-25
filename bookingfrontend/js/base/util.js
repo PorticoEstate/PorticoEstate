44c04b243d2e7c17860ab1f0b2bb54fcd25f819e
Sigurd Nes <sigurdne@online.no>
2021-11-25 11:12:41 +0100

    Bookingfrontend: dateformat for initiate dateobject in javascript

--- bookingfrontend/js/base/util.js
+++ bookingfrontend/js/base/util.js
@@ -1,44 +1,55 @@
-var Util = function () {
+/* global monthList, months */
+
+var Util = function ()
+{
 
 
 	//Formattering
-	var format = function() {
+	var format = function ()
+	{
 
-		var formatDateForBackend = function(date) {
-			if (date === "") {
+		var formatDateForBackend = function (date)
+		{
+			if (date === "")
+			{
 				return "";
 			}
 			var fDate = new Date(date);
-			return fDate.getFullYear()+"-"+(fDate.getMonth()+1)+"-"+fDate.getDate()+" "+(fDate.getHours())+":"+fDate.getMinutes()+":"+fDate.getSeconds()+"";
-		}
+			return fDate.getFullYear() + "-" + (fDate.getMonth() + 1) + "-" + fDate.getDate() + " " + (fDate.getHours()) + ":" + fDate.getMinutes() + ":" + fDate.getSeconds() + "";
+		};
 
-		var getDateFormat = function(from, to) {
+		var getDateFormat = function (from, to)
+		{
 			let ret = [];
 			let fromDate = new Date(from);
 			let toDate = new Date(to);
 
-			if (fromDate.getDate() === toDate.getDate()) {
-				ret.push(fromDate.getDate()+". ")
+			if (fromDate.getDate() === toDate.getDate())
+			{
+				ret.push(fromDate.getDate() + ". ");
 				let month = monthList[fromDate.getMonth()];
 				ret.push(months[month]);
 				return ret;
-			} else {
+			}
+			else
+			{
 				ret.push(fromDate.getDate() + ".-" + toDate.getDate() + ".");
 				let month = monthList[fromDate.getMonth()];
 				ret.push(months[month]);
 				return ret;
 			}
-		}
+		};
 
-		var getTimeFormat = function(from, to) {
+		var getTimeFormat = function (from, to)
+		{
 			let fromDate = new Date(from);
 			let toDate = new Date(to);
 			let ret;
 
-			ret = (fromDate.getHours() < 10 ? '0'+fromDate.getHours() : fromDate.getHours()) + ":"
-				+ (fromDate.getMinutes() < 10 ? '0'+fromDate.getMinutes() : fromDate.getMinutes()) + " - "
-				+ (toDate.getHours() < 10 ? '0'+toDate.getHours() : toDate.getHours()) + ":"
-				+ (toDate.getMinutes() < 10 ? '0'+toDate.getMinutes() : toDate.getMinutes());
+			ret = (fromDate.getHours() < 10 ? '0' + fromDate.getHours() : fromDate.getHours()) + ":"
+				+ (fromDate.getMinutes() < 10 ? '0' + fromDate.getMinutes() : fromDate.getMinutes()) + " - "
+				+ (toDate.getHours() < 10 ? '0' + toDate.getHours() : toDate.getHours()) + ":"
+				+ (toDate.getMinutes() < 10 ? '0' + toDate.getMinutes() : toDate.getMinutes());
 			return ret;
 		}
 
@@ -46,12 +57,12 @@ var Util = function () {
 			FormatDateForBackend: formatDateForBackend,
 			GetDateFormat: getDateFormat,
 			GetTimeFormat: getTimeFormat
-		}
+		};
 	}();
 
 
 	return {
 		Format: format
-	}
+	};
 
 }();
