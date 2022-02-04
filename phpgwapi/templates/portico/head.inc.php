d8cb94bae39dd339d612be390dfa271f15b781f0
Sigurd Nes <sigurdne@online.no>
2022-02-04 15:42:03 +0100

    resolve conflict

--- phpgwapi/templates/portico/head.inc.php
+++ phpgwapi/templates/portico/head.inc.php
@@ -57,10 +57,11 @@
 	$stylesheets[] = "/phpgwapi/templates/pure/css/pure-min.css";
 	$stylesheets[] = "/phpgwapi/templates/pure/css/pure-extension.css";
 	$stylesheets[] = "/phpgwapi/templates/pure/css/grids-responsive-min.css";
-//	$stylesheets[] = "/phpgwapi/js/DataTables/DataTables/css/jquery.dataTables.min.css";
-//	$stylesheets[] = "/phpgwapi/js/DataTables/DataTables/css/dataTables.jqueryui.min.css";
-//	$stylesheets[] = "/phpgwapi/js/DataTables/Responsive/css/responsive.dataTables.min.css";
-	$stylesheets[] = "/phpgwapi/templates/bookingfrontend/css/fontawesome.all.css";
+	$stylesheets[] = "/phpgwapi/js/DataTables/DataTables/css/jquery.dataTables.min.css";
+	$stylesheets[] = "/phpgwapi/js/DataTables/DataTables/css/dataTables.jqueryui.min.css";
+	$stylesheets[] = "/phpgwapi/js/DataTables/Responsive/css/responsive.dataTables.min.css";
+//	$stylesheets[] = "/phpgwapi/templates/bookingfrontend/css/fontawesome.all.css";
+	$stylesheets[] = "/phpgwapi/templates/base/css/fontawesome/css/all.min.css";
 
 	$stylesheets[] = "/phpgwapi/templates/base/css/base.css";
 	$stylesheets[] = "/phpgwapi/templates/portico/css/base.css";
