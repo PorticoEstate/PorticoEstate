5e084a208938f75900a1c6b7e0993033fce4b9f5
Sigurd Nes <sigurdne@online.no>
2022-03-30 14:56:28 +0200

    Update guzzle from upstream

--- phpgwapi/inc/guzzle/vendor/composer/autoload_static.php
+++ phpgwapi/inc/guzzle/vendor/composer/autoload_static.php
@@ -8,8 +8,8 @@ class ComposerStaticInitd3df6becc845b793df60df63e209f524
 {
     public static $files = array (
         '7b11c4dc42b3b3023073cb14e519683c' => __DIR__ . '/..' . '/ralouphie/getallheaders/src/getallheaders.php',
+        '6e3fae29631ef280660b3cdad06f25a8' => __DIR__ . '/..' . '/symfony/deprecation-contracts/function.php',
         'c964ee0ededf28c96ebd9db5099ef910' => __DIR__ . '/..' . '/guzzlehttp/promises/src/functions_include.php',
-        'a0edc8309cc5e1d60e3047b5df6b7052' => __DIR__ . '/..' . '/guzzlehttp/psr7/src/functions_include.php',
         '37a3dc5111fe8f707ab4c132ef1dbc62' => __DIR__ . '/..' . '/guzzlehttp/guzzle/src/functions_include.php',
     );
 
@@ -31,6 +31,7 @@ class ComposerStaticInitd3df6becc845b793df60df63e209f524
         'Psr\\Http\\Message\\' => 
         array (
             0 => __DIR__ . '/..' . '/psr/http-message/src',
+            1 => __DIR__ . '/..' . '/psr/http-factory/src',
         ),
         'Psr\\Http\\Client\\' => 
         array (
