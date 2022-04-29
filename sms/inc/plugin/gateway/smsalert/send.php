9d18e0425a0b70541df31c8740674e275b38a5ad
Sigurd Nes <sigurdne@online.no>
2022-04-29 11:03:47 +0200

    SMS: CURLOPT_MUTE is deprecated

--- sms/inc/plugin/gateway/smsalert/send.php
+++ sms/inc/plugin/gateway/smsalert/send.php
@@ -58,7 +58,6 @@
 				curl_setopt($ch, CURLOPT_PROXY, "{$this->param['proxy_host']}:{$this->param['proxy_port']}");
 			}
 
-			curl_setopt($ch, CURLOPT_MUTE, 1);
 			curl_setopt($ch, CURLOPT_POST, true);
 			curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)");
 			curl_setopt($ch, CURLOPT_POSTFIELDS, $post_string);
