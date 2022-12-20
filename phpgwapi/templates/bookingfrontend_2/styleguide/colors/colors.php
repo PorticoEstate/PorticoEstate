228c55a65efd9f5f136c0f7247b3da2004978f8d
Kenneth Frøiland <kenneth.froiland@gmail.com>
2022-12-20 16:18:11 +0100

    Implements styleguide inlcuding text and color

---
+++ phpgwapi/templates/bookingfrontend_2/styleguide/colors/colors.php
@@ -0,0 +1,86 @@
+
+<div class="container">
+  <div class="d-flex justify-content-between">
+    <a href="#home">Home</a> <span>Color</span>
+  </div>
+  <div class="row g-0 border-top border-2 py-4">
+    <div class="row">
+        <div class="d-flex align-items-center text-center mb-2">
+          <div class="colorbox bg-purple"></div>
+          <span class="text-center ps-3">Primary color (purple)</span>
+        </div>
+        <div class="d-flex align-items-center text-center mb-5">
+          <div class="colorbox bg-blue"></div>
+          <span class="text-center ps-3">Secondary color (blue)</span>
+        </div>
+    </div>
+    
+    <?php
+      $colors = array(
+        'purple' => '#793C8A',
+        'blue' => '#28358B',
+        'black' => '#E6E3DB',
+        'beige-dark' => '#E6E3DB',
+        'beige' => '#F7F5F0',
+        'grey' => '#D6D6D6',
+        'grey-light' => '#F5F5F5',
+        'white' => '#FFFFFF',
+      );
+
+      $opacities = array(
+        '80', '64', '56', '40', '24', '16', '8', '4'
+      );
+
+      
+      echo '<div class="row mb-5">';
+        foreach ( $colors as $color => $hexColor) {
+          echo '<div class="col-3 d-flex align-items-center mb-3">
+                  <div class="colorbox bg-'.$color.'"></div>
+                  <div class="d-flex flex-column ps-3 ">
+                    <span>'.$hexColor.'</span>
+                    <span>'.$color.'</span>
+                  </div>
+                </div>';
+        }
+      echo '</div>';
+      
+      
+      echo '<div class="d-flex align-items-center justify-content-center mb-5">';
+      foreach ( $colors as $color => $hexColor) {
+        echo '<div class="d-flex flex-column justify-content-center pe-4">
+                <span class="text-center text-lg text-'.$color.'">text-'.$color.'</span>
+                <span class="text-center">text-'.$color.'</span>
+              </div>';
+      }
+      echo '</div>';
+
+      foreach ( $colors as $color => $hexColor) {
+        echo '<div class="row">';
+          echo '<div class="col-2 d-flex flex-column align-items-center pe-4 mb-4">
+                  <div class="colorbox bg-'.$color.'"></div>
+                  <span class="text-center">bg-'.$color.'</span>
+                </div>';
+          
+          foreach ( $opacities as $opacity ) {
+            echo '<div class="col d-flex flex-column align-items-center text-center px-4">
+                  <div class="colorbox bg-'.$color.'-'.$opacity.'"></div>
+                  <span class="text-center">*-'.$opacity.'</span>
+                </div>';
+          }
+        echo '</div>';
+      };
+    ?>
+  </div>
+</div>
+
+
+<style>
+  .colorbox {
+    height: 50px;
+    width: 50px;
+    border-radius: 5px;
+    flex: none;
+
+  }
+
+</style>
