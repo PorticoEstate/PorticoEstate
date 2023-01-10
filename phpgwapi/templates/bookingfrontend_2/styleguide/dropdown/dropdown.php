bc0f089b3c5803f1565f5e9a18361cc28f286672
Kenneth Frøiland <kenneth.froiland@gmail.com>
2023-01-10 18:33:16 +0100

    Adds dropdown to styleguide

---
+++ phpgwapi/templates/bookingfrontend_2/styleguide/dropdown/dropdown.php
@@ -0,0 +1,14 @@
+<script>
+  $(document).ready(function() {
+    $('.js-example-basic-single').select2();
+  });
+</script>
+
+<div class="container">
+  <div class="row g-0 border-top border-2 py-5">
+    <select class="js-example-basic-single">
+      <option value="AL">Alabama</option>
+      <option value="WY">Wyoming</option>
+    </select>
+  </div>
+</div>
