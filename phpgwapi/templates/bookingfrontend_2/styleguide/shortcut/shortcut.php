f70edb6fac1ca28ab4ff371111f927166ded2b37
Kenneth Frøiland <kenneth.froiland@gmail.com>
2023-01-27 14:26:51 +0100

    Adds shortcut component

---
+++ phpgwapi/templates/bookingfrontend_2/styleguide/shortcut/shortcut.php
@@ -0,0 +1,28 @@
+<div class="container border-top border-2 py-5"  style="background-color: #c7e8fa">
+  <div class="row">
+    <div class="col col-md-6 col-lg-4 col-xl-3 d-flex">
+      <div class="shortcut mb-5">
+        <img src="gfx/person_calculator.png" alt="">
+        <button type="button" class="btn btn-primary btn--large">
+          Selskapslokaler
+        </button> 
+      </div>
+    </div>
+    <div class="col col-md-6 col-lg-4 col-xl-3 d-flex">
+      <div class="shortcut mb-5">
+        <img src="gfx/person_calculator.png" alt="">
+        <button type="button" class="btn btn-primary btn--large">
+          Selskapslokaler
+        </button> 
+      </div>
+    </div>
+    <div class="col col-md-6 col-lg-4 col-xl-3 d-flex">
+      <div class="shortcut mb-5">
+        <img src="gfx/person_calculator.png" alt="">
+        <button type="button" class="btn btn-primary btn--large">
+          Selskapslokaler
+        </button> 
+      </div>
+    </div>
+  </div>
+</div>
