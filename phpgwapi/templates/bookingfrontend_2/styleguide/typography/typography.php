5713ff31e58a3f5e6ae522a06a380ef60ac26e65
Kenneth Frøiland <kenneth.froiland@gmail.com>
2023-02-21 18:00:29 +0100

    Added responsive fonts and adjusted bootstrap override

--- phpgwapi/templates/bookingfrontend_2/styleguide/typography/typography.php
+++ phpgwapi/templates/bookingfrontend_2/styleguide/typography/typography.php
@@ -1,20 +1,22 @@
-<div class="container">
-  <div class="row g-0 border-top border-2 py-5">
+<section class="container py-5">
+  <div class="row">
     <div class="col-lg">
-      <h1>H1 - text-bolder - 36px</h1>
-      <h2>H2 - text-bolder - 28px</h2>
-      <h3>H3 - text-bolder - 22px</h3>
-      <h4>H3 - text-bold - 20px</h4>
+      <h1>H1 - text-bolder - 28px / 36px</h1>
+      <h2>H2 - text-bolder - 22px / 28px</h2>
+      <h3>H3 - text-bolder - 20px / 22px</h3>
+      <h4>H4 - text-bold - 18px / 20px</h4>
       <p class="text-bold">Body - text-bold - 16px</p>
       <p>Body - Regular - 16px</p>
     </div>
     <div class="col-lg">
-      <p class="text-4xl">text-4xl - 56px</h1>
-      <p class="text-3xl">text-3xl - 36px</h1>
-      <p class="text-2xl">text-2xl - 28px</h1>
-      <p class="text-xl">text-xl - 22px</h1>
-      <p class="text-lg">text-lg - 20px</h1>
-      <p class="text-body">text-body - 16px</h1>
+      <p class="text-4xl">text-4xl - 56px</p>
+      <p class="text-3xl">text-3xl - 36px</p>
+      <p class="text-2xl">text-2xl - 28px</p>
+      <p class="text-xl">text-xl - 22px</p>
+      <p class="text-lg">text-lg - 20px</p>
+      <p class="text-medium">text-medium - 18px</p>
+      <p class="text-body">text-body - 16px</p>
+      <p class="text-small">text-small - 14px</p>
     </div>
   </div>
-</div>
+</section>
