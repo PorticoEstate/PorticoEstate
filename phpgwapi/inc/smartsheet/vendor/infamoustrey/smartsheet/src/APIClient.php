f77186aa301742110a9d4f25447b6650870b2832
Sigurd Nes <sigurdne@online.no>
2023-03-09 16:04:50 +0100

    smartsheet

---
+++ phpgwapi/inc/smartsheet/vendor/infamoustrey/smartsheet/src/APIClient.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Smartsheet;
+
+use GuzzleHttp\Client as GuzzleClient;
+
+/**
+ * The Guzzle Client Wrapper
+ */
+class APIClient
+{
+    protected const BASE_URL = "https://api.smartsheet.com/2.0/";
+
+    protected GuzzleClient $guzzleClient;
+
+    protected string $token;
+
+    /**
+     * Configure API Client
+     *
+     * config
+     *     token => A valid smartsheet API Token
+     *
+     * @param array $config
+     */
+    public function __construct(array $config)
+    {
+        $this->token = $config['token'];
+
+        $authHeader = "Bearer " . $this->token;
+
+        $this->guzzleClient = new GuzzleClient([
+            'base_uri' => self::BASE_URL,
+            'headers' => [
+                'Authorization' => $authHeader
+            ],
+            'proxy' => !empty($config['proxy']) ? $config['proxy'] : null
+        ]);
+    }
+
+    public function get(string $uri, array $options = [])
+    {
+        return json_decode($this->guzzleClient->get($uri, $options)->getBody()->getContents());
+    }
+
+    public function put(string $uri, array $options = [])
+    {
+        return json_decode($this->guzzleClient->put($uri, $options)->getBody()->getContents());
+    }
+
+    public function post(string $uri, array $options = [])
+    {
+        return json_decode($this->guzzleClient->post($uri, $options)->getBody()->getContents());
+    }
+
+    public function delete(string $uri, array $options = [])
+    {
+        return json_decode($this->guzzleClient->delete($uri, $options)->getBody()->getContents());
+    }
+
+    /**
+     * @return string
+     */
+    public function getToken(): string
+    {
+        return $this->token;
+    }
+}
---
+++ phpgwapi/inc/smartsheet/vendor/infamoustrey/smartsheet/src/SmartsheetClient.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace Smartsheet;
+
+use Illuminate\Support\Collection;
+use Smartsheet\Resources\Contact;
+use Smartsheet\Resources\Sheet;
+use Smartsheet\Resources\Row;
+use Smartsheet\Resources\Folder;
+use Smartsheet\Resources\Workspace;
+
+class SmartsheetClient extends APIClient
+{
+    public function __construct(array $config = [])
+    {
+        parent::__construct($config);
+    }
+
+    protected function instantiateCollection(string $class, array $target): Collection
+    {
+        $temp = collect([]);
+
+        foreach ($target as $t) {
+            $temp->add(new $class($this, (array)$t));
+        }
+
+        return $temp;
+    }
+
+    /**
+     * List Account Contacts
+     */
+    public function listContacts(): Collection
+    {
+        $response = $this->get('contacts');
+
+        if ($response->totalPages > 1) {
+
+            $contacts = $response->data;
+
+            for ($page = 2; $page <= $response->totalPages; $page++) {
+                $contacts = array_merge($contacts, $this->get("contacts?page=$page")->data);
+            }
+
+            return $this->instantiateCollection(Contact::class, $contacts);
+        } else {
+            return $this->instantiateCollection(Contact::class, $response->data);
+        }
+    }
+
+    /**
+     * List Account Sheets
+     */
+    public function listSheets(): Collection
+    {
+        return $this->instantiateCollection(Sheet::class, $this->get('sheets')->data);
+    }
+
+    /**
+     * Fetch a specific sheet
+     *
+     * @param string $sheetId
+     * @return Sheet
+     */
+    public function getSheet(string $sheetId): Sheet
+    {
+        $response = $this->get("sheets/$sheetId");
+
+        return new Sheet($this, (array)$response);
+    }
+
+    /**
+     * Fetch a specific row in a sheet
+     *
+     * @param string $sheetId
+     * @param string $rowId
+     * @return Row
+     */
+    public function getRow(string $sheetId, string $rowId): Row
+    {
+        $response = $this->get("sheets/$sheetId/rows/$rowId");
+
+        return new Row($this, (array)$response);
+    }
+
+    /**
+     * Fetch a folder with a given ID
+     *
+     * @param string $folderId
+     * @return Folder
+     */
+    public function getFolder(string $folderId): Folder
+    {
+        return new Folder($this, (array) $this->get("folders/$folderId"));
+    }
+
+    /**
+     * Fetch a workspace with a given ID
+     *
+     * @param string $workspaceId
+     * @return Workspace
+     */
+    public function getWorkspace(string $workspaceId): Workspace
+    {
+        return new Workspace($this, (array) $this->get("workspaces/$workspaceId"));
+    }
+
+    /**
+     * Returns a list of workspaces
+     */
+    public function listWorkspaces(): Collection
+    {
+        return $this->instantiateCollection(Workspace::class, $this->get("workspaces")->data);
+    }
+}
