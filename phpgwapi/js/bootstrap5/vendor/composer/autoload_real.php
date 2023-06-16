e02853cb9e5c01ad8c4afec90aeb911b0e55b07b
Sigurd Nes <sigurdne@online.no>
2023-06-16 12:57:58 +0200

    update bootstrap from upstream

--- phpgwapi/js/bootstrap5/vendor/composer/autoload_real.php
+++ phpgwapi/js/bootstrap5/vendor/composer/autoload_real.php
@@ -23,11 +23,30 @@ class ComposerAutoloaderInit770c7bb2c48ef6e2c97e7f118fe7e84a
         }
 
         spl_autoload_register(array('ComposerAutoloaderInit770c7bb2c48ef6e2c97e7f118fe7e84a', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInit770c7bb2c48ef6e2c97e7f118fe7e84a', 'loadClassLoader'));
 
-        require __DIR__ . '/autoload_static.php';
-        call_user_func(\Composer\Autoload\ComposerStaticInit770c7bb2c48ef6e2c97e7f118fe7e84a::getInitializer($loader));
+        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
+        if ($useStaticLoader) {
+            require __DIR__ . '/autoload_static.php';
+
+            call_user_func(\Composer\Autoload\ComposerStaticInit770c7bb2c48ef6e2c97e7f118fe7e84a::getInitializer($loader));
+        } else {
+            $map = require __DIR__ . '/autoload_namespaces.php';
+            foreach ($map as $namespace => $path) {
+                $loader->set($namespace, $path);
+            }
+
+            $map = require __DIR__ . '/autoload_psr4.php';
+            foreach ($map as $namespace => $path) {
+                $loader->setPsr4($namespace, $path);
+            }
+
+            $classMap = require __DIR__ . '/autoload_classmap.php';
+            if ($classMap) {
+                $loader->addClassMap($classMap);
+            }
+        }
 
         $loader->register(true);
 
