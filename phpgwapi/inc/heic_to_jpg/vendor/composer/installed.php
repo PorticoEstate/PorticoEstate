d19502e42e19d7b492fadadc6d4a2c766b0e30c4
Sigurd Nes <sigurdne@online.no>
2023-10-25 16:24:30 +0200

    Image converter

---
+++ phpgwapi/inc/heic_to_jpg/vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'ca141cfd297a394ea6cdce1623d1a36c0f5b6581',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'ca141cfd297a394ea6cdce1623d1a36c0f5b6581',
+            'dev_requirement' => false,
+        ),
+        'maestroerror/heif-converter' => array(
+            'pretty_version' => '0.2',
+            'version' => '0.2.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../maestroerror/heif-converter',
+            'aliases' => array(),
+            'reference' => '714f3e6c2c18f85ffe3495057874cb01ca89ce63',
+            'dev_requirement' => false,
+        ),
+        'maestroerror/php-heic-to-jpg' => array(
+            'pretty_version' => 'v1.0.4',
+            'version' => '1.0.4.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../maestroerror/php-heic-to-jpg',
+            'aliases' => array(),
+            'reference' => 'bc1e640f62bb05c5c755d9758bc3eb3533ef32b0',
+            'dev_requirement' => false,
+        ),
+    ),
+);
