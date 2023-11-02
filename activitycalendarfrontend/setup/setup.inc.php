39e988df4977592ae5b88d792920837d4ce1ccc0
Sigurd Nes <sigurdne@online.no>
2023-11-02 09:42:35 +0100

    booking: add missing column

--- activitycalendarfrontend/setup/setup.inc.php
+++ activitycalendarfrontend/setup/setup.inc.php
@@ -22,7 +22,7 @@
 	$setup_info['activitycalendarfrontend']['depends'][] = array(
 		'appname' => 'booking',
 		'versions' => array(
-			'0.2.90', '0.2.91', '0.2.92', '0.2.93', '0.2.94')
+			'0.2.90', '0.2.91', '0.2.92', '0.2.93', '0.2.94', '0.2.95')
 	);
 
 	$setup_info['activitycalendarfrontend']['depends'][] = array(
