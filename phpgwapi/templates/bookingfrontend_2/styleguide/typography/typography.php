03688012fadd4fddd882321269fd0f278e37105f
Henning Berge <henning.ber@gmail.com>
2024-02-12 17:11:59 +0100

    update typography demo

--- phpgwapi/templates/bookingfrontend_2/styleguide/typography/typography.php
+++ phpgwapi/templates/bookingfrontend_2/styleguide/typography/typography.php
@@ -1,22 +1,23 @@
 <section class="container py-5">
-  <div class="row">
-    <div class="col-lg">
-      <h1>H1 - text-bolder - 28px / 36px</h1>
-      <h2>H2 - text-bolder - 22px / 28px</h2>
-      <h3>H3 - text-bolder - 20px / 22px</h3>
-      <h4>H4 - text-bold - 18px / 20px</h4>
-      <p class="text-bold">Body - text-bold - 16px</p>
-      <p>Body - Regular - 16px</p>
+    <div class="row">
+        <div class="col-lg">
+            <h1 class="h1">H1 - text-bolder - 56px / 60px</h1>
+            <h2 class="h2">H2 - text-bolder - 36px / 40px</h2>
+            <h3 class="h3">H3 - text-bold - 28px / 32px</h3>
+            <h4 class="h4">H4 - text-bold - 22px / 24px</h4>
+            <p class="text-bold">Body - text-bold - 20px / 24px</p>
+            <p class="text">text - Regular - 18px / 22px</p>
+        </div>
+        <div class="col-lg">
+            <p class="text-4xl">text-4xl - 56px / 60px</p>
+            <p class="text-3xl">text-3xl - 36px / 40px</p>
+            <p class="text-2xl">text-2xl - 28px / 32px</p>
+            <p class="text-xl">text-xl - 22px / 24px</p>
+            <p class="text-lg">text-lg - 20px / 24px</p>
+            <p class="text-medium">text-medium - 18px / 22px</p>
+            <p class="text-body">text-body - 16px / 20px</p>
+            <p class="text-overline">text-overline - 18px / 22px</p>
+            <p class="text-small">text-small - 14px / 16px</p>
+        </div>
     </div>
-    <div class="col-lg">
-      <p class="text-4xl">text-4xl - 56px</p>
-      <p class="text-3xl">text-3xl - 36px</p>
-      <p class="text-2xl">text-2xl - 28px</p>
-      <p class="text-xl">text-xl - 22px</p>
-      <p class="text-lg">text-lg - 20px</p>
-      <p class="text-medium">text-medium - 18px</p>
-      <p class="text-body">text-body - 16px</p>
-      <p class="text-small">text-small - 14px</p>
-    </div>
-  </div>
 </section>
